a6b33f9f04a9d89f787d39a8ca92d58bf164ad1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:23 +0000

    Add package drf-typed/0.2.0/drf-typed-0.2.0.tar.gz

---
+++ packages/drf-typed/drf-typed-0.2.0.tar.gz/drf-typed-0.2.0/rest_typed/serializers/field_factory.py
@@ -0,0 +1,40 @@
+from datetime import date, datetime, time, timedelta
+from enum import Enum
+from typing import Any
+from uuid import UUID
+
+from rest_framework import serializers
+from rest_framework.serializers import empty
+from rest_typed import ParsedType
+
+FIELD_MAPPING = {
+    bool: serializers.BooleanField,
+    date: serializers.DateField,
+    datetime: serializers.DateTimeField,
+    # Decimal: serializers.DecimalField,
+    float: serializers.FloatField,
+    int: serializers.IntegerField,
+    str: serializers.CharField,
+    time: serializers.TimeField,
+    timedelta: serializers.DurationField,
+    UUID: serializers.UUIDField,
+    Enum: serializers.ChoiceField,
+}
+
+
+def construct(hint: Any, default_value: Any = empty):
+    parsed = ParsedType(hint)
+    kwargs = {"allow_null": parsed.is_optional}
+
+    if default_value is not empty:
+        kwargs["default"] = default_value
+    else:
+        kwargs["required"] = True
+
+    if parsed.resolved_type in FIELD_MAPPING:
+        FieldClass = FIELD_MAPPING[parsed.resolved_type]
+
+        if FieldClass is serializers.ChoiceField:
+            kwargs["choices"] = parsed.enum_values
+
+        return FieldClass(**kwargs)
---
+++ packages/drf-typed/drf-typed-0.2.0.tar.gz/drf-typed-0.2.0/rest_typed/serializers/serializers.py
@@ -0,0 +1,76 @@
+from rest_framework import serializers
+from rest_framework.fields import empty
+from typing_extensions import get_type_hints
+
+from rest_typed.serializers import field_factory
+
+
+class TSerializerMetaClass(serializers.SerializerMetaclass):
+    def __new__(cls, clsname, bases, attrs):
+
+        newclass = super(TSerializerMetaClass, cls).__new__(cls, clsname, bases, attrs)
+        attr_name_to_type_hint = get_type_hints(newclass)
+        declared_fields: dict = getattr(newclass, "_declared_fields")
+
+        for attr_name, type_hint in attr_name_to_type_hint.items():
+            if attr_name not in declared_fields and attr_name in attr_name_to_type_hint:
+                type_hint = attr_name_to_type_hint[attr_name]
+
+                default_value = (
+                    getattr(newclass, attr_name)
+                    if hasattr(newclass, attr_name)
+                    else empty
+                )
+
+                declared_fields[attr_name] = field_factory.construct(
+                    type_hint, default_value
+                )
+
+                if hasattr(newclass, attr_name):
+                    delattr(newclass, attr_name)
+
+        return newclass
+
+
+class TSerializerAttrFieldsMixin(object):
+    def __getattr__(self, name: str):
+        if name not in self.fields.keys():
+            raise AttributeError(f"{name} does not exist.")
+
+        if not hasattr(self, "_validated_data"):
+            msg = (
+                "You must call `.is_valid()` before accessing de-serialized attributes."
+            )
+            raise AssertionError(msg)
+
+        validated_data: dict = self.validated_data
+
+        if name in validated_data:
+            return validated_data[name]
+        else:
+            raise AttributeError(f"{name} does not exist.")
+
+    def asdict(self) -> dict:
+        if not hasattr(self, "_validated_data"):
+            msg = (
+                "You must call `.is_valid()` before accessing de-serialized attributes."
+            )
+            raise AssertionError(msg)
+
+        return self.validated_data
+
+
+class TModelSerializer(
+    TSerializerAttrFieldsMixin,
+    serializers.ModelSerializer,
+    metaclass=TSerializerMetaClass,
+):
+    pass
+
+
+class TSerializer(
+    TSerializerAttrFieldsMixin,
+    serializers.Serializer,
+    metaclass=TSerializerMetaClass,
+):
+    pass
