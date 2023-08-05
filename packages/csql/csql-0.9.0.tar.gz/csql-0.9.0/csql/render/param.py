06ebe171c82aa1a1cf0bd321af032c9edffe7e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:44 +0000

    Add package csql/0.9.0/csql-0.9.0.tar.gz

---
+++ packages/csql/csql-0.9.0.tar.gz/csql-0.9.0/csql/render/param.py
@@ -0,0 +1,13 @@
+# mypy: implicit-reexport
+from .._.renderer.parameters import (
+	SQL,
+
+	ParamList,
+
+	ParameterRenderer,
+	NumericParameterRenderer,
+
+	QMark,
+	ColonNumeric,
+	DollarNumeric
+)
