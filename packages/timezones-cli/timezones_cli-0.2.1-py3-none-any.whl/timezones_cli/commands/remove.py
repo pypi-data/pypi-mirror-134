9856be68fae100963d65ca02fce04fd9eef27e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:56 +0000

    Add package timezones-cli/0.2.1/timezones_cli-0.2.1-py3-none-any.whl

---
+++ packages/timezones-cli/timezones_cli-0.2.1-py3-none-any.whl/timezones_cli/commands/remove.py
@@ -0,0 +1,35 @@
+"""
+Sub command to remove saved timezones from config file.
+"""
+from typing import Optional
+
+import click
+
+from timezones_cli.utils import (console, print_help_msg, remove_timezone,
+                                 validate_timezone)
+
+
+@click.command()
+@click.option("--name", "-n", help="Name of the timezone", type=str)
+@click.option(
+    "--interactive",
+    "-i",
+    help="Delete timezones in interactive mode.",
+    is_flag=True,
+)
+def remove(name: Optional[str], interactive: bool):
+    """
+    Remove timezone to the config file.
+    """
+    exists = interactive or name
+
+    if not exists:
+        return print_help_msg(remove)
+
+    if name and interactive:
+        return console.print("Cannot use both flags at the same time.:x:")
+
+    if name:
+        validate_timezone(name)
+
+    remove_timezone(interactive, name)
