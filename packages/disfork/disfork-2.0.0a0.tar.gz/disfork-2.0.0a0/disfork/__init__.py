0b91992d9dc4006a4af110518d5c97a73216b20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:01 +0000

    Add package disfork/2.0.0a0/disfork-2.0.0a0.tar.gz

---
+++ packages/disfork/disfork-2.0.0a0.tar.gz/disfork-2.0.0a0/disfork/__init__.py
@@ -0,0 +1,75 @@
+"""
+Discord API Wrapper
+~~~~~~~~~~~~~~~~~~~
+
+A basic wrapper for the Discord API.
+
+:copyright: (c) 2015-2021 Rapptz, 2022-present disfork
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "disfork"
+__author__ = "Rapptz, disfork"
+__license__ = "MIT"
+__copyright__ = "Copyright 2015-2021 Rapptz, 2022-present disfork"
+__version__ = "2.0.0a"
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+import logging
+from typing import Literal, NamedTuple
+
+from . import abc, opus, ui, utils
+from .activity import *
+from .appinfo import *
+from .asset import *
+from .audit_logs import *
+from .channel import *
+from .client import *
+from .colour import *
+from .components import *
+from .embeds import *
+from .emoji import *
+from .enums import *
+from .errors import *
+from .file import *
+from .flags import *
+from .guild import *
+from .integrations import *
+from .interactions import *
+from .invite import *
+from .member import *
+from .mentions import *
+from .message import *
+from .object import *
+from .partial_emoji import *
+from .permissions import *
+from .player import *
+from .raw_models import *
+from .reaction import *
+from .role import *
+from .shard import *
+from .stage_instance import *
+from .sticker import *
+from .team import *
+from .template import *
+from .threads import *
+from .user import *
+from .voice_client import *
+from .webhook import *
+from .widget import *
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+version_info: VersionInfo = VersionInfo(
+    major=2, minor=0, micro=0, releaselevel="alpha", serial=0
+)
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
