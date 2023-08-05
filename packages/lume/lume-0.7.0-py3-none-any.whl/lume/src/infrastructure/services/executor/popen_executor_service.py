0b964004a8bf3b31c0fa7ffed8e23e4cdcbbc927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:32 +0000

    Add package lume/0.7.0/lume-0.7.0-py3-none-any.whl

---
+++ packages/lume/lume-0.7.0-py3-none-any.whl/lume/src/infrastructure/services/executor/popen_executor_service.py
@@ -0,0 +1,40 @@
+from subprocess import PIPE, Popen
+
+from meiga import Error, Result, isFailure, isSuccess
+
+from lume.src.domain.services.executor_service import ExecutorService
+from lume.src.domain.services.logger import ERROR, INFO, WARNING, Logger
+
+
+class PopenExecutorService(ExecutorService):
+    def __init__(self, logger: Logger):
+        self.logger = logger
+
+    def execute(self, command: str, cwd: str) -> Result[bool, Error]:
+
+        if not cwd:
+            cwd = "."
+
+        process = Popen(command, stdout=PIPE, stderr=PIPE, cwd=cwd, shell=True)
+
+        while True:
+            output = process.stdout.readline()
+            return_code = process.poll()
+            if return_code is not None:
+                break
+            if output:
+                log_output = output.rstrip().decode("utf-8")
+                if log_output != "":
+                    self.logger.log(INFO, f"{log_output}")
+
+        for output_err in iter(process.stderr.readline, b""):
+            if output_err:
+                logging_level = ERROR if return_code != 0 else WARNING
+                log_output = output_err.rstrip().decode("utf-8")
+                if log_output != "":
+                    self.logger.log(logging_level, f"{log_output}")
+
+        if return_code != 0:
+            return isFailure
+
+        return isSuccess
