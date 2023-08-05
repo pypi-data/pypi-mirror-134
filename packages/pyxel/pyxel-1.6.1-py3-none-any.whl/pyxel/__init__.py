a7f9e9fae60c14be8f54cdb860e2d5adebd0a91e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:28 +0000

    Add package pyxel/1.6.1/pyxel-1.6.1-py3-none-any.whl

---
+++ packages/pyxel/pyxel-1.6.1-py3-none-any.whl/pyxel/__init__.py
@@ -0,0 +1,18 @@
+import platform
+
+_system = platform.system()
+
+if _system == "Darwin":
+    from .lib.macos.pyxel_wrapper import *  # type: ignore  # noqa F403
+elif _system == "Windows":
+    from .lib.windows.pyxel_wrapper import *  # type: ignore  # noqa F403
+elif _system == "Linux":
+    from .lib.linux.pyxel_wrapper import *  # type: ignore  # noqa F403
+else:
+    raise Exception("unsupported platform")
+
+
+from .cli import cli  # noqa E402
+
+if __name__ == "__main__":
+    cli()
