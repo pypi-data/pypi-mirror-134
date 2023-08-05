3bf5a35738347cbc6b0c3c3c09197868f734ff29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:24 +0000

    Add package network-prog-it19/0.0.7/network_prog_it19-0.0.7.tar.gz

---
+++ packages/network-prog-it19/network_prog_it19-0.0.7.tar.gz/network_prog_it19-0.0.7/network_prog_it19/sqlite_net.py
@@ -0,0 +1,11 @@
+from sqlite3 import *
+
+conn = sqlite3.connect(db_file) #.db 
+
+cursor = conn.cursor()
+
+cursor.execute(query_string)
+
+cursor.execute('update user set name = ? where id = ?', [name, user_id])
+
+rows = cursor.fetchall()
