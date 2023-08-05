29e79e1913762f79e1bb8b746b063aedf0171730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:24 +0000

    Add package notion-dump-kernel/0.0.3/notion-dump-kernel-0.0.3.tar.gz

---
+++ packages/notion-dump-kernel/notion-dump-kernel-0.0.3.tar.gz/notion-dump-kernel-0.0.3/NotionDump/__init__.py
@@ -0,0 +1,15 @@
+__author__ = "delta1037 <geniusrabbit@qq.com>"
+__version__ = "1.0"
+
+# 临时存放文件夹
+TMP_DIR = "./.tmp/"
+
+# Markdown的分割条语法
+MD_DIVIDER = "------"
+
+# 开启调试
+DUMP_DEBUG = False
+
+DUMP_TYPE_BLOCK = 1
+DUMP_TYPE_PAGE = 2
+DUMP_TYPE_DB_TABLE = 4
