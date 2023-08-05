b32a4d101ccd97655ff46ecbe2e8535a9816f437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:26 +0000

    Add package hnget/0.1.0/hnget-0.1.0.tar.gz

---
+++ packages/hnget/hnget-0.1.0.tar.gz/hnget-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hnget']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['lxml>=4.6.3,<5.0.0', 'requests>=2.26.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['hnget = hnget.application:main']}
+
+setup_kwargs = {
+    'name': 'hnget',
+    'version': '0.1.0',
+    'description': 'Shows and opens links on Hacker news from the terminal',
+    'long_description': None,
+    'author': 'Jordan Sweet',
+    'author_email': 'hello@jordandsweet.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
