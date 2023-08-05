daceb9ff4c593ec93846345cb6b8f42aee5f808c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:15 +0000

    Add package log-creator/0.0.5/log_creator-0.0.5.tar.gz

---
+++ packages/log-creator/log_creator-0.0.5.tar.gz/log_creator-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Log Creator package'
+
+# Setting up
+setup(
+    name="log_creator",
+    version=VERSION,
+    author="lUckYtHRteeN13 (Nethan Quinn Jael) ",
+    author_email="<nethanquinnjael13@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'log', 'logs', 'log creator'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
