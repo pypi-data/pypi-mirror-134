0cbc76d9a7e4c79b7bd8a6387c88190559a24292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:40 +0000

    Add package nonebot-plugin-puppet/0.2.1/nonebot_plugin_puppet-0.2.1.tar.gz

---
+++ packages/nonebot-plugin-puppet/nonebot_plugin_puppet-0.2.1.tar.gz/nonebot_plugin_puppet-0.2.1/nonebot_plugin_puppet/__init__.py
@@ -0,0 +1,181 @@
+from time import strftime, localtime
+from nonebot.permission import SUPERUSER
+from nonebot.plugin import on_message, on_request, on_shell_command
+from nonebot.typing import T_State
+from nonebot.adapters.cqhttp import (
+    Bot,
+    Message,
+    RequestEvent,
+    FriendRequestEvent,
+    GroupRequestEvent,
+    MessageEvent,
+    GroupMessageEvent,
+    PrivateMessageEvent,
+    unescape,
+)
+
+from nonebot_plugin_puppet.parser import parser, Namespace
+from nonebot_plugin_puppet.handle import Handle
+from nonebot_plugin_puppet.mapping import ConvMapping
+from nonebot_plugin_puppet.request import ReqList
+
+command = on_shell_command("puppet", parser=parser, priority=1, permission=SUPERUSER)
+message = on_message(priority=10, block=False)
+request = on_request(priority=10, block=False)
+
+
+@command.handle()
+async def _(bot: Bot, event: MessageEvent, state: T_State):
+    ConvMapping().update_conv(
+        {
+            "user": [user["user_id"] for user in (await bot.get_friend_list())],
+            "group": [group["group_id"] for group in (await bot.get_group_list())],
+        },
+    )
+
+    args: Namespace = state["args"]
+    args.conv_s = {
+        "user": [event.user_id] if isinstance(event, PrivateMessageEvent) else [],
+        "group": [event.group_id] if isinstance(event, GroupMessageEvent) else [],
+    }
+    args.conv_r = {"user": {}, "group": {}}
+
+    if hasattr(args, "handle"):
+        if hasattr(args, "message"):
+            args.message = unescape(args.message)
+        args = getattr(Handle, args.handle)(args)
+        if hasattr(args, "req"):
+            for type in args.req:
+                for flag in args.req[type]:
+                    try:
+                        if type == "friend":
+                            await bot.set_friend_add_request(
+                                flag=flag, approve=args.req[type][flag]
+                            )
+                        elif type in ["add", "invite"]:
+                            await bot.set_group_add_request(
+                                flag=flag,
+                                sub_type=type,
+                                approve=args.req[type][flag],
+                            )
+                    except:
+                        pass
+        for type in args.conv_r:
+            for id in args.conv_r[type]:
+                try:
+                    await bot.send_msg(
+                        user_id=id if type == "user" else None,
+                        group_id=id if type == "group" else None,
+                        message=Message(args.conv_r[type][id]),
+                    )
+                except:
+                    pass
+        if args.handle == "exit":
+            for id in args.group:
+                try:
+                    await bot.set_group_leave(group_id=id)
+                except:
+                    pass
+    else:
+        await bot.send(event, args.message)
+
+
+@message.handle()
+async def _(bot: Bot, event: MessageEvent):
+    ConvMapping().update_conv(
+        {
+            "user": [user["user_id"] for user in (await bot.get_friend_list())],
+            "group": [group["group_id"] for group in (await bot.get_group_list())],
+        },
+    )
+
+    conv_s = {
+        "user": [event.user_id] if isinstance(event, PrivateMessageEvent) else [],
+        "group": [event.group_id] if isinstance(event, GroupMessageEvent) else [],
+    }
+    conv_r = ConvMapping().get_conv(conv_s)
+
+    header = ""
+    sender = ""
+    message = unescape(str(event.get_message()))
+    if str(event.user_id) not in bot.config.superusers:
+        if isinstance(event, GroupMessageEvent):
+            header = (await bot.get_group_info(group_id=event.group_id))[
+                "group_name"
+            ] + f" ({event.group_id})\n"
+        sender = (
+            event.sender.card
+            if isinstance(event, GroupMessageEvent) and event.sender.card
+            else event.sender.nickname
+        ) + f" {strftime('%Y-%m-%d %H:%M:%S', localtime(event.time))} \n"
+    message = header + sender + message
+
+    for type in conv_r:
+        for id in conv_r[type]:
+            try:
+                await bot.send_msg(
+                    user_id=id if type == "user" else None,
+                    group_id=id if type == "group" else None,
+                    message=Message(message),
+                )
+            except:
+                pass
+
+
+@request.handle()
+async def _(bot: Bot, event: RequestEvent):
+    conv_s = {
+        "user": [event.user_id] if isinstance(event, FriendRequestEvent) else [],
+        "group": [event.group_id] if isinstance(event, GroupRequestEvent) else [],
+    }
+    type = (
+        event.sub_type if isinstance(event, GroupRequestEvent) else event.request_type
+    )
+    ReqList().add_req(
+        {
+            "friend": [event.flag] if type == "friend" else [],
+            "invite": [event.flag] if type == "invite" else [],
+            "add": [event.flag] if type == "add" else [],
+        }
+    )
+    conv_r = (
+        ConvMapping().get_conv(conv_s)
+        if type == "add"
+        else {"group": [], "user": [int(i) for i in bot.config.superusers]}
+    )
+    if type == "add":
+        conv_r["group"].append(event.group_id)
+
+    if str(event.user_id) not in bot.config.superusers:
+        header = f"【请求】{event.flag}\n"
+        sender = (await bot.get_stranger_info(user_id=event.user_id))[
+            "nickname"
+        ] + f" {strftime('%Y-%m-%d %H:%M:%S', localtime(event.time))} \n"
+        message = (
+            ("邀请" if type == "invite" else "请求")
+            + (
+                "添加为好友"
+                if type == "friend"
+                else "加入群聊"
+                + (await bot.get_group_info(group_id=event.group_id))["group_name"]
+                + f" ({event.group_id})\n"
+            )
+            + (f"\n备注：{event.comment}" if event.comment else "")
+        )
+        message = header + sender + message
+
+        for type in conv_r:
+            for id in conv_r[type]:
+                try:
+                    await bot.send_msg(
+                        user_id=id if type == "user" else None,
+                        group_id=id if type == "group" else None,
+                        message=Message(message),
+                    )
+                except:
+                    pass
+    else:
+        try:
+            await event.approve(bot)
+        except:
+            pass
---
+++ packages/nonebot-plugin-puppet/nonebot_plugin_puppet-0.2.1.tar.gz/nonebot_plugin_puppet-0.2.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nonebot_plugin_puppet']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['nonebot-adapter-cqhttp>=2.0.0a11,<2.0.0-beta.1',
+ 'nonebot2>=2.0.0-alpha.11,<2.0.0-beta.1']
+
+setup_kwargs = {
+    'name': 'nonebot-plugin-puppet',
+    'version': '0.2.1',
+    'description': 'Make NoneBot your puppet',
+    'long_description': '# Nonebot Plugin Puppet\n\n基于 [nonebot2](https://github.com/nonebot/nonebot2) 和 [go-cqhttp](https://github.com/Mrs4s/go-cqhttp) 的会话转接插件\n\n[![License](https://img.shields.io/github/license/Jigsaw111/nonebot_plugin_puppet?style=flat-square)](LICENSE)\n![Python Version](https://img.shields.io/badge/python-3.7.3+-blue.svg?style=flat-square)\n![NoneBot Version](https://img.shields.io/badge/nonebot-2.0.0a11+-red.svg?style=flat-square&log")\n![Pypi Version](https://img.shields.io/pypi/v/nonebot-plugin-puppet.svg?style=flat-square)\n\n## 安装\n\n### 从 PyPI 安装（推荐）\n\n- 使用 nb-cli  \n\n```shell\nnb plugin install nonebot_plugin_puppet\n```\n\n- 使用 poetry\n\n```shell\npoetry add nonebot_plugin_puppet\n```\n\n- 使用 pip\n\n```shell\npip install nonebot_plugin_puppet\n```\n\n### 从 GitHub 安装（不推荐）\n\n```shell\ngit clone https://github.com/Jigsaw111/nonebot_plugin_puppet.git\n```\n\n## 使用\n\n**仅限超级用户使用**\n\n**不建议同时链接多个会话（尤其是大群），如被风控概不负责**\n\n- `puppet ln/link`链接会话\n  - `-u user_id..., -ua user_id..., --user-a user_id...`可选参数，指定源会话的 QQ 号\n  - `-g group_id..., -ga group_id..., --group-a group_id...`可选参数，指定源会话的群号\n    至少需要设置一个\n  - `-ub user_id..., --user-b user_id...`可选参数，指定链接会话的 QQ 号\n  - `-gb group_id..., --group-b group_id...`可选参数，指定链接会话的群号\n    不设置的话默认为当前会话的 QQ 号/群号\n  - `-q, --quiet`可选参数，静默链接（不发送链接成功消息）\n  - `-U, --unilateral`可选参数，单方面链接\n- `puppet rm/unlink`删除会话链接\n  - `-u user_id..., -ua user_id..., --user-a user_id...`可选参数，指定源会话的 QQ 号\n  - `-g group_id..., -ga group_id..., --group-a group_id...`可选参数，指定源会话的群号\n    不设置的话，默认为当前会话链接的所有会话\n  - `-ub user_id..., --user-b user_id...`可选参数，指定链接会话的 QQ 号\n  - `-gb group_id..., --group-b group_id...`可选参数，指定链接会话的群号\n    不设置的话默认为当前会话的 QQ 号/群号\n  - `-q, --quiet`可选参数，静默链接（不发送解除链接成功消息）\n  - `-U, --unilateral`可选参数，单方面解除链接\n- `puppet ls/list` 查看链接到当前会话的会话列表\n  - `-u user_id, --user user_id` 互斥参数，指定会话的 QQ 号\n  - `-g group_id, --group group_id` 互斥参数，指定会话的群号\n    不设置的话默认为当前会话的 QQ 号/群号\n- `puppet send message` 向指定会话发送消息，支持 CQ 码\n  - `message` 需要发送的消息，支持 CQ 码，如含空格请用 `""` 包裹\n  - `-u user_id..., --user user_id...`可选参数，指定接收会话的 QQ 号\n  - `-g group_id..., --group group_id...`可选参数，指定接收会话的群号\n    不设置的话默认为当前会话链接的所有会话\n  - `--a, --all`可选参数，指定所有群聊\n- `puppet aprv/approve` 同意请求/邀请\n  - `-f flag..., --flag flag...`可选参数，指定请求的 flag\n  - `--a, --all`可选参数，指定所有请求\n- `puppet rej/reject` 拒绝请求/邀请\n  - `-f flag..., --flag flag...`可选参数，指定请求的 flag\n  - `--a, --all`可选参数，指定所有请求\n- `puppet exit` 退出指定群聊\n  - `-g group_id..., --group group_id...`可选参数，指定要退出的群号\n\n## Bug\n\n- [x] 不允许多个超级用户链接到同一会话\n- [x] 如果指定的会话不在会话列表里会产生错误\n\n## To Do\n\n- [x] 允许单向转接\n- [x] 转接请求事件\n- [x] 提供退群功能\n- [ ] 提供默认设置\n\n## 原理\n\n```mermaid\ngraph LR\n用户 & 群 ---> Puppet ---> 用户 & 群\n```\n',
+    'author': 'Jigsaw',
+    'author_email': 'j1g5aw@foxmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/Jigsaw111/nonebot_plugin_puppet',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.3,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
