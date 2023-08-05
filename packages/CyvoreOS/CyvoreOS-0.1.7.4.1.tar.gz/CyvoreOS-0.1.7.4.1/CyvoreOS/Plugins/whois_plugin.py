c73e5666e9b3c6b734e3dbd0d137cd2c1156a1ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:19 +0000

    Add package CyvoreOS/0.1.7.4.1/CyvoreOS-0.1.7.4.1.tar.gz

---
+++ packages/CyvoreOS/CyvoreOS-0.1.7.4.1.tar.gz/CyvoreOS-0.1.7.4.1/CyvoreOS/Plugins/whois_plugin.py
@@ -0,0 +1,22 @@
+import whois
+import logging
+
+def whois_plugin(data):
+    try:
+        hostDict = whois.whois(data)
+    except Exception as e:
+        logging.warning(e)
+
+def run_check(chk):
+    plugin_name = "Whois"
+    output = whois_plugin(chk.raw)
+    chk.add_plugin(plugin_name,output)
+        
+def describe():
+    desc = """This plugin query domain/ip in whois database """
+    return desc
+
+def tags():
+    # Todo: consider adding support for ips
+    tags_list = ["domain"]
+    return tags_list
