4cde4ffd529039e201bf9284e18e631f05a77873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:48 +0000

    Add package CsuSort/1.0.12/CsuSort-1.0.12.tar.gz

---
+++ packages/CsuSort/CsuSort-1.0.12.tar.gz/CsuSort-1.0.12/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages  # 这个包没有的可以pip一下
+
+setup(
+    name="CsuSort",  # 这里是pip项目发布的名称
+    version="1.0.12",  # 版本号，数值大的会优先被pip
+    keywords=["pip", "sort"],
+    description="person or car",
+    license="MIT Licence",
+    author="csu_ywj",
+    packages=find_packages(),
+    data_files=[],
+    include_package_data=True,
+    platforms="any",
+    install_requires=["Keras", "tensorflow-gpu", "numpy", "opencv-python", "scikit-learn", "scipy", "Pillow", "torch",
+                      "torchvision"]
+)
