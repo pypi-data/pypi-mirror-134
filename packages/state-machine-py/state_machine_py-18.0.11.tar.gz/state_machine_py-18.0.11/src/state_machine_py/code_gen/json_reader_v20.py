7a3f959d285dcbe670c288726d69ed66d63e73bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:41 +0000

    Add package state-machine-py/18.0.11/state_machine_py-18.0.11.tar.gz

---
+++ packages/state-machine-py/state_machine_py-18.0.11.tar.gz/state_machine_py-18.0.11/src/state_machine_py/code_gen/json_reader_v20.py
@@ -0,0 +1,17 @@
+import json
+from collections import OrderedDict
+
+
+class JsonReaderV20:
+    @classmethod
+    def read_file(clazz, file_path):
+
+        # Pivotファイル（JSON形式）を読込みます
+        with open(file_path, encoding="utf-8") as f:
+            in_text = f.read()
+            # print(f"in_text={in_text}")
+
+        transition_conf_data = json.loads(in_text, object_pairs_hook=OrderedDict)
+        # print(f"transition_conf_data={transition_conf_data}")
+
+        return transition_conf_data
