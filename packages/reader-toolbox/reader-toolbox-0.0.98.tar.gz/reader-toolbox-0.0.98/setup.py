202c7c75c1e0816fd7f3d43efa8aff9cd32c7273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:14 +0000

    Add package reader-toolbox/0.0.98/reader-toolbox-0.0.98.tar.gz

---
+++ packages/reader-toolbox/reader-toolbox-0.0.98.tar.gz/reader-toolbox-0.0.98/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+# install_requires=[ 'srsly<1.1.0,>=1.0.2', 'catalogue<1.1.0,>=0.0.7', 'click', 'datasette', 'requests', 'scipy', 'scikit-learn<0.24.0,>=0.19.0', 'matplotlib', 'nltk', 'pandas', 'textacy==0.10.1', 'word2vec' ],
+
+setup(
+    name='reader-toolbox',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[ 'srsly<1.1.0,>=1.0.2', 'catalogue<1.1.0,>=0.0.7', 'click', 'datasette', 'requests', 'scipy', 'scikit-learn', 'matplotlib', 'nltk', 'pandas', 'textacy==0.10.1', 'word2vec' ],
+    entry_points={ 'console_scripts': [ 'rdr = rdr.rdr:rdr' ] }
+)
