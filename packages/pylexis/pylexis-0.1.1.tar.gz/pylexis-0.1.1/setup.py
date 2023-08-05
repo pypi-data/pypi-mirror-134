2827ec3420819a72fb5fb0469436e34a4ddbbe1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:53 +0000

    Add package pylexis/0.1.1/pylexis-0.1.1.tar.gz

---
+++ packages/pylexis/pylexis-0.1.1.tar.gz/pylexis-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pylexis",
+    version="0.1.1",
+    author="Carlos A. Crespo",
+    author_email="lvccrespo@gmail.com",
+    description="Quickly and easily draw basic Lexis diagrams",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cacrespo/pylexis",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "matplotlib",
+    ],
+    python_requires=">=3.6",
+    packages=['pylexis']
+)
