701487b5d37b184ed5a4ce7d9b176ff580af474f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:33 +0000

    Add package arknights/0.1.9/arknights-0.1.9.tar.gz

---
+++ packages/arknights/arknights-0.1.9.tar.gz/arknights-0.1.9/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['arknights']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.21.3,<0.22.0', 'pydantic>=1.9.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'arknights',
+    'version': '0.1.9',
+    'description': 'arknights CN request library',
+    'long_description': None,
+    'author': 'djkcyl',
+    'author_email': 'cyl@cyllive.cn',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/djkcyl/py_arknights',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
