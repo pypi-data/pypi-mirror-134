3b00d6e0a77938ecef394e17b462a12773ad05a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:57 +0000

    Add package binary-tools/0.2.0/binary_tools-0.2.0.tar.gz

---
+++ packages/binary-tools/binary_tools-0.2.0.tar.gz/binary_tools-0.2.0/binary_tools/byte_encoder.py
@@ -0,0 +1,74 @@
+from binary_tools import bit_encoder
+
+
+def _string_len(data: bytes):
+    pos = 0
+    while len(data) > pos and data[pos] != 0x00:
+        pos += 1
+    return pos
+
+
+class ByteEncoder:
+    def __init__(self):
+        self.__bit_buf = bit_encoder.BitEncoder()
+
+    def put_string(self, string: str):
+        binarised_str = string.encode('ascii') + b'\x00'
+        self.__bit_buf.put_bytes(binarised_str)
+
+    def put_int(self, number: int, length: int):
+        binarised_int = int.to_bytes(number, length, 'big')
+        self.__bit_buf.put_bytes(binarised_int)
+
+    def put_bool(self, val: bool):
+        self.__bit_buf.put_bits([val])
+
+    def put_byte(self, byte: int, repeat: int = 1):
+        for _ in range(repeat):
+            self.__bit_buf.put_byte(byte)
+
+    def put_bytes(self, data: bytes):
+        self.__bit_buf.put_bytes(data)
+
+    def get_data(self):
+        return self.__bit_buf.finalise()
+
+
+class ByteDecoder:
+    def __init__(self, data: bytes):
+        self.__buf = bit_encoder.BitDecoder(data)
+
+    def has_more(self):
+        return self.__buf.has_more()
+
+    def consume_bytes(self, length: int):
+        self.__buf.advance_bytes(length)
+
+    def peek(self, length: int):
+        return self.__buf.as_bytes()[:length]
+
+    def get_string(self):
+        length = _string_len(self.__buf.as_bytes())
+        string = self.__buf.as_bytes()[0:length]
+        self.__buf.advance_bytes(length + 1)
+
+        return string.decode('ascii')
+
+    def get_int(self, length: int):
+        number = int.from_bytes(self.__buf.as_bytes()[0:length], 'big')
+        self.__buf.advance_bytes(length)
+
+        return number
+
+    def get_bool(self):
+        val = self.__buf.as_bit_list()[0]
+        self.__buf.advance_bits(1)
+        return val
+
+    def get_byte(self):
+        return self.get_bytes(1)
+
+    def get_bytes(self, length: int):
+        b = self.__buf.as_bytes()[0:length]
+        self.__buf.advance_bytes(length)
+        return b
---
+++ packages/binary-tools/binary_tools-0.2.0.tar.gz/binary_tools-0.2.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='binary_tools',
+    version='0.2.0',
+    author='Guilherme Borges',
+    author_email='guilherme@guilhermeborges.net',
+    description='Pyhton tools for binary data',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/sgtpepperpt/binary_tools',
+    packages=setuptools.find_packages(exclude=['test', 'test.*']),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9'
+)
