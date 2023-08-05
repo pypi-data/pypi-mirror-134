7946feb23e9a233b52b1c0572b809fef2b7e4ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:21 +0000

    Add package bpp-iplweb/202201.1081/bpp_iplweb-202201.1081-py3-none-any.whl

---
+++ packages/bpp-iplweb/bpp_iplweb-202201.1081-py3-none-any.whl/ewaluacja2021/const.py
@@ -0,0 +1,15 @@
+LATA_2019_2021 = 0
+LATA_2017_2018 = 1
+
+DEC2INT = 10000
+
+IMPORT_MAKSYMALNYCH_SLOTOW_COLUMNS = [
+    "Stopień / Tytuł",
+    "Nazwisko",
+    "Imię",
+    "ORCID",
+    "Dyscyplina",
+    "Uwzględniony w ewaluacji",
+    "maksymalna suma udziałów jednostkowych - wszystkie dyscypliny",
+    "maksymalna suma udziałów jednostkowych - monografie",
+]
