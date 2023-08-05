09ea40f363a27e1baa6a9bcb1bfb3958c3a8643e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:54 +0000

    Add package otmcm-backend/0.0.9/otmcm-backend-0.0.9.tar.gz

---
+++ packages/otmcm-backend/otmcm-backend-0.0.9.tar.gz/otmcm-backend-0.0.9/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="otmcm-backend",
+    version="0.0.9",
+    author="Huan Tran",
+    author_email="huankimtran@gmail.com",
+    description="Backend package for otmcm algotrading environment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/huankimtran/OTMCM",
+    project_urls={
+        "Bug Tracker": "https://github.com/huankimtran/OTMCM",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+    install_requires=[
+        "pyzmq >= 18.1.1"
+    ],
+    scripts=[
+        "bin/otmcm_launch.py"
+    ]
+)
