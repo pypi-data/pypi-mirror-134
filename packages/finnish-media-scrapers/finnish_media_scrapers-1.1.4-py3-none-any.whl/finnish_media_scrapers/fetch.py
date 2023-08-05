fc52baeee1f9932cd2cf2de292f349de296a1028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:16 +0000

    Add package finnish-media-scrapers/1.1.4/finnish_media_scrapers-1.1.4-py3-none-any.whl

---
+++ packages/finnish-media-scrapers/finnish_media_scrapers-1.1.4-py3-none-any.whl/finnish_media_scrapers/fetch.py
@@ -0,0 +1,89 @@
+"""Utilities for fetching articles.
+Currently only affects Helsingin Sanomat. Scraping of the other sources can be done just using requests,
+but HS needs a user to be logged in, as well as renders their articles using dynamic javascript,
+thus requiring a Selenium session to enable fetching the articles.
+"""
+
+from pyppeteer.browser import Page
+from pyppeteer.errors import NetworkError
+
+async def prepare_session_hs(
+        session: Page,
+        username: str, password: str, max_web_driver_wait: int = 30):
+    """Prepare a pyppeteer session for scraping articles from Helsingin Sanomat
+    by logging in using the provided user id and password.
+
+    Raises:
+        TimeoutError: if the web driver is unable to find the elements it is looking for in 30 seconds.
+                      May indicate changes to the loging page structure.
+
+    Args:
+        session (Page): the pyppeteer session to use
+        username (str): the username to log in as
+        password (str): the password to use for logging in
+        max_web_driver_wait (int): the maximum number of seconds to wait for the webdriver to
+                                   render a page before failing (default: 30)
+
+    """
+    max_web_driver_wait = 1000 * max_web_driver_wait
+    await session.goto("https://www.hs.fi", timeout=max_web_driver_wait)
+    cookies_frame = await session.waitForXPath("//iframe[@title='SP Consent Message']", timeout=max_web_driver_wait)
+    frame = await cookies_frame.contentFrame()
+    ok_button = await frame.waitForXPath("//button[@title='OK']", timeout=max_web_driver_wait)
+    await ok_button.click()
+    login = await session.waitForXPath("//*[contains(text(), 'Kirjaudu')]", timeout=max_web_driver_wait)
+    await login.click()
+    user = await session.waitForSelector("#username", timeout=max_web_driver_wait)
+    await user.type(username)
+    passw = await session.waitForSelector("#password", timeout=max_web_driver_wait)
+    await passw.type(password)
+    submit = await session.waitForSelector("button[type=submit]", timeout=max_web_driver_wait)
+    await submit.click()
+    await session.waitForNavigation()
+
+
+async def fetch_article_hs(
+        session: Page,
+        url: str, max_web_driver_wait: int = 30) -> str:
+    """Fetch the HTML of a single article using a pyppeteer session where
+    prepare_session_hs has been called before.
+
+    Args:
+        session (Page): the pyppeteer session to use
+        url (str): the HS article URL to fetch article content from
+        max_web_driver_wait (int): the maximum number of seconds to wait for the webdriver to
+                                   render a page before failing (default: 30)
+
+
+    Raises:
+        ValueError: If parsing the article fails, probably due to encountering a prevously unknown layout
+
+    Returns:
+        str: the HTML of the article
+    """
+    max_web_driver_wait = 1000 * max_web_driver_wait
+    try:
+        response = await session.goto(url, timeout=max_web_driver_wait)
+        if response.status == 404:
+            raise ValueError(
+                f"The page doesn't exist for {url}."
+            )
+    except NetworkError as network_exception:
+        raise ValueError(
+            f"The page doesn't exist for {url}."
+        ) from network_exception
+    try:
+        main_content = await session.waitForXPath("//div[@id='page-main-content']/following-sibling::*", timeout=max_web_driver_wait)
+        tag_name = await (await main_content.getProperty('tagName')).jsonValue()
+        if tag_name == 'IFRAME':
+            content = await main_content.contentFrame()
+            if len(await session.xpath("//div[@class='paywall-container']")) != 0:
+                await session.waitForXPath("//div[@class='paywall-content']|//div[@id='paid-content']")
+        else:
+            content = session
+    except TimeoutError as timeout_exception:
+        raise ValueError(
+            f"Couldn't find the dynamic content I was looking for in {url}. There may be a class of HS articles we're not yet handling."
+        ) from timeout_exception
+    article = await content.content()
+    return article
---
+++ packages/finnish-media-scrapers/finnish_media_scrapers-1.1.4-py3-none-any.whl/finnish_media_scrapers/scripts/fetch_open.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""Command-line script for fetching article HTML from openly available sources
+"""
+
+import argparse
+import asyncio
+import csv
+import logging
+import os
+import random
+from time import sleep
+
+import aiohttp
+
+logging.basicConfig(level=logging.INFO)
+
+
+def _parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-i', '--input', help="input CSV file containing articles to fetch (from query-il, query-is or query-yle)", required=True)
+    parser.add_argument(
+        '-o', '--output', help="directory to fetch articles into", required=True)
+    parser.add_argument(
+        '-d', '--delay', help="number of seconds to wait between consecutive requests", default=1.0, type=float)
+    parser.add_argument('--quiet', default=False,
+                        action='store_true', help="Log only errors")
+    return parser.parse_args()
+
+
+async def _amain():
+    args = _parse_arguments()
+    if args.quiet:
+        logging.basicConfig(level=logging.ERROR)
+    os.makedirs(args.output, exist_ok=True)
+    with open(args.input, encoding="utf-8") as input_file:
+        async with aiohttp.ClientSession() as session:
+            csv_input = csv.DictReader(input_file)
+            for article in csv_input:
+                article_file_name = os.path.join(args.output, article['id']+".html")
+                if not os.path.exists(article_file_name):
+                    async with session.get(article['url']) as response:
+                        content = await response.text()
+                        with open(article_file_name, "w", encoding="utf-8") as article_file:
+                            article_file.write(content)
+                    logging.info("wrote %s into %s", article['url'], article_file_name)
+                    sleep(random.randrange(args.delay*2))
+
+
+def main():
+    asyncio.run(_amain())
+
+
+if __name__ == '__main__':
+    main()
