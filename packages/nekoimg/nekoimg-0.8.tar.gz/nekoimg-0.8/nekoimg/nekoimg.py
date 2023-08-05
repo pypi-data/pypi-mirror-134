6150aaf14e363dea57545c956d8ce3c363f80e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:39 +0000

    Add package nekoimg/0.8/nekoimg-0.8.tar.gz

---
+++ packages/nekoimg/nekoimg-0.8.tar.gz/nekoimg-0.8/nekoimg/nekoimg.py
@@ -0,0 +1,15 @@
+class nekoimg:
+    def __init__():
+        pass
+    def nekoimg():
+        import requests
+        import webbrowser
+        import pyautogui
+        import time
+        r = requests.get('https://nekos.life/api/v2/img/neko')
+        res = r.json()
+        print('Here is your neko img! you have 9 seconds to see it! and dont move until the tab close!')
+        time.sleep(3)
+        webbrowser.open(res['url'])
+        time.sleep(9)
+        pyautogui.hotkey('ctrl', 'w')
---
+++ packages/nekoimg/nekoimg-0.8.tar.gz/nekoimg-0.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+readme = open("./README.md", "r")
+
+
+setup(
+    name='nekoimg',
+    packages=['nekoimg'],  # this must be the same as the name above
+    version='0.8',
+    description='This shows you a random neko image',
+    long_description='For more info of the project go to see The github!\nhttps://github.com/Cl1ckerr/nekoimg',
+    long_description_content_type='text/markdown',
+    author='Cl1cker',
+    author_email='tji85047@gmail.com',
+    # use the URL to the github repo
+    url='https://github.com/Cl1ckerr/nekoimg',
+    install_requires=['requests', 'pyautogui', 'requests'],
+    download_url='https://github.com/Cl1ckerr/nekoimg/tarball/0.4',
+    keywords=['webbrowser', 'random', 'neko'],
+    classifiers=[ ],
+    license='MIT',
+    include_package_data=True
+)
