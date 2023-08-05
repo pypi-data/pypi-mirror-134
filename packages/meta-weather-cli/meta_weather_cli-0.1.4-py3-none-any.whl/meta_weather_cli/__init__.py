ac554bc6090dc674f7686207cf7d2ab0d37fed45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:01 +0000

    Add package meta-weather-cli/0.1.4/meta_weather_cli-0.1.4-py3-none-any.whl

---
+++ packages/meta-weather-cli/meta_weather_cli-0.1.4-py3-none-any.whl/meta_weather_cli/__init__.py
@@ -0,0 +1,1 @@
+from meta_weather_cli import main
---
+++ packages/meta-weather-cli/meta_weather_cli-0.1.4-py3-none-any.whl/meta_weather_cli/meta-weather-cli.py
@@ -0,0 +1,36 @@
+import fire
+from city import get_temperature, get_world_id
+from country import get_capital
+
+def city(city):
+  '''
+  Returns the tempreature in a city.
+  Parameters
+  --------------
+  city: string 
+    Name of the city
+  '''
+  world_id = get_world_id(city)
+  return get_temperature(world_id)
+
+def country(country):
+  '''
+  Returns the tempreature in the capital of a country.
+  Parameters
+  --------------
+  country: string
+    Name of the country
+  '''
+  capital =  get_capital(country)
+  temperatures = city(capital)
+  temperatures["City"] = capital
+  return temperatures
+
+def main():
+  fire.Fire({
+      'city': city,
+      'country': country
+  })
+
+if __name__ == '__main__':
+  main()
