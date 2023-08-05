448959a3dc077b4a539709658094b27867d36114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:58 +0000

    Add package latestnews-kemendikbud/0.0.2/latestnews-kemendikbud-0.0.2.tar.gz

---
+++ packages/latestnews-kemendikbud/latestnews-kemendikbud-0.0.2.tar.gz/latestnews-kemendikbud-0.0.2/README.md
@@ -0,0 +1,9 @@
+# Latest Kemendikbud News
+this package will get the latest news from Indonesian Ministry of Education and Culture (Kemdikbud)
+
+## How it Work?
+This package will scrape from [Kemendikbud](https://kemendikbud.go.id) to get latest news from kemendikbud website
+This package will use BeautifulSoup4 and Requests, to produce output in the form of JSON that is ready to be used in web or mobile applications
+
+## Author
+Irfan Basyar
---
+++ packages/latestnews-kemendikbud/latestnews-kemendikbud-0.0.2.tar.gz/latestnews-kemendikbud-0.0.2/setup.py
@@ -0,0 +1,32 @@
+"""
+https://packaging.python.org/en/latest/tutorials/packaging-projects/
+"""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="latestnews-kemendikbud",
+    version="0.0.2",
+    author="Irfan Basyar",
+    author_email="irfanbasyar.ib@gmail.com",
+    description="this package will get the latest news from Indonesian Ministry of Education and Culture (Kemdikbud)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dikopini/latest-kemendikbud-news",
+    project_urls={
+        "Website": "https://dicopynee.com",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta"
+    ],
+    #package_dir={"": "src"},
+    #packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
