00e7c3c872e7b03163bf3d0f9abcc075172e4e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:35 +0000

    Add package CsuPMTD/1.0.17/CsuPMTD-1.0.17.tar.gz

---
+++ packages/CsuPMTD/CsuPMTD-1.0.17.tar.gz/CsuPMTD-1.0.17/PMTD/maskrcnn_benchmark/layers/nms.py
@@ -0,0 +1,14 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+# from ._utils import _C
+import ctypes
+import os
+abPath = os.path.abspath(__file__)
+_C = ctypes.CDLL(abPath + '/_C.cpython-36m-x86_64-linux-gnu.so')
+
+from PMTD.maskrcnn_benchmark.apex.apex import amp
+
+# Only valid with fp32 inputs - give AMP the hint
+nms = amp.float_function(_C.nms)
+
+# nms.__doc__ = """
+# This function performs Non-maximum suppresion"""
---
+++ packages/CsuPMTD/CsuPMTD-1.0.17.tar.gz/CsuPMTD-1.0.17/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages  # 这个包没有的可以pip一下
+
+setup(
+    name="CsuPMTD",  # 这里是pip项目发布的名称
+    version="1.0.17",  # 版本号，数值大的会优先被pip
+    keywords=["pip", "pmtd"],
+    description="word",
+    license="MIT Licence",
+    author="csu_ywj",
+    packages=find_packages(),
+    data_files=['PMTD/maskrcnn_benchmark/_C.cpython-36m-x86_64-linux-gnu.so'],
+    include_package_data=True,
+    platforms="any",
+    install_requires=["ninja", "yacs", "cython", "matplotlib", "tqdm", "pyclipper"]
+)
