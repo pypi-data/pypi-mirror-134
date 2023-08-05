bc9ac0ce57b81701d7c1f8f0043286f5acab9866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:04 +0000

    Add package konoise/1.0.5.8/konoise-1.0.5.8.tar.gz

---
+++ packages/konoise/konoise-1.0.5.8.tar.gz/konoise-1.0.5.8/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from setuptools_rust import Binding, RustExtension
+
+with open("README.md", mode="r", encoding="utf-8") as readme:
+    long_description = readme.read()
+
+setup(
+    name='konoise',
+    version='1.0.5.8',
+    rust_extensions=[RustExtension("konoise/rust_generator", binding=Binding.PyO3)],
+    description='Korean Noise Generator',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wisenut-research/konoise",
+    author="Eddie",
+    author_email="hkjeo13@gmail.com",
+    zip_safe=False,
+
+    license="MIT",
+
+    py_modules=["konoise"],
+
+    python_requires=">=3",
+
+    install_requires=["tqdm"],
+
+    packages=["konoise"],
+
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+)
