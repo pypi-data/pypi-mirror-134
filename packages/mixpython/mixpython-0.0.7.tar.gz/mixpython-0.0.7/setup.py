6c0657b1c579785252ec6807fec9d643e8967dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:55 +0000

    Add package mixpython/0.0.7/mixpython-0.0.7.tar.gz

---
+++ packages/mixpython/mixpython-0.0.7.tar.gz/mixpython-0.0.7/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("version.txt", "r", encoding="utf-8") as ver:
+    vers = ver.read()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="mixpython",
+    version=vers,
+    author="Andrew Astakhov",
+    author_email="aw.astakhov@gmail.com",
+    description="mix",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sth-v/mixpython",
+    project_urls={
+        "Bug Tracker": "https://github.com/sth-v/mixpython/issues",
+    },
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
