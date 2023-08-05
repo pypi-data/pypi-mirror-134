a555d2e098729830bc6bf58060c3229f52d0cf44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:18 +0000

    Add package kamilib/0.1.2a0/kamilib-0.1.2a0.tar.gz

---
+++ packages/kamilib/kamilib-0.1.2a0.tar.gz/kamilib-0.1.2a0/kami/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding : utf-8 -*-
+
+__name__ = "kami"
+__title__ = "kamilib"
+# PiPy == "0.1.2a"
+# testPiPy == "0.1.1b45"
+__version__ = "0.1.2a"
+__licence__ = "MIT"
+__doc__ = "HTR / OCR models evaluation agnostic Python package, originally based on the Kraken transcription system."
