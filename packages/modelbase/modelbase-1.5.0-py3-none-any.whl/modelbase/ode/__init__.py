4a7c9fabab84c70fe6bcfb6d9bd9a31344915a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:51 +0000

    Add package modelbase/1.5.0/modelbase-1.5.0-py3-none-any.whl

---
+++ packages/modelbase/modelbase-1.5.0-py3-none-any.whl/modelbase/ode/__init__.py
@@ -0,0 +1,3 @@
+from .models import LabelModel, LinearLabelModel, Model
+from .simulators import Simulator, _LabelSimulate, _LinearLabelSimulate, _Simulate
+from .utils import algebraicfunctions, mca, ratefunctions, ratelaws
---
+++ packages/modelbase/modelbase-1.5.0-py3-none-any.whl/modelbase/ode/models/__init__.py
@@ -0,0 +1,9 @@
+from typing import TypeVar
+
+from .abstract_model import _AbstractRateModel, _AbstractStoichiometricModel
+from .labelmodel import LabelModel
+from .linearlabelmodel import LinearLabelModel
+from .model import Model
+
+BASE_MODEL_TYPE = TypeVar("BASE_MODEL_TYPE", bound=_AbstractStoichiometricModel)
+RATE_MODEL_TYPE = TypeVar("RATE_MODEL_TYPE", bound=Model)
