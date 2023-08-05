798f257b1dcb7f97298dfcf81e4799a6fdb057cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:25 +0000

    Add package django-qd/0.0/django_qd-0.0.tar.gz

---
+++ packages/django-qd/django_qd-0.0.tar.gz/django_qd-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='django_qd',
+      version='0.0',
+      description='Django Query Debbuger',
+      packages=['django_qd'],
+      author_email='quantumastelata@gmail.com',
+      zip_safe=False)
