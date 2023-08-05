4503ead7adb31839045ed9a978114f52e48cb3e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:11 +0000

    Add package obs2mk/0.2.11/obs2mk-0.2.11-py3-none-any.whl

---
+++ packages/obs2mk/obs2mk-0.2.11-py3-none-any.whl/mkdocs_obsidian/common/convert_one.py
@@ -0,0 +1,31 @@
+import os
+from datetime import datetime
+from pathlib import Path
+
+import frontmatter
+
+from mkdocs_obsidian.common import (
+    file_checking as check,
+    conversion as convert,
+    config as gl,
+)
+
+
+def convert_one(ori, git, meta):
+    file_name = os.path.basename(ori).upper()
+    yaml_front = frontmatter.load(ori)
+    priv = Path(gl.post)
+    clipKey = "notes"
+    if "category" in yaml_front.keys():
+        priv = check.create_folder(yaml_front["category"])
+        clipKey = yaml_front["category"]
+    contents = convert.file_convert(ori, 1)
+    checkFile = convert.file_write(ori, contents, priv, 1, meta)
+    if checkFile and not git:
+        COMMIT = f"Pushed {file_name.lower()} to blog"
+        gl.git_push(COMMIT)
+        convert.clipboard(ori, clipKey)
+    elif checkFile and git:
+        print(
+            f"[{datetime.now().strftime('%H:%M:%S')}] 🎉 Successfully converted {file_name.lower()}"
+        )
