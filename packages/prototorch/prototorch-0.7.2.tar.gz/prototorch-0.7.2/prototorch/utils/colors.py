09620ed2a02775b9f9bf2dbc18907c676c416499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:52 +0000

    Add package prototorch/0.7.2/prototorch-0.7.2.tar.gz

---
+++ packages/prototorch/prototorch-0.7.2.tar.gz/prototorch-0.7.2/prototorch/utils/colors.py
@@ -0,0 +1,15 @@
+"""ProtoFlow color utilities"""
+
+
+def hex_to_rgb(hex_values):
+    for v in hex_values:
+        v = v.lstrip('#')
+        lv = len(v)
+        c = [int(v[i:i + lv // 3], 16) for i in range(0, lv, lv // 3)]
+        yield c
+
+
+def rgb_to_hex(rgb_values):
+    for v in rgb_values:
+        c = "%02x%02x%02x" % tuple(v)
+        yield c
