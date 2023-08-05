31e676cd4784b5621a074cb560a05048b91c007e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:44 +0000

    Add package stellar-model/0.3.1b0/stellar-model-0.3.1b0.tar.gz

---
+++ packages/stellar-model/stellar-model-0.3.1b0.tar.gz/stellar-model-0.3.1b0/stellar_model/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.3.1b0"
+__supported_horizon_version__ = "2.9.0"
+__url__ = "https://github.com/StellarCN/stellar-model"
+__issues__ = f"{__url__}/issues"
+
+from stellar_model.response import *
