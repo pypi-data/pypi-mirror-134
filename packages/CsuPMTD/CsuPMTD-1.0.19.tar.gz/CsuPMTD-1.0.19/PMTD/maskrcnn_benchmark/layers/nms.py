035f245e6737ab127251c440cd02fef0b4b4ee8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:50 +0000

    Add package CsuPMTD/1.0.19/CsuPMTD-1.0.19.tar.gz

---
+++ packages/CsuPMTD/CsuPMTD-1.0.19.tar.gz/CsuPMTD-1.0.19/PMTD/maskrcnn_benchmark/layers/nms.py
@@ -0,0 +1,15 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+# from ._utils import _C
+
+import ctypes
+import os
+absPath = os.path.abspath(__file__)[0:-14]
+print(absPath)
+_C = ctypes.CDLL(absPath.join('./_C.cpython-36m-x86_64-linux-gnu.so'))
+from PMTD.maskrcnn_benchmark.apex.apex import amp
+
+# Only valid with fp32 inputs - give AMP the hint
+nms = amp.float_function(_C.nms)
+
+# nms.__doc__ = """
+# This function performs Non-maximum suppresion"""
