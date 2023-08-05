4d850d50730cc9cfdddccd9d92363b2c0b06991f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:05 +0000

    Add package super-stream-tools/0.1.1/super-stream-tools-0.1.1.tar.gz

---
+++ packages/super-stream-tools/super-stream-tools-0.1.1.tar.gz/super-stream-tools-0.1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
+    name='super-stream-tools',
+    version='0.1.1',
+    description='A tool to interact with streaming URLs easily',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url='https://github.com/korigamik/media-tools',
+    author='KorigamiK',
+    author_email='korigamik@gmail.com',
+    license='MIT',
+    packages=['super_stream_tools', 'super_stream_tools.utils'],
+    install_requires=['asyncio',
+                      'aiohttp',
+                      'aiofiles'
+                      ],
+    scripts=["bin/stream-tools"],
+    keywords=['python', 'video', 'stream',
+              'video stream', 'encode', 'ffmpeg'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.10',
+        "Intended Audience :: Developers",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
