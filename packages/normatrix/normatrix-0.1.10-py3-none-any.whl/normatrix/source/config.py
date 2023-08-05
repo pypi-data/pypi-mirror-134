20b4a480692aa128c7e0ed9b7f071dc67d2922cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:44 +0000

    Add package normatrix/0.1.10/normatrix-0.1.10-py3-none-any.whl

---
+++ packages/normatrix/normatrix-0.1.10-py3-none-any.whl/normatrix/source/config.py
@@ -0,0 +1,17 @@
+LIBC_BANNED_FUNC = [
+        'printf',
+        'memset',
+        'strcpy',
+        'strcat',
+        'calloc'
+]
+
+BAD_FILE_EXTENSION = [
+        '.a',
+        '.o',
+        '.so',
+        '.gch',
+        '~',
+        '#',
+        '.d'
+]
