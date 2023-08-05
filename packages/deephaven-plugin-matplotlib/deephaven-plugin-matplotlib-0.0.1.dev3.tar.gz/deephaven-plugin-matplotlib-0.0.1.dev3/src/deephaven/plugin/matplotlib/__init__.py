f97f6ccca5245a1d89cd4b76038e4b00de506f14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:54 +0000

    Add package deephaven-plugin-matplotlib/0.0.1.dev3/deephaven-plugin-matplotlib-0.0.1.dev3.tar.gz

---
+++ packages/deephaven-plugin-matplotlib/deephaven-plugin-matplotlib-0.0.1.dev3.tar.gz/deephaven-plugin-matplotlib-0.0.1.dev3/src/deephaven/plugin/matplotlib/__init__.py
@@ -0,0 +1,9 @@
+from deephaven.plugin import Registration
+
+__version__ = "0.0.1.dev3"
+
+class MatplotlibRegistration(Registration):
+    @classmethod
+    def register_into(cls, callback: Registration.Callback) -> None:
+        from . import figure_type 
+        callback.register(figure_type.FigureType)
