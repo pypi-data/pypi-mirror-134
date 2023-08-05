2b6941ebd51a7a2828faad3e78dc9cc720f51d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:25 +0000

    Add package cg/22.38.1/cg-22.38.1.tar.gz

---
+++ packages/cg/cg-22.38.1.tar.gz/cg-22.38.1/cg/constants/priority.py
@@ -0,0 +1,20 @@
+"""Priority specific constants"""
+from cgmodels.cg.constants import StrEnum
+
+PRIORITY_MAP = {"research": 0, "standard": 1, "priority": 2, "express": 3, "clinical trials": 4}
+
+REV_PRIORITY_MAP = {value: key for key, value in PRIORITY_MAP.items()}
+
+PRIORITY_OPTIONS = list(PRIORITY_MAP.keys())
+
+
+class SlurmQos(StrEnum):
+    LOW: str = "low"
+    NORMAL: str = "normal"
+    HIGH: str = "high"
+
+
+SLURM_ACCOUNT_TO_QOS = {
+    "production": SlurmQos.NORMAL,
+    "development": SlurmQos.LOW,
+}
