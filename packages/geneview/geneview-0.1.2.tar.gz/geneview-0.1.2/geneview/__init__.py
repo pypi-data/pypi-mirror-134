4b15fc86d21fb217e6378faedc16cbdebb3a3220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:13 +0000

    Add package geneview/0.1.2/geneview-0.1.2.tar.gz

---
+++ packages/geneview/geneview-0.1.2.tar.gz/geneview-0.1.2/geneview/__init__.py
@@ -0,0 +1,6 @@
+from .palette import *
+
+from .karyotype import karyoplot
+from .baseplot import venn, generate_petal_labels
+from .gwas import manhattanplot, qqplot, qqnorm
+from .popgene import admixtureplot
