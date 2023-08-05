a2750b6cee78e8526f8792d2a0a35fd4459ede6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:34 +0000

    Add package brewmaster/0.0.2/brewmaster-0.0.2-py3-none-any.whl

---
+++ packages/brewmaster/brewmaster-0.0.2-py3-none-any.whl/brewmaster/__init__.py
@@ -0,0 +1,11 @@
+from .brewmaster import (
+    SeriesAccessor, DataFrameAccessor, DataFrameGroupByAccessor
+)
+
+__all__ = [
+    'SeriesAccessor',
+    'DataFrameAccessor',
+    'DataFrameGroupByAccessor'
+]
+
+__version__ = '0.0.2'
