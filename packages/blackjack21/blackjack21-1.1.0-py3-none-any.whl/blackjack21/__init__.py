8d886acbfceebb0e7b503e34e126bb021e20c1a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:38 +0000

    Add package blackjack21/1.1.0/blackjack21-1.1.0-py3-none-any.whl

---
+++ packages/blackjack21/blackjack21-1.1.0-py3-none-any.whl/blackjack21/__init__.py
@@ -0,0 +1,10 @@
+"""
+blackjack21
+Documentation: https://github.com/rahul-nanwani/blackjack21/wiki
+"""
+from .table import Table
+
+__title__ = "blackjack21"
+__version__ = "1.1.0"
+__author__ = "Rahul Nanwani"
+__license__ = "MIT License"
