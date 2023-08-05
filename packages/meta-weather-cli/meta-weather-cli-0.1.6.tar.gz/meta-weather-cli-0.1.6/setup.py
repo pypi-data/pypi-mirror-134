28583c010213fd2118dde09e6c4ee1bf994fa8bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:06 +0000

    Add package meta-weather-cli/0.1.6/meta-weather-cli-0.1.6.tar.gz

---
+++ packages/meta-weather-cli/meta-weather-cli-0.1.6.tar.gz/meta-weather-cli-0.1.6/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['meta_weather_cli']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['meta-weather-cli = meta_weather_cli:cli']}
+
+setup_kwargs = {
+    'name': 'meta-weather-cli',
+    'version': '0.1.6',
+    'description': 'A CLI to check weather in cities around the globe',
+    'long_description': None,
+    'author': 'Omer Ben David',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
