7f7ed9c21362a763f425b0b7b861e3e15b6eff48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:15 +0000

    Add package CsuOpenpose/1.0.10/CsuOpenpose-1.0.10.tar.gz

---
+++ packages/CsuOpenpose/CsuOpenpose-1.0.10.tar.gz/CsuOpenpose-1.0.10/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages  # 这个包没有的可以pip一下
+
+setup(
+    name="CsuOpenpose",  # 这里是pip项目发布的名称
+    version="1.0.10",  # 版本号，数值大的会优先被pip
+    keywords=["pip", "openpose"],
+    description="pose",
+    license="MIT Licence",
+    author="csu_ywj",
+    packages=find_packages(),
+    data_files=[],
+    include_package_data=True,
+    platforms="any",
+    install_requires=["numpy", "matplotlib", "opencv-python", "scipy", "scikit-image", "tqdm"]
+)
