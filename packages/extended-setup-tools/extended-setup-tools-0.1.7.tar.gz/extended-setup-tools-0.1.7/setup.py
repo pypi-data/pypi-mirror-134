1c223d21fbdb7122696fb0d262e4e817a7a6dcb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:11 +0000

    Add package extended-setup-tools/0.1.7/extended-setup-tools-0.1.7.tar.gz

---
+++ packages/extended-setup-tools/extended-setup-tools-0.1.7.tar.gz/extended-setup-tools-0.1.7/setup.py
@@ -0,0 +1,23 @@
+from extended_setup import *
+
+SingleScriptModuleSetup('extended_setup').setup \
+(
+    short_description = "Python tool for helping making shorter and smarter setup.py scripts",
+    url = 'https://gitlab.com/Hares-Lab/tools/extended-setup-tools',
+    min_python_version = '3.6',
+    classifiers =
+    [
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
