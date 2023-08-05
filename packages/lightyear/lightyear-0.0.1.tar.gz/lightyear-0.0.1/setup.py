ced8919006e68c34850d18072d0561cccf381056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:58 +0000

    Add package lightyear/0.0.1/lightyear-0.0.1.tar.gz

---
+++ packages/lightyear/lightyear-0.0.1.tar.gz/lightyear-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+  name = 'lightyear',
+  packages = ['lightyear'],
+  version = '0.0.1',
+  description = 'lightyear',
+  long_description = '',
+  author = '',
+  url = 'https://github.com/alvations/lightyear',
+  keywords = [],
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ]
+)
