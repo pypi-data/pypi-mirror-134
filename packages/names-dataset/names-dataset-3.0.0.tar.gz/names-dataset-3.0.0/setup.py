34bcbcd8091cd7591eb3c95d8e4719e5f1c03d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:51 +0000

    Add package names-dataset/3.0.0/names-dataset-3.0.0.tar.gz

---
+++ packages/names-dataset/names-dataset-3.0.0.tar.gz/names-dataset-3.0.0/setup.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+VERSION = '3.0.0'
+
+packages = find_packages()
+package_name = packages[0]
+data_files = list(Path(package_name).glob('**/*.txt')) + list(Path(package_name).glob('**/*.zip'))
+data_files = [str(d) for d in data_files]
+
+setup(
+    name='names-dataset',
+    version=VERSION,
+    description='Probably the biggest dataset of Names, worldwide',
+    author='Philippe Remy',
+    license='MIT',
+    install_requires=['pycountry'],
+    packages=[package_name],
+    include_package_data=True,
+    data_files=[(package_name, data_files)]
+)
