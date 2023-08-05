941390a7947984e8b1a1da7437c6915783fd5a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:32 +0000

    Add package proman-workflows/0.1.0a5/proman_workflows-0.1.0a5-py3-none-any.whl

---
+++ packages/proman-workflows/proman_workflows-0.1.0a5-py3-none-any.whl/proman_workflows/__main__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# copyright: (c) 2020 by Jesse Johnson.
+# license: MPL-2.0, see LICENSE for more details.
+"""Provide CLI management."""
+
+from proman_workflows import workflow
+
+if __name__ == '__main__':
+    workflow.run()
