2429fb74a3a027ca3a12c2fda73f10ddb9602c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:34 +0000

    Add package pypoh/0.101/pypoh-0.101.tar.gz

---
+++ packages/pypoh/pypoh-0.101.tar.gz/pypoh-0.101/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name="pypoh",
+    packages=["pypoh"],
+    version="0.101",
+    license="MIT",
+    description="A Python Wrapper for the Proof of Humanity Rest API",
+    author="Luciano Bruna",
+    author_email="lucianobruna007@gmail.com",
+    url="https://github.com/jamesluc007",
+    download_url="https://github.com/jamesluc007/pypoh/archive/refs/tags/v_0101.tar.gz",
+    keywords=["PROOF", "HUMANITY", "ETHEREUM", "BLOCKCHAIN"],
+    install_requires=[
+        "requests",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
