a26ac2c7b2e016b3b08d4583f15f34613917932d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:08 +0000

    Add package super-stream-tools/0.1.6/super-stream-tools-0.1.6.tar.gz

---
+++ packages/super-stream-tools/super-stream-tools-0.1.6.tar.gz/super-stream-tools-0.1.6/README.md
@@ -0,0 +1,3 @@
+# Super Stream Tools
+
+A nice, fast and asynchronous library written for python developers to interact easily with streaming URLs.
---
+++ packages/super-stream-tools/super-stream-tools-0.1.6.tar.gz/super-stream-tools-0.1.6/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
+    name='super-stream-tools',
+    version='0.1.6',
+    description='A tool to interact with streaming URLs easily',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url='https://github.com/korigamik/media-tools',
+    author='KorigamiK',
+    author_email='korigamik@gmail.com',
+    license='MIT',
+    install_requires=['asyncio',
+                      'aiohttp',
+                      'aiofiles'
+                      ],
+    scripts=["bin/stream-tools"],
+    keywords=['python', 'video', 'stream',
+              'video stream', 'encode', 'ffmpeg'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.10',
+        "Intended Audience :: Developers",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    package_dir={"": "super_stream_tools"},
+    packages=['utils', ],
+    python_requires=">=3.9",
+)
