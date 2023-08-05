a8ba2d6391403e85b2b27e95ce876659c88ae87d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:10 +0000

    Add package bstats/1.0.6/bstats-1.0.6-py3-none-any.whl

---
+++ packages/bstats/bstats-1.0.6-py3-none-any.whl/bstats/__init__.py
@@ -0,0 +1,18 @@
+"""
+Brawl Stars API wrapper
+~~~~~~~~
+
+A fundamental wrapper for the Brawl Stars API
+covering all endpoints and including many features!
+
+Copyright (c) 2022-present Bimi05
+"""
+
+__title__ = "bstats"
+__author__ = "Bimi05"
+__license__ = "MIT"
+__version__ = "1.0.6"
+
+from .clients import SyncClient, AsyncClient
+from .models import *
+from .errors import *
