a7730b2b3adc4817f14ecbd3685e90864df19697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:22 +0000

    Add package odoo12-addon-product-standard-margin-kanban/12.0.1.0.1/odoo12_addon_product_standard_margin_kanban-12.0.1.0.1-py3-none-any.whl

---
+++ packages/odoo12-addon-product-standard-margin-kanban/odoo12_addon_product_standard_margin_kanban-12.0.1.0.1-py3-none-any.whl/odoo/addons/product_standard_margin_kanban/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021-Coopdevs Treball SCCL (<https://coopdevs.org>)
+# - César López Ramírez - <cesar.lopez@coopdevs.org>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Standard Margin in Kanban view",
+    "version": "12.0.1.0.1",
+    "depends": ["product_standard_margin"],
+    "author": "Coopdevs Treball SCCL",
+    "category": "Sales",
+    "website": "https://coopdevs.org",
+    "license": "AGPL-3",
+    "summary": """
+        Print Product Standard Margin Rate in Kanban view
+    """,
+    "data": ["views/product_views.xml"],
+    "installable": True,
+}
