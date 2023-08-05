b7b45304de5e840a14ea3ff1ee3c39d0b35626d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:36 +0000

    Add package normatrix/0.1.8/normatrix-0.1.8-py3-none-any.whl

---
+++ packages/normatrix/normatrix-0.1.8-py3-none-any.whl/normatrix/plugged/snake_case.py
@@ -0,0 +1,15 @@
+try:
+    from normatrix.source.file_parser import CFileParse
+    from normatrix.source.file_parser import TypeLine
+except:
+    from normatrix.normatrix.source.file_parser import CFileParse
+    from normatrix.normatrix.source.file_parser import TypeLine
+
+import os
+
+def check(file: CFileParse) -> (int, int):
+    for char in os.path.basename(file.basename):
+        if char in [chr(i) for i in range(ord('A'), ord('Z') + 1)]:
+            print(f"{file.basename}: only lower case in file name")
+            return (1, 0)
+    return (0, 0)
