266ccb850a6cf50d3089dec41c18866bc5fc7885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:56 +0000

    Add package sunnyday-JuanFernandez/1.0.0/sunnyday-JuanFernandez-1.0.0.tar.gz

---
+++ packages/sunnyday-JuanFernandez/sunnyday-JuanFernandez-1.0.0.tar.gz/sunnyday-JuanFernandez-1.0.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="sunnyday-JuanFernandez",
+    version="1.0.0",
+    author="Juan Fernandez",
+    author_email="juanjo.fde@gmail.com",
+    description="Weather forecasy data",
+    url="https://github.com/JuanjoFernandez/udemyPythonOOP/tree/main/App-X_WeatherLibrary",
+    install_requires = ['requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6"
+)
