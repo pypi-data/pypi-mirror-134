2b4a0de644099bd77dec7bb27899e76ddd551faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:50 +0000

    Add package homeinfotools/1.2.2/homeinfotools-1.2.2-py3-none-any.whl

---
+++ packages/homeinfotools/homeinfotools-1.2.2-py3-none-any.whl/homeinfotools/rpc/main.py
@@ -0,0 +1,28 @@
+"""Terminal batch updating utility."""
+
+from json import dump
+from logging import basicConfig
+from multiprocessing import Pool
+
+from homeinfotools.functions import get_log_level, handle_keyboard_interrupt
+from homeinfotools.logging import LOG_FORMAT
+from homeinfotools.rpc.argparse import get_args
+from homeinfotools.rpc.worker import Worker
+
+
+__all__ = ['main']
+
+
+@handle_keyboard_interrupt
+def main() -> None:
+    """Runs the script."""
+
+    args = get_args()
+    basicConfig(format=LOG_FORMAT, level=get_log_level(args))
+
+    with Pool(processes=args.processes) as pool:
+        result = pool.map(Worker(args), args.system)
+
+    if args.json is not None:
+        with args.json.open('w') as file:
+            dump(dict(result), file, indent=2)
