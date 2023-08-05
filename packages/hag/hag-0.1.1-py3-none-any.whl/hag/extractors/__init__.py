761e0efea088f3eb113d7ec3a907e84893c29be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:42 +0000

    Add package hag/0.1.1/hag-0.1.1-py3-none-any.whl

---
+++ packages/hag/hag-0.1.1-py3-none-any.whl/hag/extractors/__init__.py
@@ -0,0 +1,29 @@
+from .lazygit import Lazygit
+from .lf import Lf
+from .mpv import Mpv
+from .neovim import Neovim
+from .qutebrowser import Qutebrowser
+from .rofi import Rofi
+from .sxhkd import Sxhkd
+from .sxiv import Sxiv
+from .termite import Termite
+from .vim import Vim
+from .vimiv import Vimiv
+from .zathura import Zathura
+from .zsh import Zsh
+
+__all__ = [
+    "Lazygit",
+    "Lf",
+    "Mpv",
+    "Neovim",
+    "Qutebrowser",
+    "Rofi",
+    "Sxhkd",
+    "Sxiv",
+    "Termite",
+    "Vim",
+    "Vimiv",
+    "Zathura",
+    "Zsh",
+]
