77feaaefe90a2412915ca7f1ea5772fe53b166cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:06 +0000

    Add package ffe/0.0.5/ffe-0.0.5.tar.gz

---
+++ packages/ffe/ffe-0.0.5.tar.gz/ffe-0.0.5/recipes/move-new-files.py
@@ -0,0 +1,171 @@
+"""move-new-files: 移动 n 个指定后缀的新文件。
+dependencies = ["humanfriendly"]
+
+只能用来移动一个文件夹内的第一层文件，不能移动文件夹，也不会递归处理子文件夹。
+
+本插件用来移动新文件，通过 st_ctime 来对文件排序。
+    st_ctime:
+        the time of most recent metadata change on Unix,
+        the time of creation on Windows, expressed in seconds.
+
+使用 shutil.move 来移动文件，因此会先尝试改名，改名失败再进行复制和删除操作。
+
+https://github.com/ahui2016/ffe/raw/main/recipes/move-new-files.py
+"""
+
+# 每个插件都应如上所示在文件开头写简单介绍，以便 "ffe install --peek" 功能窥视插件概要。
+
+from humanfriendly import format_size
+import shutil
+from pathlib import Path
+from ffe.model import (
+    Recipe,
+    ErrMsg,
+    must_exist,
+    get_bool,
+    must_folders,
+    names_limit,
+)
+
+
+# 每个插件都必须继承 model.py 里的 Recipe
+class MoveNewFiles(Recipe):
+    @property  # 注意: 必须设为 @property
+    def name(self) -> str:
+        return "move-new-files"
+
+    @property  # 注意: 必须设为 @property
+    def help(self) -> str:
+        return """
+[[tasks]]
+recipe = "move-new-files"  # 转移最新的 n 个文件
+names = [                 # names 必须是（不多不少）两个文件夹
+    'target_dir',         # 第一个是目标文件夹
+    'src_dir',            # 第二个是源头文件夹
+]
+
+[tasks.options]
+n = 1              # 移动多少个最新的文件
+suffix = ".jpg"    # 指定文件名的末尾，空字符串表示不限
+overwrite = false  # 是否覆盖同名文件
+copy_only = false  # 设为 true 则只是复制，不删除源头文件
+names = []         # 只有当多个任务组合时才使用此项代替命令行输入
+
+# 注意：本插件在设计上并未对移动大量文件的场景进行优化，建议只用来移动少量文件。
+# version: 2022-01-11
+"""
+
+    @property  # 注意: 必须设为 @property
+    def default_options(self) -> dict:
+        return dict(n=1, suffix="", overwrite=False, copy_only=False, names=[])
+
+    def validate(self, names: list[str], options: dict) -> ErrMsg:
+        """初步检查参数（比如文件数量与是否存在），并初始化以下项目：
+
+        - self.target_dir
+        - self.src_dir
+        - self.n
+        - self.suffix
+        - self.overwrite
+        - self.copy_only
+        """
+        # 要在 dry_run, exec 中确认 is_validated
+        self.is_validated = True
+
+        # 优先采用 options 里的 names, 方便多个任务组合。
+        options_names = options.get("names", [])
+        if options_names:
+            names = options_names
+
+        names, err = names_limit(names, 2, 2)
+        if err:
+            return err
+        err = must_exist(names)
+        if err:
+            return err
+        err = must_folders(names)
+        if err:
+            return err
+
+        self.target_dir = names[0]
+        self.src_dir = names[1]
+
+        self.n = options.get("n", 1)
+        if self.n < 1:
+            return '"n" should be 1 or larger'
+
+        self.suffix = options.get("suffix", "").strip().lower()
+        self.overwrite, err = get_bool(options, "overwrite")
+        self.copy_only = options.get("copy_only", False)
+        return err
+
+    def dry_run(self, really_run: bool = False) -> ErrMsg:
+        assert self.is_validated, "在执行 dry_run 之前必须先执行 validate"
+
+        src_files, files_size, free_space = self.get_new_files()
+        verb = "Copy" if self.copy_only else "Move"
+        print(
+            f"{verb} [{len(src_files)}] files from [{self.src_dir}] to [{self.target_dir}]"
+        )
+
+        print(
+            f"files size: {format_size(files_size)}, free space: {format_size(free_space)}"
+        )
+        if free_space <= files_size:
+            return f"Not enough space in {self.target_dir}"
+
+        print_and_move(Path(self.target_dir), src_files, self.overwrite, self.copy_only, really_run)
+        return ""
+
+    def exec(self) -> ErrMsg:
+        assert self.is_validated, "在执行 exec 之前必须先执行 validate"
+        return self.dry_run(really_run=True)
+
+    def get_new_files(self) -> tuple[list[Path], int, int]:
+        src_files = Path(self.src_dir).glob("*")
+        src_files = [
+            x
+            for x in src_files
+            if x.is_file() and x.__str__().lower().endswith(self.suffix)
+        ]
+        src_files.sort(key=lambda x: x.lstat().st_ctime, reverse=True)
+        src_files = src_files[: self.n]
+        files_size = sum([x.lstat().st_size for x in src_files])
+        free_space = shutil.disk_usage(self.target_dir).free
+        return src_files, files_size, free_space
+
+
+__recipe__ = MoveNewFiles
+
+
+def print_and_move(
+    dst_folder: Path, src_files: list[Path], overwrite: bool, copy_only: bool, really_run: bool = False
+) -> None:
+    for src in src_files:
+        dst = dst_folder.joinpath(src.name)
+        dst_exists = dst.exists()
+
+        # 优先、重点处理覆盖文件的情形。
+        if dst_exists and overwrite:
+            print(f"-- overwrite {dst}")
+            if really_run:
+                copy_or_move(src, dst, copy_only)
+            continue
+
+        # 不覆盖文件。
+        if dst_exists and not overwrite:
+            print(f"-- skip {dst}")
+            continue
+
+        # 此时 dst 必然不存在，正常移动文件即可。
+        verb = "copy" if copy_only else "move"
+        print(f"-- {verb} {dst}")
+        if really_run:
+            copy_or_move(src, dst, copy_only)
+
+
+def copy_or_move(src: Path, dst: Path, copy_only: bool) -> None:
+    if copy_only:
+        shutil.copyfile(src, dst)
+    else:
+        shutil.move(src, dst)
---
+++ packages/ffe/ffe-0.0.5.tar.gz/ffe-0.0.5/recipes/tar-xz.py
@@ -0,0 +1,160 @@
+"""tar-xz: 打包压缩/解压缩文件
+
+使用打包压缩功能时，需要先进入一个文件夹，用相对路径选择需要打包的文件/文件夹。
+采用 lzma 压缩方法，打包压缩后的后缀名是 '.tar.xz'
+
+https://github.com/ahui2016/ffe/raw/main/recipes/tar-xz.py
+"""
+
+# 每个插件都应如上所示在文件开头写简单介绍，以便 "ffe install --peek" 功能窥视插件概要。
+
+import tarfile
+from pathlib import Path
+from enum import Enum, auto
+from ffe.model import Recipe, ErrMsg, must_exist, names_limit
+
+
+suffix = ".tar.xz"
+
+
+class Mode(Enum):
+    Auto = auto()
+    Zip = auto()
+    Unzip = auto()
+
+
+# 每个插件都必须继承 model.py 里的 Recipe
+class TarXZ(Recipe):
+    @property  # 必须设为 @property
+    def name(self) -> str:
+        return "tar-xz"
+
+    @property  # 必须设为 @property
+    def help(self) -> str:
+        return """
+[[tasks]]
+recipe = "tar-xz"  # 打包压缩/解压缩文件
+names = [          # 如果提供 1 个文件，并且后缀是 '.tar.xz',
+    'file.tar.xz'  # 则自动进入解压缩模式，否则进入打包压缩模式。
+]
+
+[tasks.options]
+output = ""       # 指定压缩后的文件名或解压缩时的目标文件夹，
+                  # 如果留空，本插件会为你自动设置。
+auto_wrap = true  # 解压缩出来不止一个文件时，用一个文件夹包裹它们
+names = []        # 只有当多个任务组合时才使用此项代替命令行输入
+
+使用打包压缩功能时，需要先进入一个文件夹，用相对路径选择需要打包的文件/文件夹。
+采用 lzma 压缩方法，打包压缩后的后缀名是 '.tar.xz'
+解压缩时每次只能解压缩一个文件。
+"""
+
+    @property  # 必须设为 @property
+    def default_options(self) -> dict:
+        return dict(
+            output="",
+            auto_wrap=True,
+            names=[],
+        )
+
+    def validate(self, names: list[str], options: dict) -> ErrMsg:
+        """初步检查参数（比如文件数量与是否存在），并初始化以下项目：
+
+        - self.output
+        - self.mode
+        - self.names
+        """
+        # 要在 dry_run, exec 中确认 is_validated
+        self.is_validated = True
+
+        # 优先采用 options 里的 names, 方便多个任务组合。
+        options_names = options.get("names", [])
+        if options_names:
+            names = options_names
+
+        names, err = names_limit(names, 1)
+        if err:
+            return err
+        err = must_exist(names)
+        if err:
+            return err
+
+        if len(names) == 1 and Path(names[0]).is_file() and names[0].endswith(suffix):
+            self.mode = Mode.Unzip
+        else:
+            self.mode = Mode.Zip
+        print(f"mode: {self.mode.name}")
+
+        self.names = names
+        output = options.get("output", "").strip()
+        auto_wrap = options.get("auto_wrap", True)
+
+        match self.mode:
+            case Mode.Unzip:
+                with tarfile.open(self.names[0]) as tar:
+                    only_one = len(tar.getnames()) == 1
+
+                if output:
+                    # 如果指定了 output, 则必须确保那是一个存在的文件夹
+                    self.output = Path.cwd().joinpath(output).resolve()
+                else:
+                    # 未指定 output，则解压缩到当前文件夹。
+                    self.output = Path.cwd()
+
+                # auto_wrap 为真并且不止一个文件时，利用压缩文件的文件名作为文件夹。
+                if auto_wrap and not only_one:
+                    folder = self.names[0].removesuffix(suffix)
+                    self.output = self.output.joinpath(folder).resolve()
+
+            case Mode.Zip:
+                # 如果 options 里未指定 output 文件名，则自动赋予一个合理的文件名。
+                if not output:
+                    if len(self.names) == 1:
+                        output = self.names[0]
+                    else:
+                        output = Path.cwd().name
+                if output.endswith(suffix):
+                    self.output = Path(output)
+                else:
+                    # 如果 output 文件名后缀不是 '.tar.xz', 则自动添加后缀。
+                    self.output = Path(output + suffix)
+
+        return ""
+
+    def dry_run(self) -> ErrMsg:
+        assert self.is_validated, "在执行 dry_run 之前必须先执行 validate"
+        match self.mode:
+            case Mode.Unzip:
+                with tarfile.open(self.names[0]) as tar:
+                    for name in tar.getnames():
+                        if Path(name).is_absolute():
+                            return "压缩包内含有绝对路径的文件名，请使用专业工具处理。"
+                        if name.startswith(".."):
+                            return f"{name} 可能会解压缩到父目录，请使用专业工具处理。"
+                        f = self.output.joinpath(name).resolve()
+                        if f.exists():
+                            return f"Already Exists: '{f}'"
+                        print(f)
+            case Mode.Zip:
+                if self.output.exists():
+                    return f"File exists: '{self.output}'"
+                print(f"Create '{self.output}'")
+        return ""
+
+    def exec(self) -> ErrMsg:
+        assert self.is_validated, "在执行 exec 之前必须先执行 validate"
+        err = self.dry_run()
+        if err:
+            return err
+        match self.mode:
+            case Mode.Unzip:
+                with tarfile.open(self.names[0]) as tar:
+                    tar.extractall(self.output)
+            case Mode.Zip:
+                with tarfile.open(self.output, "w:xz") as tar:
+                    for name in self.names:
+                        tar.add(name)
+        return ""
+
+
+__recipe__ = TarXZ
