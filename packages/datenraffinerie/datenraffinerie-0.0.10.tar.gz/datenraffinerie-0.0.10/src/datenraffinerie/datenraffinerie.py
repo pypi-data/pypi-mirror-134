0c3104f544185fa3be4119f0301e986f63181187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:17 +0000

    Add package datenraffinerie/0.0.10/datenraffinerie-0.0.10.tar.gz

---
+++ packages/datenraffinerie/datenraffinerie-0.0.10.tar.gz/datenraffinerie-0.0.10/src/datenraffinerie/datenraffinerie.py
@@ -0,0 +1,37 @@
+import sys
+import click
+from .valve_yard import ValveYard
+from .config_utilities import ConfigFormatError
+from .control_adapter import DAQConfigError
+import logging
+import luigi
+
+
+@click.command()
+@click.argument('config', type=click.Path(exists=True))
+@click.argument('procedure', type=str)
+@click.argument('output', type=click.Path)
+@click.option('-w', '--workers', 'workers', type=int, default=4)
+def cli(config, procedure, workers, output):
+    try:
+        run_result = luigi.build([ValveYard(
+            click.format_filename(config),
+            procedure,
+            output)],
+            local_scheduler=True,
+            workers=workers,
+            resources={'hexacontroller': 1}
+        )
+        print(run_result)
+    except ConfigFormatError as err:
+        print(err.message)
+        sys.exit(1)
+    except DAQConfigError as err:
+        print("The Configuration of one of the executed"
+              " DAQ procedures is malformed: ")
+        print(err.message)
+        sys.exit(1)
+    except Exception as err:
+        print("An error occured that was not properly caught")
+        print(err)
+        sys.exit(1)
