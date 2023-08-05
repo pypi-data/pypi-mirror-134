eb210960005f19e3e18878f85ac9a71b95688035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:43 +0000

    Add package vizex/2.0.8/vizex-2.0.8-py3-none-any.whl

---
+++ packages/vizex/vizex-2.0.8-py3-none-any.whl/files.py
@@ -0,0 +1,192 @@
+# Class to collect and organize data about files and directories
+import os
+import sys
+import concurrent.futures
+import magic
+
+from tabulate import tabulate
+from colored import fg, stylize
+from tools import bytes_to_human_readable, normalize_date, DecoratedData
+from dataclasses import dataclass
+
+
+@dataclass
+class DirectoryFiles:
+    """
+    Creates the tabular listing of all the folders and files in a given path.
+    This module can be seen as a substitute for du/df Linux terminal commands.
+    """
+
+    path: str = os.getcwd()
+    show_hidden: bool = False
+    sort_by: str = None
+    desc: bool = False
+
+    @staticmethod
+    def get_dir_size(start_path: str) -> int:
+        """
+        Calculates the cumulative size of a given directory.
+
+        Args:
+            start_path (str): a path to the folder that's
+                    the cumulative file size is calculated.
+
+        Returns:
+            int: the size of all files in a given path in bytes
+        """
+        total_size = 0
+        for dirpath, _, filenames in os.walk(start_path):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                try:
+                    total_size += os.path.getsize(fp)
+                except FileNotFoundError:
+                    # Could be a broken symlink or some other weirdness.
+                    # Trap the error here so that the directory can continue
+                    # to be successfully processed.
+                    continue
+        return total_size
+
+    @classmethod
+    def sort_data(cls, data: list, by: str, desc: bool) -> None:
+        """
+        Sorts data in place, which is inputted as a list,
+        based on a given index(key) and reverses if
+        user has selected descending order.
+
+        Args:
+            data: data with several columns
+            by: key as a string to sort by
+            desc: to sort in descending order
+        """
+        if by == 'name':
+            column = 0
+        elif by == 'dt':
+            column = 1
+        elif by == 'size':
+            column = 2
+        else:
+            column = -1
+
+        # Sort data inplace based on user's choice
+        data.sort(key=lambda x: x[column], reverse=desc)
+
+    @classmethod
+    def _decorate_dir_entry(cls, entry) -> tuple:
+        """
+        Decorates given entry for a directory. Decorate means that creates
+        a colored representation of a name of the entry, grabs
+        the date it was last modified and size in bytes and decorates.
+        collects everything and returns as a list.
+        """
+
+        # Gives orange color to the string & truncate to 32 chars
+        name = entry.split('/')[-1]
+        current = [stylize("■ " + name[:33] + "/", fg(202))]
+
+        # Get date and convert in to a human readable format
+        date = os.stat(entry).st_mtime
+        current.append(
+            DecoratedData(date, normalize_date('%h %d %Y %H:%M', date))
+        )
+
+        # recursively calculates the total size of a folder
+        b = DirectoryFiles().get_dir_size(entry)
+        current.append(
+            DecoratedData(b, bytes_to_human_readable(b))
+        )
+
+        current.append('-')  # add directory type identifier
+        return tuple(current)
+
+    @classmethod
+    def _decorate_file_entry(cls, entry) -> tuple:
+        """
+        Decorates given entry for a file. By decorate it means that creates
+        a colored representation of a name of the entry, grabs
+        the date it was last modified and size in bytes and decorates,
+        determines file type. collects everything and returns as a list.
+        """
+
+        # Gives yellow color to the string & truncate to 32 chars
+        name = entry.split('/')[-1]
+        current = [stylize("» " + name[:33], fg(226))]
+
+        # Convert last modified time (which is in nanoseconds)
+        date = os.stat(entry).st_mtime
+        current.append(
+            DecoratedData(date, normalize_date('%h %d %Y %H:%M', date))
+        )
+
+        b = os.stat(entry).st_size
+        current.append(
+            DecoratedData(b, bytes_to_human_readable(b))
+        )
+
+        # Evaluate the file type
+        current.append(
+            magic.from_file(entry, mime=True)
+        )
+        return tuple(current)
+
+    def get_usage(self) -> list:
+        """
+        Collects the data for a given path like the name of a file/folder
+        and calculates its size if it's a directory, otherwise
+        just grabs a file size. If the current entry in a given
+        path is a file method evaluates its type. Finally, gives
+        us the date when the given file/folder was last modified.
+
+        Program runs asynchronously using multiple threads or separate process
+
+        Returns:
+            list: which is a collection of each entry
+                (files and folders) in a given path.
+        """
+        data = []
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            with os.scandir(self.path) as it:
+                for entry in it:
+                    try:
+                        current = []
+
+                        # Deal with hidden files and folders
+                        if entry.name.startswith('.') and not self.show_hidden:
+                            continue
+                        elif entry.is_file():
+                            current = executor.submit(
+                                self._decorate_file_entry, entry.path)
+                        elif entry.is_dir():
+                            current = executor.submit(
+                                self._decorate_dir_entry, entry.path)
+
+                        # Add current list to the main list
+                        data.append(current.result())
+                    except Exception as e:
+                        print(f"Bad Entry ::> {e}", file=sys.stderr)
+        return data
+
+    def print_tabulated_data(self) -> tabulate:
+        """
+        Creates the tabular representation of the data.
+        Adds headers and sorts the list's data as rows.
+
+        Returns:
+            tabulate: a tabulated form of the current
+                    the directory's folders and files.
+        """
+        headers = [
+            'name',
+            'last modified (dt)',
+            'size',
+            'type'
+        ]
+        result = self.get_usage()
+        if self.sort_by:
+            self.sort_data(result, self.sort_by, self.desc)
+        print(tabulate(result, headers, tablefmt="rst"))
+
+
+if __name__ == '__main__':
+    files = DirectoryFiles(sort_by='type', desc=True)
+    files.print_tabulated_data()
---
+++ packages/vizex/vizex-2.0.8-py3-none-any.whl/tools.py
@@ -0,0 +1,223 @@
+'''
+Utility functions for vizex/vizexdf
+'''
+
+import os
+import re
+import json
+import time
+from typing import Optional, Match
+
+import pandas as pd
+
+from math import ceil
+from colored import fg, attr, stylize
+from dataclasses import dataclass
+
+
+@dataclass(order=True)
+class DecoratedData:
+    """
+    Custom class to compare numerical data for sorting
+    which appears in the stylized representation of a string.
+    """
+    size: int
+    to_string: str
+
+    def __str__(self):
+        """String representation of the class"""
+        return self.to_string
+
+
+def bytes_to_human_readable(bytes_in: int, suffix='B') -> str:
+    """
+    Converts bytes into the appropriate human
+    readable unit with a relevant suffix.
+
+    Args:
+        bytes_in (int): to convert
+        suffix (str, optional): suffix of a size string.
+                                Defaults to 'b'.
+
+    Returns:
+        str: size in a human readable
+    """
+    for unit in ['', 'k', 'M', 'G', 'T', 'P', 'E', 'Z']:
+        if abs(bytes_in) < 1024.0:
+            return f'{bytes_in:3.1f} {unit}{suffix}'
+        bytes_in /= 1024.0
+    return f'{bytes_in:.1f} {"Y"}{suffix}'
+
+
+def ints_to_human_readable(disk: dict) -> dict:
+    """Converts the dictionary of integers
+    into the human readable size formats.
+
+    Args:
+        disk [dict]: of large byte numbers
+
+    Returns:
+        dict: same dict but in human readable format
+    """
+    result = {}
+    for key in disk:
+        try:
+            result[key] = bytes_to_human_readable(disk[key])
+        except Exception:
+            result[key] = disk[key]  # If not able to convert return input back
+    return result
+
+
+def printml(folder: list, cols: int = 1) -> None:
+    """Prints multiline strings side by side.
+
+    Args:
+        folder (list): list of folders to print
+        cols (int, optional): On how many lines should it be printed.
+                            Defaults to 1.
+    """
+    size = len(folder)
+    incr = ceil(size / cols)
+    end, start = 0, 0
+    while True:
+        if end >= size:
+            break
+        end += incr
+        # Check if the end index exceeds the last index
+        if end > size:
+            end = size
+        lines = [folder[i].splitlines() for i in range(start, end)]
+        for line in zip(*lines):
+            print(*line, sep='  ')
+        print()
+        start = end
+
+
+def create_usage_warning(usage_pct: float,
+                         red_flag: int,
+                         yellow_flag: int) -> str:
+    """Create disk usage percent with warning color
+
+    Args:
+        usage_pct (float): of a given space
+        red_flag (int): threshold that decides if print should be red
+        yellow_flag (int): threshold that decides if print is yellow
+
+    Returns:
+        str: stylized warning string
+    """
+    if usage_pct < 0:
+        usage_pct = 0
+
+    if usage_pct > 100:
+        usage_pct = 100
+
+    use = str(usage_pct) + '% used'
+
+    if usage_pct >= red_flag:
+        return f"{stylize(use, attr('blink') + fg(9))}"
+    if usage_pct >= yellow_flag:
+        return f"{stylize(use, fg(214))}"
+    return f"{stylize(use, fg(82))}"
+
+
+def save_to_csv(data: dict,
+                filename: str,
+                orient: str = 'index') -> None:
+    """Outputs disks/partitions data as a CSV file
+
+    Args:
+        data (dict): to be saved to a CSV file
+        filename (str): to name a saved file
+        orient (str, optional): how lines are saved.
+                                Defaults to 'index'.
+
+    Raises:
+        NameError: if filename doesn't contain .csv at the end
+    """
+    if filename.split(".")[-1].lower() == 'csv':
+        df = pd.DataFrame.from_dict(data, orient=orient)
+        df.to_csv(filename, mode='a')
+    else:
+        raise NameError('Please include ".csv" in the filename')
+
+
+def save_to_json(data: dict,
+                 filename: str,
+                 indent: int = 4) -> None:
+    """Saves disk/partitions data as a JSON file
+
+    Args:
+        data (dict): to be saved to a JSON file
+        filename (str): to name a saved file
+        indent (int, optional): of each new line.
+                                Defaults to 4.
+
+    Raises:
+        NameError: if filename doesn't contain .json at the end
+    """
+    if filename.split(".")[-1].lower() == 'json':
+        with open(filename, "w") as file:
+            json.dump(data, file, indent=indent)
+    else:
+        raise NameError('Please include ".json" in the filename')
+
+
+def append_to_bash(alias: str, line: str) -> None:
+    """
+    Appends terminal command line as an alias in .bashrc for reuse
+
+    Args:
+        alias[str]: To set up in the bash
+        line[str]: line which will be assigned to an alias
+    """
+    bash = os.path.expanduser("~") + '/.bash_aliases'
+    print(remove_if_exists(alias, bash))
+    with open(bash, 'a+') as file:
+        file.write('alias ' + alias + f"='{line}'")
+
+
+def remove_if_exists(alias: str, path: str) -> None:
+    """
+    Removes if the given line/alias exists in a given file
+
+    Args:
+        alias (str): to check if exists in bash
+        path (str): path to the file to read
+    """
+    if not os.path.exists(path):
+        return
+    with open(path, "r") as file:
+        lines = file.readlines()
+
+    with open(path, "w") as file:
+        for line in lines:
+            # We only write back lines which is not alias
+            if f'alias {alias}' not in line.strip("\n"):
+                file.write(line)
+
+
+def normalize_date(formatting: str, date: int) -> str:
+    """
+    Converts date from nanoseconds to the human readable form
+
+    Args:
+        format (str): example %h-%d-%Y for mm-dd-yyyy
+        date (int): date in nanoseconds
+
+    Returns:
+        str: Human readable format of a date
+    """
+    return time.strftime(formatting, time.localtime(date))
+
+
+def find_word(word, src) -> Optional[Match[str]]:
+    """Find word in a src using regex"""
+    return re.compile(r'\b({0})\b'.format(word),
+                      flags=re.IGNORECASE).search(src)
+
+
+if __name__ == '__main__':
+    file1 = DecoratedData(55456, '54.2 kB')
+    file2 = DecoratedData(123233419, '117.5 MB')
+    print(f'{file1} is less than {file2} : {file1 < file2}')
