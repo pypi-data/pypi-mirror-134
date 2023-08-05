facc7c3980eadcab496596e16fc390860fe9eb2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:14 +0000

    Add package finance-datareader-my/0.0.1/finance-datareader-my-0.0.1.tar.gz

---
+++ packages/finance-datareader-my/finance-datareader-my-0.0.1.tar.gz/finance-datareader-my-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="finance-datareader-my",
+    version="0.0.1",
+    license='MIT',
+    author="2p990i9hpral",
+    author_email="4fkq3kpnszzo@protonmail.com",
+    url="https://github.com/2p990i9hpral/FinanceDataReader",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
