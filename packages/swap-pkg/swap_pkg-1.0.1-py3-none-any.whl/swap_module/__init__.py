07c8f9d2fed980b72859161675fe84f5163ebed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:21 +0000

    Add package swap-pkg/1.0.1/swap_pkg-1.0.1-py3-none-any.whl

---
+++ packages/swap-pkg/swap_pkg-1.0.1-py3-none-any.whl/swap_module/__init__.py
@@ -0,0 +1,17 @@
+def sum(n1,n2):
+	c = n1+n2
+	print('Result is:', c)
+
+def sub(n1,n2):
+	c = n1-n2
+	print('Result is:', c)
+def mul(n1,n2):
+	c = n1*n2
+	print('Result is:', c)
+
+def div(n1,n2):
+	c = n1/n2
+	print('Result is:', c)
+def power(n1,n2):
+	c = n1**n2
+	print('Result is:', c)
