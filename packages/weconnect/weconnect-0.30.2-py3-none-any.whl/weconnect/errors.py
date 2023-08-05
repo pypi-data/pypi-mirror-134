b41014fc754d9596697fbf6ba3cd647150db1897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:12 +0000

    Add package weconnect/0.30.2/weconnect-0.30.2-py3-none-any.whl

---
+++ packages/weconnect/weconnect-0.30.2-py3-none-any.whl/weconnect/errors.py
@@ -0,0 +1,22 @@
+class RetrievalError(Exception):
+    pass
+
+
+class SetterError(Exception):
+    pass
+
+
+class ControlError(SetterError):
+    pass
+
+
+class AuthentificationError(Exception):
+    pass
+
+
+class APICompatibilityError(Exception):
+    pass
+
+
+class APIError(Exception):
+    pass
