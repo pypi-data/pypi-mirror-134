5865f823936575b088bf7196c3fc715cb46e6196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:11 +0000

    Add package school-algorithms/1.3.8/school_algorithms-1.3.8.tar.gz

---
+++ packages/school-algorithms/school_algorithms-1.3.8.tar.gz/school_algorithms-1.3.8/src/school_algorithms/circle.py
@@ -0,0 +1,36 @@
+from math import pi as _pi
+from ._if_not_valid_raise import (_if_not_int_or_float_raise,
+                                   _if_not_positive_raise)
+
+
+def circle_area(r):
+    """
+    Calculates the area of a trapezium using the formula:
+        area = \u03C0(radius squared)
+
+    Parameters
+    ----------
+    r: int or float
+        The radius in the equation.
+
+    Returns
+    -------
+        Float
+            \u03C0 * (r**2)
+
+    Raises
+    ------
+    ValueError
+        If r::
+            Is not an integer or float.
+            Is not positive.
+
+    Examples
+    --------
+    >>> school_algorithms.circle_area(10)
+    314.1592653589793
+
+    """
+    _if_not_int_or_float_raise(r)
+    _if_not_positive_raise(r)
+    return _pi * (r**2)
