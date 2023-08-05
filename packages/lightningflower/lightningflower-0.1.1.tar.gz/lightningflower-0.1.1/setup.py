626e7f1c46f7c385704d1c49bbccbc0d128c3a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:52 +0000

    Add package lightningflower/0.1.1/lightningflower-0.1.1.tar.gz

---
+++ packages/lightningflower/lightningflower-0.1.1.tar.gz/lightningflower-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+required_packages = [
+    "torch", "flwr", "pytorch-lightning", "numpy"
+]
+
+setup(
+    name="lightningflower",
+    version="0.1.1",
+    description="Flower based Pytorch lightning framework",
+    url="https://github.com/ManuelRoeder/lightningflower",
+    author="Manuel Roeder",
+    author_email="manuel.roeder@web.de",
+    license="MIT",
+    packages=["lightningflower"],
+    install_requires=required_packages,
+    python_requires='>=3.8.12',
+    package_data={"": ["README.md", "LICENSE"]},
+)
