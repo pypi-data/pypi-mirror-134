cd7e0ebfa0e2d1095af0153f6b76be2bfa745b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:46 +0000

    Add package tiletool/1.0.8/tiletool-1.0.8.tar.gz

---
+++ packages/tiletool/tiletool-1.0.8.tar.gz/tiletool-1.0.8/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+#requirements = ["requests<=2.21.0"]
+include_package_data=True
+setup(
+    name='tiletool',
+    version='1.0.8',
+    packages=setuptools.find_packages(),
+    url='https://github.com/gibon228/tiletool',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Vincent Vega',
+    author_email='kharitonov.oe@gmail.com',
+    python_requires='>=3.5',
+    include_package_data=True,
+    description='TileTool is a simple, base module for working with tiles, based on pygame.sprite.Sprite. With TileTool you can transfer tile-levels and drawings from different format files to tile games created on pygame. Имеется документация на русском.',
+    #install_requires=requirements,
+    
+)
+
+
+
+
+
