624cc43d905d3f9ab6262645fe4c4d1aec109d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:31 +0000

    Add package wikibaseintegrator/0.12.0.dev7/wikibaseintegrator-0.12.0.dev7.tar.gz

---
+++ packages/wikibaseintegrator/wikibaseintegrator-0.12.0.dev7.tar.gz/wikibaseintegrator-0.12.0.dev7/wikibaseintegrator/models/__init__.py
@@ -0,0 +1,11 @@
+from .aliases import Alias, Aliases
+from .claims import Claim, Claims
+from .descriptions import Descriptions
+from .forms import Form, Forms
+from .labels import Labels
+from .language_values import LanguageValue, LanguageValues
+from .lemmas import Lemmas
+from .references import Reference, References
+from .senses import Glosses, Sense, Senses
+from .sitelinks import Sitelink, Sitelinks
+from .snaks import Snak, Snaks
