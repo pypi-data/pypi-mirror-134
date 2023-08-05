ba74038de1601eb03cdd4cb0ca3eb6872ebda047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:36 +0000

    Add package zencore-json2csv/0.2.1/zencore-json2csv-0.2.1.tar.gz

---
+++ packages/zencore-json2csv/zencore-json2csv-0.2.1.tar.gz/zencore-json2csv-0.2.1/setup.py
@@ -0,0 +1,42 @@
+import os
+from io import open
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), "r", encoding="utf-8") as fobj:
+    long_description = fobj.read()
+
+with open(os.path.join(here, 'requirements.txt'), "r", encoding="utf-8") as fobj:
+    requires = [x.strip() for x in fobj.readlines() if x.strip()]
+
+
+setup(
+    name="zencore-json2csv",
+    version="0.2.1",
+    description="Convert json array data to csv.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="zencore",
+    author_email="dobetter@zencore.cn",
+    license="MIT",
+    license_files=("LICENSE",),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords=['zencore-json2csv', 'json2csv'],
+    install_requires=requires,
+    packages=find_packages("."),
+    py_modules=["json2csv"],
+    entry_points={
+        "console_scripts": [
+            "json2csv = json2csv:main",
+        ]
+    },
+)
