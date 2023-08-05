9f164b31aeab8cdf8e88891aaa0855b791f3d95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:41 +0000

    Add package startrek/0.2.19/startrek-0.2.19.tar.gz

---
+++ packages/startrek/startrek-0.2.19.tar.gz/startrek-0.2.19/startrek/net/base_channel.py
@@ -0,0 +1,257 @@
+# -*- coding: utf-8 -*-
+#
+#   Star Trek: Interstellar Transport
+#
+#                                Written in 2021 by Moky <albert.moky@gmail.com>
+#
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2021 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+import socket
+from abc import ABC
+from typing import Optional
+
+from ..types import AddressPairObject
+from .channel import Channel
+from .channel import get_local_address, get_remote_address, is_blocking, is_closed
+
+
+class BaseChannel(AddressPairObject, Channel, ABC):
+
+    def __init__(self, remote: Optional[tuple], local: Optional[tuple], sock: socket.socket):
+        super().__init__(remote=remote, local=local)
+        self.__sock = sock
+        # flags
+        self.__blocking = False
+        self.__opened = False
+        self.__connected = False
+        self.__bound = False
+        self._refresh_flags()
+
+    def _refresh_flags(self):
+        sock = self.sock
+        if sock is None:
+            self.__blocking = False
+            self.__opened = False
+            self.__connected = False
+            self.__bound = False
+        else:
+            self.__blocking = is_blocking(sock=sock)
+            self.__opened = not is_closed(sock=sock)
+            self.__connected = get_remote_address(sock=sock) is not None
+            self.__bound = get_local_address(sock=sock) is not None
+
+    @property
+    def sock(self) -> Optional[socket.socket]:
+        return self.__sock
+
+    # Override
+    def configure_blocking(self, blocking: bool):
+        sock = self.sock
+        if sock is None:
+            raise socket.error('socket closed: remote=%s, local=%s' % (self.remote_address, self.local_address))
+        else:
+            sock.setblocking(blocking)
+        self.__blocking = blocking
+        return sock
+
+    @property  # Override
+    def blocking(self) -> bool:
+        return self.__blocking
+
+    @property  # Override
+    def opened(self) -> bool:
+        return self.__opened
+
+    @property  # Override
+    def connected(self) -> bool:
+        return self.__connected
+
+    @property  # Override
+    def bound(self) -> bool:
+        return self.__bound
+
+    @property  # Override
+    def alive(self) -> bool:
+        return self.opened and (self.connected or self.bound)
+
+    # Override
+    def bind(self, address: Optional[tuple] = None, host: Optional[str] = '0.0.0.0', port: Optional[int] = 0):
+        if address is None:
+            address = (host, port)
+        sock = self.sock
+        if sock is None:
+            raise socket.error('cannot bind socket: local=(%s:%d)' % address)
+        sock.bind(address)
+        self._local = address
+        self.__bound = True
+        self.__opened = True
+        self.__blocking = is_blocking(sock=sock)
+        return sock
+
+    # Override
+    def connect(self, address: Optional[tuple] = None, host: Optional[str] = '127.0.0.1', port: Optional[int] = 0):
+        if address is None:
+            address = (host, port)
+        sock = self.sock
+        if sock is None:
+            raise socket.error('cannot connect socket: remote=(%s:%d)' % address)
+        sock.connect(address)
+        self._remote = address
+        self.__connected = True
+        self.__opened = True
+        self.__blocking = is_blocking(sock=sock)
+        return sock
+
+    # Override
+    def disconnect(self) -> Optional[socket.socket]:
+        sock = self.__sock
+        self.close()
+        return sock
+
+    # Override
+    def close(self):
+        sock = self.__sock
+        if sock is not None and not is_closed(sock=sock):
+            # sock.shutdown(socket.SHUT_RDWR)
+            sock.close()
+        self.__sock = None
+        # update flags
+        self.__blocking = False
+        self.__opened = False
+        self.__connected = False
+        self.__bound = False
+
+    #
+    #   Input/Output
+    #
+
+    # Override
+    def read(self, max_len: int) -> Optional[bytes]:
+        # check socket first
+        sock = self.sock
+        if sock is None:
+            raise socket.error('socket lost, cannot read data: %s' % self)
+        # try to receive data
+        try:
+            data = sock.recv(max_len)
+        except socket.error as error:
+            error = self._check_receiving_error(error=error)
+            if error is not None:
+                # connection lost?
+                raise error
+            # received nothing
+            return None
+        # check data
+        error = self._check_received_data(data=data)
+        if error is not None:
+            # connection lost!
+            raise error
+        # OK
+        return data
+
+    # Override
+    def write(self, data: bytes) -> int:
+        # check socket first
+        sock = self.sock
+        if sock is None:
+            raise socket.error('socket lost, cannot write data (%d bytes): %s' % (len(data), self))
+        # try to send data
+        try:
+            # sent = sock.sendall(data)
+            return sendall(data=data, sock=sock)
+        except socket.error as error:
+            error = self._check_sending_error(error=error)
+            if error is not None:
+                # connection lost?
+                raise error
+            # buffer overflow!
+            return -1
+
+    #
+    #   Check for socket errors
+    #
+    ResourceTemporarilyUnavailable = 'Resource temporarily unavailable'
+
+    def _check_socket_error(self, error: socket.error) -> Optional[socket.error]:
+        # the socket will raise 'Resource temporarily unavailable'
+        # when received nothing in non-blocking mode,
+        # or buffer overflow while sending too many bytes,
+        # here we should ignore this exception.
+        if not self.blocking:
+            # if error.errno == socket.EAGAIN:
+            if error.strerror == self.ResourceTemporarilyUnavailable:
+                # ignore it
+                return None
+        # in blocking mode, the socket wil wait until sent/received data,
+        # but if timeout was set, it will raise 'timeout' error on timeout,
+        # here we should ignore this exception.
+        if isinstance(error, socket.timeout):
+            if self.sock.gettimeout() is not None:  # or not self.blocking:
+                # ignore it
+                return None
+        # print('[NET] socket error: %s' % error)
+        return error
+
+    def _check_sending_error(self, error: socket.error) -> Optional[socket.error]:
+        return self._check_socket_error(error=error)
+
+    def _check_receiving_error(self, error: socket.error) -> Optional[socket.error]:
+        return self._check_socket_error(error=error)
+
+    def _check_received_data(self, data: Optional[bytes]) -> Optional[socket.error]:
+        # in blocking mode, the socket will wait until received something,
+        # but if timeout was set, it will return None too, it's normal;
+        # otherwise, we know the connection was lost.
+        if data is None or len(data) == 0:
+            if self.sock.gettimeout() is None:  # and self.blocking:
+                # print('[NET] socket error: remote peer reset socket')
+                return socket.error('remote peer reset socket')
+
+
+def sendall(data: bytes, sock: socket) -> int:
+    """ Return the number of bytes sent;
+        this may be less than len(data) if the network is busy. """
+    sent = 0
+    rest = len(data)
+    assert rest > 0, 'cannot send empty data'
+    while True:  # not is_closed(sock=sock):
+        cnt = sock.send(data)
+        if cnt == 0:
+            # buffer overflow?
+            break
+        elif cnt < 0:
+            # socket error?
+            if sent == 0:
+                return -1
+            break
+        # something sent, check remaining data
+        sent += cnt
+        rest -= cnt
+        if rest > 0:
+            data = data[cnt:]
+        else:
+            # done!
+            break
+    return sent
---
+++ packages/startrek/startrek-0.2.19.tar.gz/startrek-0.2.19/startrek/net/base_conn.py
@@ -0,0 +1,240 @@
+# -*- coding: utf-8 -*-
+#
+#   Star Trek: Interstellar Transport
+#
+#                                Written in 2021 by Moky <albert.moky@gmail.com>
+#
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2021 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+import socket
+import time
+import weakref
+from typing import Optional
+
+from ..fsm import StateDelegate
+from ..types import AddressPairObject
+
+from .hub import Hub
+from .channel import Channel
+from .connection import Connection
+from .delegate import ConnectionDelegate
+from .state import ConnectionState, StateMachine, TimedConnection
+
+
+class BaseConnection(AddressPairObject, Connection, TimedConnection, StateDelegate):
+
+    EXPIRES = 16  # seconds
+
+    def __init__(self, remote: tuple, local: Optional[tuple], channel: Channel, delegate: ConnectionDelegate, hub: Hub):
+        super().__init__(remote=remote, local=local)
+        self.__channel_ref = weakref.ref(channel)
+        # active times
+        self.__last_sent_time = 0
+        self.__last_received_time = 0
+        # handlers
+        self.__delegate = weakref.ref(delegate)
+        self.__hub_ref = weakref.ref(hub)
+        # Finite State Machine
+        self.__fsm = self._create_state_machine()
+
+    # protected
+    def _create_state_machine(self) -> StateMachine:
+        fsm = StateMachine(connection=self)
+        fsm.delegate = self
+        return fsm
+
+    @property
+    def delegate(self) -> ConnectionDelegate:
+        return self.__delegate()
+
+    @property
+    def hub(self) -> Hub:
+        return self.__hub_ref()
+
+    @property
+    def channel(self) -> Optional[Channel]:
+        return self._get_channel()
+
+    def _get_channel(self) -> Optional[Channel]:
+        ref = self.__channel_ref
+        if ref is not None:
+            return ref()
+
+    def _set_channel(self, channel: Channel):
+        if channel is None:
+            self.__channel_ref = None
+        else:
+            self.__channel_ref = weakref.ref(channel)
+
+    @property  # Override
+    def opened(self) -> bool:
+        sock = self.channel
+        return sock is not None and sock.opened
+
+    @property  # Override
+    def bound(self) -> bool:
+        sock = self.channel
+        return sock is not None and sock.bound
+
+    @property  # Override
+    def connected(self) -> bool:
+        sock = self.channel
+        return sock is not None and sock.connected
+
+    @property  # Override
+    def alive(self) -> bool:
+        # sock = self.channel
+        # return sock is not None and sock.alive
+        return self.opened and (self.connected or self.bound)
+
+    # Override
+    def close(self):
+        self.__close_channel()
+        self.__fsm.stop()
+
+    def __close_channel(self):
+        sock = self._get_channel()
+        if sock is not None:
+            self.__channel_ref = None
+            hub = self.hub
+            if hub is not None:
+                hub.close_channel(channel=sock)
+
+    @property  # Override
+    def last_sent_time(self) -> int:
+        return self.__last_sent_time
+
+    @property  # Override
+    def last_received_time(self) -> int:
+        return self.__last_received_time
+
+    # Override
+    def is_sent_recently(self, now: int) -> bool:
+        return now < self.__last_sent_time + self.EXPIRES
+
+    # Override
+    def is_received_recently(self, now: int) -> bool:
+        return now < self.__last_received_time + self.EXPIRES
+
+    # Override
+    def is_long_time_not_received(self, now: int) -> bool:
+        return now > self.__last_received_time + (self.EXPIRES << 3)
+
+    # Override
+    def received(self, data: bytes, remote: Optional[tuple], local: Optional[tuple]):
+        self.__last_received_time = int(time.time())
+        delegate = self.delegate
+        if delegate is not None:
+            delegate.connection_received(data=data, source=remote, destination=local, connection=self)
+
+    # protected
+    def _send(self, data: bytes, target: Optional[tuple]) -> int:
+        sock = self.channel
+        if sock is None or not sock.alive:
+            raise socket.error('socket channel lost: %s' % sock)
+        sent = sock.send(data=data, target=target)
+        if sent > 0:
+            self.__last_sent_time = int(time.time())
+        return sent
+
+    # Override
+    def send(self, data: bytes, target: Optional[tuple] = None) -> int:
+        # try to send data
+        error = None
+        sent = -1
+        try:
+            sent = self._send(data=data, target=target)
+            if sent <= 0:  # == -1:
+                error = ConnectionError('failed to send: %d byte(s) to %s' % (len(data), target))
+                # self.__close_channel()
+        except socket.error as e:
+            error = e
+            self.__close_channel()
+        # callback
+        delegate = self.delegate
+        if delegate is not None:
+            # get local address as source
+            source = self._local
+            if source is None:
+                sock = self._get_channel()
+                if sock is not None:
+                    source = sock.local_address
+            if error is None:
+                if sent < len(data):
+                    data = data[:sent]
+                delegate.connection_sent(data=data, source=source, destination=target, connection=self)
+            else:
+                delegate.connection_error(error=error, data=data, source=source, destination=target, connection=self)
+        return sent
+
+    #
+    #   States
+    #
+
+    @property  # Override
+    def state(self) -> ConnectionState:
+        return self.__fsm.current_state
+
+    # Override
+    def tick(self):
+        # drive state machine forward
+        self.__fsm.tick()
+
+    def start(self):
+        self.__fsm.start()
+
+    def stop(self):
+        self.__close_channel()
+        self.__fsm.stop()
+
+    #
+    #   StateDelegate
+    #
+
+    # Override
+    def enter_state(self, state: ConnectionState, ctx: StateMachine):
+        pass
+
+    # Override
+    def exit_state(self, state: ConnectionState, ctx: StateMachine):
+        current = ctx.current_state
+        if current == ConnectionState.READY:
+            if state != ConnectionState.MAINTAINING:
+                # change state to 'connected', reset times to just expired
+                timestamp = int(time.time()) - self.EXPIRES - 1
+                self.__last_sent_time = timestamp
+                self.__last_received_time = timestamp
+        # callback
+        delegate = self.delegate
+        if delegate is not None:
+            delegate.connection_state_changed(previous=state, current=current, connection=self)
+
+    # Override
+    def pause_state(self, state: ConnectionState, ctx: StateMachine):
+        pass
+
+    # Override
+    def resume_state(self, state: ConnectionState, ctx: StateMachine):
+        pass
