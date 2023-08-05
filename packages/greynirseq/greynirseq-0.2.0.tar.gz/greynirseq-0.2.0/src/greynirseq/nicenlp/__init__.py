2408b135d63083d74f166b6d196b83cc463c117b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:51 +0000

    Add package greynirseq/0.2.0/greynirseq-0.2.0.tar.gz

---
+++ packages/greynirseq/greynirseq-0.2.0.tar.gz/greynirseq-0.2.0/src/greynirseq/nicenlp/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+from greynirseq.nicenlp.criterions import (
+    multiclass_token_classification_criterion,
+    multilabel_token_classification_criterion,
+)
+from greynirseq.nicenlp.data import datasets
+from greynirseq.nicenlp.models import multiclass, multilabel
+from greynirseq.nicenlp.tasks import (
+    multiclass_token_classification_task,
+    multilabel_token_classification_task,
+    translation_with_backtranslation,
+    translation_with_glossary,
+)
