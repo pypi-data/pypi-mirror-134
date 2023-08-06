a1f0b477c7c555a16396449f400660afc72398de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:39 +0000

    Add package showdown.ssh/0.0.6/showdown.ssh-0.0.6.tar.gz

---
+++ packages/showdown.ssh/showdown.ssh-0.0.6.tar.gz/showdown.ssh-0.0.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="showdown.ssh",
+    version="0.0.6",
+    author="Boshimoto",
+    license="MIT License",
+    author_email="author@example.com",
+    description="A tool to automate local network health checks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/boshimoto/showdown",
+    project_urls={
+        "Bug Tracker": "https://github.com/boshimoto/showdown/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "showdown"},
+    install_requires=[
+        'netmiko',
+    ],
+    packages=setuptools.find_packages(where="showdown"),
+    python_requires=">=3.6",
+)
