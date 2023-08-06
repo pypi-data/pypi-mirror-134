7cfd4e49a74e2eaf9c339b96244c733c080e55ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:48 +0000

    Add package Bot-Base/1.3.11/Bot_Base-1.3.11-py3-none-any.whl

---
+++ packages/Bot-Base/Bot_Base-1.3.11-py3-none-any.whl/bot_base/context.py
@@ -0,0 +1,19 @@
+try:
+    from nextcord.ext import commands
+except ModuleNotFoundError:
+    from discord.ext import commands
+
+from bot_base.wraps import Meta, WrappedChannel, WrappedMember, WrappedUser
+
+
+class BotContext(commands.Context, Meta):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._bot = self.bot
+
+        self.message.channel = WrappedChannel(self.message.channel, bot=self.bot)
+
+        if not self.guild:
+            self.message.author = WrappedUser(self.message.author, bot=self.bot)
+        else:
+            self.message.author = WrappedMember(self.message.author, bot=self.bot)
