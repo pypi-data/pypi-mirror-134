8af4a7044af9899b122da9ad7c7a0531c07f3d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:34 +0000

    Add package orbit-ml/1.1.0/orbit-ml-1.1.0.tar.gz

---
+++ packages/orbit-ml/orbit-ml-1.1.0.tar.gz/orbit-ml-1.1.0/orbit/utils/plot.py
@@ -0,0 +1,37 @@
+import pkg_resources
+import functools
+from matplotlib import pyplot as plt
+
+STYLE_FILE_NAME = 'plot_style.mplstyle'
+
+
+def get_orbit_style():
+    path = pkg_resources.resource_filename(
+        'orbit',
+        STYLE_FILE_NAME
+    )
+    return path
+
+
+def orbit_style_decorator(func):
+    orbit_style = get_orbit_style()
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if "use_orbit_style" in kwargs.keys():
+            use_orbit_style = kwargs['use_orbit_style']
+            del kwargs['use_orbit_style']
+        else:
+            # default to be True if arg is not specified
+            use_orbit_style = True
+        # try:
+        if use_orbit_style:
+            with plt.style.context(orbit_style):
+                return func(*args, **kwargs)
+        else:
+            return func(*args, **kwargs)
+        # except:
+        #     kwargs['use_orbit_style'] = True
+        #     with plt.style.context(orbit_style):
+        #         return func(*args, **kwargs)
+
+    return wrapper
