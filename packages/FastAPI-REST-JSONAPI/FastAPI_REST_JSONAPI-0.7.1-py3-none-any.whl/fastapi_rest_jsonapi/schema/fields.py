770709b3d2ddbc41ce17aa4f246a69f9c15fbb8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:28 +0000

    Add package FastAPI-REST-JSONAPI/0.7.1/FastAPI_REST_JSONAPI-0.7.1-py3-none-any.whl

---
+++ packages/FastAPI-REST-JSONAPI/FastAPI_REST_JSONAPI-0.7.1-py3-none-any.whl/fastapi_rest_jsonapi/schema/fields.py
@@ -0,0 +1,12 @@
+from marshmallow_jsonapi.fields import (
+    Integer as MarshmallowInteger,
+    String as MarshmallowString,
+)
+
+
+class Integer(MarshmallowInteger):
+    ...
+
+
+class String(MarshmallowString):
+    ...
