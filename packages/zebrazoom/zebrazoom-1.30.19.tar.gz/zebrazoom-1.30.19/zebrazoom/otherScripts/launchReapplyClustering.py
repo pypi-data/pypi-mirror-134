2f478e8c3356aa088fb767679468641a565da58e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:06 +0000

    Add package zebrazoom/1.30.19/zebrazoom-1.30.19.tar.gz

---
+++ packages/zebrazoom/zebrazoom-1.30.19.tar.gz/zebrazoom-1.30.19/zebrazoom/otherScripts/launchReapplyClustering.py
@@ -0,0 +1,20 @@
+from zebrazoom.dataAnalysis.dataanalysis.reapplyClustering import reapplyClustering
+
+def launchReapplyClustering():
+
+  outputFolderResult = 'zebrazoom/dataAnalysis/resultsClustering/parkinsonFreelySwimForClustering_OutliersRemoved_BASSstyleParam2/activeLearning/'
+  nbConditions  = 2
+  nbCluster     = 4
+
+  if False:
+    
+    for modelUsed in ['KMeans', 'GaussianMixture', 'BIRCH', 'DBSCAN', 'MeanShift', 'SpectralClustering', 'Ward', 'AgglomerativeClustering', 'OPTICS', 'AffinityPropagation']:
+      try:
+        reapplyClustering(modelUsed, nbConditions, nbCluster, outputFolderResult)
+      except:
+        print(modelUsed, "failed")
+    
+  else:
+    
+    reapplyClustering('KMeans', nbConditions, nbCluster, outputFolderResult)
+    
