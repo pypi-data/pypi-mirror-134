1f9e2b2d52f2aa9cf391fb756e6020b51c1a160e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:17 +0000

    Add package xrpy/0.0.4/xrpy-0.0.4.tar.gz

---
+++ packages/xrpy/xrpy-0.0.4.tar.gz/xrpy-0.0.4/src/xrpy/__init__.py
@@ -0,0 +1,29 @@
+from .main import create_wallet
+from .main import send_transaction
+from .main import set_trust_line
+from .main import create_offer_buy
+from .main import create_offer_sell
+from .main import cancel_offer
+from .main import get_account_info
+from .main import get_account_trustlines
+from .main import JsonRpcClient
+from .main import Wallet
+
+
+__all__ = [
+    'create_wallet',
+    'send_transaction',
+    'set_trust_line',
+    'create_offer_buy',
+    'create_offer_sell',
+    'get_account_info',
+    'get_account_trustlines',
+    'cancel_offer',
+    'JsonRpcClient',
+    'Wallet',
+]
+
+
+__version__ = "0.0.4"
+__author__ = "amiwrpremium"
+__reason__ = 'OK'
