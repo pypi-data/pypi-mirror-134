bafc67fa209e30d266f10d9bf2b1b11c8d23f487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:43 +0000

    Add package dash-auth-external/0.2.2/dash-auth-external-0.2.2.tar.gz

---
+++ packages/dash-auth-external/dash-auth-external-0.2.2.tar.gz/dash-auth-external-0.2.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+REQUIRES = ["requests", "requests_oauthlib", "flask"]
+
+NAME = "dash-auth-external"
+
+setup(
+    name=NAME,
+    version="0.2.2",
+    description="Integrate Dash with 3rd Parties and external providers",
+    author_email="jholcombe@hotmail.co.uk",
+    url="https://github.com/jamesholcombe/dash-auth-external",
+    keywords=["Dash", "Plotly", "Authentication", "Auth", "External"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""\
+    Integrate Plotly Dash with 3rd Parties and external providers."
+    """,
+)
