1697c3fee92dbed7972ba6cf6581218e226b3abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:50 +0000

    Add package glyles/0.1.2/glyles-0.1.2.tar.gz

---
+++ packages/glyles/glyles-0.1.2.tar.gz/glyles-0.1.2/glyles/glycans/utils.py
@@ -0,0 +1,35 @@
+from enum import Enum
+
+
+class Config(Enum):
+    """
+    Configuration enum to represent if the monomer is in alpha, beta, or undefined configuration.
+    """
+    UNDEF = 0
+    ALPHA = 1
+    BETA = 2
+
+
+class Enantiomer(Enum):
+    """
+    Configuration of the whole monomer regarding L and D forms in terms of enantiomerism.
+    """
+    D = 0
+    L = 1
+
+
+class Lactole(Enum):
+    """
+    Specification if a monomer is a pyranose (6-ring) or a furanose (5-ring)
+    """
+    FURANOSE = 5
+    PYRANOSE = 6
+
+
+class Mode(Enum):
+    """
+    Enumerate different modes how to represent the monomers in the tree.
+    """
+    DEFAULT_MODE = "rdkit"
+    NETWORKX_MODE = "nx"
+    RDKIT_MODE = "rdkit"
