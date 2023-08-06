a26ba4756ba4e3fd93d90edd03220957429520ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:05 +0000

    Add package pygifconverter-test06/1.0.0/pygifconverter_test06-1.0.0.tar.gz

---
+++ packages/pygifconverter-test06/pygifconverter_test06-1.0.0.tar.gz/pygifconverter_test06-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pygifconverter_test06",
+    version="1.0.0",
+    description="Test package for distribution",
+    author="Paul0620",
+    author_email="dlrhkdghsp@gmail.com",
+    url="",
+    download_url="",
+    install_requires=["pillow"],
+    include_package_data=True,
+    packages=find_packages(),
+    keywords=["GIFCONVERTER", "gifconverter"],
+    python_requires=">=3",
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
