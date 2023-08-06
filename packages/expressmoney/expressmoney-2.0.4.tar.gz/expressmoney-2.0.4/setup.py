0cd5275167e0e44d82300059b54fc6c176695e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:26 +0000

    Add package expressmoney/2.0.4/expressmoney-2.0.4.tar.gz

---
+++ packages/expressmoney/expressmoney-2.0.4.tar.gz/expressmoney-2.0.4/setup.py
@@ -0,0 +1,17 @@
+"""
+py setup.py sdist
+twine upload dist/expressmoney-2.0.4.tar.gz
+"""
+import setuptools
+
+setuptools.setup(
+    name='expressmoney',
+    packages=setuptools.find_packages(),
+    version='2.0.4',
+    description='SDK ExpressMoney',
+    author='Development team',
+    author_email='dev@expressmoney.com',
+    install_requires=('google-cloud-secret-manager', 'google-cloud-error-reporting', 'google-cloud-pubsub',
+                      'google-cloud-tasks', 'google-cloud-storage', 'requests'),
+    python_requires='>=3.7',
+)
