88a99d6fde769f22e9d2a89ab943f7b36c140f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:35 +0000

    Add package short-pygame/0.0.0/short_pygame-0.0.0.tar.gz

---
+++ packages/short-pygame/short_pygame-0.0.0.tar.gz/short_pygame-0.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+    name='short_pygame',
+    packages=find_packages(include=['short_pygame']),
+    install_requires=["pygame"],
+    author="Ivanchik burinki#4376 in discord",
+    description="this is a plugin for shorten your pygame project description normal domain be soon"
+)
---
+++ packages/short-pygame/short_pygame-0.0.0.tar.gz/short_pygame-0.0.0/short_pygame/start.py
@@ -0,0 +1,5 @@
+class start_script():
+    def starting(self):
+        pygame.init()
+        clock = pygame.time.Clock()
+        clock.tick(60)
