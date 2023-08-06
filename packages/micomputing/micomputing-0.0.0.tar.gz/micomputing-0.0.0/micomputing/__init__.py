8314316151c072eb28b4780c2c18db1f43426a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:14 +0000

    Add package micomputing/0.0.0/micomputing-0.0.0.tar.gz

---
+++ packages/micomputing/micomputing-0.0.0.tar.gz/micomputing-0.0.0/micomputing/__init__.py
@@ -0,0 +1,116 @@
+
+from pycamia import info_manager
+
+__info__ = info_manager(
+    project = 'PyCAMIA',
+    package = 'micomputing',
+    author = 'Yuncheng Zhou',
+    create = '2021-12',
+    version = '1.1.29',
+    contact = 'bertiezhou@163.com',
+    keywords = ['medical image', 'image registration', 'image similarities'],
+    description = "'micomputing' is a package for medical image computing. ",
+    requires = ['numpy', 'torch>=1.5.1', 'pycamia', 'pyoverload', 'batorch']
+).check()
+
+from .stdio import NII, DCM #*
+from .sim import MutualInformation, NormalizedMutualInformation, KLDivergence, CorrelationOfLocalEstimation, NormalizedVectorInformation, Cos2Theta, SumSquaredDifference, MeanSquaredErrors, PeakSignalToNoiseRatio, CrossEntropy, CrossCorrelation, NormalizedCrossCorrelation, StructuralSimilarity, Dice, LabelDice #*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
