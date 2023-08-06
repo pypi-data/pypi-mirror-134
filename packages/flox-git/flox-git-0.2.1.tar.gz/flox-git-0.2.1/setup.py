0dbc720e26b7d92f50b5be49b56cff675bdb478e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:49 +0000

    Add package flox-git/0.2.1/flox-git-0.2.1.tar.gz

---
+++ packages/flox-git/flox-git-0.2.1.tar.gz/flox-git-0.2.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['flox_git']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['GitPython>=3.1.0,<4.0.0', 'flox-core>=0.2,<1.0.0']
+
+entry_points = \
+{'flox.plugin': ['git = flox_git:plugin']}
+
+setup_kwargs = {
+    'name': 'flox-git',
+    'version': '0.2.1',
+    'description': 'Bootstrap git repository for your project managed by flox.',
+    'long_description': None,
+    'author': 'Michal Przytulski',
+    'author_email': 'michal@przytulski.pl',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/getflox/flox-git',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
