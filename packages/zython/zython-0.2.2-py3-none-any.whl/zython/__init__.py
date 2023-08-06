d145681d80958dcb9876d9c3e1bd61a79169470f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:43 +0000

    Add package zython/0.2.2/zython-0.2.2-py3-none-any.whl

---
+++ packages/zython/zython-0.2.2-py3-none-any.whl/zython/__init__.py
@@ -0,0 +1,19 @@
+import warnings
+
+from minizinc import find_driver
+
+if not find_driver():
+    warnings.warn("Solver wasn't found, are you sure you specify minizinc folder to $PATH environment variable")
+
+
+import zython.var_par.types
+from zython.var_par.var import var
+from zython.var_par.par import par
+from zython.var_par.collections.array import Array
+from zython.var_par.collections.set import Set
+from zython.operations.functions_and_predicates import exists, forall, sum, alldifferent, circuit, count, min, max,\
+    allequal, ndistinct, increasing, decreasing
+from zython.model import Model
+from zython.result import as_original
+
+range = zython.var_par.types._range
---
+++ packages/zython/zython-0.2.2-py3-none-any.whl/zython/var_par/types.py
@@ -0,0 +1,40 @@
+import enum
+
+import zython  # for type hints
+from typing import Union, Sequence
+
+from zython.operations.operation import Operation
+
+
+class _range:
+    def __new__(cls, start, stop=None, step=1):
+        if stop is None:
+            stop = start
+            start = 0
+        if (isinstance(start, (Operation, float))
+                or isinstance(stop, (Operation, float))
+                or isinstance(step, (Operation, float))):
+            self = object.__new__(cls)
+            self.start = start
+            self.stop = stop
+            self.step = step
+            return self
+        else:
+            return range(start, stop, step)
+
+
+Ranges = range, _range
+RangesType = Union[range, _range]
+ZnSequence = Union[RangesType, "zython.var_par.array.ArrayMixin", Sequence["zython.var_par.var.var"]]
+
+
+def is_range(obj):
+    return isinstance(obj, Ranges)
+
+
+def is_enum(arg):
+    return isinstance(arg, enum.EnumMeta)
+
+
+def get_type(arg):
+    return getattr(arg, "type", type(arg))
