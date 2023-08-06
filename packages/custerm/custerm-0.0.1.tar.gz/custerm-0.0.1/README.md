4c154588dadfe20a070c8bfac39edcd5d3fcbb18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:59 +0000

    Add package custerm/0.0.1/custerm-0.0.1.tar.gz

---
+++ packages/custerm/custerm-0.0.1.tar.gz/custerm-0.0.1/README.md
@@ -0,0 +1,46 @@
+# CusTERM
+
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+
+CusTERM is a Python library for making your own bootleg command line!
+
+## Installing
+
+This package is not yet on pip but you can either add the package to the Lib folder or simply bundle the script with the project files.
+
+## Usage
+
+```python
+# import the package
+import custerm as ct
+
+# define the functions
+def command1():
+	ct.log("Ran a command!")
+
+# you can accept parameters
+def command2(inp):
+	ct.log("You wrote: "+inp)
+
+# you can log as errors and warnings
+def command3():
+	# errors take an error type and a message
+	ct.error("FakeError","This is an error!")
+	# warnings take a message
+	ct.warning("This is a warning!")
+
+# you can make assertions that use ct errors
+def command4():
+	ct.assertion(0==0,"This is an assertion!")
+	ct.assertion(0==0,"This is an assertion that raises a real error too!",hard=True)
+
+# register the commands
+ct.regcmd("cmd1",command1)
+ct.regcmd("cmd2",command2)
+ct.regcmd("cmd3",command3)
+ct.regcmd("cmd4",command4)
+
+# initialise the interface
+ct.show()
+ct.fetchroot().mainloop()
+```
---
+++ packages/custerm/custerm-0.0.1.tar.gz/custerm-0.0.1/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup
+import runpy
+
+def read(fname):
+    f = open(os.path.join(os.path.dirname(__file__), fname))
+    r = f.read()
+    f.close()
+    return r
+
+setup(
+    name="custerm",
+    version=runpy.run_path("./custerm/__version__.py")["__version__"],
+    packages=["custerm"],
+    license="MIT",
+    description="CusTERM is a Python library for making your own bootleg command line!",
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    author="obfuscatedgenerated",
+    author_email="pip@obfuscatedgenerated.ml",
+    url="https://github.com/obfuscatedgenerated/CusTERM",
+    keywords=["terminal", "tkinter", "ui"],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
