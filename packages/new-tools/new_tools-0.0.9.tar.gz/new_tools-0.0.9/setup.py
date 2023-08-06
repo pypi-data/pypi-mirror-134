8b2f9bd83f97817b08bc06c07099646f4f81fd2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:58 +0000

    Add package new-tools/0.0.9/new_tools-0.0.9.tar.gz

---
+++ packages/new-tools/new_tools-0.0.9.tar.gz/new_tools-0.0.9/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="new_tools",
+    version="0.0.9",
+    author="Overcomer",
+    author_email="michael31703@gmail.com",
+    description="Multiple tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Michael07220823/new_tools.git",
+    keywords="tools",
+    install_requires=["opencv-python", "numpy"],
+    license="MIT License",
+    packages=setuptools.find_packages(include=["new_tools", "new_tools.*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+)
