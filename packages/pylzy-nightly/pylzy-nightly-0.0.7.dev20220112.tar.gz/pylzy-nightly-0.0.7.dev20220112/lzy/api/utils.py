b85c8c845b6a6519a8057a6d3fd3df872e81cb80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:02 +0000

    Add package pylzy-nightly/0.0.7.dev20220112/pylzy-nightly-0.0.7.dev20220112.tar.gz

---
+++ packages/pylzy-nightly/pylzy-nightly-0.0.7.dev20220112.tar.gz/pylzy-nightly-0.0.7.dev20220112/lzy/api/utils.py
@@ -0,0 +1,56 @@
+from typing import Iterable, Callable, Tuple, get_type_hints, Optional, Any, Type, Dict, TypeVar
+
+# noinspection PyProtectedMember
+from lzy.api._proxy import proxy
+from .lazy_op import LzyOp
+
+T = TypeVar('T')
+
+
+def print_lzy_op(op: LzyOp) -> None:
+    input_types = ", ".join(str(t) for t in op.input_types)
+    print(f'{op.func} {op.func.__name__}({input_types}) -> {op.return_type}')
+
+
+# TODO: remove?
+def print_lzy_ops(ops: Iterable[LzyOp]) -> None:
+    for lzy_op in ops:
+        print_lzy_op(lzy_op)
+
+
+def infer_return_type(func: Callable) -> Optional[type]:
+    hints = get_type_hints(func)
+    if 'return' not in hints:
+        return None
+
+    or_type = hints['return']
+    if hasattr(or_type, '__origin__'):
+        return or_type.__origin__  # type: ignore
+    elif type(or_type) == type:
+        return or_type
+    else:
+        return None
+
+
+def infer_arg_types(*args) -> Tuple[type, ...]:
+    # noinspection PyProtectedMember
+    return tuple(
+        arg._op.return_type if is_lazy_proxy(arg) else type(arg)
+        for arg in args
+    )
+
+
+def is_lazy_proxy(obj: Any) -> bool:
+    cls = type(obj)
+    return hasattr(cls, '__lzy_proxied__') and cls.__lzy_proxied__
+
+
+def lazy_proxy(materialization: Callable[[], T], return_type: Type[T], obj_attrs: Dict[str, Any]) -> Any:
+    return proxy(
+        lambda: materialization(),
+        return_type,
+        cls_attrs={
+            '__lzy_proxied__': True
+        },
+        obj_attrs=obj_attrs
+    )
---
+++ packages/pylzy-nightly/pylzy-nightly-0.0.7.dev20220112.tar.gz/pylzy-nightly-0.0.7.dev20220112/lzy/startup.py
@@ -0,0 +1,60 @@
+import base64
+import inspect
+import os
+from pathlib import Path
+from typing import Any
+
+import cloudpickle
+import sys
+import time
+
+from lzy.api.lazy_op import LzyRemoteOp
+from lzy.api.utils import lazy_proxy
+from lzy.model.zygote_python_func import FuncContainer
+from lzy.servant.bash_servant_client import BashServantClient
+from lzy.servant.servant_client import ServantClient
+
+
+def load_arg(path: Path) -> Any:
+    with open(path, 'rb') as f:
+        # Wait for slot become open
+        while f.read(1) is None:
+            time.sleep(0)  # Thread.yield
+        f.seek(0)
+        return cloudpickle.load(f)
+
+
+def main():
+    argv = sys.argv[1:]
+    servant: ServantClient = BashServantClient()
+
+    print("Loading function")
+    container: FuncContainer = cloudpickle.loads(base64.b64decode(argv[0].encode('ascii')))
+    print("Function loaded: " + container.func.__name__)
+
+    params_names = list(inspect.signature(container.func).parameters)
+    args = tuple(
+        lazy_proxy(
+            lambda i=i: load_arg(Path(os.path.join(os.path.sep, servant.mount(), container.func.__name__, params_names[i]))),
+            container.input_types[i],
+            {})
+        for i in range(len(params_names))
+    )
+    print(f"Loaded {len(args)} args")
+
+    print(f'Running {container.func.__name__}')
+    op = LzyRemoteOp(servant, container.func, container.input_types,
+                     container.output_type, deployed=True, args=args)
+    result = op.materialize()
+    print(f'Result of execution {result}')
+
+    result_path = os.path.join(os.path.sep, servant.mount(), container.func.__name__, "return")
+    print(f'Writing result to file {result_path}')
+    with open(result_path, 'wb') as out_handle:
+        cloudpickle.dump(result, out_handle)
+        out_handle.flush()
+        os.fsync(out_handle.fileno())
+
+
+if __name__ == "__main__":
+    main()
