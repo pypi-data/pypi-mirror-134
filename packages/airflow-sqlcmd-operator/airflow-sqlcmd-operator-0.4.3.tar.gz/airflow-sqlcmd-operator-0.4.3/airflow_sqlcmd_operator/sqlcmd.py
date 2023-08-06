7390a2683f19b458fc470cfe29dfb9bb0bbe3466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:37 +0000

    Add package airflow-sqlcmd-operator/0.4.3/airflow-sqlcmd-operator-0.4.3.tar.gz

---
+++ packages/airflow-sqlcmd-operator/airflow-sqlcmd-operator-0.4.3.tar.gz/airflow-sqlcmd-operator-0.4.3/airflow_sqlcmd_operator/sqlcmd.py
@@ -0,0 +1,32 @@
+import os
+
+from airflow.hooks.base_hook import BaseHook
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.decorators import apply_defaults
+
+
+class SqlcmdOperator(BashOperator):
+
+    template_fields = ("task_id", "bash_command", "sql_command", "sql_folder", "sql_file")
+    # Currently works only with fixed sqlcmd binary
+    # Must keep a whitespace at the end of the string.
+    sql_command = "/opt/mssql-tools/bin/sqlcmd -b -C -S {{ params.host }} -U {{ params.login }} -P {{ params.password }} -i {{ params.file }} "
+
+    @apply_defaults
+    def __init__(self, *, mssql_conn_id, sql_folder, sql_file, **kwargs):
+
+        super().__init__(bash_command=self.sql_command, **kwargs)
+
+        db = BaseHook.get_connection(mssql_conn_id)
+
+        self.params = {
+            "host": db.host,
+            "login": db.login,
+            "password": db.password,
+            "file": self.sql_script_path(sql_folder, sql_file),
+        }
+
+    def sql_script_path(self, sql_folder, sql_file):
+        """Returns the corrected file path with quotation marks."""
+        path = os.path.join(sql_folder, sql_file)
+        return f"'{path}'"
