1e35d4e31888de6c25735582c62a3de78cb73cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:30 +0000

    Add package tcolor-livereboot/0.0.2/tcolor-livereboot-0.0.2.tar.gz

---
+++ packages/tcolor-livereboot/tcolor-livereboot-0.0.2.tar.gz/tcolor-livereboot-0.0.2/src/tcolor/__init__.py
@@ -0,0 +1,6 @@
+from src.tcolor.classes.Code import Code
+from src.tcolor.classes.Text import Text
+from src.tcolor.classes.Background import Background
+from src.tcolor.classes.Format import Format
+from src.tcolor.classes.Print import Print
+
