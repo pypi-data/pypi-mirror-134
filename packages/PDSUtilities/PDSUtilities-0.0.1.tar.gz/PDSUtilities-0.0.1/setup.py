687c8171fcf046ab2396802297ab61311a579453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:56 +0000

    Add package PDSUtilities/0.0.1/PDSUtilities-0.0.1.tar.gz

---
+++ packages/PDSUtilities/PDSUtilities-0.0.1.tar.gz/PDSUtilities-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name="PDSUtilities",
+      version="0.0.1",
+      description="Utilities for data science in python",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/DrJohnWagner/PDSUtilities",
+      author="Dr. John Wagner",
+      author_email="Dr.John.Wagner@gmail.com",
+      license="Apache-2.0",
+      packages=[
+          "PDSUtilities"
+      ],
+      install_requires=[
+          "numpy",
+          "xgboost",
+          "plotly",
+      ],
+      tests_require=[
+          "pytest",
+      ],
+      zip_safe=False)
+
+# package_dir={"": "src"},
+# packages=setuptools.find_packages(where="src"),
