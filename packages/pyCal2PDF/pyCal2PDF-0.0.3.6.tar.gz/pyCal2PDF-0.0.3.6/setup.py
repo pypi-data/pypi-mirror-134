6b7c154dbd0af816fa654eb3216442fe6847d7a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:50 +0000

    Add package pyCal2PDF/0.0.3.6/pyCal2PDF-0.0.3.6.tar.gz

---
+++ packages/pyCal2PDF/pyCal2PDF-0.0.3.6.tar.gz/pyCal2PDF-0.0.3.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyCal2PDF",
+    version="0.0.3.6",
+    author="Oberron",
+    author_email="one.annum@gmail.com",
+    description="PDF calendar generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/1-annum/pyCal2PDF",
+    project_urls={
+        "Bug Tracker": "https://github.com/1-annum/pyCal2PDF/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=["reportlab","lunardate","svglib","pyICSParser"]
+    )
