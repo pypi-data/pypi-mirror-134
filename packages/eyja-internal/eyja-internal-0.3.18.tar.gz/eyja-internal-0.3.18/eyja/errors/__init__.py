897880a6f694e8ecbd762f3ca29d8000fc1deaf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:27 +0000

    Add package eyja-internal/0.3.18/eyja-internal-0.3.18.tar.gz

---
+++ packages/eyja-internal/eyja-internal-0.3.18.tar.gz/eyja-internal-0.3.18/eyja/errors/__init__.py
@@ -0,0 +1,20 @@
+from .hub_errors import (
+    LoadConfigError,
+    WrongConnectionError,
+)
+from .model_errors import (
+    ParseModelNamespaceError,
+    MissedRepresentationError,
+    ObjectAlreadyExistsError,
+    RequiredFieldError,
+)
+
+
+__all__ = [
+    'LoadConfigError',
+    'ParseModelNamespaceError',
+    'WrongConnectionError',
+    'MissedRepresentationError',
+    'ObjectAlreadyExistsError',
+    'RequiredFieldError',
+]
---
+++ packages/eyja-internal/eyja-internal-0.3.18.tar.gz/eyja-internal-0.3.18/eyja/errors/model_errors.py
@@ -0,0 +1,18 @@
+from .base_error import BaseError
+
+
+class ParseModelNamespaceError(BaseError):
+    def __init__(self, message: str = None, *args: object) -> None:
+        super().__init__('PARSE_NAMESPACE', message, *args)
+
+class MissedRepresentationError(BaseError):
+    def __init__(self, message: str = None, *args: object) -> None:
+        super().__init__('MISSED_REPRESENTATION', message, *args)
+
+class ObjectAlreadyExistsError(BaseError):
+    def __init__(self, message: str = None, *args: object) -> None:
+        super().__init__('OBJECT_ALREADY_EXISTS', message, *args)
+
+class RequiredFieldError(BaseError):
+    def __init__(self, message: str = None, *args: object) -> None:
+        super().__init__('REQUIRED_FIELD', message, *args)
