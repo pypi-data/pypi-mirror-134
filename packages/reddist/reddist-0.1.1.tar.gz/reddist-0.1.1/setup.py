03027fcde8904f9926ca3b008d23a63d31e54e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:57 +0000

    Add package reddist/0.1.1/reddist-0.1.1.tar.gz

---
+++ packages/reddist/reddist-0.1.1.tar.gz/reddist-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['reddist', 'reddist.cachers', 'reddist.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiofiles==0.6.0', 'aioredis>=2.0.1,<3.0.0', 'asyncpraw>=7.5.0,<8.0.0']
+
+setup_kwargs = {
+    'name': 'reddist',
+    'version': '0.1.1',
+    'description': 'Just a simple library for caching reddit posts',
+    'long_description': None,
+    'author': 'CaffieneDuck',
+    'author_email': 'samrid.pandit@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<3.11',
+}
+
+
+setup(**setup_kwargs)
