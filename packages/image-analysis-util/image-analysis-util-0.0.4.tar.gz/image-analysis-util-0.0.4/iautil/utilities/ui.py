eaae04867c71e64b093990e6d375c8da6b9f4e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:24 +0000

    Add package image-analysis-util/0.0.4/image-analysis-util-0.0.4.tar.gz

---
+++ packages/image-analysis-util/image-analysis-util-0.0.4.tar.gz/image-analysis-util-0.0.4/iautil/utilities/ui.py
@@ -0,0 +1,49 @@
+"""
+UI widget classes.
+"""
+
+# ----------------------------------------------------------------------------------
+
+import pyqtgraph as pg
+from pyqtgraph import QtGui, QtCore
+import xarray as xr
+
+# ----------------------------------------------------------------------------------
+
+__all__ = (
+    "DataArrayImageView",
+    "DataArrayPlot"
+)
+
+# ----------------------------------------------------------------------------------
+
+class DataArrayImageView(pg.ImageView):
+    """
+    A custom PyQtGraph ImageView.
+    """
+    
+    def __init__(self, parent=None, view=pg.PlotWidget()) -> None:
+        super(DataArrayImageView, self).__init__(parent, view)
+
+    # ------------------------------------------------------------------------------
+
+    def setImage(self, data_array_slice: xr.DataArray):
+        """
+        
+        """
+
+        image = data_array_slice.values
+        self.setImage(image)
+
+# ----------------------------------------------------------------------------------
+
+class DataArrayPlot(pg.PlotWidget):
+    """
+    A custom PyQtGraph PlotWidget.
+    """
+    
+    def __init__(self, parent=None, plotItem=None) -> None:
+        super(DataArrayPlot, self).__init__(parent, plotItem)
+
+# ----------------------------------------------------------------------------------
+
