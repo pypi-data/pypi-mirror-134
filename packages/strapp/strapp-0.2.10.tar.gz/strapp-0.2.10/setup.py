235fe4ccdf11d84ff44410d6fe84a5b6ae2b3781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:04 +0000

    Add package strapp/0.2.10/strapp-0.2.10.tar.gz

---
+++ packages/strapp/strapp-0.2.10.tar.gz/strapp-0.2.10/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['strapp', 'strapp.click', 'strapp.flask', 'strapp.http', 'strapp.sqlalchemy']
+
+package_data = \
+{'': ['*']}
+
+extras_require = \
+{'click': ['click'],
+ 'click:python_version >= "3.6" and python_version < "3.7"': ['dataclasses'],
+ 'flask': ['flask', 'flask_reverse_proxy'],
+ 'flask:python_version >= "3.6" and python_version < "3.7"': ['dataclasses'],
+ 'http': ['setuplog>=0.2.2,<0.3.0', 'backoff>=1.11.1,<2.0.0'],
+ 'sentry': ['sentry-sdk'],
+ 'sqlalchemy': ['sqlalchemy']}
+
+setup_kwargs = {
+    'name': 'strapp',
+    'version': '0.2.10',
+    'description': '',
+    'long_description': None,
+    'author': None,
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'extras_require': extras_require,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
