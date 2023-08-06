5d9a291904dd4606cbeede3e4d6d8fcd6a7966f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:56 +0000

    Add package sfe/0.1/sfe-0.1.tar.gz

---
+++ packages/sfe/sfe-0.1.tar.gz/sfe-0.1/sfe/sfe/__init__.py
@@ -0,0 +1,10 @@
+
+# The author: haoyan zhang
+#E-mail: haoy.zhang@foxmail.com
+
+from .Node import *
+from .Element import *
+from .Element_Type import *
+from .Cal_Comb import *
+from .solve import *
+from .figure import *
