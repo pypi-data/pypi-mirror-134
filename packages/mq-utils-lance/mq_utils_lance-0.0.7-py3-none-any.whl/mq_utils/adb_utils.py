08d66354a4579f560bc206517180e3e080b0c98b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:10 +0000

    Add package mq-utils-lance/0.0.7/mq_utils_lance-0.0.7-py3-none-any.whl

---
+++ packages/mq-utils-lance/mq_utils_lance-0.0.7-py3-none-any.whl/mq_utils/adb_utils.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/1/6 10:02 下午
+# @Author  : lance.txl
+# @Site    : 
+# @File    : adb_utils.py
+# @Software: PyCharm
+
+import os
+
+def get_serial_list():
+    cmd = "adb devices"
+    lines = os.popen(cmd).readlines()
+    devices = [x.split("\t")[0].strip() for x in lines[1:] if re.search("device",x)]
+    return devices
+
+def adb_head(serial):
+    if serial:
+        return "adb -s {}".format(serial)
+    else:
+        return "adb"
+
+def get_new_current_activity(topseq=0,serial=None):
+    cmd = "{} shell dumpsys activity | grep -i 'Run #.: ActivityRecord'".format(adb_head(serial))
+    lines = [x.strip() for x in os.popen(cmd).readlines() if x.strip()]
+    if lines:
+        parts = [x.strip() for x in lines[topseq].split(" ") if x.strip()]
+        return parts[4] if len(parts) > 5 else ""
+    return ""
