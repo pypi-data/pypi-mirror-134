0da470013c9afee5a7b0f996b0176b45e044ac72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:16 +0000

    Add package mwxlib/0.51.0/mwxlib-0.51.0-py3-none-any.whl

---
+++ packages/mwxlib/mwxlib-0.51.0-py3-none-any.whl/mwx/__init__.py
@@ -0,0 +1,37 @@
+#! python3
+# -*- coding: utf-8 -*-
+"""mwxlib (based on matplotlib/wx)
+"""
+from __future__ import division, print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+## from . import framework
+from .framework import __version__, __author__
+from .framework import apropos, typename
+from .framework import SSM, FSM
+from .framework import pack, hotkey
+from .framework import Menu, MenuBar, StatusBar
+from .framework import Frame, MiniFrame, ShellFrame, Editor, Nautilus
+from .framework import funcall # curry spices
+from .framework import deb
+
+## widgets
+## from .controls import Param, LParam, Knob, ControlPanel
+## from .controls import Icon, Button, ToggleButton, TextCtrl, Choice
+
+## matplot
+from .matplot2 import MatplotPanel
+from .matplot2g import GraphPlot
+from .matplot2lg import LinePlot
+from .matplot2lg import Histogram
+from .matplot2lg import LineProfile
+
+## from . import mgplt3 as mgplt
+from .mgplt3 import Gplot
+from .mgplt3 import GplotFrame
+
+try:
+    from importlib import reload
+except ImportError:
+    reload = reload
---
+++ packages/mwxlib/mwxlib-0.51.0-py3-none-any.whl/mwx/wxmon.py
@@ -0,0 +1,291 @@
+#! python3
+# -*- coding: utf-8 -*-
+import inspect
+import wx
+from wx import aui
+from wx import stc
+import wx.lib.eventwatcher as ew
+
+if wx.VERSION < (4,1,0):
+    from wx.lib.mixins.listctrl import CheckListCtrlMixin
+    
+    class CheckList(wx.ListCtrl, CheckListCtrlMixin):
+        def __init__(self, *args, **kwargs):
+            wx.ListCtrl.__init__(self, *args, **kwargs)
+            CheckListCtrlMixin.__init__(self)
+            
+            self.IsItemChecked = self.IsChecked # for wx 4.1 compatibility
+else:
+    class CheckList(wx.ListCtrl):
+        def __init__(self, *args, **kwargs):
+            wx.ListCtrl.__init__(self, *args, **kwargs)
+            self.EnableCheckBoxes()
+
+
+def where(obj):
+    try:
+        filename = inspect.getsourcefile(obj)
+        src, lineno = inspect.getsourcelines(obj)
+        return "{!s}:{}:{!s}".format(filename, lineno, src[0].rstrip())
+    except Exception:
+        return repr(obj)
+
+
+class EventMonitor(CheckList):
+    """Event monitor with check-list
+*** Inspired by wx.lib.eventwatcher ***
+
+Args:
+    parent : inspector of the shell
+    """
+    data = property(lambda self: self.__items)
+    parent = property(lambda self: self.__inspector)
+    target = property(lambda self: self.__watchedWidget)
+    
+    def __init__(self, parent, **kwargs):
+        CheckList.__init__(self, parent,
+                           style=wx.LC_REPORT|wx.LC_HRULES, **kwargs)
+        
+        self.__inspector = parent
+        self.__watchedWidget = None
+        
+        self.__dir = True # sort direction
+        self.__items = []
+        
+        self.Font = wx.Font(9, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
+        
+        self.alist = ( # assoc list of column names
+            ("typeId",    62),
+            ("typeName", 200),
+            ("source",   200),
+        )
+        for k, (header, w) in enumerate(self.alist):
+            self.InsertColumn(k, header, width=w)
+        
+        ## self.Bind(wx.EVT_MOTION, self.OnMotion)
+        self.Bind(wx.EVT_LIST_COL_CLICK, self.OnSortItems)
+        ## self.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.OnItemActivated) # left-dclick, space
+        self.Bind(wx.EVT_LEFT_DCLICK, self.OnItemDClick) # left-dclick
+        self.Bind(wx.EVT_WINDOW_DESTROY, self.OnDestroy)
+        
+        self.ew = ew
+        self.add_module = ew.addModuleEvents
+    
+    def OnDestroy(self, evt):
+        self.unwatch()
+        evt.Skip()
+    
+    ## --------------------------------
+    ## EventWatcher wrapper interfaces 
+    ## --------------------------------
+    
+    ew.buildWxEventMap() # build ew._eventBinders and ew._eventIdMap
+    ew.addModuleEvents(aui) # + some additives
+    ew.addModuleEvents(stc)
+    
+    ## Events that should not be watched by default
+    ew._noWatchList = [
+        wx.EVT_PAINT,
+        wx.EVT_NC_PAINT,
+        wx.EVT_ERASE_BACKGROUND,
+        wx.EVT_IDLE,
+        wx.EVT_UPDATE_UI,
+        wx.EVT_UPDATE_UI_RANGE,
+        wx.EVT_TOOL,
+        wx.EVT_TOOL_RANGE, # menu items (typeId=10018)
+        wx.EVT_MENU,
+    ]
+    
+    @staticmethod
+    def get_name(event):
+        return ew._eventIdMap.get(event, 'Unknown')
+    
+    @staticmethod
+    def get_binder(event):
+        return next(x for x in ew._eventBinders if x.typeId == event)
+    
+    @staticmethod
+    def get_watchlist():
+        """All watched event binders except noWatchList"""
+        return filter(lambda v: v not in ew._noWatchList,
+                      ew._eventBinders)
+    
+    def get_actions(self, event, widget=None):
+        """Wx.PyEventBinder and the handlers"""
+        widget = widget or self.target
+        if widget and hasattr(widget, '__event_handler__'):
+            try:
+                handlers = widget.__event_handler__[event]
+                ## Exclude ew:onWatchedEvent by comparing names instead of objects
+                ## return [a for a in handlers if a != self.onWatchedEvent]
+                return [a for a in handlers if a.__name__ != 'onWatchedEvent']
+            except KeyError:
+                print("- No such event: {}".format(event))
+    
+    def watch(self, widget):
+        """Begin watching"""
+        if not isinstance(widget, wx.Object):
+            return
+        self.unwatch()
+        self.clear()
+        self.__watchedWidget = widget
+        ssmap = self.dump(widget, verbose=1)
+        for binder in self.get_watchlist():
+            widget.Bind(binder, self.onWatchedEvent)
+            if binder.typeId in ssmap:
+                self.append(binder.typeId)
+        self.__inspector.handler("add_page", self, show=1)
+        self.__inspector.handler("monitor_begin", self.target)
+    
+    def unwatch(self):
+        """End watching"""
+        if not self.target:
+            return
+        for binder in self.get_watchlist():
+            if not self.target.Unbind(binder, handler=self.onWatchedEvent):
+                print("- Failed to unbind {}:{}".format(binder.typeId, binder))
+        self.__inspector.handler("monitor_end", self.target)
+        self.__watchedWidget = None
+    
+    def onWatchedEvent(self, evt):
+        if self:
+            self.update(evt)
+        evt.Skip()
+    
+    def dump(self, widget, verbose=True):
+        """Dump all event handlers bound to the widget"""
+        exclusions = [x.typeId for x in ew._noWatchList]
+        ssmap = {}
+        if not hasattr(widget, '__event_handler__'):
+            return ssmap
+        for event in sorted(widget.__event_handler__):
+            actions = self.get_actions(event)
+            if actions and event not in exclusions:
+                ssmap[event] = actions
+                if verbose:
+                    name = self.get_name(event)
+                    values = ('\n'+' '*41).join(where(a) for a in actions)
+                    print("{:8d}:{:32s}{!s}".format(event, name, values))
+        return ssmap
+    
+    ## --------------------------------
+    ## Actions for event-logger items
+    ## --------------------------------
+    
+    def update(self, evt):
+        event = evt.EventType
+        obj = evt.EventObject
+        name = self.get_name(event)
+        source = ew._makeSourceString(obj)
+        attribs = ew._makeAttribString(evt)
+        
+        if wx.VERSION < (4,1,0): # ignore self insert
+            if event == wx.EVT_LIST_INSERT_ITEM.typeId\
+              and obj is self:
+                return
+        
+        for i, item in enumerate(self.__items):
+            if item[0] == event:
+                item[1:] = [name, source, attribs]
+                break
+        else:
+            i = len(self.__items)
+            item = [event, name, source, attribs]
+            self.__items.append(item)
+            self.InsertItem(i, event)
+        
+        for j, v in enumerate(item[:-1]):
+            self.SetItem(i, j, str(v))
+        
+        if i == self.FocusedItem:
+            self.__inspector.handler("put_scratch", attribs)
+        
+        if self.IsItemChecked(i):
+            actions = self.get_actions(evt.EventType)
+            if actions:
+                self.CheckItem(i, False)
+                for f in actions:
+                    self.__inspector.debugger.trace(f, evt)
+        
+        if self.GetItemBackgroundColour(i) != wx.Colour('yellow'):
+            ## Don't run out of all timers and get warnings
+            self.SetItemBackgroundColour(i, "yellow")
+            def reset_color():
+                if self:
+                    self.SetItemBackgroundColour(i, 'white')
+            wx.CallLater(1000, reset_color)
+    
+    def clear(self):
+        self.DeleteAllItems()
+        self.__items = []
+    
+    def append(self, event, bold=True):
+        if event in (item[0] for item in self.__items):
+            return
+        
+        i = len(self.__items)
+        name = self.get_name(event)
+        item = [event, name, '-', 'no data']
+        self.__items.append(item)
+        self.InsertItem(i, event)
+        for j, v in enumerate(item[:-1]):
+            self.SetItem(i, j, str(v))
+        if bold:
+            self.SetItemFont(i, self.Font.Bold())
+    
+    def OnSortItems(self, evt): #<wx._controls.ListEvent>
+        n = self.ItemCount
+        lc = [self.__items[j] for j in range(n) if self.IsItemChecked(j)]
+        ls = [self.__items[j] for j in range(n) if self.IsSelected(j)]
+        f = self.__items[self.FocusedItem]
+        
+        col = evt.GetColumn()
+        self.__dir = not self.__dir
+        self.__items.sort(key=lambda v: v[col], reverse=self.__dir) # sort data
+        
+        for i, item in enumerate(self.__items):
+            for j, v in enumerate(item[:-1]):
+                self.SetItem(i, j, str(v))
+            self.CheckItem(i, item in lc)  # check
+            self.Select(i, item in ls)     # seleciton
+            if self.get_actions(item[0]):
+                self.SetItemFont(i, self.Font.Bold())
+            else:
+                self.SetItemFont(i, self.Font)
+        self.Focus(self.__items.index(f))  # focus (one)
+    
+    ## def OnItemActivated(self, evt): #<wx._controls.ListEvent>
+    ##     i = evt.Index
+    ##     item = self.__items[i]
+    ##     attribs = item[-1]
+    ##     wx.CallAfter(wx.TipWindow, self, attribs, 512)
+    ##     self.__inspector.handler("put_scratch", attribs)
+    
+    def OnItemDClick(self, evt): #<wx._core.MouseEvent>
+        i, flag = self.HitTest(evt.Position)
+        if i >= 0:
+            item = self.__items[i]
+            attribs = item[-1]
+            wx.CallAfter(wx.TipWindow, self, attribs, 512)
+            self.__inspector.handler("put_scratch", attribs)
+        evt.Skip()
+    
+    ## def OnMotion(self, evt): #<wx._core.MouseEvent>
+    ##     i, flag = self.HitTest(evt.Position)
+    ##     if i >= 0:
+    ##         item = self.__items[i]
+    ##     evt.Skip()
+
+
+if __name__ == "__main__":
+    import mwx
+    app = wx.App()
+    frm = mwx.Frame(None)
+    if 1:
+        self = frm.inspector
+        frm.mon = EventMonitor(self)
+        frm.mon.Show(0)
+        self.rootshell.write("self.mon.watch(self.mon)")
+        self.Show()
+    frm.Show()
+    app.MainLoop()
