fec17e9ba587ee57c41abd7603f088578e5daa02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:10 +0000

    Add package spindex/0.0.4/spindex-0.0.4-py3-none-any.whl

---
+++ packages/spindex/spindex-0.0.4-py3-none-any.whl/dex/__init__.py
@@ -0,0 +1,13 @@
+r"""
+:mod:`dex` is an index under construction"""
+
+from .library import load_library
+
+__all__ = ["library"]
+
+__author__ = "Louis Maddox"
+__license__ = "MIT"
+__description__ = "Indexer under construction."
+__url__ = "https://github.com/lmmx/dex"
+__uri__ = __url__
+__email__ = "louismmx@gmail.com"
