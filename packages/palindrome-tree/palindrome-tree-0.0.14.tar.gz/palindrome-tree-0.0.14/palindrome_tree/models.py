cda08f7702e2cd63575c090959450a73329c5248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:07 +0000

    Add package palindrome-tree/0.0.14/palindrome-tree-0.0.14.tar.gz

---
+++ packages/palindrome-tree/palindrome-tree-0.0.14.tar.gz/palindrome-tree-0.0.14/palindrome_tree/models.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, asdict
+from typing import Dict
+
+@dataclass
+class PalindromesApiResponse:
+    """
+    Palindrome api response used for serializing 
+    into dataframe format
+    """
+    original_index: int
+    after: str
+    before: str
+    mismatches: int
+    opposite: str
+    position: int
+    sequence: str
+    signature: str
+    spacer: str
+    sequence: int
+    stability_NNModel: Dict[str, int]
---
+++ packages/palindrome-tree/palindrome-tree-0.0.14.tar.gz/palindrome-tree-0.0.14/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['palindrome_tree']
+
+package_data = \
+{'': ['*'], 'palindrome_tree': ['model/*']}
+
+install_requires = \
+['pandas>=1.3.5,<2.0.0',
+ 'requests>=2.26.0,<3.0.0',
+ 'scikit-learn>=1.0.2,<2.0.0',
+ 'xgboost>=1.5.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'palindrome-tree',
+    'version': '0.0.14',
+    'description': 'Gradient boosted decision tree palindrome predictor, used to locate regions for further investigation thru http://palindromes.ibp.cz/',
+    'long_description': None,
+    'author': 'patrik.kaura',
+    'author_email': '160702@vutbr.cz',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
