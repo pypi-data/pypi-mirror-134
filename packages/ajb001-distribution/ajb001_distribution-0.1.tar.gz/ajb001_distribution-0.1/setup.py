a3daa3c69f1e8200c06b4c998d25658d3fa2bdf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:52 +0000

    Add package ajb001-distribution/0.1/ajb001_distribution-0.1.tar.gz

---
+++ packages/ajb001-distribution/ajb001_distribution-0.1.tar.gz/ajb001_distribution-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ajb001_distribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ajb001_distribution'],
+      zip_safe=False)
