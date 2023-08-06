7b47732eefc51c9e1725a60f67a4415585914953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:07 +0000

    Add package maaml/0.1.0/maaml-0.1.0-py3-none-any.whl

---
+++ packages/maaml/maaml-0.1.0-py3-none-any.whl/maaml/__init__.py
@@ -0,0 +1,7 @@
+from maaml.Datasets.UAH_dataset import time_series
+from maaml import preprocessing
+from maaml import machine_learning
+from maaml.functions.Datasets import time_series
+from maaml.functions import preprocessing
+from maaml.functions import machine_learning
+from maaml.functions import deep_learning
