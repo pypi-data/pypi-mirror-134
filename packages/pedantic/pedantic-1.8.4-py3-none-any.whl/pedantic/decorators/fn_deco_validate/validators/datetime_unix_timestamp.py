95a9e08d1cf53a07bd009581a4d8f2c9e86f4572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:00 +0000

    Add package pedantic/1.8.4/pedantic-1.8.4-py3-none-any.whl

---
+++ packages/pedantic/pedantic-1.8.4-py3-none-any.whl/pedantic/decorators/fn_deco_validate/validators/datetime_unix_timestamp.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+from typing import Union
+
+from pedantic import overrides
+from pedantic.decorators.fn_deco_validate.validators import Validator
+
+
+class DateTimeUnixTimestamp(Validator):
+    @overrides(Validator)
+    def validate(self, value: Union[int, float, str]) -> datetime:
+        if not isinstance(value, (int, float, str)):
+            self.raise_exception(msg=f'Invalid seconds since 1970: {value}', value=value)
+
+        try:
+            seconds = float(value)
+        except ValueError:
+            return self.raise_exception(msg=f'Could parse {value} to float.', value=value)
+
+        return datetime(year=1970, month=1, day=1) + timedelta(seconds=seconds)
