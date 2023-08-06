e23845e7fe25bb2c88468553279207a5783a2699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:38 +0000

    Add package jax-models/0.0.2/jax_models-0.0.2.tar.gz

---
+++ packages/jax-models/jax_models-0.0.2.tar.gz/jax_models-0.0.2/jax_models/models/__init__.py
@@ -0,0 +1,7 @@
+from . import poolformer
+from . import patchconvnet
+from . import mpvit
+from . import mlp_mixer
+from . import conv_mixer
+from . import segformer
+from .model_registry import list_models, load_model
