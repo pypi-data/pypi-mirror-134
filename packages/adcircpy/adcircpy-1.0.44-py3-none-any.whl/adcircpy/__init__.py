078ab4318e60cc96664fb09ceab9e1cfb9cd9f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:44 +0000

    Add package adcircpy/1.0.44/adcircpy-1.0.44-py3-none-any.whl

---
+++ packages/adcircpy/adcircpy-1.0.44-py3-none-any.whl/adcircpy/__init__.py
@@ -0,0 +1,27 @@
+from importlib import util
+
+import matplotlib as mpl
+from pandas.plotting import register_matplotlib_converters
+
+from adcircpy.driver import AdcircRun
+from adcircpy.forcing import TidalSource, Tides, WaveForcing, WindForcing
+from adcircpy.fort15 import Fort15
+from adcircpy.mesh import AdcircMesh
+
+__all__ = [
+    'AdcircMesh',
+    'AdcircRun',
+    'Tides',
+    'TidalSource',
+    'WaveForcing',
+    'WindForcing',
+    'Fort15',
+]
+
+mpl.rcParams['agg.path.chunksize'] = 10000
+register_matplotlib_converters()
+
+if util.find_spec('colored_traceback') is not None:
+    import colored_traceback
+
+    colored_traceback.add_hook(always=True)
