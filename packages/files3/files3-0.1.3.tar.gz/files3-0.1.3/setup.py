b8377cd868f64559b059fabc818032a7ed9cee95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:57 +0000

    Add package files3/0.1.3/files3-0.1.3.tar.gz

---
+++ packages/files3/files3-0.1.3.tar.gz/files3-0.1.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+from setuptools import find_packages, setup
+
+setup(
+name='files3',
+version='0.1.3',
+description='(pickle based) save Python objects in binary to the file system and manage them (more convenient?)',
+author_email='2229066748@qq.com',
+maintainer="Eagle'sBaby",
+maintainer_email='2229066748@qq.com',
+packages=find_packages(),
+platforms=["all"],
+license='Apache Licence 2.0',
+classifiers=[
+'Programming Language :: Python',
+'Programming Language :: Python :: 3',
+],
+keywords = ['file', 'pickle', 'bin'],
+python_requires='>=3',
+install_requires=[
+    'pycparser',
+    'pycryptodome',
+],
+)
