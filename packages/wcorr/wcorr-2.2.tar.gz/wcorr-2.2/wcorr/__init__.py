aac9f7d679456db74d8f20ce3d06553bc4d84233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:40 +0000

    Add package wcorr/2.2/wcorr-2.2.tar.gz

---
+++ packages/wcorr/wcorr-2.2.tar.gz/wcorr-2.2/wcorr/__init__.py
@@ -0,0 +1,7 @@
+"""
+Weighted correlation in Python. Pandas based implementation of weighted Pearson and Spearman correlations.
+"""
+from .wcorr import wpearson, wspearman, WeightedCorr
+
+__version__ = "2.2"
+
