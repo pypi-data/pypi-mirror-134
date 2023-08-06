6b7252d87e87e1960e4e37f5827d386d22fed99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:33 +0000

    Add package state-machine-py/18.0.24/state_machine_py-18.0.24.tar.gz

---
+++ packages/state-machine-py/state_machine_py-18.0.24.tar.gz/state_machine_py-18.0.24/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="state_machine_py",
+    version="18.0.24",
+    author="muzudho",
+    author_email="muzudho1@gmail.com",
+    description="A state diagram machine package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/muzudho/state-machine-py",
+    project_urls={
+        "Bug Tracker": "https://github.com/muzudho/state-machine-py/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
