e503d4c37d68fffcf80822301dd7734012c76d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:44 +0000

    Add package pyreference/0.6.2/pyreference-0.6.2-py3-none-any.whl

---
+++ packages/pyreference/pyreference-0.6.2-py3-none-any.whl/pyreference/settings.py
@@ -0,0 +1,23 @@
+"""
+Created on 23Jan.,2018
+
+@author: dlawrence
+"""
+
+
+# Change this when you introduce breaking changes
+PYREFERENCE_JSON_VERSION = 5
+PYREFERENCE_JSON_VERSION_KEY = "pyreference_json_version"
+
+# Keys used in dictionary (serialized to JSON)
+CHROM = "chr"
+START = "start"
+END = "stop"
+STRAND = "strand"
+
+# Other 
+IS_CODING = "is_coding"
+
+
+BEST_REGION_TYPE_ORDER = ["coding", "5PUTR", "3PUTR", "non coding", "intron"]
+
