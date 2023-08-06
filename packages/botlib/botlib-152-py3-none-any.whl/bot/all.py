aa9e9ab479b32bdead7ded504b9f33a3e5cd93fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:35 +0000

    Add package botlib/152/botlib-152-py3-none-any.whl

---
+++ packages/botlib/botlib-152-py3-none-any.whl/bot/all.py
@@ -0,0 +1,56 @@
+# This file is placed in the Public Domain.
+
+
+"bot package modules"
+
+from bot.tbl import Tbl
+
+
+from bot import bus
+from bot import cfg
+from bot import clt
+from bot import dbg
+from bot import dbs
+from bot import evt
+from bot import fnc
+from bot import hdl
+from bot import jsn
+from bot import krn
+from bot import opt
+from bot import prs
+from bot import tbl
+from bot import thr
+from bot import tms
+
+
+Tbl.add(bus)
+Tbl.add(cfg)
+Tbl.add(clt)
+Tbl.add(dbs)
+Tbl.add(evt)
+Tbl.add(fnc)
+Tbl.add(hdl)
+Tbl.add(jsn)
+Tbl.add(krn)
+Tbl.add(opt)
+Tbl.add(prs)
+Tbl.add(tbl)
+Tbl.add(thr)
+Tbl.add(tms)
+
+
+from bot import bsc
+from bot import irc
+from bot import opt
+from bot import rss
+from bot import udp
+from bot import usr
+
+
+Tbl.add(bsc)
+Tbl.add(dbg)
+Tbl.add(irc)
+Tbl.add(opt)
+Tbl.add(rss)
+Tbl.add(udp)
+Tbl.add(usr)
---
+++ packages/botlib/botlib-152-py3-none-any.whl/bot/clt.py
@@ -0,0 +1,30 @@
+# This file is placed in the Public Domain.
+
+
+"client"
+
+
+from .bus import Bus
+from .obj import Object
+
+
+def __dir__():
+    return (
+        "Client"
+    )
+
+
+class Client(Object):
+
+    def __init__(self):
+        Object.__init__(self)
+        Bus.add(self)
+
+    def announce(self, txt):
+        self.raw(txt)
+
+    def raw(self, txt):
+        pass
+
+    def say(self, channel, txt):
+        self.raw(txt)
