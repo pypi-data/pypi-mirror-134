832bb0765ef0dc07ad46f7dc804903137600f68c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:45 +0000

    Add package tsod/0.1.4/tsod-0.1.4.tar.gz

---
+++ packages/tsod/tsod-0.1.4.tar.gz/tsod-0.1.4/tsod/__init__.py
@@ -0,0 +1,12 @@
+from .detectors import (
+    RangeDetector,
+    DiffDetector,
+    ConstantGradientDetector,
+    GradientDetector,
+    ConstantValueDetector,
+    CombinedDetector,
+    RollingStandardDeviationDetector,
+)
+from .base import load
+
+__version__ = "0.1.4"
