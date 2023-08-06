36cc12575c13405deacf1291a67f7851a94938cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:30 +0000

    Add package scipp/0.11.1/scipp-0.11.1-cp39-cp39-win_amd64.whl

---
+++ packages/scipp/scipp-0.11.1-cp39-cp39-win_amd64.whl/scipp/include/scipp/core/element/logical.h
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+/// @file
+/// @author Simon Heybrock
+#pragma once
+
+#include "scipp/common/overloaded.h"
+#include "scipp/core/element/arg_list.h"
+#include "scipp/core/transform_common.h"
+
+namespace scipp::core::element {
+
+constexpr auto logical =
+    overloaded{arg_list<bool>, dimensionless_unit_check_return};
+
+constexpr auto logical_and =
+    overloaded{logical, [](const auto &a, const auto &b) { return a && b; }};
+constexpr auto logical_or =
+    overloaded{logical, [](const auto &a, const auto &b) { return a || b; }};
+constexpr auto logical_xor =
+    overloaded{logical, [](const auto &a, const auto &b) { return a != b; }};
+constexpr auto logical_not =
+    overloaded{logical, [](const auto &x) { return !x; }};
+
+constexpr auto logical_inplace =
+    overloaded{arg_list<bool>, dimensionless_unit_check};
+
+constexpr auto logical_and_equals =
+    overloaded{logical_inplace, [](auto &&a, const auto &b) { a = a && b; }};
+constexpr auto logical_or_equals =
+    overloaded{logical_inplace, [](auto &&a, const auto &b) { a = a || b; }};
+constexpr auto logical_xor_equals =
+    overloaded{logical_inplace, [](auto &&a, const auto &b) { a = a != b; }};
+
+} // namespace scipp::core::element
