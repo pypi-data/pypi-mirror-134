9d35a9f712610cec8de035f32c6806323aa9c738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:35 +0000

    Add package jc/1.17.7/jc-1.17.7.tar.gz

---
+++ packages/jc/jc-1.17.7.tar.gz/jc-1.17.7/jc/parsers/du.py
@@ -0,0 +1,156 @@
+"""jc - JSON CLI output utility `du` command output parser
+
+Usage (cli):
+
+    $ du | jc --du
+
+    or
+
+    $ jc du
+
+Usage (module):
+
+    import jc.parsers.du
+    result = jc.parsers.du.parse(du_command_output)
+
+Schema:
+
+    [
+      {
+        "size":     integer,
+        "name":     string
+      }
+    ]
+
+Examples:
+
+    $ du /usr | jc --du -p
+    [
+      {
+        "size": 104608,
+        "name": "/usr/bin"
+      },
+      {
+        "size": 56,
+        "name": "/usr/standalone/firmware/iBridge1_1Customer.bundle/Contents/_CodeSignature"
+      },
+      {
+        "size": 0,
+        "name": "/usr/standalone/firmware/iBridge1_1Customer.bundle/Contents/Resources/Firmware/usr/local/standalone"
+      },
+      {
+        "size": 0,
+        "name": "/usr/standalone/firmware/iBridge1_1Customer.bundle/Contents/Resources/Firmware/usr/local"
+      },
+      {
+        "size": 0,
+        "name": "/usr/standalone/firmware/iBridge1_1Customer.bundle/Contents/Resources/Firmware/usr"
+      },
+      {
+        "size": 1008,
+        "name": "/usr/standalone/firmware/iBridge1_1Customer.bundle/Contents/Resources/Firmware/dfu"
+      },
+      ...
+    ]
+
+    $ du /usr | jc --du -p -r
+    [
+      {
+        "size": "104608",
+        "name": "/usr/bin"
+      },
+      {
+        "size": "56",
+        "name": "/usr/standalone/firmware/iBridge1_1Customer.bundle/Contents/_CodeSignature"
+      },
+      {
+        "size": "0",
+        "name": "/usr/standalone/firmware/iBridge1_1Customer.bundle/Contents/Resources/Firmware/usr/local/standalone"
+      },
+      {
+        "size": "0",
+        "name": "/usr/standalone/firmware/iBridge1_1Customer.bundle/Contents/Resources/Firmware/usr/local"
+      },
+      {
+        "size": "0",
+        "name": "/usr/standalone/firmware/iBridge1_1Customer.bundle/Contents/Resources/Firmware/usr"
+      },
+      {
+        "size": "1008",
+        "name": "/usr/standalone/firmware/iBridge1_1Customer.bundle/Contents/Resources/Firmware/dfu"
+      },
+      ...
+    ]
+"""
+import jc.utils
+import jc.parsers.universal
+
+
+class info():
+    """Provides parser metadata (version, author, etc.)"""
+    version = '1.5'
+    description = '`du` command parser'
+    author = 'Kelly Brazil'
+    author_email = 'kellyjonbrazil@gmail.com'
+    # details = 'enter any other details here'
+
+    # compatible options: linux, darwin, cygwin, win32, aix, freebsd
+    compatible = ['linux', 'darwin', 'aix', 'freebsd']
+    magic_commands = ['du']
+
+
+__version__ = info.version
+
+
+def _process(proc_data):
+    """
+    Final processing to conform to the schema.
+
+    Parameters:
+
+        proc_data:   (List of Dictionaries) raw structured data to process
+
+    Returns:
+
+        List of Dictionaries. Structured data to conform to the schema.
+    """
+    int_list = ['size']
+    for entry in proc_data:
+        for key in entry:
+            if key in int_list:
+                entry[key] = jc.utils.convert_to_int(entry[key])
+
+    return proc_data
+
+
+def parse(data, raw=False, quiet=False):
+    """
+    Main text parsing function
+
+    Parameters:
+
+        data:        (string)  text data to parse
+        raw:         (boolean) output preprocessed JSON if True
+        quiet:       (boolean) suppress warning messages if True
+
+    Returns:
+
+        List of Dictionaries. Raw or processed structured data.
+    """
+    jc.utils.compatibility(__name__, info.compatible, quiet)
+    jc.utils.input_type_check(data)
+
+    raw_output = []
+
+    # Clear any blank lines
+    cleandata = list(filter(None, data.splitlines()))
+
+    if jc.utils.has_data(data):
+
+        cleandata.insert(0, 'size name')
+        raw_output = jc.parsers.universal.simple_table_parse(cleandata)
+
+    if raw:
+        return raw_output
+    else:
+        return _process(raw_output)
