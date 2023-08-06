2f1fe0ae26b8b861e0b0885f28f4168f6482c39c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:32 +0000

    Add package airflow-sqlcmd-operator/0.4.4/airflow-sqlcmd-operator-0.4.4.tar.gz

---
+++ packages/airflow-sqlcmd-operator/airflow-sqlcmd-operator-0.4.4.tar.gz/airflow-sqlcmd-operator-0.4.4/airflow_sqlcmd_operator/sqlcmd.py
@@ -0,0 +1,32 @@
+import os
+
+from airflow.hooks.base_hook import BaseHook
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.decorators import apply_defaults
+
+
+class SqlcmdOperator(BashOperator):
+
+    template_fields = ("task_id", "bash_command", "sql_command", "sql_folder", "sql_file")
+    # Currently works only with fixed sqlcmd binary
+    # Must keep a whitespace at the end of the string.
+    sql_command = "/opt/mssql-tools/bin/sqlcmd -b -C -S {{ params.host }} -U {{ params.login }} -P {{ params.password }} -i {{ params.file }} "
+
+    @apply_defaults
+    def __init__(self, *, mssql_conn_id, sql_folder, sql_file, **kwargs):
+
+        db = BaseHook.get_connection(mssql_conn_id)
+
+        params = {
+            "host": db.host,
+            "login": db.login,
+            "password": db.password,
+            "file": self.sql_script_path(sql_folder, sql_file),
+        }
+
+        super().__init__(bash_command=self.sql_command, params=params, **kwargs)
+
+    def sql_script_path(self, sql_folder, sql_file):
+        """Returns the corrected file path with quotation marks."""
+        path = os.path.join(sql_folder, sql_file)
+        return f"'{path}'"
---
+++ packages/airflow-sqlcmd-operator/airflow-sqlcmd-operator-0.4.4.tar.gz/airflow-sqlcmd-operator-0.4.4/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="airflow-sqlcmd-operator",
+    packages=["airflow_sqlcmd_operator"],
+    version="0.4.4",
+    license="MIT",
+    description="Custom Airflow BashOperator for Microsoft sqlcmd",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Rodrigo Dewes",
+    author_email="rdewes@gmail.com",
+    url="https://github.com/dewes/airflow-sqlcmd-operator",
+    download_url="https://github.com/dewes/airflow-sqlcmd-operator/archive/refs/tags/v_03.tar.gz",
+    keywords=["Airflow", "operator", "SQLServer", "sqlcmd"],
+    install_requires=[
+        "pathlib",
+        "apache-airflow>=2.1",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
