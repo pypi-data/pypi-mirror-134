6a29289ecc3c4abef06303204e83bf42278b9642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:22 +0000

    Add package reciprocalspaceship/0.9.18/reciprocalspaceship-0.9.18.tar.gz

---
+++ packages/reciprocalspaceship/reciprocalspaceship-0.9.18.tar.gz/reciprocalspaceship-0.9.18/reciprocalspaceship/dtypes/batch.py
@@ -0,0 +1,22 @@
+from pandas.core.dtypes.dtypes import register_extension_dtype
+
+from reciprocalspaceship.dtypes.base import MTZInt32Dtype, MTZIntegerArray
+
+
+@register_extension_dtype
+class BatchDtype(MTZInt32Dtype):
+    """Dtype for representing batch numbers"""
+
+    name = "Batch"
+    mtztype = "B"
+
+    @classmethod
+    def construct_array_type(cls):
+        return BatchArray
+
+
+class BatchArray(MTZIntegerArray):
+    """ExtensionArray for supporting BatchDtype"""
+
+    _dtype = BatchDtype()
+    pass
