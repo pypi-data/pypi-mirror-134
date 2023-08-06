1610b3049b49ca4db0c728aab15b350d335cd1f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:55 +0000

    Add package flow360client/21.4.1.0/flow360client-21.4.1.0.tar.gz

---
+++ packages/flow360client/flow360client-21.4.1.0.tar.gz/flow360client-21.4.1.0/flow360client/config.py
@@ -0,0 +1,13 @@
+class Config:
+    S3_REGION="us-gov-west-1"
+    CASE_BUCKET="flow360cases"
+    MESH_BUCKET="flow360meshes"
+    STUDIO_BUCKET="flow360studio"
+    WEB_API_ENDPONT="https://webapi.flexcompute.com"
+
+    # auth info
+    auth = None
+    user = None
+
+    #other
+    auth_retry = 0
