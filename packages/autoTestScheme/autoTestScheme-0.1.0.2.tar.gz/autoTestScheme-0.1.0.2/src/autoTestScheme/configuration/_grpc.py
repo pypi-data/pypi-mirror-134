05ed7f427d09cdffeaf1fadff5501930e589fe76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:41 +0000

    Add package autoTestScheme/0.1.0.2/autoTestScheme-0.1.0.2.tar.gz

---
+++ packages/autoTestScheme/autoTestScheme-0.1.0.2.tar.gz/autoTestScheme-0.1.0.2/src/autoTestScheme/configuration/_grpc.py
@@ -0,0 +1,13 @@
+import grpc
+from .api import Api
+
+
+class Grpc(Api):
+
+    def __init__(self, host):
+        self.channel = grpc.insecure_channel(host)
+
+    def get_grpc(self, api_id):
+        pb2, pb2_grpc = grpc.protos_and_services(self.get_api(api_id))
+        
+
