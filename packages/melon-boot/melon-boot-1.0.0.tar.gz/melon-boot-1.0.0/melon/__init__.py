cf534bf8a876e115c3a3145833379b16dcbe9c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:29 +0000

    Add package melon-boot/1.0.0/melon-boot-1.0.0.tar.gz

---
+++ packages/melon-boot/melon-boot-1.0.0.tar.gz/melon-boot-1.0.0/melon/__init__.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+"""
+@Author  : zh_o
+"""
+import os
+
+
+ROOT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+CONFIG_PATH = os.path.join(ROOT_DIR, 'melon.toml')
