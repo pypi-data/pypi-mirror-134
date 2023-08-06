e9942ffd3f5bca676c73c5e8865848ade8a9aaee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:13 +0000

    Add package compysitions/0.0.2/compysitions-0.0.2.tar.gz

---
+++ packages/compysitions/compysitions-0.0.2.tar.gz/compysitions-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='compysitions',
+    version='0.0.2',
+    packages=['compysitions'],
+    package_dir={'compysitions': 'src'},
+    url='https://github.com/zganger/compysitions',
+    license='',
+    author='zganger',
+    author_email='zganger@icloud.com',
+    description='Extensions for working with Python Dataclasses'
+)
