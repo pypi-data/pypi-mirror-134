a5b87ca3e90a58b6c6f30bf2421cc9964487b5d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:27 +0000

    Add package state-machine-py/18.0.19/state_machine_py-18.0.19.tar.gz

---
+++ packages/state-machine-py/state_machine_py-18.0.19.tar.gz/state_machine_py-18.0.19/src/state_machine_py/main_finally.py
@@ -0,0 +1,69 @@
+import sys
+import signal
+
+
+class MainFinally:
+    """アプリケーション終了時に、必ず終了処理を実行するための仕掛けです。
+    📖 [Python で終了時に必ず何か実行したい](https://qiita.com/qualitia_cdev/items/f536002791671c6238e3)
+
+    Examples
+    --------
+    import sys
+    from state_machine_py.main_finally import MainFinally
+
+    class Main:
+        def on_main(self):
+            # ここで通常の処理
+            return 0
+
+        def on_except(self, e):
+            # ここで例外キャッチ
+            print(e)
+
+        def on_finally(self):
+            # ここで終了処理
+            return 1
+
+
+    # このファイルを直接実行したときは、以下の関数を呼び出します
+    if __name__ == "__main__":
+        sys.exit(MainFinally.run(Main()))
+    """
+
+    @classmethod
+    def run(clazz, target):
+        """アプリケーション終了時に必ず on_finally()メソッドを呼び出します。
+        通常の処理は on_main()メソッドに書いてください
+
+        Parameters
+        ----------
+        target : class
+            on_main()メソッドと、on_finally()メソッドが定義されたクラスです
+        """
+        def sigterm_handler(_signum, _frame) -> None:
+            sys.exit(1)
+
+        # 強制終了のシグナルを受け取ったら、強制終了するようにします
+        signal.signal(signal.SIGTERM, sigterm_handler)
+
+        try:
+            # ここで何か処理
+            return_code = target.on_main()
+
+        except Exception as e:
+            # ここで例外キャッチ
+            target.on_except(e)
+
+        finally:
+            # 強制終了のシグナルを無視するようにしてから、クリーンアップ処理へ進みます
+            signal.signal(signal.SIGTERM, signal.SIG_IGN)
+            signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+            # ここで終了処理
+            return_code = target.on_finally()
+
+            # 強制終了のシグナルを有効に戻します
+            signal.signal(signal.SIGTERM, signal.SIG_DFL)
+            signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+        return return_code
