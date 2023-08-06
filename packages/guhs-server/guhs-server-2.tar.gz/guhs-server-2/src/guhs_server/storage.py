acc055bae39245981c3f7bafe14ccb95fc302e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:04 +0000

    Add package guhs-server/2/guhs-server-2.tar.gz

---
+++ packages/guhs-server/guhs-server-2.tar.gz/guhs-server-2/src/guhs_server/storage.py
@@ -0,0 +1,28 @@
+import json
+
+from guhs_server import shell
+
+STORAGE_FILE = '~/.guhs_server.json'
+
+
+def save(name, value):
+    if not shell.file_exists(STORAGE_FILE):
+        shell.write_file(STORAGE_FILE, json.dumps({name: value}))
+        return
+
+    configuration = json.loads(shell.read_file(STORAGE_FILE))
+    configuration[name] = value
+
+    shell.write_file(STORAGE_FILE, json.dumps(configuration))
+
+
+def get(name):
+    if not shell.file_exists(STORAGE_FILE):
+        return None
+
+    configuration = json.loads(shell.read_file(STORAGE_FILE))
+
+    if name not in configuration:
+        return None
+
+    return configuration[name]
