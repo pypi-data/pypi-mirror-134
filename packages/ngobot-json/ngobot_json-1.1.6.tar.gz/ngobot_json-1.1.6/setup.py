94beb8683c0f7b3a3fbde75effb4e2c72dbc609c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:25 +0000

    Add package ngobot-json/1.1.6/ngobot_json-1.1.6.tar.gz

---
+++ packages/ngobot-json/ngobot_json-1.1.6.tar.gz/ngobot_json-1.1.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ngobot_json',
+    version='1.1.6',
+    packages=['ngobot_json'],
+    url='https://github.com/ngobot/ngobot_json',
+    license='MIT License',
+    author='Nseobong Gregory Obot',
+    author_email='obot.greg@icloud.com',
+    description='HTTP Response code Python formatting.'
+
+)
