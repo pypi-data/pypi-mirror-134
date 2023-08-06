c04ac3e21540eadc11d5f4436b0b43473460706d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:15 +0000

    Add package srsparser/1.0.2/srsparser-1.0.2.tar.gz

---
+++ packages/srsparser/srsparser-1.0.2.tar.gz/srsparser-1.0.2/srsparser/sections_tree.py
@@ -0,0 +1,42 @@
+from anytree import AnyNode, PreOrderIter, LevelOrderGroupIter
+from anytree.exporter import DictExporter
+from anytree.importer import DictImporter
+
+importer = DictImporter()
+exporter = DictExporter()
+
+
+class Section(AnyNode):
+    """
+    A class representing a section of the text document.
+    """
+
+    def __init__(self, name: str, parent=None, children=None, **kwargs):
+        super().__init__(parent, children, **kwargs)
+        self.name = name
+        self.parent = parent
+        if children:
+            self.children = children
+
+
+class SectionsTree:
+    """
+    A class representing the sections tree of the text document.
+    """
+
+    def __init__(self, template: dict):
+        """
+        :param template: sections tree template.
+        """
+        self.root = importer.import_(template)
+
+    def get_leaf_sections(self) -> list:
+        return [node for node in PreOrderIter(self.root, filter_=lambda n: hasattr(n, "text"))]
+
+    def get_dict_from_root(self) -> dict:
+        # exclude sections from the result for which there were no matches
+        for children in LevelOrderGroupIter(self.root):
+            for node in children:
+                if hasattr(node, "text") and node.text == "":
+                    node.parent = None
+        return exporter.export(self.root)
