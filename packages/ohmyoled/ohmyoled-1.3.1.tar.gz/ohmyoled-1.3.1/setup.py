b98badd4a5ce27299b59aadb2d9240dac536d753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:26 +0000

    Add package ohmyoled/1.3.1/ohmyoled-1.3.1.tar.gz

---
+++ packages/ohmyoled/ohmyoled-1.3.1.tar.gz/ohmyoled-1.3.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ohmyoled",
+    version="1.3.1",
+    python_requires='>=3.8.9',
+    packages=find_packages(),
+    description="64x32 Oled Matrix Display",
+    author="thefinaljoke",
+    install_requires=[
+        "wheel",
+        "cython",
+        "numpy",
+        "noaa-sdk",
+        "env-canada==0.0.35",
+        "ephem==3.7.7.0",
+        "fastjsonschema>=2.14.4",
+        "geocoder==1.38.1",
+        "gpiozero==1.5.1",
+        "noaa-sdk>=0.1.18",
+        "printtools==1.2",
+        "PyInstaller==3.6",
+        "python-tsl2591==0.2.0",
+        "questionary>=1.5.2",
+        "regex>=2020.4.4",
+        "RPi.GPIO==0.7.0",
+        "APScheduler>=3.6.3",
+        "lastversion>=1.1.6",
+        "nameparser==1.0.6",
+        "pillow==7.1.2",
+        "dbus-next",
+        "aiohttp",
+        "iso6709",
+        "sportsipy",
+        "wget",
+        "suntime"
+        ],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest==4.4.1"],
+    test_suite="tests",
+    url="https://github.com/TheFinalJoke/ohmyoled"
+)
