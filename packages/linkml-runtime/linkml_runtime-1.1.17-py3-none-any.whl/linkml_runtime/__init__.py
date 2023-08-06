5c7b80d9911ff3d2802547265c5aac2074c11ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:14 +0000

    Add package linkml-runtime/1.1.17/linkml_runtime-1.1.17-py3-none-any.whl

---
+++ packages/linkml-runtime/linkml_runtime-1.1.17-py3-none-any.whl/linkml_runtime/__init__.py
@@ -0,0 +1,25 @@
+from linkml_runtime.utils.curienamespace import CurieNamespace
+from linkml_runtime.utils.schemaview import SchemaView
+from rdflib import RDF, RDFS, SKOS, XSD, OWL
+import rdflib_shim
+shim = rdflib_shim.RDFLIB_SHIM
+
+LINKML = CurieNamespace('linkml', 'https://w3id.org/linkml/')
+TCCM = CurieNamespace('tccm', 'https://ontologies.r.us/tccm/')
+OWL = CurieNamespace('owl', OWL)
+RDF = CurieNamespace('rdf', RDF)
+RDFS = CurieNamespace('rdfs', RDFS)
+SKOS = CurieNamespace('skos', SKOS)
+XSD = CurieNamespace('xsd', XSD)
+
+class MappingError(ValueError):
+    """
+    An error when mapping elements of a LinkML model to runtime objects
+    """
+    pass
+
+class DataNotFoundError(ValueError):
+    """
+    An error in which data cannot be found
+    """
+    pass
