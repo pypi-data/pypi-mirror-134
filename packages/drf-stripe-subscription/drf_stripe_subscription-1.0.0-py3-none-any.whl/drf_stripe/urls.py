92d1472f2c1dbdb8544b0ed22d571b341fd68c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:55 +0000

    Add package drf-stripe-subscription/1.0.0/drf_stripe_subscription-1.0.0-py3-none-any.whl

---
+++ packages/drf-stripe-subscription/drf_stripe_subscription-1.0.0-py3-none-any.whl/drf_stripe/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from drf_stripe import views
+
+urlpatterns = [
+    path('my-subscription/', views.Subscription.as_view()),
+    path('subscribable-product/', views.SubscribableProductPrice.as_view()),
+    path('checkout/', views.CreateStripeCheckoutSession.as_view()),
+    path('webhook/', views.StripeWebhook.as_view()),
+    path('customer-portal/', views.StripeCustomerPortal.as_view())
+]
