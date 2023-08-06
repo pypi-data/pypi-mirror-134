e9ec9bef5c8b43d75b4a47cf464712fffbead9b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:32 +0000

    Add package dicom2hdf/0.0.1/dicom2hdf-0.0.1.tar.gz

---
+++ packages/dicom2hdf/dicom2hdf-0.0.1.tar.gz/dicom2hdf-0.0.1/dicom2hdf/__init__.py
@@ -0,0 +1,11 @@
+from .data_generators.patient_data_generator import PatientDataGenerator
+from .datasets.hdf5_dataset import Hdf5Dataset
+
+__author__ = "Maxence Larose"
+__version__ = "0.1.1"
+__copyright__ = "Copyright 2022, Maxence Larose"
+__credits__ = ["Maxence Larose"]
+__license__ = "GPL"
+__maintainer__ = "Maxence Larose"
+__email__ = "maxencelarose@hotmail.com"
+__status__ = "Production"
