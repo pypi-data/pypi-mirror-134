77939c72b1266f59c445aa88708a765599c0a232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:31 +0000

    Add package graph-transformer-pytorch/0.0.3/graph-transformer-pytorch-0.0.3.tar.gz

---
+++ packages/graph-transformer-pytorch/graph-transformer-pytorch-0.0.3.tar.gz/graph-transformer-pytorch-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'graph-transformer-pytorch',
+  packages = find_packages(),
+  version = '0.0.3',
+  license='MIT',
+  description = 'Graph Transformer - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/graph-transformer-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'graphs'
+  ],
+  install_requires=[
+    'einops>=0.3',
+    'rotary-embedding-torch',
+    'torch>=1.6'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
