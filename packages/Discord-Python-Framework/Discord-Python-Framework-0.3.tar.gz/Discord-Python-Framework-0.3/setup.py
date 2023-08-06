82d5d79db5d06b627820629f2979343bfaa8aed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:11 +0000

    Add package Discord-Python-Framework/0.3/Discord-Python-Framework-0.3.tar.gz

---
+++ packages/Discord-Python-Framework/Discord-Python-Framework-0.3.tar.gz/Discord-Python-Framework-0.3/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+     name='Discord-Python-Framework',  
+     version='0.3',
+     author="Icy Flames",
+     author_email="icyflames@gmail.com",
+     description="Discord python tools",
+     long_description=long_description,
+     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+     ],
+)
