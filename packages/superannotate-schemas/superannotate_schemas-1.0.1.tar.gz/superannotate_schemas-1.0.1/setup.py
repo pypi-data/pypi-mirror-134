2bbf45aa149a8c0731bfa9db2ab6717a4507e4f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:38 +0000

    Add package superannotate-schemas/1.0.1/superannotate_schemas-1.0.1.tar.gz

---
+++ packages/superannotate-schemas/superannotate_schemas-1.0.1.tar.gz/superannotate_schemas-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name='superannotate_schemas',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='1.0.1',
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    description='SuperAnnotate JSON Schemas',
+    author='Vaghinak Basentsyan',
+    author_email='vaghinak@superannotate.con',
+    url='https://www.superannotate.com/',
+    license='MIT',
+    description_file="README.md",
+    entry_points={
+            'console_scripts': ['superannotate_schemas = superannotate_schemas.bin.app:main']
+        },
+      )
