0850fd4e63f78ecade50aaa7753fd2cbb16e612c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:29 +0000

    Add package hek/0.1.5/hek-0.1.5-py3-none-any.whl

---
+++ packages/hek/hek-0.1.5-py3-none-any.whl/hek/info.py
@@ -0,0 +1,30 @@
+
+class sock:
+    DEFAULT_SOCKET_TIMEOUT = 10
+    RDP_PORT = 3389
+    SSH_PORT = 22
+
+class system:
+    WIN_DEFAULT_CMD_SAFE_RETURNCODE = 0
+    LINUX_DEFAULT_CMD_SAFE_RETURNCODE = 0
+class request:
+    DEFAULT_REQUEST_TIMEOUT = 10
+
+class Tools:
+    class nmcli:
+        SSID_ONLY = "sudo nmcli dev wifi connect (ssid)"
+        SECRET_CONNECT = 'sudo nmcli dev wifi connect (ssid) password "(secret)"'
+
+    class aircrack:
+        MONITOR_START = "airmon-ng start"
+        MONITOR_STOP = "airmon-ng stop"
+
+    class ettercap:
+        ARP_START = "sudo ettercap -T -S -i (interface) -M arp:remote /(router)// /(device)//"
+
+    class wireshark:
+        MONITOR_DEVICE = 'wireshark -i (interface) -k -Y "ip.addr == (device)"'
+
+
+class API:
+    IPINFO_API = 'http://ip-api.com/json'
