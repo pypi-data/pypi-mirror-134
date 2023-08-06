c7afefa6605fa6910578f392c2b8e0612ca82114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:38 +0000

    Add package pypatternfinder/1.0.1/pypatternfinder-1.0.1.tar.gz

---
+++ packages/pypatternfinder/pypatternfinder-1.0.1.tar.gz/pypatternfinder-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pypatternfinder',
+    version='1.0.1',
+    packages=['pypatternfinder'],
+    url='https://www.github.com/FaithBeam/pypatternfinder',
+    license='MIT',
+    license_files='LICENSE',
+    author='FaithBeam',
+    author_email='pigeonhopper68@gmail.com',
+    description='Signature matcher/wildcard pattern finder.'
+)
