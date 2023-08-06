23e66129b4e10360b0f16392a79551e8cbc362eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:41 +0000

    Add package vitis-quantizer/0.1.0/vitis-quantizer-0.1.0.tar.gz

---
+++ packages/vitis-quantizer/vitis-quantizer-0.1.0.tar.gz/vitis-quantizer-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.1.0"
+DESCRIPTION = "A standalone package to do the vitis quantization step."
+
+REQUIRED_PACKAGES = [
+    "tensorflow>=2.7.0",
+    "numpy>=1.22.1",
+    "tensorflow-model-optimization>=0.7.0",
+]
+
+
+setup(
+    name="vitis-quantizer",
+    version=VERSION,
+    description=DESCRIPTION,
+    url="https://github.com/CruxML/vitis-quantizer",
+    author="CruxML",
+    author_email="stephen.tridgell@cruxml.com",
+    install_requires=REQUIRED_PACKAGES,
+    download_url="https://github.com/CruxML/vitis-quantizer/archive/refs/tags/0.1.0.tar.gz",
+    packages=find_packages(exclude=["*_test.py"]),
+    license="Apache 2.0",
+    include_package_data=True,
+    keywords="vitis quantize machine learning xilinx",
+)
