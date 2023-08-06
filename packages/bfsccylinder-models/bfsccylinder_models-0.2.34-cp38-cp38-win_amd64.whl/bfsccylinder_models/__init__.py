a57d1d1ccbaa39ce896a62144b0b3adddd1d41a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:41 +0000

    Add package bfsccylinder-models/0.2.34/bfsccylinder_models-0.2.34-cp38-cp38-win_amd64.whl

---
+++ packages/bfsccylinder-models/bfsccylinder_models-0.2.34-cp38-cp38-win_amd64.whl/bfsccylinder_models/__init__.py
@@ -0,0 +1,15 @@
+"""
+BFSCCYLINDER MODELS - Models using the BFSC cylinder finite element
+===================================================================
+
+Author: Saullo G. P. Castro
+
+.. automodule:: bfsccylinder_models.models
+    :members:
+
+.. automodule:: bfsccylinder_models.vatfunctions
+    :members:
+
+"""
+pass
+
