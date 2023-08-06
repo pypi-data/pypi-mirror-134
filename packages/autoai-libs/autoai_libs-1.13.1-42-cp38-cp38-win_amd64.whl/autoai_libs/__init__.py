04ec2edbd0f2b92cdc228b1526a3ca48fab30b6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:58 +0000

    Add package autoai-libs/1.13.1/autoai_libs-1.13.1-42-cp38-cp38-win_amd64.whl

---
+++ packages/autoai-libs/autoai_libs-1.13.1-42-cp38-cp38-win_amd64.whl/autoai_libs/__init__.py
@@ -0,0 +1,24 @@
+################################################################################
+#
+# Licensed Materials - Property of IBM
+# (C) Copyright IBM Corp. 2019, 2020, 2021
+# US Government Users Restricted Rights - Use, duplication disclosure restricted
+# by GSA ADP Schedule Contract with IBM Corp.
+#
+################################################################################
+
+### Uncomment, once we are ready to expose this functionality
+
+# pkg_name = "autoai-libs"
+#
+# try:
+#     from importlib.metadata import version
+#     __version__ = version(pkg_name)
+#
+# except (ModuleNotFoundError, AttributeError):
+#     try:
+#         from importlib_metadata import version as imp_lib_ver
+#         __version__ = imp_lib_ver(pkg_name)
+#
+#     except ModuleNotFoundError:
+#         __version__ = None
