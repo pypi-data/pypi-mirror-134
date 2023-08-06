6bb000c83b101beb291262a09ac120d40632968b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:32 +0000

    Add package PyWin10UI/0.0.9/PyWin10UI-0.0.9.tar.gz

---
+++ packages/PyWin10UI/PyWin10UI-0.0.9.tar.gz/PyWin10UI-0.0.9/pywin10ui/demo.py
@@ -0,0 +1,34 @@
+# coding:utf-8
+import __init__ as pywin10ui, tkinter.messagebox, pygame, sys
+
+
+def quit_def():
+    if tkinter.messagebox.askokcancel('提示', '要执行此操作吗'):
+        pygame.quit()
+        sys.exit()
+
+
+def button_commond():
+    print("button down")
+
+
+window = pywin10ui.Window("测试", 300, 200, Exit_the_method=quit_def)
+
+button = pywin10ui.Button(window, 150, 1, "Button", 80, 32, common=button_commond)
+entry = pywin10ui.Entry(window, 1, 25, "Entry", 100, 20)
+progress_bar = pywin10ui.Progress_bar(window, 1, 50, 100, 20, 100, 50)
+select_box = pywin10ui.Select_box(window, 1, 75, "Select_box")
+picture_box = pywin10ui.Picture_box(window, 1, 100, 150, 100, img=None)
+
+window.append_component([button, entry, progress_bar, select_box, picture_box])
+
+while 1:
+    window.mainloop()
+    window.canvas.fill((240, 240, 240))
+    button.mainloop()
+    entry.mainloop()
+    progress_bar.mainloop()
+    select_box.mainloop()
+    picture_box.mainloop()
+
+    window.set_tiltle("测试 DEMO")
---
+++ packages/PyWin10UI/PyWin10UI-0.0.9.tar.gz/PyWin10UI-0.0.9/setup.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+from setuptools import setup
+
+setup(name='PyWin10UI',
+      version='0.0.9',
+      description='基于Pygame的UI模块,支持模块以外的pygame组件  最新版本发布日期:2021/12/30',
+      url='https://github.com/bytfr/pywin10ui',
+      author='bytfr 啃竹子',
+      author_email='2678509244@qq.com',
+      license='MIT',
+      packages=['pywin10ui'])
