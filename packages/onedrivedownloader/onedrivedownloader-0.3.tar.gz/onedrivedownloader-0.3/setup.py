0a8aaf2e9f7ffc7ae6d582fa7f59d8b5a41b4c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:39 +0000

    Add package onedrivedownloader/0.3/onedrivedownloader-0.3.tar.gz

---
+++ packages/onedrivedownloader/onedrivedownloader-0.3.tar.gz/onedrivedownloader-0.3/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+    name='onedrivedownloader',
+    packages=['onedrivedownloader'],
+    version='0.3',
+    license='MIT',
+    description='Python utility to download files through OneDrive',
+    author='Lorenzo Bonicelli',
+    author_email='loribonna@gmail.com',
+    url='https://github.com/loribonna/onedrivedownloader',
+    download_url='https://github.com/loribonna/onedrivedownloader/archive/refs/tags/v_01.zip',
+    keywords=['onedrive', 'downloader', 'python', 'utility'],
+    install_requires=[
+        'requests',
+        'tqdm'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
