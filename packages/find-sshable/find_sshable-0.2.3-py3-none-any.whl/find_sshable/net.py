82be8d1ffb8a51fdf51e762f542fc025a96905ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:19 +0000

    Add package find-sshable/0.2.3/find_sshable-0.2.3-py3-none-any.whl

---
+++ packages/find-sshable/find_sshable-0.2.3-py3-none-any.whl/find_sshable/net.py
@@ -0,0 +1,84 @@
+import dataclasses
+import re
+import socket
+from ipaddress import ip_network, IPv4Network, ip_address, IPv4Address
+from typing import Optional, Dict, List
+
+import nmap3
+import tqdm_thread
+
+from find_sshable import tools
+
+
+def get_ip_addr() -> IPv4Address:
+    hn = socket.gethostname()
+    h = socket.gethostbyname(hn)
+    return h
+
+
+def get_network() -> IPv4Network:
+    addr = get_ip_addr()
+    pieces = str(addr).split('.')
+    pieces[-1] = '0'
+    net_str = f"{'.'.join(pieces)}/24"
+    return ip_network(net_str)
+
+
+def is_sshable(target: str, port: Optional[str] = "22") -> bool:
+    nmap = nmap3.Nmap()
+
+    result = nmap.run_command(cmd=f"{nmap.nmaptool} -oX - -p {port} "
+                                  f"--script ssh-auth-methods {ip_address(target)}".split())
+    et = nmap.get_xml_et(result)
+    try:
+        script_out = et.find("host").find("ports").find("port").find("script").get("output")
+    except AttributeError:
+        return False
+
+    return (
+            "Supported authentication methods:" in script_out and
+            ("publickey" in script_out or "password" in script_out)
+    )
+
+
+def scan_open_port(host_timeout: Optional[str] = None, port: Optional[str] = "22") -> Dict:
+    """Find devices on current network with port 22 open."""
+    if host_timeout is None:
+        host_timeout = "3s"
+
+    host_timeout = tools.simple_time_spec(host_timeout)
+
+    nm_scan = nmap3.NmapScanTechniques()
+    with tqdm_thread.tqdm_thread(desc="scanning for devices..."):
+        return nm_scan.nmap_tcp_scan(get_network(), args=f"--host-timeout {host_timeout} -T5 --open -p {port}")
+
+
+@dataclasses.dataclass
+class Host:
+    name: Optional[str]
+    ip: IPv4Address
+
+    @property
+    def ip_str(self):
+        return str(self.ip)
+
+
+def find_sshable(host_timeout: Optional[str] = None,
+                 host_pattern: Optional[re.Pattern] = None,
+                 passive: Optional[bool] = False) -> List[Host]:
+    result = scan_open_port(host_timeout=host_timeout)
+
+    # pull these guys off
+    stats = result.pop('stats', None)
+    runtime = result.pop('runtime', None)
+
+    # everything left is devices
+    hosts = []
+    for ip, data in result.items():
+        for host_data in data["hostname"]:
+            if host_pattern and not host_pattern.search(host_data["name"]):
+                continue
+            if not passive and not is_sshable(ip):
+                continue
+            hosts.append(Host(name=host_data["name"], ip=ip_address(ip)))
+    return hosts
---
+++ packages/find-sshable/find_sshable-0.2.3-py3-none-any.whl/find_sshable/sshconf.py
@@ -0,0 +1,103 @@
+import dataclasses
+import logging
+import os
+import shutil
+from typing import Optional, List
+
+SSH_CONFIG_FILE = '~/.ssh/config'
+
+DELIMITER = "#! managed by find_sshable; this section may be overwritten !#"
+
+logger = logging.getLogger(os.path.basename(__file__))
+
+
+@dataclasses.dataclass
+class HostEntry:
+    name: str
+    User: Optional[str] = None
+    HostName: Optional[str] = None
+
+    def serialize(self) -> str:
+        val = f"Host {self.name}\n"
+        if self.User:
+            val += f"    User {self.User}\n"
+        if self.HostName:
+            val += f"    HostName {self.HostName}\n"
+        val += "\n"
+        return val
+
+
+def _load_found(ifp) -> str:
+    contents = ""
+    for line in ifp:
+        if line.startswith(DELIMITER):
+            return contents
+        contents += line
+
+
+def _load_config_parts(path: str) -> (str, str):
+    found, other = "", ""
+    if os.path.exists(path):
+        with open(path, 'r', encoding='utf8') as ifp:
+            for line in ifp:
+                if line.startswith(DELIMITER):
+                    found = _load_found(ifp)
+                else:
+                    other += line
+    return found.strip(), other.strip()
+
+
+def _parse(string: str) -> List[HostEntry]:
+    entries = []
+    current = None
+    for line in string.split("\n"):
+        if line.startswith("Host"):
+            if current:
+                entries.append(current)
+            current = HostEntry(line.split(" ")[-1])
+
+        if line.strip().startswith("User"):
+            current.User = line.split(" ")[-1]
+
+        if line.strip().startswith("HostName"):
+            current.HostName = line.split(" ")[-1]
+
+    if current:
+        entries.append(current)
+
+    return entries
+
+
+def _serialize(entries: List[HostEntry]) -> str:
+    val = "\n\n" + DELIMITER + "\n\n"
+
+    for entry in entries:
+        val += entry.serialize()
+
+    return val + DELIMITER + "\n"
+
+
+def get_found_hosts(config_path: Optional[str] = None) -> List[HostEntry]:
+    if not config_path:
+        config_path = os.path.expanduser(SSH_CONFIG_FILE)
+
+    found, other = _load_config_parts(config_path)
+
+    return _parse(found)
+
+
+def update_found_hosts(entries: List[HostEntry],
+                       config_path: Optional[str] = None,
+                       backup: Optional[bool] = True):
+    if not config_path:
+        config_path = os.path.expanduser(SSH_CONFIG_FILE)
+
+    if backup and os.path.exists(config_path):
+        shutil.copy(config_path, f'{config_path}.bak')
+        logger.debug("Made copy of ssh config %s", f"{config_path}.bak")
+
+    found, other = _load_config_parts(config_path)
+
+    with open(config_path, 'w', encoding='utf8') as ofp:
+        ofp.write(other)
+        ofp.write(_serialize(entries))
