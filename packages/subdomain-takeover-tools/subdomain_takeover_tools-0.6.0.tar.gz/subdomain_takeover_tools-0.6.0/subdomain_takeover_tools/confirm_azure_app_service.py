d57aad15a9af78d41c8426355904dd31038091bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:49 +0000

    Add package subdomain-takeover-tools/0.6.0/subdomain_takeover_tools-0.6.0.tar.gz

---
+++ packages/subdomain-takeover-tools/subdomain_takeover_tools-0.6.0.tar.gz/subdomain_takeover_tools-0.6.0/subdomain_takeover_tools/confirm_azure_app_service.py
@@ -0,0 +1,28 @@
+import dns.resolver
+
+from subdomain_takeover_tools.helper.main import bootstrap
+from subdomain_takeover_tools.helper.prepare import resolve_cname
+
+
+def is_valid(hostname, cname):
+    if hostname == cname:
+        cname = resolve_cname(hostname)
+
+    if cname is None:
+        return False
+
+    if cname.count('.') == 2:
+        try:
+            dns.resolver.resolve('asuid.' + hostname, 'TXT')
+        except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
+            return True
+
+    return False
+
+
+def confirm_azure_app_service():
+    bootstrap(is_valid)
+
+
+if __name__ == "__main__":
+    confirm_azure_app_service()
