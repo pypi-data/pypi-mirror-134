a86473ab7710b5bfcecaafd9ccde1e0c957dac38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:50 +0000

    Add package XLML/0.1.2/XLML-0.1.2.tar.gz

---
+++ packages/XLML/XLML-0.1.2.tar.gz/XLML-0.1.2/README.md
@@ -0,0 +1,2 @@
+# xlml
+Easy Excel to HTML library for Python (3.x). Also accounts for merged cells unlike other Excel to HTML libraries.
---
+++ packages/XLML/XLML-0.1.2.tar.gz/XLML-0.1.2/setup.py
@@ -0,0 +1,29 @@
+
+from distutils.core import setup
+
+setup(
+  name = 'XLML',
+  packages = ['XLML'],
+  version = '0.1.2',
+  license='MIT',
+  description = 'Easy Excel to HTML library for Python (3.x).',
+  author = 'Sarthak Jain',
+  author_email = 'sarthakjain10.11@gmail.com',
+  url = 'https://github.com/Sarthak049/htmxl',
+  download_url = 'https://github.com/Sarthak049/xlml/archive/refs/tags/xlml-0.1.2.tar.gz',
+  keywords = ['Excel', 'CSV', 'HTML', 'XLML'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
+  ],
+)
