beffd8e30258b5089bf61c04fc3c25dac7ff6f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:05 +0000

    Add package sergiu-presentation/0.1.0/sergiu-presentation-0.1.0.tar.gz

---
+++ packages/sergiu-presentation/sergiu-presentation-0.1.0.tar.gz/sergiu-presentation-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sergiu-presentation",
+    version="0.1.0",
+    author="Matei Sergiu-Răzvan",
+    author_email="mateisergiu777@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    description="This is just a demo library"
+)
