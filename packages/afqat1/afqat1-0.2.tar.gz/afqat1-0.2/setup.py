bb48ff2aed7f4565e63ebe714b52b5c3b8c20f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:09 +0000

    Add package afqat1/0.2/afqat1-0.2.tar.gz

---
+++ packages/afqat1/afqat1-0.2.tar.gz/afqat1-0.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+from setuptools import setup, find_packages
+
+VERSION = '0.2' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+       # 
+        name="afqat1", 
+        version=0.2,
+        author="Andrew",
+        author_email="<cranberry314@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['AlphaVantage', 'quantitative'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
