510f824fcbcfbb641bd0f3965680a96c0a7b9909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:39 +0000

    Add package pytemperature/1.1/pytemperature-1.1.tar.gz

---
+++ packages/pytemperature/pytemperature-1.1.tar.gz/pytemperature-1.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+with open('README.md','r',encoding='utf-8-sig') as f:
+    readme = f.read()
+
+
+setup(
+	name='pytemperature',
+	version='1.1',
+	packages=['pytemperature'],
+	url='https://github.com/wannaphong/pytemperature',
+	license='MIT',
+	author='Wannaphong Phatthiyaphaibun',
+	author_email='wannaphong@yahoo.com',
+	keywords = 'temperature science',
+	description='Scale of temperature',
+	long_description=readme,
+	long_description_content_type='text/markdown',
+	python_requires=">=3.6",
+	classifiers= [
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Topic :: Scientific/Engineering',
+	],
+)
