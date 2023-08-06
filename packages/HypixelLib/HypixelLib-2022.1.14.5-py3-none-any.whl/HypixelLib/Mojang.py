44bcda45e26b33aeaeefe592c3ffdee77400325c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:49 +0000

    Add package HypixelLib/2022.1.14.5/HypixelLib-2022.1.14.5-py3-none-any.whl

---
+++ packages/HypixelLib/HypixelLib-2022.1.14.5-py3-none-any.whl/HypixelLib/Mojang.py
@@ -0,0 +1,15 @@
+import aiohttp
+import json as JSON
+from .exceptions import InvalidPlayerNameError
+
+MOJANG_API = "https://api.mojang.com"
+
+async def get_uuid(name):
+	async with aiohttp.ClientSession() as client:
+		async with client.get(f"{MOJANG_API}/users/profiles/minecraft/{name}") as response:
+			try:
+				json = await response.json()
+				return json["id"]
+			except JSON.decoder.JSONDecodeError:
+				raise InvalidPlayerNameError
+		
