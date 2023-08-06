4afe4fd8eba760a9e7e67e2afa359c2eef6ec110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:33 +0000

    Add package opensearch-service/0.22/opensearch-service-0.22.tar.gz

---
+++ packages/opensearch-service/opensearch-service-0.22.tar.gz/opensearch-service-0.22/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='opensearch-service',
+      version='0.22',
+      description='easy access to OpenSearch based on opensearch-dsl',
+      url='https://github.com/bertrandchevrier/opensearch-service.git',
+      author='The data handyman team',
+      author_email='chevrierbertrand@yahoo.fr',
+      license='MIT',
+      classifiers=['Development Status :: 3 - Alpha'],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+
+      packages=['opensearch_service'],
+      install_requires=[
+          'opensearch','opensearch-dsl','pandas'
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      python_requires='>=3.5')
