6fc442dc985eb712f756657517c7cc86e7087567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:49 +0000

    Add package authc/0.0.8/authc-0.0.8.tar.gz

---
+++ packages/authc/authc-0.0.8.tar.gz/authc-0.0.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="authc",
+    version="0.0.8",  # Latest version .
+    author="R2FsCg",
+    author_email="r2fscg@gmail.com",
+    description="For authentication.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/private_repo/codefast",
+    package_data={setuptools.find_packages()[0]: [
+                      'bin/*',
+                  ]},
+    packages=setuptools.find_packages(),
+    install_requires=['codefast'],
+    entry_points={'console_scripts': ['auth=authc:authc']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
