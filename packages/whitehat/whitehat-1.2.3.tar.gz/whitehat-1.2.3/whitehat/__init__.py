35c1878911ed92e91d7808319702a870808bd852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:30 +0000

    Add package whitehat/1.2.3/whitehat-1.2.3.tar.gz

---
+++ packages/whitehat/whitehat-1.2.3.tar.gz/whitehat-1.2.3/whitehat/__init__.py
@@ -0,0 +1,16 @@
+from .ddos import *
+from .phonenumber import *
+from .wifi import *
+from .ip import *
+from .keylogger import *
+from .port import *
+from .reverse_shell import *
+from .browser import *
+from .bruteforce import *
+from .cryptography import *
+
+__title__ = "Whitehat"
+__version__ = "1.2.3"
+__authors__ = "LyQuid and EterNomm"
+__license__ = "Apache License 2.0"
+__copyright__ = "Copyright 2021-present EterNomm"
