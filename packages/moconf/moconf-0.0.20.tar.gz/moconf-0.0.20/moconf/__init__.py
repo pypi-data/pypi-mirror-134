4adac6a94682ea531da75057c78eae6a39ca0ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:14 +0000

    Add package moconf/0.0.20/moconf-0.0.20.tar.gz

---
+++ packages/moconf/moconf-0.0.20.tar.gz/moconf-0.0.20/moconf/__init__.py
@@ -0,0 +1,16 @@
+### This is a python package used for configuring a Morpheus appliance ###
+
+from . import (
+   appliance,
+   infra
+)
+
+# from .appliance import (
+#    morpheusAppliance
+# )
+
+# from .configs import (
+#     createMorpheusGroup,
+#     createMorpheusAwsCloud,
+#     createMorpheusGitIntegration
+# )
