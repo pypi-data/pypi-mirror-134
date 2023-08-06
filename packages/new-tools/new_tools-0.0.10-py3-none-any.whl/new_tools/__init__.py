c006ff2a48386f9781723fe84396dc7ecaefe68b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:39 +0000

    Add package new-tools/0.0.10/new_tools-0.0.10-py3-none-any.whl

---
+++ packages/new-tools/new_tools-0.0.10-py3-none-any.whl/new_tools/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Overcomer"
+__version__ = "0.0.10"
+__module__ = ["media", "string"]
+
+from .media import check_image, IMAGE_FORMAT
+from .media import FPS, VIDEO_FORMAT
+from .string import check_bool
