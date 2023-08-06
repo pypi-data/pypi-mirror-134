fd3b9577d7c082afb640a2996b67ecf2533e5c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:24 +0000

    Add package terraform-install/0.1.2/terraform_install-0.1.2.tar.gz

---
+++ packages/terraform-install/terraform_install-0.1.2.tar.gz/terraform_install-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['terraform_install', 'terraform_install.cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['terraform_version']
+
+setup_kwargs = {
+    'name': 'terraform-install',
+    'version': '0.1.2',
+    'description': 'Installs Terraform to virtualenv bin',
+    'long_description': None,
+    'author': 'Josh Wycuff',
+    'author_email': 'Joshua.Wycuff@turner.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
