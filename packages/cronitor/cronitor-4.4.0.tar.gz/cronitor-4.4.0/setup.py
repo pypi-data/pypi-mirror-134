5c0ca3ed01675c9b2e9a913c80bf96c765729e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:17 +0000

    Add package cronitor/4.4.0/cronitor-4.4.0.tar.gz

---
+++ packages/cronitor/cronitor-4.4.0.tar.gz/cronitor-4.4.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cronitor',
+    version='4.4.0',
+    packages=find_packages(),
+    url='https://github.com/cronitorio/cronitor-python',
+    license='MIT License',
+    author='August Flanagan',
+    author_email='august@cronitor.io',
+    description='A lightweight Python client for Cronitor.',
+    install_requires=[
+        'requests',
+        'pyyaml',
+        'humanize',
+    ],
+    entry_points=dict(console_scripts=['cronitor = cronitor.__main__:main'])
+)
