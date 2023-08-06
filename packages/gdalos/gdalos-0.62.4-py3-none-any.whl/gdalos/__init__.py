1f0468a3eb5741d63cb7cc9a603ca1b5d1f618af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:22 +0000

    Add package gdalos/0.62.4/gdalos-0.62.4-py3-none-any.whl

---
+++ packages/gdalos/gdalos-0.62.4-py3-none-any.whl/gdalos/__init__.py
@@ -0,0 +1,13 @@
+from osgeo.osr import OAMS_TRADITIONAL_GIS_ORDER
+from osgeo_utils.auxiliary.osr_util import set_default_axis_order
+set_default_axis_order(OAMS_TRADITIONAL_GIS_ORDER)
+
+version = (0, 62, 4)
+
+__package_name__ = "gdalos"
+__version__ = '.'.join(str(v) for v in version)
+__author__ = "Idan Miara, Ben Avrahami"
+__author_email__ = "idan@miara.com"
+__license__ = "MIT"
+__url__ = r"https://github.com/talos-gis/gdalos"
+__description__ = "a simple gdal translate/warp/addo python wrapper for raster batch processing"
