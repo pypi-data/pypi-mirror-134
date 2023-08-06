5414e6efcef0ab29e914dec0ae342f0cdd9347cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:04 +0000

    Add package ShynaTime/0.12/ShynaTime-0.12.tar.gz

---
+++ packages/ShynaTime/ShynaTime-0.12.tar.gz/ShynaTime-0.12/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup_args = dict(
+     name='ShynaTime',
+     version='0.12',
+     packages=find_packages(),
+     author="Shivam Sharma",
+     author_email="shivamsharma1913@gmail.com",
+     description="Shyna Backend Functionality Package For Time manipulation",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+    )
+
+install_requires = [
+    'datefinder',
+    'datetime',
+    "setuptools",
+    "wheel",
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
