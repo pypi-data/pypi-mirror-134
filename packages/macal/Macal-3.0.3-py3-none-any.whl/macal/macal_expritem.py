fae4b5f8322d57d57f0ad28609ef5a84b45f38a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:05 +0000

    Add package macal/3.0.3/Macal-3.0.3-py3-none-any.whl

---
+++ packages/macal/Macal-3.0.3-py3-none-any.whl/macal/macal_expritem.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------------------------#
+# Filename:       | macal_expritem.py                                                             #
+# Author:         | Marco Caspers                                                                 #
+# Description:    |                                                                               #
+#-------------------------------------------------------------------------------------------------#
+#                                                                                                 #
+# Macal 2.0 Expression Item Type                                                                  #
+#                                                                                                 #
+###################################################################################################
+
+"""Expression item class used as value for left and right brances of the expression"""
+
+# mutable variant of ExpressionItem = namedtuple('ExpressionItem', ["value", "item_type"])
+
+class ExpressionItem:
+    """Expression item class, used by expressions and interpreter"""
+    def __init__(self, value, item_type):
+        """Initializes expression item"""
+        self.value     = value
+        self.item_type = item_type
+        self.ref       = False
+        self.format    = False
