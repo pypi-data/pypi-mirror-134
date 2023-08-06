55d3675959531d256794f51805ff879279e57e09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:08 +0000

    Add package flask-redis-ext/1.0.0/flask_redis_ext-1.0.0-py3-none-any.whl

---
+++ packages/flask-redis-ext/flask_redis_ext-1.0.0-py3-none-any.whl/flask_redis/__init__.py
@@ -0,0 +1,16 @@
+# !/usr/local/python/bin/python
+# -*- coding: utf-8 -*-
+# (C) Wu Dong, 2021
+# All rights reserved
+# @Author: 'Wu Dong <wudong@eastwu.cn>'
+# @Time: '2021/8/10 5:32 下午'
+from .macro import (
+    K_RDS_BINDS,
+    K_RDS_DEFAULT_BIND_KEY,
+    K_RDS_IGNORE_CACHE,
+    K_RDS_PREFIX,
+    K_RDS_URL
+)
+from .cluster import RedisCluster
+from .redis import Redis
+from .sentinel import Sentinel
