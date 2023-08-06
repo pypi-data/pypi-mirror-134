60f41d2f82bc8b397e8065d78e4f034aae52a7d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:05 +0000

    Add package machine-common-sense/0.5.0/machine_common_sense-0.5.0.tar.gz

---
+++ packages/machine-common-sense/machine_common_sense-0.5.0.tar.gz/machine_common_sense-0.5.0/machine_common_sense/step_metadata.py
@@ -0,0 +1,213 @@
+import copy
+
+from .goal_metadata import GoalMetadata
+from .return_status import ReturnStatus
+from .stringifier import Stringifier
+
+
+class StepMetadata:
+    """
+    Defines output metadata from an action step in the MCS 3D environment.
+
+    Attributes
+    ----------
+    action_list : list of (string, dict) tuples
+        The list of all actions that are available for the next step.
+        Each action is returned as a tuple containing the action string and
+        the action's restricted parameters, if any.
+        For example: ("Pass", {}) forces a Pass action; ("PickupObject", {})
+        forces a PickupObject action with any parameters; and
+        ("PickupObject", {"objectId": "a"}) forces a PickupObject action with
+        the specific parameters objectId=a.
+        An action_list of None or an empty list means that all actions will
+        be available for the next step.
+
+        To "step" using the first action from the action_list:
+
+        .. highlight:: python
+        .. code-block:: python
+
+            step_metadata = controller.start_scene(scene_data)
+            action, params = step_metadata.action_list[0]
+            step_metadata = controller.step(action, **params)
+
+        Derived from :mod:`GoalMetadata.action_list[step_number]
+        <machine_common_sense.GoalMetadata>`.
+        May be a subset of all possible actions. See
+        :mod:`Action <machine_common_sense.Action>`.
+    camera_aspect_ratio : (float, float)
+        The player camera's aspect ratio. This will remain constant for the
+        whole scene.
+    camera_clipping_planes : (float, float)
+        The player camera's near and far clipping planes. This will remain
+        constant for the whole scene.
+    camera_field_of_view : float
+        The player camera's field of view. This will remain constant for
+        the whole scene.
+    camera_height : float
+        The player camera's height.
+    depth_map_list : list of 2D numpy arrays
+        The list of 2-dimensional numpy arrays of depth float data from the
+        scene after the last action and physics simulation were run. This is
+        usually a list with 1 array, except for the output from start_scene
+        for a scene with a scripted Preview Phase (Preview Phase case details
+        TBD).
+        Each depth float in a 2-dimensional numpy array is a value between 0
+        and the camera's far clipping plane (default 15) correspondings to the
+        depth in simulation units at that pixel in the image.
+        Note that this list will be empty if the metadata level is 'none'.
+    goal : GoalMetadata or None
+        The goal for the whole scene. Will be None in "Exploration" scenes.
+    habituation_trial : int or None
+        The current habituation trial (as a positive integer), or None if the
+        scene is not currently in a habituation trial (meaning this scene is
+        in a test trial).
+    head_tilt : float
+        How far your head is tilted up/down in degrees (between 90 and -90).
+        Changed by setting the "horizon" parameter in a "RotateLook" action.
+    image_list : list of Pillow.Image objects
+        The list of images from the scene after the last action and physics
+        simulation were run. This is usually a list with 1 image, except for
+        the output from start_scene for a scene with a scripted Preview Phase.
+        (Preview Phase case details TBD).
+    object_list : list of ObjectMetadata objects
+        The list of metadata for all the visible interactive objects in the
+        scene. This list will be empty if using a metadata level below
+        the 'oracle' level. For metadata on structural objects like walls,
+        please see structural_object_list
+    object_mask_list : list of Pillow.Image objects
+        The list of object mask (instance segmentation) images from the scene
+        after the last action and physics simulation were run. This is usually
+        a list with 1 image, except for the output from start_scene for a
+        scene with a scripted Preview Phase (Preview Phase case details TBD).
+        The color of each object in the mask corresponds to the "color"
+        property in its ObjectMetadata object.
+        Note that this list will be empty if the metadata level is 'none'
+        or 'level1'.
+    performer_radius: float
+        The radius of the performer.
+    performer_reach: float
+        The max reach of the performer.
+    position : dict
+        The "x", "y", and "z" coordinates for your global position.
+        Will be set to 'None' if using a metadata level below the
+        'oracle' level.
+    return_status : string
+        The return status from your last action. See
+        :mod:`Action <machine_common_sense.Action>`.
+    reward : integer
+        Reward is 1 on successful completion of a task, 0 otherwise.
+    rotation : float
+        Your current rotation angle in degrees. Will be set to 'None'
+        if using a metadata level below the 'oracle' level.
+    step_number : integer
+        The step number of your last action, recorded since you started the
+        current scene.
+    physics_frames_per_second : float
+        The frames per second of the physics engine
+    structural_object_list : list of ObjectMetadata objects
+        The list of metadata for all the visible structural objects (like
+        walls, occluders, and ramps) in the scene. This list will be empty
+        if using a metadata level below the 'oracle' level.
+        Occluders are composed of two separate objects,
+        the "wall" and the "pole", with corresponding object IDs
+        (occluder_wall_<uuid> and occluder_pole_<uuid>), and ramps are
+        composed of between one and three objects (depending on the type
+        of ramp), with corresponding object IDs.
+    """
+
+    def __init__(
+        self,
+        action_list=None,
+        camera_aspect_ratio=None,
+        camera_clipping_planes=None,
+        camera_field_of_view=0.0,
+        camera_height=0.0,
+        depth_map_list=None,
+        goal=None,
+        habituation_trial=None,
+        head_tilt=0.0,
+        image_list=None,
+        object_list=None,
+        object_mask_list=None,
+        performer_radius=0.0,
+        performer_reach=0.0,
+        physics_frames_per_second=0,
+        position=None,
+        return_status=ReturnStatus.UNDEFINED.value,
+        reward=0,
+        rotation=0.0,
+        step_number=0,
+        structural_object_list=None
+    ):
+        self.action_list = [] if action_list is None else action_list
+        self.camera_aspect_ratio = (
+            0.0, 0.0) if camera_aspect_ratio is None else camera_aspect_ratio
+        self.camera_clipping_planes = (
+            (0.0, 0.0)
+            if camera_clipping_planes is None
+            else camera_clipping_planes
+        )
+        self.camera_field_of_view = camera_field_of_view
+        self.camera_height = camera_height
+        self.depth_map_list = (
+            [] if depth_map_list is None else depth_map_list
+        )
+        self.goal = GoalMetadata() if goal is None else goal
+        self.habituation_trial = habituation_trial
+        self.head_tilt = head_tilt
+        self.image_list = [] if image_list is None else image_list
+        self.object_list = [] if object_list is None else object_list
+        self.object_mask_list = (
+            [] if object_mask_list is None else object_mask_list
+        )
+        self.performer_radius = performer_radius
+        self.performer_reach = performer_reach
+        self.physics_frames_per_second = physics_frames_per_second
+        self.position = {} if position is None else position
+        self.return_status = return_status
+        self.reward = reward
+        self.rotation = rotation
+        self.step_number = step_number
+        self.structural_object_list = [
+        ] if structural_object_list is None else structural_object_list
+
+    def __str__(self):
+        return Stringifier.class_to_str(self)
+
+    def check_list_none(self, obj_list):
+        if obj_list is None:
+            return None
+        else:
+            return {obj.uuid: dict(obj) for obj in obj_list}
+
+    def copy_without_depth_or_images(self):
+        """Return a deep copy of this StepMetadata with default depth_map_list,
+        image_list, and object_mask_list properties."""
+        step_metadata_copy = StepMetadata()
+        for key, _ in self:
+            setattr(step_metadata_copy, key, copy.deepcopy(getattr(self, key)))
+        return step_metadata_copy
+
+    # Allows converting the class to a dictionary, along with allowing
+    #   certain fields to be left out of output file
+    def __iter__(self):
+        yield 'action_list', self.action_list
+        yield 'camera_aspect_ratio', self.camera_aspect_ratio
+        yield 'camera_clipping_planes', self.camera_clipping_planes
+        yield 'camera_field_of_view', self.camera_field_of_view
+        yield 'camera_height', self.camera_height
+        yield 'goal', dict(self.goal)
+        yield 'habituation_trial', self.habituation_trial
+        yield 'head_tilt', self.head_tilt
+        yield 'object_list', self.check_list_none(self.object_list)
+        yield 'performer_radius', self.performer_radius
+        yield 'performer_reach', self.performer_reach
+        yield 'physics_frames_per_second', self.physics_frames_per_second
+        yield 'position', self.position
+        yield 'return_status', self.return_status
+        yield 'reward', self.reward
+        yield 'rotation', self.rotation
+        yield 'step_number', self.step_number
+        yield 'structural_object_list', self.check_list_none(
+            self.structural_object_list)
---
+++ packages/machine-common-sense/machine_common_sense-0.5.0.tar.gz/machine_common_sense-0.5.0/tests/test_plotter.py
@@ -0,0 +1,383 @@
+import unittest
+from unittest.case import skip
+
+import ai2thor
+import PIL
+
+from machine_common_sense.config_manager import (FloorHolesAndTexturesXZConfig,
+                                                 FloorTexturesConfig,
+                                                 SceneConfiguration, Vector3d)
+from machine_common_sense.plotter import TopDownPlotter, XZHeading
+
+
+class TestTopDownPlotter(unittest.TestCase):
+
+    def setUp(self):
+        scene_config = SceneConfiguration(
+            name="test", room_dimensions=Vector3d(
+                x=10, y=4, z=10))
+        self.plotter = TopDownPlotter(
+            team="test",
+            scene_config=scene_config
+        )
+
+    def test_convert_color_empty(self):
+        color = self.plotter._convert_color('')
+        self.assertEqual(color, "ivory")
+
+    def test_convert_color_none(self):
+        color = self.plotter._convert_color(None)
+        self.assertEqual(color, "ivory")
+
+    def test_convert_color_black(self):
+        color = self.plotter._convert_color('black')
+        self.assertEqual(color, "ivory")
+
+    def test_convert_color(self):
+        color = self.plotter._convert_color('red')
+        self.assertEqual(color, "red")
+
+    def test_plot_image_size(self):
+        metadata = {
+            'screenWidth': 600,
+            'screenHeight': 400,
+            'objects': [],
+            'structuralObjects': [],
+            'agent': {
+                'position': {
+                    'x': 0.0,
+                    'y': 0.0,
+                    'z': 0.0
+                },
+                'rotation': {
+                    'x': 0.0,
+                    'y': 0.0,
+                    'z': 0.0
+                }
+            }}
+        scene_event = ai2thor.server.Event(metadata=metadata)
+        img = self.plotter.plot(scene_event=scene_event, step_number=1)
+        self.assertIsInstance(img, PIL.Image.Image)
+
+    def test_plot_twice(self):
+        metadata = {
+            'screenWidth': 600,
+            'screenHeight': 400,
+            'objects': [],
+            'structuralObjects': [],
+            'agent': {
+                'position': {
+                    'x': 0.0,
+                    'y': 0.0,
+                    'z': 0.0
+                },
+                'rotation': {
+                    'x': 0.0,
+                    'y': 0.0,
+                    'z': 0.0
+                }
+            }}
+        scene_event = ai2thor.server.Event(metadata=metadata)
+        img1 = self.plotter.plot(scene_event=scene_event, step_number=1)
+        metadata = {
+            'screenWidth': 600,
+            'screenHeight': 400,
+            'objects': [],
+            'structuralObjects': [],
+            'agent': {
+                'position': {
+                    'x': 2.0,
+                    'y': 0.0,
+                    'z': 2.0
+                },
+                'rotation': {
+                    'x': 0.0,
+                    'y': 0.0,
+                    'z': 0.0
+                }
+            }}
+        scene_event = ai2thor.server.Event(metadata=metadata)
+        img2 = self.plotter.plot(scene_event=scene_event, step_number=2)
+        self.assertNotEqual(img1, img2)
+        self.assertIsInstance(img1, PIL.Image.Image)
+        self.assertIsInstance(img2, PIL.Image.Image)
+
+    def test_calculate_heading_zero_degrees(self):
+        heading = self.plotter._calculate_heading(
+            0, TopDownPlotter.HEADING_LENGTH)
+        self.assertAlmostEqual(heading.x, 0.0)
+        self.assertAlmostEqual(heading.z, TopDownPlotter.HEADING_LENGTH)
+        self.assertIsInstance(heading, XZHeading)
+
+    def test_calculate_heading_ninety_degrees(self):
+        heading = self.plotter._calculate_heading(
+            90, TopDownPlotter.HEADING_LENGTH)
+        self.assertAlmostEqual(heading.x, -TopDownPlotter.HEADING_LENGTH)
+        self.assertAlmostEqual(heading.z, 0.0)
+        self.assertIsInstance(heading, XZHeading)
+
+    def test_calculate_heading_one_eighty_degrees(self):
+        heading = self.plotter._calculate_heading(
+            180, TopDownPlotter.HEADING_LENGTH)
+        self.assertAlmostEqual(heading.x, 0.0)
+        self.assertAlmostEqual(heading.z, -TopDownPlotter.HEADING_LENGTH)
+        self.assertIsInstance(heading, XZHeading)
+
+    def test_calculate_heading_two_seventy_degrees(self):
+        heading = self.plotter._calculate_heading(
+            270, TopDownPlotter.HEADING_LENGTH)
+        self.assertAlmostEqual(heading.x, TopDownPlotter.HEADING_LENGTH)
+        self.assertAlmostEqual(heading.z, 0.0)
+        self.assertIsInstance(heading, XZHeading)
+
+    def test_calculate_heading_three_sixty_degrees(self):
+        heading = self.plotter._calculate_heading(
+            360, TopDownPlotter.HEADING_LENGTH)
+        self.assertAlmostEqual(heading.x, 0.0)
+        self.assertAlmostEqual(heading.z, TopDownPlotter.HEADING_LENGTH)
+        self.assertIsInstance(heading, XZHeading)
+
+    def test_calculate_heading_seven_twenty_degrees(self):
+        heading = self.plotter._calculate_heading(
+            720, TopDownPlotter.HEADING_LENGTH)
+        self.assertAlmostEqual(heading.x, 0.0)
+        self.assertAlmostEqual(heading.z, TopDownPlotter.HEADING_LENGTH)
+        self.assertIsInstance(heading, XZHeading)
+
+    def test_calculate_heading_negative_ninety_degrees(self):
+        heading = self.plotter._calculate_heading(
+            -90, TopDownPlotter.HEADING_LENGTH)
+        self.assertAlmostEqual(heading.x, TopDownPlotter.HEADING_LENGTH)
+        self.assertAlmostEqual(heading.z, 0.0)
+        self.assertIsInstance(heading, XZHeading)
+
+    def test_calculate_heading_negative_seven_twenty_degrees(self):
+        heading = self.plotter._calculate_heading(
+            -720, TopDownPlotter.HEADING_LENGTH)
+        self.assertAlmostEqual(heading.x, 0.0)
+        self.assertAlmostEqual(heading.z, TopDownPlotter.HEADING_LENGTH)
+        self.assertIsInstance(heading, XZHeading)
+
+    def test_create_robot(self):
+        robot_metadata = {
+            'position': {
+                'x': 2.70,
+                'y': 1.23,
+                'z': 3.14
+            },
+            'rotation': {
+                'x': 0.0,
+                'y': 78.0,
+                'z': 0.0
+            }
+        }
+        robot = self.plotter._create_robot(robot_metadata)
+
+        self.assertAlmostEqual(robot.x, 2.70)
+        self.assertAlmostEqual(robot.z, 3.14)
+        self.assertAlmostEqual(robot.heading.x, 0.19562952)
+        self.assertAlmostEqual(robot.heading.z, 0.04158233)
+
+    def test_create_robot_missing_position(self):
+        robot_metadata = {
+            'rotation': {
+                'x': 0.0,
+                'y': 78.0,
+                'z': 0.0
+            }
+        }
+        robot = self.plotter._create_robot(robot_metadata)
+
+        self.assertAlmostEqual(robot.x, 0.0)
+        self.assertAlmostEqual(robot.z, 0.0)
+        self.assertAlmostEqual(robot.heading.x, 0.19562952)
+        self.assertAlmostEqual(robot.heading.z, 0.04158233)
+
+    def test_create_robot_missing_rotation(self):
+        robot_metadata = {
+            'position': {
+                'x': 2.70,
+                'y': 1.23,
+                'z': 3.14
+            }
+        }
+        robot = self.plotter._create_robot(robot_metadata)
+
+        self.assertAlmostEqual(robot.x, 2.70)
+        self.assertAlmostEqual(robot.z, 3.14)
+        self.assertAlmostEqual(robot.heading.x, 0.0)
+        self.assertAlmostEqual(robot.heading.z, 0.2)
+
+    def test_create_object(self):
+        object_metadata = {
+            'isPickedUp': True,
+            'visibleInCamera': True,
+            'objectId': 'test-uuid',
+            'colorsFromMaterials': [
+                'orange'
+            ],
+            'objectBounds': {
+                'objectBoundsCorners': [
+                    {'x': 0, 'y': 0, 'z': 0},
+                    {'x': 0, 'y': 0, 'z': 1},
+                    {'x': 1, 'y': 0, 'z': 1},
+                    {'x': 1, 'y': 0, 'z': 0},
+                    {'x': 0, 'y': 1, 'z': 0},
+                    {'x': 0, 'y': 1, 'z': 1},
+                    {'x': 1, 'y': 1, 'z': 1},
+                    {'x': 1, 'y': 1, 'z': 0}
+                ]
+            }
+        }
+        obj = self.plotter._create_object(object_metadata)
+
+        self.assertTrue(obj.held)
+        self.assertTrue(obj.visible)
+        self.assertEqual(obj.uuid, 'test-uuid')
+        self.assertIsInstance(obj.bounds, list)
+        self.assertEqual(len(obj.bounds), 8)
+        self.assertEqual(obj.color, "orange")
+
+    def test_create_object_empty(self):
+        object_metadata = {}
+        obj = self.plotter._create_object(object_metadata)
+
+        self.assertIsNone(obj.held)
+        self.assertIsNone(obj.visible)
+        self.assertIsNone(obj.uuid)
+        self.assertIsNone(obj.bounds)
+        self.assertEqual(obj.color, "ivory")
+
+    def test_find_plottable_objects_empty(self):
+        metadata = {
+            'screenWidth': 600,
+            'screenHeight': 400,
+            'objects': [],
+            'structuralObjects': [],
+            'agent': {
+                'position': {
+                    'x': 0.0,
+                    'y': 0.0,
+                    'z': 0.0
+                },
+                'rotation': {
+                    'x': 0.0,
+                    'y': 0.0,
+                    'z': 0.0
+                }
+            }
+        }
+        scene_event = ai2thor.server.Event(metadata=metadata)
+        filtered_objects = self.plotter._find_plottable_objects(scene_event)
+        self.assertEqual(len(filtered_objects), 0)
+
+    def test_find_plottable_objects_combined(self):
+        metadata = {
+            'screenWidth': 600,
+            'screenHeight': 400,
+            'objects': [
+                {'objectId': 'test-uuid1'},
+                {'objectId': 'test-uuid2'},
+                {'objectId': 'test-uuid3'}
+            ],
+            'structuralObjects': [
+                {'objectId': 'occluder1'},
+                {'objectId': 'occluder2'},
+                {'objectId': 'wall1'}
+            ]
+        }
+        scene_event = ai2thor.server.Event(metadata=metadata)
+        filtered_objects = self.plotter._find_plottable_objects(scene_event)
+        self.assertEqual(len(filtered_objects), 6)
+        self.assertEqual(len(
+            [k for k in filtered_objects
+             if k['objectId'].startswith('test-uuid')]), 3)
+        self.assertEqual(len(
+            [k for k in filtered_objects
+             if k['objectId'].startswith('occluder')]), 2)
+        self.assertEqual(len(
+            [k for k in filtered_objects
+             if k['objectId'].startswith('wall')]), 1)
+
+    def test_find_plottable_objects_filtered(self):
+        metadata = {
+            'screenWidth': 600,
+            'screenHeight': 400,
+            'objects': [
+                {'objectId': 'test-uuid1'},
+                {'objectId': 'test-uuid2'},
+                {'objectId': 'test-uuid3'}
+            ],
+            'structuralObjects': [
+                {'objectId': 'occluder1'},
+                {'objectId': 'occluder2'},
+                {'objectId': 'wall1'},
+                {'objectId': 'future-object'},
+                {'objectId': 'floor-gets-filtered'},
+                {'objectId': 'ceiling-gets-filtered'}
+            ]
+        }
+        scene_event = ai2thor.server.Event(metadata=metadata)
+        filtered_objects = self.plotter._find_plottable_objects(scene_event)
+        self.assertEqual(len(filtered_objects), 7)
+        self.assertEqual(filtered_objects[0]['objectId'], 'occluder1')
+        self.assertEqual(filtered_objects[1]['objectId'], 'occluder2')
+        self.assertEqual(filtered_objects[2]['objectId'], 'wall1')
+        self.assertEqual(filtered_objects[3]['objectId'], 'future-object')
+        self.assertEqual(filtered_objects[4]['objectId'], 'test-uuid1')
+        self.assertEqual(filtered_objects[5]['objectId'], 'test-uuid2')
+        self.assertEqual(filtered_objects[6]['objectId'], 'test-uuid3')
+
+    def test_scene_name(self):
+        self.assertEqual(self.plotter._scene_name, "test")
+
+    def test_scene_name_prefix(self):
+        scene_config = SceneConfiguration(
+            name="prefix/test", room_dimensions=Vector3d(
+                x=10, y=4, z=10))
+        plotter = TopDownPlotter(
+            team="test",
+            scene_config=scene_config
+        )
+
+        self.assertEqual(plotter._scene_name, "test")
+
+    @skip("ResourceWarning")
+    def test_draw_holes(self):
+        holes = [
+            FloorHolesAndTexturesXZConfig(**{"x": 0, "z": 0}),
+            FloorHolesAndTexturesXZConfig(**{"x": 1, "z": 0}),
+            FloorHolesAndTexturesXZConfig(**{"x": 2, "z": 2})
+        ]
+        goal = {'metadata': {
+            'target': {'image': [0]},
+            'target_1': {'image': [1]},
+            'target_2': {'image': [2]}
+        }}
+        scene_config = SceneConfiguration(
+            name="testscene",
+            version=1,
+            goal=goal,
+            holes=holes,
+            room_dimensions=Vector3d(
+                x=10,
+                y=3,
+                z=10),
+            floor_textures=[FloorTexturesConfig(material="Lava", positions=[
+                FloorHolesAndTexturesXZConfig(x=-2, z=-2),
+                FloorHolesAndTexturesXZConfig(x=-1, z=-2),
+                FloorHolesAndTexturesXZConfig(x=-3, z=-3)
+            ])])
+
+        plotter = TopDownPlotter(
+            team="test",
+            scene_config=scene_config)
+        img = plotter.base_room_img.copy()
+        img = plotter._draw_holes(img, scene_config.holes)
+        img = plotter._draw_floor_textures(img, scene_config.floor_textures)
+        pil_img = plotter._export_plot(img)
+        pil_img.show()
+
+
+if __name__ == '__main__':
+    unittest.main()
