d24ab16a9c9d1bf1feb4e46a5d39b5086fc98b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:23 +0000

    Add package nobrainer/0.3.0/nobrainer-0.3.0.tar.gz

---
+++ packages/nobrainer/nobrainer-0.3.0.tar.gz/nobrainer-0.3.0/nobrainer/__init__.py
@@ -0,0 +1,20 @@
+from . import _version
+
+__version__ = _version.get_versions()["version"]
+
+from . import (  # noqa: F401
+    _version,
+    dataset,
+    io,
+    layers,
+    losses,
+    metrics,
+    models,
+    prediction,
+    training,
+    transform,
+    utils,
+    volume,
+)
+
+__version__ = _version.get_versions()["version"]
