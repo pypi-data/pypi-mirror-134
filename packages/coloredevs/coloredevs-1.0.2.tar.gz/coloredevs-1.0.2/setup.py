f4c1ca87831ae8d556d30c42f45225d7ba0a3e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:46 +0000

    Add package coloredevs/1.0.2/coloredevs-1.0.2.tar.gz

---
+++ packages/coloredevs/coloredevs-1.0.2.tar.gz/coloredevs-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="coloredevs",
+    version="1.0.2",
+    desciption="Mini string colorizer library by JohnLesterDev :>",
+    packages=find_packages(),
+    author="John Lester Dev :>",
+    author_email = "johnlesterincbusiness@gmail.com",
+    install_requires=[]
+)
