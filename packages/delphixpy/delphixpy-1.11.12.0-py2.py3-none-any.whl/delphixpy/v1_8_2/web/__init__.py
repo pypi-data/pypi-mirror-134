44eb0abba3cf7c1d6291d437ea4cc68edd9fb01d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:42 +0000

    Add package delphixpy/1.11.12.0/delphixpy-1.11.12.0-py2.py3-none-any.whl

---
+++ packages/delphixpy/delphixpy-1.11.12.0-py2.py3-none-any.whl/delphixpy/v1_8_2/web/__init__.py
@@ -0,0 +1,62 @@
+# coding: utf8
+#
+# Copyright 2022 by Delphix
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Package "web"
+"""
+API_VERSION = "1.8.2"
+
+from . import about
+from . import sourceconfig
+from . import connectivity
+from . import environment
+from . import job
+from . import data
+from . import policy
+from . import domain
+from . import user
+from . import maskingjob
+from . import action
+from . import group
+from . import network
+from . import snapshot
+from . import namespace
+from . import database
+from . import alert
+from . import toolkit
+from . import transformation
+from . import replication
+from . import service
+from . import analytics
+from . import session
+from . import jetstream
+from . import system
+from . import fault
+from . import role
+from . import repository
+from . import authorization
+from . import notification
+from . import capacity
+from . import delphix
+from . import registration
+from . import timezone
+from . import storage
+from . import objects
+from . import source
+from . import host
+from . import permission
+from . import timeflow
