d5e4930d327d913aa77080d32946bd83acf0bb49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:16 +0000

    Add package shakah/1.1.1/shakah-1.1.1.tar.gz

---
+++ packages/shakah/shakah-1.1.1.tar.gz/shakah-1.1.1/shakah/utils/decorators/classes.py
@@ -0,0 +1,43 @@
+class ClassPropertyDescriptor(object):
+
+    def __init__(self, fget, fset=None):
+        self.fget = fget
+        self.fset = fset
+
+    def __get__(self, obj, klass=None):
+        if klass is None:
+            klass = type(obj)
+        return self.fget.__get__(obj, klass)()
+
+    def __set__(self, obj, value):
+        if not self.fset:
+            raise AttributeError("can't set attribute")
+        type_ = type(obj)
+        return self.fset.__get__(obj, type_)(value)
+
+    def setter(self, func):
+        if not isinstance(func, (classmethod, staticmethod)):
+            func = classmethod(func)
+        self.fset = func
+        return self
+
+def classproperty(func):
+    if not isinstance(func, (classmethod, staticmethod)):
+        func = classmethod(func)
+
+    return ClassPropertyDescriptor(func)
+
+class ClassPropertyMetaClass(type):
+    def __setattr__(self, key, value):
+        if key in self.__dict__:
+            obj = self.__dict__.get(key)
+        if obj and type(obj) is ClassPropertyDescriptor:
+            return obj.__set__(self, value)
+
+        return super(ClassPropertyMetaClass, self).__setattr__(key, value)
+
+
+# Create class that decorates all functions with return type in annotation
+# class ShakahModelBuiltinMetaclass(type):
+    
+    
