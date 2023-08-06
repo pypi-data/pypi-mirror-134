ba13e6d5511c2aa9bc0c0814a72f01b533bd7fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:59 +0000

    Add package testlyh/0.0.1/testlyh-0.0.1.tar.gz

---
+++ packages/testlyh/testlyh-0.0.1.tar.gz/testlyh-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="testlyh",
+    version="0.0.1",
+    author="lyh",
+    ahthor_email="1271521082@qq.com",
+    description="this is a test",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
