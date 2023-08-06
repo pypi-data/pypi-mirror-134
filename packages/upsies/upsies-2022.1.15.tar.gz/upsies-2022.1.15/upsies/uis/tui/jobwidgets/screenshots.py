2547b22678f024774725b2ca270abba07570279c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:52 +0000

    Add package upsies/2022.1.15/upsies-2022.1.15.tar.gz

---
+++ packages/upsies/upsies-2022.1.15.tar.gz/upsies-2022.1.15/upsies/uis/tui/jobwidgets/screenshots.py
@@ -0,0 +1,32 @@
+from ....utils import cached_property, fs
+from .. import utils, widgets
+from . import JobWidgetBase
+
+import logging  # isort:skip
+_log = logging.getLogger(__name__)
+
+
+class ScreenshotsJobWidget(JobWidgetBase):
+    def setup(self):
+        content_name = fs.basename(self.job.kwargs['content_path'])
+        self._throbber = utils.Throbber(
+            format=f'Analyzing {content_name} {{throbber}}',
+            callback=self.handle_throbber_state,
+            active=True,
+        )
+        self._screenshot_progress = widgets.ProgressBar()
+        self.job.signal.register('output', self.handle_screenshot_path)
+        self.job.signal.register('finished', lambda _: self.invalidate())
+
+    def handle_throbber_state(self, state):
+        self.job.info = state
+
+    def handle_screenshot_path(self, path):
+        self._throbber.active = False
+        self.job.info = ''
+        if self.job.screenshots_total > 0:
+            self._screenshot_progress.percent = self.job.screenshots_created / self.job.screenshots_total * 100
+
+    @cached_property
+    def runtime_widget(self):
+        return self._screenshot_progress
