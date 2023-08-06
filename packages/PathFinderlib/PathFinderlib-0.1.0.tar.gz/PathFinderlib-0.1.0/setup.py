a744ec4baaee3f79f55ba44010efd761d9b57b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:02 +0000

    Add package PathFinderlib/0.1.0/PathFinderlib-0.1.0.tar.gz

---
+++ packages/PathFinderlib/PathFinderlib-0.1.0.tar.gz/PathFinderlib-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='PathFinderlib',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A package to implement Pathfinding into your games',
+    author='Me', 
+    license='MIT'
+)
