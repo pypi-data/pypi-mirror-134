6a4295e8259918d3be1a8005389085fd35356a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:00 +0000

    Add package nonebot-plugin-shindan/0.2.0/nonebot_plugin_shindan-0.2.0.tar.gz

---
+++ packages/nonebot-plugin-shindan/nonebot_plugin_shindan-0.2.0.tar.gz/nonebot_plugin_shindan-0.2.0/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nonebot_plugin_shindan']
+
+package_data = \
+{'': ['*'], 'nonebot_plugin_shindan': ['templates/*']}
+
+install_requires = \
+['Jinja2>=3.0.0,<4.0.0',
+ 'httpx>=0.19.0',
+ 'lxml>=4.6.5,<5.0.0',
+ 'nonebot-adapter-onebot>=2.0.0-beta.1,<3.0.0',
+ 'nonebot-plugin-htmlrender>=0.0.1',
+ 'nonebot2>=2.0.0-beta.1,<3.0.0',
+ 'playwright>=1.9.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'nonebot-plugin-shindan',
+    'version': '0.2.0',
+    'description': 'Nonebot2 plugin for using ShindanMaker',
+    'long_description': None,
+    'author': 'meetwq',
+    'author_email': 'meetwq@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.3,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
