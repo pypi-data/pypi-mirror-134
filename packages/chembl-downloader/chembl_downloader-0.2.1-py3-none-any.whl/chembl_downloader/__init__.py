e260b8177fe7370abb1f72dfc18298c237cf837d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:54 +0000

    Add package chembl-downloader/0.2.1/chembl_downloader-0.2.1-py3-none-any.whl

---
+++ packages/chembl-downloader/chembl_downloader-0.2.1-py3-none-any.whl/chembl_downloader/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""Download, open, and query ChEMBL through SQLite."""
+
+from .api import (  # noqa:F401
+    chemfp_load_fps,
+    connect,
+    cursor,
+    download_chemreps,
+    download_extract_sqlite,
+    download_fps,
+    download_sdf,
+    download_sqlite,
+    get_chemreps_df,
+    get_substructure_library,
+    latest,
+    query,
+    supplier,
+)
