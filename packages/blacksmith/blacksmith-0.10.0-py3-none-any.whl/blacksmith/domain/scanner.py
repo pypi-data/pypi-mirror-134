97138b0820e10e698ceaf1a2175e79e2e6cd74cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:58:14 +0000

    Add package blacksmith/0.10.0/blacksmith-0.10.0-py3-none-any.whl

---
+++ packages/blacksmith/blacksmith-0.10.0-py3-none-any.whl/blacksmith/domain/scanner.py
@@ -0,0 +1,26 @@
+"""
+Convenient method to fillout an blacksmith registry.
+
+::
+
+  import blacksmith
+  blacksmith.scan('mypkg.resources', 'other.resources')
+"""
+import importlib
+import pkgutil
+
+
+def scan(*modules: str):
+    """
+    Collect all resources to fillout the registry.
+
+    Basically, it import modules containins :func:`blacksmith.register` calls.
+
+    """
+    for mod in modules:
+        mod = importlib.import_module(mod)
+        for loader, submod, _is_pkg in pkgutil.walk_packages(
+            path=mod.__path__,
+            prefix=mod.__name__ + ".",
+        ):
+            loader.find_module(submod).load_module(submod)
