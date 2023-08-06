a17cc3dd014f9d5ed8ce0caaa11bb9c4e615ebae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:04 +0000

    Add package haohaninfo/0.2.22/haohaninfo-0.2.22-cp39-none-win32.whl

---
+++ packages/haohaninfo/haohaninfo-0.2.22-cp39-none-win32.whl/haohaninfo/__init__.py
@@ -0,0 +1,8 @@
+try:
+    from haohaninfo import GOrder
+    from haohaninfo import MicroPlay
+    from haohaninfo import MicroTest
+    from haohaninfo import MarketInfo
+    from haohaninfo import version
+except:
+    print('請確認python版本為3.7~3.9')
