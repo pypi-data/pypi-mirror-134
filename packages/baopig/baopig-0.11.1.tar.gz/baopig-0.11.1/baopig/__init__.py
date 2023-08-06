53099dbbfba0f1471b8c9aef146f0bdbcb3dba4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:47 +0000

    Add package baopig/0.11.1/baopig-0.11.1.tar.gz

---
+++ packages/baopig/baopig-0.11.1.tar.gz/baopig-0.11.1/baopig/__init__.py
@@ -0,0 +1,34 @@
+#! /Library/Frameworks/Python.framework/Versions/3.6/bin/python3.6
+
+
+"""
+Welcome to BaoPig
+
+BAOPIG : Boite A Outils Pour Interfaces Graphiques
+
+"""
+
+# TODO : compliation executable
+from baopig.version import *
+print("Hello, this is baopig version", version)
+from pygame import *
+from .pybao.issomething import *
+from .pybao.objectutilities import Object, PrefilledFunction, PackedFunctions, \
+                                         TypedDict, TypedList, TypedDeque, TypedSet
+
+from .ressources import *
+from .io import *
+from .time import *
+from ._lib import *
+from .widgets import *
+
+display = None  # protection for pygame.display
+
+__version__ = str(version)
+
+
+def debug_with_logging():
+
+    LOGGER.add_debug_filehandler()
+    LOGGER.cons_handler.setLevel(LOGGER.DEBUG)
+
