c748946759086510e9c53aa5478598b9b697e7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:58 +0000

    Add package moconf/0.0.30/moconf-0.0.30-py3-none-any.whl

---
+++ packages/moconf/moconf-0.0.30-py3-none-any.whl/moconf/__init__.py
@@ -0,0 +1,12 @@
+### This is a python package used for configuring a Morpheus appliance ###
+
+from . import (
+   appliance,
+   infra
+)
+
+# from .configs import (
+#     createMorpheusGroup,
+#     createMorpheusAwsCloud,
+#     createMorpheusGitIntegration
+# )
