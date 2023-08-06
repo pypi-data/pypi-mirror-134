cda7a50b1d32ffd36a0a7e90550490172220a043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:37 +0000

    Add package Mymacroprocessor/0.0.3/Mymacroprocessor-0.0.3.tar.gz

---
+++ packages/Mymacroprocessor/Mymacroprocessor-0.0.3.tar.gz/Mymacroprocessor-0.0.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+with open("README.md", "r") as f:
+	long_description = f.read()
+import setuptools
+setuptools.setup(name='Mymacroprocessor',
+    version='0.0.3',
+    description='python file macro processor',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='nufeng',
+    author_email='18478162@qq.com',
+    requires= ['re'],
+    url='https://github.com/nufeng1999/Mymacroprocessor/',
+    download_url='https://github.com/nufeng1999/Mymacroprocessor/releases/tag/0.0.3',
+    packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+    keywords=['python', 'macro', 'processor', 'if','ifdef','ifndef','elif','else','endif','defined','define','undef'],
+    license="apache 3.0",
+    install_requires=[],
+    include_package_data=True
+    )
