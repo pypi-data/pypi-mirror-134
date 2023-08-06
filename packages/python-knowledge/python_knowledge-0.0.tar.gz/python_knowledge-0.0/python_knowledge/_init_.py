104b72787ab3dba9d6a9b4e4c2a3b7763be586a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:05 +0000

    Add package python-knowledge/0.0/python_knowledge-0.0.tar.gz

---
+++ packages/python-knowledge/python_knowledge-0.0.tar.gz/python_knowledge-0.0/python_knowledge/_init_.py
@@ -0,0 +1,15 @@
+print('команды: help(1)')
+def help(f):
+    with open('pyp.txt',encoding='ANSI') as f:
+        line = f.readline()
+        file = open("copy.txt", "w") 
+        
+       
+        while line:
+            line = f.readline()
+            file.write(line) 
+            #print(line)
+            
+        file.close() 
+        print('В директории, где был создан .py файл, создан .txt док со шпаргалкой по питону')
+
