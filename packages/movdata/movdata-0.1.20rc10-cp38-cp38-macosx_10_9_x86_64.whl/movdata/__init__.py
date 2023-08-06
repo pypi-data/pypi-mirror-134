669edd18452cc227113e55f22e65b8b98aa08a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:14 +0000

    Add package movdata/0.1.20rc10/movdata-0.1.20rc10-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/movdata/movdata-0.1.20rc10-cp38-cp38-macosx_10_9_x86_64.whl/movdata/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+from .version import __version__
+from movdata import base, cluster, datanalyzer, dataschedule, geofence, kde_range, movingwindow, utils, etd_range
+from movdata import proximity, raster
+
+__all__ = [base, cluster, datanalyzer, dataschedule, geofence, kde_range, movingwindow,
+           utils, proximity, raster]
+
+
