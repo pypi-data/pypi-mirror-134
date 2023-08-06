12bcb34549e16371a0dfe9fff498a9da9d3dab70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:34 +0000

    Add package drf-stripe-subscription/1.1.1/drf_stripe_subscription-1.1.1-py3-none-any.whl

---
+++ packages/drf-stripe-subscription/drf_stripe_subscription-1.1.1-py3-none-any.whl/tests/api/base.py
@@ -0,0 +1,30 @@
+import json
+from pathlib import Path
+
+from django.test import TestCase
+
+from drf_stripe.stripe_api.products import stripe_api_update_products_prices
+
+
+class BaseTest(TestCase):
+    def setUp(self) -> None:
+        products = self._load_test_data("v1/stripe_product_list.json")
+        prices = self._load_test_data("v1/stripe_price_list.json")
+        stripe_api_update_products_prices(products=products, prices=prices)
+
+    def tearDown(self) -> None:
+        pass
+
+    @staticmethod
+    def _load_test_data(file_name):
+        p = Path("tests/mock_responses") / file_name
+        with open(p, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+
+        return data
+
+    @staticmethod
+    def _print(v):
+        print("$$$$$$$ DEBUG $$$$$")
+        print(v)
+        assert False
