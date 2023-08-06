b06738c531dee55408af3c565809e341fd608bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:05 +0000

    Add package docarray/0.1.7.dev5/docarray-0.1.7.dev5.tar.gz

---
+++ packages/docarray/docarray-0.1.7.dev5.tar.gz/docarray-0.1.7.dev5/docarray/document/mixins/__init__.py
@@ -0,0 +1,39 @@
+from .attribute import GetAttributesMixin
+from .audio import AudioDataMixin
+from .buffer import BufferDataMixin
+from .content import ContentPropertyMixin
+from .convert import ConvertMixin
+from .dump import UriFileMixin
+from .featurehash import FeatureHashMixin
+from .image import ImageDataMixin
+from .mesh import MeshDataMixin
+from .plot import PlotMixin
+from .porting import PortingMixin
+from .property import PropertyMixin
+from .protobuf import ProtobufMixin
+from .sugar import SingletonSugarMixin
+from .text import TextDataMixin
+from .video import VideoDataMixin
+
+
+class AllMixins(
+    ProtobufMixin,
+    PropertyMixin,
+    ContentPropertyMixin,
+    ConvertMixin,
+    AudioDataMixin,
+    ImageDataMixin,
+    TextDataMixin,
+    MeshDataMixin,
+    VideoDataMixin,
+    BufferDataMixin,
+    PlotMixin,
+    UriFileMixin,
+    SingletonSugarMixin,
+    PortingMixin,
+    FeatureHashMixin,
+    GetAttributesMixin,
+):
+    """All plugins that can be used in :class:`Document`. """
+
+    ...
