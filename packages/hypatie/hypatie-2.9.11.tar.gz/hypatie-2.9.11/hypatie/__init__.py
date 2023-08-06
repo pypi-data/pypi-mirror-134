3d61d733c37e0c5bff087bfaa5dd44b9d5c1eca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:44 +0000

    Add package hypatie/2.9.11/hypatie-2.9.11.tar.gz

---
+++ packages/hypatie/hypatie-2.9.11.tar.gz/hypatie-2.9.11/hypatie/__init__.py
@@ -0,0 +1,9 @@
+from .horizons import Vector, Observer
+from .animation import Body, play, play2d
+from .plots import star_chart, plot_xyz, plot_altaz, plot_radec, Telescope
+from .transform import radec_to_altaz, altaz_to_radec, radec_to_cartesian, to_xy_plane, rotating_coords
+from .simbad import bright_objects, get_objects, explore_region, search_region, search_sky, explore_sky
+from .catalogues import available_catalogues, Catalogue
+from .cosmology import CosModel, Planck18
+
+__version__ = "2.9.11"
