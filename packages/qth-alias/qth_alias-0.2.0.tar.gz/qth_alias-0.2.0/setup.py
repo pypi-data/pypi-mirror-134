2e5c0d653d073f8e941c1d42f1d5d613f19aaabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:03 +0000

    Add package qth-alias/0.2.0/qth_alias-0.2.0.tar.gz

---
+++ packages/qth-alias/qth_alias-0.2.0.tar.gz/qth_alias-0.2.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+with open("qth_alias/version.py", "r") as f:
+    exec(f.read())
+
+setup(
+    name="qth_alias",
+    version=__version__,
+    packages=find_packages(),
+
+    # Metadata for PyPi
+    url="https://github.com/mossblaser/qth_alias",
+    author="Jonathan Heathcote",
+    description="Utility for creating aliases of Qth properties and events.",
+    license="GPLv2",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+
+        "Intended Audience :: Developers",
+
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+    ],
+    keywords="mqtt asyncio home-automation messaging",
+
+    # Requirements
+    install_requires=["qth>=0.7.0", "qth_ls>=0.2.0"],
+
+    # Scripts
+    entry_points={
+        "console_scripts": [
+            "qth_alias = qth_alias.server:main",
+        ],
+    }
+)
