308f054b32f4a5245ec942251b19d9cc0f3bb316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:07 +0000

    Add package nonebot-plugin-petpet/0.1.20/nonebot_plugin_petpet-0.1.20.tar.gz

---
+++ packages/nonebot-plugin-petpet/nonebot_plugin_petpet-0.1.20.tar.gz/nonebot_plugin_petpet-0.1.20/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nonebot_plugin_petpet']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Pillow>=8.4.0,<9.0.0',
+ 'aiocache>=0.11.0',
+ 'httpx>=0.19.0',
+ 'imageio>=2.12.0,<3.0.0',
+ 'nonebot-adapter-cqhttp>=2.0.0-alpha.15,<2.0.0-beta.1',
+ 'nonebot2>=2.0.0-alpha.15,<2.0.0-beta.1',
+ 'numpy>=1.20.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'nonebot-plugin-petpet',
+    'version': '0.1.20',
+    'description': 'Nonebot2 plugin for making fun pictures',
+    'long_description': None,
+    'author': 'meetwq',
+    'author_email': 'meetwq@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.3,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
