e1c45c3db6ce3ac83684b28627718c86cd78612d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:20 +0000

    Add package BetterTools/0.0.4/BetterTools-0.0.4.tar.gz

---
+++ packages/BetterTools/BetterTools-0.0.4.tar.gz/BetterTools-0.0.4/BetterTools/__init__.py
@@ -0,0 +1,53 @@
+import os
+import time
+    
+"""
+    TODO:
+        ->  Bprint() with speed option
+"""
+
+
+def cls():
+    if os.name in ('nt', 'dos'):
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+def Binput(Input_message = "", Input_type = "str", error_message = "", clear = False, delay = 0,
+           func = False, **kwargs):
+    """
+    I recommend to write all arguments if you want to callback a func,
+    for the function arguments you have to assign var to values, like :
+    Binput(Input_message=..., ..., delay=..., func=function to callback, func_args="hahahahahaha", other_func_args="test")
+    """
+    try:
+        return Input_type(input(Input_message))
+    except:
+        if clear:
+            cls()
+        if error_message:
+            print(error_message)
+            if delay > 0:
+                time.sleep(delay)
+        else:
+            if delay > 0:
+                time.sleep(delay)
+            print(f"You didn't write an {Input_type.__name__} !")
+        if func:
+            func(**kwargs)
+            
+def Btype(var):
+    """
+    Like type()function but return the name only name of a var like: int, not <class 'int'>
+    """
+    return type(var).__name__
+
+# some function to make coding faster, but not more easier to understand
+def i(text):    
+    return input(text)
+
+def p(text):
+    return print(text)
+
+print(type(4))
