142ab1de668ef17bee05b6096a2d795f83cd51e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:10 +0000

    Add package bioimageio.spec/0.4.2/bioimageio.spec-0.4.2-py3-none-any.whl

---
+++ packages/bioimageio.spec/bioimageio.spec-0.4.2-py3-none-any.whl/bioimageio/spec/rdf/v0_2/converters.py
@@ -0,0 +1,13 @@
+from typing import Any, Dict
+
+
+def maybe_convert(data: Dict[str, Any]) -> Dict[str, Any]:
+    # we unofficially accept strings as author entries...
+    authors = data.get("authors")
+    if isinstance(authors, list):
+        data["authors"] = [{"name": a} if isinstance(a, str) else a for a in authors]
+
+    if data.get("format_version") in ("0.2.0", "0.2.1"):
+        data["format_version"] = "0.2.2"
+
+    return data
