7b8cdd0b953c3ef7a5a799152fbba0f1d03f7e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:29 +0000

    Add package m63/0.0.1/m63-0.0.1.tar.gz

---
+++ packages/m63/m63-0.0.1.tar.gz/m63-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+    name="m63", # Replace with your own username
+    version="0.0.1",
+    author="m63#0001",
+    author_email="jihonight@gmail.com",
+    description="m63",
+    long_description="m63",
+    long_description_content_type="text/markdown",
+    url="https://github.com/DevM63/DevM63",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
