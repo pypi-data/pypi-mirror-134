5e45ad6a99c648bcbdd7dbf2073f08e855ba846f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:38 +0000

    Add package pysyncrosim/1.0.6/pysyncrosim-1.0.6.tar.gz

---
+++ packages/pysyncrosim/pysyncrosim-1.0.6.tar.gz/pysyncrosim-1.0.6/setup.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2021 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
+# MIT License
+
+from setuptools import setup, find_packages
+from pysyncrosim._version import __version__
+
+setup(name="pysyncrosim",
+      version=__version__,
+      description="Python interface to SyncroSim",
+      author="ApexRMS",
+      author_email="info@apexrms.com",
+      url="https://www.apexrms.com",
+      packages=find_packages(exclude="tests"))
+
+install_requires=['numpy', 'pandas', 'rasterio']
+
+extras_require=['numpy', 'pandas', 'rasterio']
