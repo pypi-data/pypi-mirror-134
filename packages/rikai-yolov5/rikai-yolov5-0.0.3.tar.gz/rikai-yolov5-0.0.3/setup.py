90a26a9d2c0c124aa23102c06e9ef7bbbca416c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:14 +0000

    Add package rikai-yolov5/0.0.3/rikai-yolov5-0.0.3.tar.gz

---
+++ packages/rikai-yolov5/rikai-yolov5-0.0.3.tar.gz/rikai-yolov5-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_namespace_packages, setup
+
+setup(
+    name="rikai-yolov5",
+    version="0.0.3",
+    license="Apache License, Version 2.0",
+    author="Rikai authors",
+    author_email="rikai-dev@eto.ai",
+    url="https://github.com/da-tubi/rikai-yolov5",
+    python_requires=">=3.7",
+    install_requires=["rikai >= 0.0.20", "yolov5 >=5.0.0, <6.0.0"],
+    extras_require={
+        "dev": [
+            "black",
+            "isort",
+            # for testing
+            "pytest",
+            "mlflow",
+            "rikai-torchhub"
+        ]
+    },
+    packages=find_namespace_packages(include=["rikai.*"]),
+)
