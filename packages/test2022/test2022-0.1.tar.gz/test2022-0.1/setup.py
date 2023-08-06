b0aae8b5d8f7b6b99fa35fc9d7ac81ef6006fcbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:27 +0000

    Add package test2022/0.1/test2022-0.1.tar.gz

---
+++ packages/test2022/test2022-0.1.tar.gz/test2022-0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from setuptools import setup, find_packages
+setup(
+    name='test2022',
+    version='0.1',
+    author='Tector Pro',
+    description='Testing something',
+    packages=find_packages(),
+)
