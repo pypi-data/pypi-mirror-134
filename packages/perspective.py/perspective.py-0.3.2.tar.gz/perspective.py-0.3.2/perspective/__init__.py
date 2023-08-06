508a9c68f227b6dadb0aded363232f392b15fb9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:06 +0000

    Add package perspective.py/0.3.2/perspective.py-0.3.2.tar.gz

---
+++ packages/perspective.py/perspective.py-0.3.2.tar.gz/perspective.py-0.3.2/perspective/__init__.py
@@ -0,0 +1,16 @@
+"""
+Perspective API Wrapper
+~~~~~~~~~~~~~~~~~~~~~~~
+
+An easy-to-use API wrapper for Perspective API written in Python.
+"""
+
+__title__ = "perspective.py"
+__author__ = "Yilmaz04"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021 Yilmaz Alpaslan"
+__version__ = "0.3.2"
+
+from .main import Client
+from .attributes import Attributes
+from .utils import Utils as utils
