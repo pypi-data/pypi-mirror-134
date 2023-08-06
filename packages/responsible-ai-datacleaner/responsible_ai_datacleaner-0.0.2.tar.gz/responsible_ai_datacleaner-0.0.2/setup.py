383538199411526f5827d0ea6b2393e1a053a82d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:30 +0000

    Add package responsible-ai-datacleaner/0.0.2/responsible_ai_datacleaner-0.0.2.tar.gz

---
+++ packages/responsible-ai-datacleaner/responsible_ai_datacleaner-0.0.2.tar.gz/responsible_ai_datacleaner-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='responsible_ai_datacleaner',
+    version='0.0.2',
+    author='Sem',
+    author_email='no@example.com',
+    packages=find_packages(),
+    url='http://pypi.python.org/pypi/responsible_ai_datacleaner/',
+    license='LICENSE.txt',
+    description='Package that can clean text for the resp-ai project',
+    long_description="Package that can clean text for the resp-ai project",
+    install_requires=[
+        "nltk",
+        "preprocessor",
+        "pandas",
+        "emoji",
+    ],
+)
