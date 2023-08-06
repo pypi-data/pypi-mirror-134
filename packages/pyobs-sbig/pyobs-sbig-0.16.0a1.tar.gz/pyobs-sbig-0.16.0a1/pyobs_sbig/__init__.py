7da525ef2fdc9a157f1111f1c01317b8a72b3e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:48 +0000

    Add package pyobs-sbig/0.16.0a1/pyobs-sbig-0.16.0a1.tar.gz

---
+++ packages/pyobs-sbig/pyobs-sbig-0.16.0a1.tar.gz/pyobs-sbig-0.16.0a1/pyobs_sbig/__init__.py
@@ -0,0 +1,8 @@
+"""
+TODO: write doc
+"""
+__title__ = "SBIG camera modules"
+
+from .sbigcamera import SbigCamera
+from .sbigfiltercamera import SbigFilterCamera
+from .sbig6303e import Sbig6303eCamera
