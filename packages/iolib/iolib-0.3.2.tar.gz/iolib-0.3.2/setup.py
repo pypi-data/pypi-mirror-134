3a3336dbc75b02d893e12525adede0bcc4db16f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:05 +0000

    Add package iolib/0.3.2/iolib-0.3.2.tar.gz

---
+++ packages/iolib/iolib-0.3.2.tar.gz/iolib-0.3.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+__version__ = "0.3.2"
+__author__ = "FloatingInt"
+__doc__ = "IO library inspired by the builtin function 'input'"
+
+setup(
+    name="iolib",
+    version=__version__,
+    description=__doc__,
+    author=__author__,
+)
