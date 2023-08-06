78bb1c1c932555354d01ae9e710e814573160b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:42 +0000

    Add package ACNH-pixel-color/0.2.0/ACNH_pixel_color-0.2.0-py3-none-any.whl

---
+++ packages/ACNH-pixel-color/ACNH_pixel_color-0.2.0-py3-none-any.whl/ACNH_pixel_color/__main__.py
@@ -0,0 +1,14 @@
+
+
+from ACNH_pixel_color import ACNH_pixel_color as ACNH
+
+if __name__ == "__main__":
+  
+  lookup = ACNH.make_lookup()
+  print("Enter the lookup number of a hex code, the next line will print the values of the hue, vividness, and brightness.")
+  while True:
+    id = input()
+
+    if not id.isnumeric(): break
+
+    print(lookup.get(id,f"Values not found for {id}"))
