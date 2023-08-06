e3584ae7f21c3891bc4a6c84d0ba5c5e74932d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:06 +0000

    Add package retranslator/0.2.1/retranslator-0.2.1.tar.gz

---
+++ packages/retranslator/retranslator-0.2.1.tar.gz/retranslator-0.2.1/retranslator/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from .sub_rule import SubRule
+from .stepped_translator import SteppedTranslator
+from .translator import Translator
+from .file_translator import FileTranslator
+from .translator_cli import TranslatorCLI
+
+__version__ = "0.2.1"
+__copyright__ = "2022"
+__authors__ = ["Ethosa", "Konard"]
+__all__ = [
+    'SubRule', 'SteppedTranslator', 'Translator',
+    'FileTranslator', 'TranslatorCLI'
+]
---
+++ packages/retranslator/retranslator-0.2.1.tar.gz/retranslator-0.2.1/retranslator/stepped_translator.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from typing import NoReturn, List
+
+from regex import search, sub
+
+from .sub_rule import SubRule
+
+
+class SteppedTranslator:
+    rules: List[SubRule] = []
+    text: str = ''
+    current: int = 0
+
+    def __init__(
+        self,
+        rules: List[SubRule] = [],
+        text: str = '',
+        current: int = 0
+    ) -> NoReturn:
+        """Initializes a new SteppedTranslator
+        """
+        self.reset(rules, text, current)
+
+    def reset(
+        self,
+        rules: List[SubRule] = [],
+        text: str = '',
+        current: int = 0
+    ) -> NoReturn:
+        """Changes current properties
+        """
+        self.rules = rules
+        self.text = text
+        self.current = current
+
+    def next(
+        self
+    ) -> bool:
+        if len(self.rules) <= self.current:
+            return False
+
+        rule = self.rules[self.current]
+        replace = -1
+
+        while search(rule.match, self.text) or rule.max_repeat > replace:
+            self.text = sub(rule.match, rule.sub, self.text)
+            replace += 1
+
+        self.current += 1
+        return True
