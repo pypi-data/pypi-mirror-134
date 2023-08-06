0c635ccf4b7fd8accc2fb64c7fab000a7bc0250c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:44 +0000

    Add package pyskeleton/1.0.0/pyskeleton-1.0.0.tar.gz

---
+++ packages/pyskeleton/pyskeleton-1.0.0.tar.gz/pyskeleton-1.0.0/pyskeleton/__main__.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+
+import sys
+import argparse
+import os
+import os.path
+import tarfile
+import shutil
+from pyskeleton import __version__
+from pkg_resources import resource_filename
+
+
+class Parser(argparse.ArgumentParser):
+    def __init__(self, **kwargs):
+        super(Parser, self).__init__(**kwargs)
+
+
+DESCRIPTION = """
+will create a python module skeleton
+"""
+
+
+def main():
+    parser = Parser(description=DESCRIPTION)
+    parser.add_argument('name', help='the target proejct name')
+    parser.add_argument('-v', '--version',
+                        action='version', version=__version__)
+    args = parser.parse_args()
+    project_name = args.name
+
+    try:
+        os.mkdir(project_name)
+        os.chdir(project_name)
+    except FileExistsError as e:
+        print('file exists error')
+        sys.exit(1)
+
+    # tar unzip
+    with tarfile.open(
+            resource_filename("pyskeleton", "pyskeleton.tar.gz")) as tar:
+        tar.extractall()
+
+    # make dir
+    os.makedirs(f'{project_name}')
+
+    pyfile = resource_filename("pyskeleton", "__init__.py")
+
+    shutil.copy(pyfile, f'{project_name}')
+
+    print('great, create {0} succeed'.format(project_name))
