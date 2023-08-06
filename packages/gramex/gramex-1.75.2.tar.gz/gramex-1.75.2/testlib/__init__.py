46900ae0afd9bab9d5dff7fc62cf646a4d306f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:39 +0000

    Add package gramex/1.75.2/gramex-1.75.2.tar.gz

---
+++ packages/gramex/gramex-1.75.2.tar.gz/gramex-1.75.2/testlib/__init__.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import logging
+
+# Import from ../tests/ folder. e.g. dbutils.py for use in test_data.py, etc.
+# This is a not elegant.
+folder = os.path.dirname(os.path.abspath(__file__))
+tests_dir = os.path.normpath(os.path.join(folder, '..', 'tests'))
+sys.path.append(tests_dir)
+import dbutils                          # noqa
+from tests import remove_if_possible    # noqa
+
+# Location of the sales data file
+sales_file = os.path.join(tests_dir, 'sales.xlsx')
+
+# Turn off matplotlib's verbose debug logging
+mpl_logger = logging.getLogger('matplotlib')
+mpl_logger.setLevel(logging.WARNING)
