7f5b1a0e5f3cc50f38ac444b1a82787c856d4a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:26 +0000

    Add package botonVariocolor/0.1/botonVariocolor-0.1.tar.gz

---
+++ packages/botonVariocolor/botonVariocolor-0.1.tar.gz/botonVariocolor-0.1/README.md
@@ -0,0 +1,4 @@
+# botonVariocolor
+Puede sentirse libre de usar este componente para su disfrute personal.
+Recomiendo visitar mi página personal para encontrar la documentación de este componente:
+https://danielmera2912.github.io/
---
+++ packages/botonVariocolor/botonVariocolor-0.1.tar.gz/botonVariocolor-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+ 
+setup(
+    name='botonVariocolor',
+    packages=['botonVariocolor'],
+    version='0.1',
+    license='MIT',
+    description='Componente que varía de color un botón con una paleta de color',
+    author='danielmera2912',
+    author_email='dmersac2912@g.educaand.es',
+    url='https://github.com/danielmera2912/botonVariocolor',
+    download_url='https://github.com/danielmera2912/botonVariocolor/archive/refs/tags/0.1.tar.gz',
+    keywords='variocolor', 
+    classifiers=['Programming Language :: Python',  
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7'],
+)
