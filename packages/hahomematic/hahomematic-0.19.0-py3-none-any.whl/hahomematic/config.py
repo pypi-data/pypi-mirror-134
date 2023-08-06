594cd968b7ea2e438ce22dc82c6134420f0b44a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:33 +0000

    Add package hahomematic/0.19.0/hahomematic-0.19.0-py3-none-any.whl

---
+++ packages/hahomematic/hahomematic-0.19.0-py3-none-any.whl/hahomematic/config.py
@@ -0,0 +1,10 @@
+"""
+Global configuration parameters.
+"""
+from __future__ import annotations
+
+from hahomematic.const import DEFAULT_INIT_TIMEOUT, DEFAULT_TIMEOUT
+
+CONNECTION_CHECKER_INTERVAL = 30
+INIT_TIMEOUT = DEFAULT_INIT_TIMEOUT
+TIMEOUT = DEFAULT_TIMEOUT
