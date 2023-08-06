c62d37cd883bc98f11ca1199215b9c42e27d2b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:52 +0000

    Add package egttools/0.1.8.dev1/egttools-0.1.8.dev1-cp37-cp37m-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/egttools/egttools-0.1.8.dev1-cp37-cp37m-manylinux_2_17_i686.manylinux2014_i686.whl/egttools/plotting/__init__.py
@@ -0,0 +1,7 @@
+"""
+API reference documentation for the `plotting` submodule.
+"""
+
+from .indicators import plot_gradient
+from .indicators import draw_stationary_distribution
+from .simplex2d import Simplex2D
