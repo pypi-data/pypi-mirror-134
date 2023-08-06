a62c0400da0ef46020283a34036e9089cf6bdf2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:51 +0000

    Add package yw-hello/0.6/yw_hello-0.6.tar.gz

---
+++ packages/yw-hello/yw_hello-0.6.tar.gz/yw_hello-0.6/README.rst
@@ -0,0 +1,21 @@
+
+YwHello Project
+===============
+This is an example project that is used to 
+demonstrate how to publishPython packages on PyPI.
+
+Installing
+============
+
+.. code-block:: bash
+
+    pip install yw_hello
+
+Usage
+=====
+
+.. code-block:: bash
+
+    >>> from yw_hello.yw_hello import yw_hello
+    >>> yw_hello('Fizz Buzz')
+    Hello FizzBuzz!
---
+++ packages/yw-hello/yw_hello-0.6.tar.gz/yw_hello-0.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='yw_hello',
+    version='0.6',
+    license='MIT',
+    author="Suchin Ravi",
+    author_email='suchin.ravi@wonksknow.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://www.youngwonks.com/',
+    keywords='Youngwonks Hello',
+    install_requires=[],
+)
