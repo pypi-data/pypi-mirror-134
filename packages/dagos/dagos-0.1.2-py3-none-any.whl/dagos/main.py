29e6c6cc56c499890306141e7440fd23fa9f031c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:10 +0000

    Add package dagos/0.1.2/dagos-0.1.2-py3-none-any.whl

---
+++ packages/dagos/dagos-0.1.2-py3-none-any.whl/dagos/main.py
@@ -0,0 +1,26 @@
+import click
+import logging
+
+from dagos.commands.manage.group import manage
+from dagos.commands.wsl.group import wsl
+from rich.logging import RichHandler
+
+
+@click.group()
+@click.option(
+    "--verbose", "-v", is_flag=True, default=False, help="Enter verbose mode."
+)
+def main(verbose):
+    log_format = "{message}"
+    date_format = "%Y-%m-%d %H:%M:%S"
+    logging.basicConfig(
+        level=logging.DEBUG if verbose else logging.INFO,
+        format=log_format,
+        datefmt=date_format,
+        style="{",
+        handlers=[RichHandler(rich_tracebacks=True)],
+    )
+
+
+main.add_command(manage)
+main.add_command(wsl)
