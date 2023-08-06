624ec98b77934cb2cad51f7938aff80e61f26168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:08 +0000

    Add package bpnsdata/0.1.7/bpnsdata-0.1.7.tar.gz

---
+++ packages/bpnsdata/bpnsdata-0.1.7.tar.gz/bpnsdata-0.1.7/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(name='bpnsdata',
+                 version='0.1.7',
+                 description='Add BPNS sea test_data to a geodataframe',
+                 author='Clea Parcerisas',
+                 author_email='clea.parcerisas@vliz.be',
+                 url="https://github.com/lifewatch/bpnsdata.git",
+                 license='',
+                 packages=setuptools.find_packages(),
+                 install_requires=['geopandas', 'owslib', 'pygeos', 'rtree', 'tqdm',
+                                   'contextily', 'importlib-resources'],
+                 extras_require={
+                     "time": ["skyfield"],
+                     "griddap": ["erddapy", 'xarray']
+                 },
+                 package_data={
+                                "bpnsdata": ["data/*.*"]
+                              },
+                 zip_safe=False)
