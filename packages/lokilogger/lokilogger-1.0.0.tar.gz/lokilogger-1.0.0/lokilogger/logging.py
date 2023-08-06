240b8fd617f2ce60b1348ba5bfa00e846e237dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:03 +0000

    Add package lokilogger/1.0.0/lokilogger-1.0.0.tar.gz

---
+++ packages/lokilogger/lokilogger-1.0.0.tar.gz/lokilogger-1.0.0/lokilogger/logging.py
@@ -0,0 +1,27 @@
+import logging
+import pkg_resources
+
+def setLogMode(mode:str):
+    path = "config.ini"
+    filepath = pkg_resources.resource_filename(__name__, path)
+    logging.config.fileConfig(filepath)
+    logger = logging.getLogger()
+    prod = logger.handlers[0]
+    dev = logger.handlers[1]
+    color = logger.handlers[2]
+    mode = mode.upper()
+    if mode == "PROD":
+        logger.removeHandler(dev)
+        logger.removeHandler(color)
+        logger.info(f"Dev env is set to production")
+    elif mode == "DEV_NO_COLOR":
+        logger.removeHandler(color)
+        logger.removeHandler(prod)
+        logger.info(f"Dev env is set to development without colorlog")
+    else:
+        logger.removeHandler(prod)
+        logger.removeHandler(dev)
+        if mode!= "DEV":
+            logger.warning(f"Unrecognized mode, default to development")
+        logger.info(f"Dev env is set to development")
+        
