752445e85a20c036b028184df0f4e5720ba08aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:02 +0000

    Add package guhs-cli/2/guhs-cli-2.tar.gz

---
+++ packages/guhs-cli/guhs-cli-2.tar.gz/guhs-cli-2/src/guhs/guhs_configuration.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional, List
+
+
+@dataclass
+class Target(object):
+    order_id: int
+    name: str
+
+
+@dataclass
+class GuhsConfiguration(object):
+    installed: bool
+    targets: List[Target]
+    server: Optional[str] = None
+    boot_selection_timeout: Optional[int] = None
+    default_target: Optional[Target] = None
+
+
+class GuhsParameters(str, Enum):
+    DEFAULT_TARGET = 'default-target'
+    BOOT_SELECTION_TIMEOUT = 'boot-selection-timeout'
+
+    @classmethod
+    def list(cls):
+        return list(map(lambda parameter: parameter.value, cls))
