eb862329f91c54dbcb76eed9833fe0adcce5dfd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:00 +0000

    Add package ACNH-pixel-color/0.0.5/ACNH_pixel_color-0.0.5.tar.gz

---
+++ packages/ACNH-pixel-color/ACNH_pixel_color-0.0.5.tar.gz/ACNH_pixel_color-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="ACNH_pixel_color",
+    version="0.0.5",
+    author="Zachary Chandler",
+    author_email="zachary.c.me@gmail.com",
+    description="A program to associate keys in one text file with values in another",
+    license="MIT",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/burnish3d/ACNH_pixel_color",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
