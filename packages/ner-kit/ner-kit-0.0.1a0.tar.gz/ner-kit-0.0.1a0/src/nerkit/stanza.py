36866e661df99cb467e240e35f4aa607a9bb12dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:08 +0000

    Add package ner-kit/0.0.1a0/ner-kit-0.0.1a0.tar.gz

---
+++ packages/ner-kit/ner-kit-0.0.1a0.tar.gz/ner-kit-0.0.1a0/src/nerkit/stanza.py
@@ -0,0 +1,27 @@
+from stanza.server import CoreNLPClient, StartServer
+import stanza
+
+def get_entity_list(text,corenlp_root_path, language="chinese",memory='6G',timeout=300000):
+    stanza.install_corenlp(
+        dir=corenlp_root_path)
+
+    client = CoreNLPClient(
+        start_server=StartServer.TRY_START,
+        annotators=['tokenize', 'ssplit', 'pos', 'lemma', 'ner', 'parse', 'depparse', 'coref'],
+        timeout=timeout,
+        memory=memory,
+        properties=language,
+        # StartServer=StartServer.TRY_START
+    )
+    list_token = []
+    ann = client.annotate(text)
+    for sentence in ann.sentence:
+        for token in sentence.token:
+            print(token.value, token.pos, token.ner)
+            token_model = {
+                "value": token.value,
+                "pos": token.pos,
+                "ner": token.ner
+            }
+            list_token.append(token_model)
+    return list_token
