730570673299a7b212341583336db85a86babc33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:04 +0000

    Add package bk7084/0.1.9/bk7084-0.1.9-py3-none-any.whl

---
+++ packages/bk7084/bk7084-0.1.9-py3-none-any.whl/bk7084/geometry/__init__.py
@@ -0,0 +1,22 @@
+from typing import Union
+
+from .ray import Ray
+from .triangle import Triangle
+from .shape import Shape
+from .line import Line
+from .box import Box
+from .grid import Grid
+
+# TODO:
+#    Box
+#    Circle
+#    Cone
+#    Cylinder
+#    Dodecahedron
+#    Icosahedron
+#    Plane
+#    Polyhedron
+#    Ring
+#    Tetrahedron
+#    Torus
+#    TorusKnot
