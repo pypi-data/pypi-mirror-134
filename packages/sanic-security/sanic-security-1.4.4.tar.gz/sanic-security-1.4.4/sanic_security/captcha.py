cb6ad964f8c647d7543d25459e19736645b67bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:43 +0000

    Add package sanic-security/1.4.4/sanic-security-1.4.4.tar.gz

---
+++ packages/sanic-security/sanic-security-1.4.4.tar.gz/sanic-security-1.4.4/sanic_security/captcha.py
@@ -0,0 +1,81 @@
+import functools
+
+from sanic import Request
+
+from sanic_security.exceptions import JWTDecodeError, DeactivatedError
+from sanic_security.models import CaptchaSession, SessionFactory
+
+session_factory = SessionFactory()
+
+
+async def request_captcha(request: Request) -> CaptchaSession:
+    """
+    Creates a captcha session.
+
+    Args:
+        request (Request): Sanic request parameter.
+
+    Returns:
+        captcha_session
+    """
+    try:
+        await CaptchaSession.redeem(request)  # Deactivates previous session.
+    except JWTDecodeError or DeactivatedError:
+        pass
+    return await session_factory.get("captcha", request)
+
+
+async def captcha(request: Request) -> CaptchaSession:
+    """
+    Validates a captcha challenge attempt.
+
+    Args:
+        request (Request): Sanic request parameter. All request bodies are sent as form-data with the following arguments: captcha.
+
+    Raises:
+        DeletedError
+        ExpiredError
+        DeactivatedError
+        JWTDecodeError
+        NotFoundError
+        ChallengeError
+
+    Returns:
+        captcha_session
+    """
+    captcha_session = await CaptchaSession.decode(request)
+    captcha_session.validate()
+    await captcha_session.check_code(request, request.form.get("captcha"))
+    return captcha_session
+
+
+def requires_captcha():
+    """
+    Validates a captcha challenge attempt.
+
+    Example:
+        This method is not called directly and instead used as a decorator:
+
+            @app.post("api/captcha/attempt")
+            @requires_captcha()
+            async def on_captcha_attempt(request, captcha_session):
+                return json("Captcha attempt successful!", captcha_session.json())
+
+    Raises:
+        DeletedError
+        ExpiredError
+        DeactivatedError
+        JWTDecodeError
+        NotFoundError
+        ChallengeError
+    """
+
+    def wrapper(func):
+        @functools.wraps(func)
+        async def wrapped(request, *args, **kwargs):
+            captcha_session = await captcha(request)
+            return await func(request, captcha_session, *args, **kwargs)
+
+        return wrapped
+
+    return wrapper
