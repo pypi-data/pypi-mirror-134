f10b977eedb98a5c9f939fe16073ff97829d5c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:00 +0000

    Add package pylabs/0.0.1/pylabs-0.0.1.tar.gz

---
+++ packages/pylabs/pylabs-0.0.1.tar.gz/pylabs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pylabs',
+    packages=['pylabs'],
+    version='0.0.1',
+    license='MIT',
+    description='This class provides methodes for using PyLabs API',
+    author='Andreas Ziegler, Benjamin Dollinger',
+    author_email='mail@pylabs.de'
+    )
