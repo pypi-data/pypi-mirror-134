b655c95fbc72ece8c6f7f596b9a237413ed56f62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:48 +0000

    Add package HypixelLib/2022.1.13.1/HypixelLib-2022.1.13.1-py3-none-any.whl

---
+++ packages/HypixelLib/HypixelLib-2022.1.13.1-py3-none-any.whl/HypixelLib/exceptions.py
@@ -0,0 +1,17 @@
+
+
+
+class APIError(Exception):
+	def __init__(self, error):
+		self.error = error
+
+class InvalidAPIKeyError(Exception):
+	def __init__(self, error):
+		self.error = error
+
+
+class InvalidPlayerNameError(Exception):
+	pass
+
+class NoInputError(Exception):
+	pass
