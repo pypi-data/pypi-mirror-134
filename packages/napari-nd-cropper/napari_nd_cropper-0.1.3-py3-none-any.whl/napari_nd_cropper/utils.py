afb1fd0401b9cec6bff352b18f6323fd2a0cd960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:50 +0000

    Add package napari-nd-cropper/0.1.3/napari_nd_cropper-0.1.3-py3-none-any.whl

---
+++ packages/napari-nd-cropper/napari_nd_cropper-0.1.3-py3-none-any.whl/napari_nd_cropper/utils.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 29 09:32:34 2021
+
+@author: Marc Boucsein
+"""
+import numpy as np 
+
+ 
+
+    
+def check_np_elements(array):
+    return array.ndim and array.size
