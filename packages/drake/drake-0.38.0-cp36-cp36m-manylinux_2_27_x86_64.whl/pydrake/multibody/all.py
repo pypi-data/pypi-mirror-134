98e705e8acac151ead895a45216e292cddd42052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:59 +0000

    Add package drake/0.38.0/drake-0.38.0-cp36-cp36m-manylinux_2_27_x86_64.whl

---
+++ packages/drake/drake-0.38.0-cp36-cp36m-manylinux_2_27_x86_64.whl/pydrake/multibody/all.py
@@ -0,0 +1,12 @@
+import warnings
+
+# Normal symbols.
+from .inverse_kinematics import *  # noqa
+from .math import *  # noqa
+from .optimization import *  # noqa
+from .parsing import *  # noqa
+from .plant import *  # noqa
+from .tree import *  # noqa
+
+# Submodules.
+from .benchmarks.all import *  # noqa
