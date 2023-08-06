aea72cff21e898467a1193eb19a11e29925162be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:16 +0000

    Add package training-rg/1.0.0/training_rg-1.0.0.tar.gz

---
+++ packages/training-rg/training_rg-1.0.0.tar.gz/training_rg-1.0.0/training_rg/classifiers.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class WeekDay(Enum):
+    SUNDAY = 0
+    MONDAY = 1
+    TUESDAY = 2
+    WEDNESDAY = 3
+    THURSDAY = 4
+    FRIDAY = 5
+    SATURDAY = 6
+
+
+class KeyResp(Enum):
+    YES = 'y'
+    NO = 'n'
---
+++ packages/training-rg/training_rg-1.0.0.tar.gz/training_rg-1.0.0/training_rg/constants.py
@@ -0,0 +1,19 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+LOGGER_NAME = 'training-rg'
+
+WEEKDAYS = (
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+)
+
+SESSIONS_COUNT = 30
+SESSIONS_DAYS_OFF = 1
+SESSIONS_STEP = 0
