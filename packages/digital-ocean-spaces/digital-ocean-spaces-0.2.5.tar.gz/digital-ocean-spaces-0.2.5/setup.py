5a65407f69ae3aa59adc82a2169427689ccccc5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:35 +0000

    Add package digital-ocean-spaces/0.2.5/digital-ocean-spaces-0.2.5.tar.gz

---
+++ packages/digital-ocean-spaces/digital-ocean-spaces-0.2.5.tar.gz/digital-ocean-spaces-0.2.5/setup.py
@@ -0,0 +1,42 @@
+import os
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+install_reqs = parse_requirements('requirements.txt')
+reqs = install_reqs
+
+setup(
+    name='digital-ocean-spaces',
+    version='0.2.5',
+    author='Jody Doolittle <https://chariot-dev.com>',
+    license='MIT',
+    url='https://github.com/ChariotDev/digital-ocean-spaces',
+    description = 'An MIT licensed python client for Digital Ocean Spaces with a built in shell.',
+    long_description = open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=['spaces'],
+    package_dir={'spaces': ''},
+    package_data={'spaces': [
+        '__init__.py',
+        'client.py',
+        'LICENSE',
+        '*.template',
+        'utils/*.py',
+        "*.txt",
+        "*.md"
+        ],
+        },
+    install_requires=reqs,
+)
