cb25cd9352d6a118faed3ac2387e974901b0a74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:13 +0000

    Add package nnk/0.0.1.dev0/nnk-0.0.1.dev0-py3-none-any.whl

---
+++ packages/nnk/nnk-0.0.1.dev0-py3-none-any.whl/nnk/variant_counter/config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# filename: config.py
+
+VPURPOSE='Data Wrangle the Variants'
+VFS1='>Ref\n{}\n'
+VFS2='>{}\n{}\n'
+VFS3='./{}'
+W='w'
+VPOSCOL='Position Name (Optional)'
+VVDNACOL='Variant DNA'
+UNIQUE='Unique'
+NUNIQUE='Not Unique'
+VSNAME='Variant Sheet'
+VCCOL=['Group Position Name',
+        'Position Name (Optional)',
+        'DNA start index',
+        'DNA end index',
+        'Upstream sequence (at least 10bp)',
+        'Downstream sequence (at least 10bp)',
+        'WT DNA',
+        'Variant DNA']
