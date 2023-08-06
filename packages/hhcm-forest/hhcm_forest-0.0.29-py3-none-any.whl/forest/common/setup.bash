334df641ec243d105f05343788866c0eb08f26f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:59 +0000

    Add package hhcm-forest/0.0.29/hhcm_forest-0.0.29-py3-none-any.whl

---
+++ packages/hhcm-forest/hhcm_forest-0.0.29-py3-none-any.whl/forest/common/setup.bash
@@ -0,0 +1,8 @@
+# setup environment
+export LD_LIBRARY_PATH=£PREFIX£/lib:$LD_LIBRARY_PATH
+export CMAKE_PREFIX_PATH=£PREFIX£:$CMAKE_PREFIX_PATH
+export PATH=£PREFIX£/bin:$PATH
+export GAZEBO_PLUGIN_PATH=£PREFIX£/lib:$GAZEBO_PLUGIN_PATH
+export PYTHONPATH=£PREFIX£/lib/python2.7/dist-packages:£PREFIX£/lib/python3/dist-packages:$PYTHONPATH
+export ROS_PACKAGE_PATH=£SRCDIR£/:£PREFIX£/share:£PREFIX£/lib:$ROS_PACKAGE_PATH
+export PKG_CONFIG_PATH=£PREFIX£/lib/pkgconfig:$PKG_CONFIG_PATH
