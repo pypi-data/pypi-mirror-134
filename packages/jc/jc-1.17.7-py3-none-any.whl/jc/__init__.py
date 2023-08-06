fb88ed412c67156ab192db6443db53cebc3b00e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:34 +0000

    Add package jc/1.17.7/jc-1.17.7-py3-none-any.whl

---
+++ packages/jc/jc-1.17.7-py3-none-any.whl/jc/__init__.py
@@ -0,0 +1,76 @@
+"""JC - JSON CLI output utility
+
+* kellyjonbrazil@gmail.com
+
+This package serializes the output of many standard unix command line tools to JSON format.
+
+For documentation on each parser, see the [documentation site](https://kellyjonbrazil.github.io/jc/).
+
+CLI Example:
+
+    $ dig example.com | jc --dig -p
+    [
+      {
+        "id": 2951,
+        "opcode": "QUERY",
+        "status": "NOERROR",
+        "flags": [
+          "qr",
+          "rd",
+          "ra"
+        ],
+        "query_num": 1,
+        "answer_num": 1,
+        "authority_num": 0,
+        "additional_num": 1,
+        "opt_pseudosection": {
+          "edns": {
+            "version": 0,
+            "flags": [],
+            "udp": 4096
+          }
+        },
+        "question": {
+          "name": "example.com.",
+          "class": "IN",
+          "type": "A"
+        },
+        "answer": [
+          {
+            "name": "example.com.",
+            "class": "IN",
+            "type": "A",
+            "ttl": 39302,
+            "data": "93.184.216.34"
+          }
+        ],
+        "query_time": 49,
+        "server": "2600:1700:bab0:d40::1#53(2600:1700:bab0:d40::1)",
+        "when": "Fri Apr 16 16:05:10 PDT 2021",
+        "rcvd": 56,
+        "when_epoch": 1618614310,
+        "when_epoch_utc": null
+      }
+    ]
+
+Module Example:
+
+    >>> import jc.parsers.dig
+    >>>
+    >>> import subprocess
+    >>> import jc.parsers.dig
+    >>>
+    >>> cmd_output = subprocess.check_output(['dig', 'example.com'], text=True)
+    >>> data = jc.parsers.dig.parse(cmd_output)
+    >>>
+    >>> data
+    [{'id': 64612, 'opcode': 'QUERY', 'status': 'NOERROR', 'flags': ['qr', 'rd', 'ra'], 'query_num': 1, 'answer_num':
+    1, 'authority_num': 0, 'additional_num': 1, 'opt_pseudosection': {'edns': {'version': 0, 'flags': [], 'udp':
+    4096}}, 'question': {'name': 'example.com.', 'class': 'IN', 'type': 'A'}, 'answer': [{'name': 'example.com.',
+    'class': 'IN', 'type': 'A', 'ttl': 29658, 'data': '93.184.216.34'}], 'query_time': 52, 'server':
+    '2600:1700:bab0:d40::1#53(2600:1700:bab0:d40::1)', 'when': 'Fri Apr 16 16:13:00 PDT 2021', 'rcvd': 56,
+    'when_epoch': 1618614780, 'when_epoch_utc': None}]
+"""
+
+name = 'jc'
+__version__ = '1.17.7'
