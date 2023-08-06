54482c1c28285cbe1616ed0660c88b4fa5c5039a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:27 +0000

    Add package ohppipeline/1.1.1/ohppipeline-1.1.1-py3-none-any.whl

---
+++ packages/ohppipeline/ohppipeline-1.1.1-py3-none-any.whl/ohppipeline/utils/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf8 -*-  
+
+""" module in order to reduce image
+
+:author: Clement Hottier
+"""
+
+
+from .imageprocessing import makemasterbias, makemasterflat, processimages, crosscorrelalign, alignstack
+
+
+__author__ = "Clement Hottier, Noel Robichon"
+__all__ = [
+        "makemasterbias",
+        "makemasterflat",
+        "processimages",
+        "crosscorrelalign",
+        "alignstack",
+]
