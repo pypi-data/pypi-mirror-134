a0c7a99b3582b6fcd15f095488ddf06958f16cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:17 +0000

    Add package scipp/0.11.0/scipp-0.11.0-cp38-cp38-win_amd64.whl

---
+++ packages/scipp/scipp-0.11.0-cp38-cp38-win_amd64.whl/scipp/include/scipp/variable/math.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+/// @file
+/// @author Simon Heybrock
+#pragma once
+
+#include "scipp-variable_export.h"
+#include "scipp/variable/variable.h"
+
+#include "scipp/variable/generated_math.h"
