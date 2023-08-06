915964f88869b5778b0fb5635645ad166bbd3edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:06 +0000

    Add package Ala-package/0.0.1/Ala_package-0.0.1.tar.gz

---
+++ packages/Ala-package/Ala_package-0.0.1.tar.gz/Ala_package-0.0.1/Ala_package/__int__.py
@@ -0,0 +1,1 @@
+from file1 import swap_tow_value, sub_number, add_number
---
+++ packages/Ala-package/Ala_package-0.0.1.tar.gz/Ala_package-0.0.1/Ala_package/file1.py
@@ -0,0 +1,9 @@
+def swap_tow_value(n1, n2):
+    n2, n1 = n1, n2
+    return n1, n2
+def add_number(n1, n2, n3=0, n4=0):
+    return n1 + n2 + n3 + n4 + n4
+
+
+def sub_number(n1, n2):
+    return n1 - n2
