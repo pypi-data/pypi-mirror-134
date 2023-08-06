db8aed9186965c53522089cd7de027c37d4f995b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:16 +0000

    Add package amino.fix/2.2.0/amino.fix-2.2.0.tar.gz

---
+++ packages/amino.fix/amino.fix-2.2.0.tar.gz/amino.fix-2.2.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+requirements = ["aiohttp", "requests", "asyncio", "typing", "websocket-client==0.58.0", "setuptools", "json_minify",
+                "six", "websockets"]
+
+with open("README.md", "r") as stream:
+    long_description = stream.read()
+
+setup(
+    name="amino.fix",
+    license='MIT',
+    author="Minori",
+    version="2.2.0",
+    author_email="",
+    description="Amino 1.2.17 fix lib. Discord - https://discord.gg/Bf3dpBRJHj",
+    url="https://github.com/Minori100/Amino.fix",
+    packages=find_packages(),
+    long_description=long_description,
+    install_requires=requirements,
+    keywords=[
+        'aminoapps',
+        'amino.fix',
+        'async',
+        'amino',
+        'amino-bot',
+        'narvii',
+        'api',
+        'python',
+        'python3',
+        'python3.x',
+        'minori'
+    ],
+    python_requires='>=3.6',
+)
