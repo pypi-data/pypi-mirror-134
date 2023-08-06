13c0ee77c55b0ca56c81e50a1d4ceb4c1ca06046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:23 +0000

    Add package sqlalchemy-faker/0.0.3/sqlalchemy-faker-0.0.3.tar.gz

---
+++ packages/sqlalchemy-faker/sqlalchemy-faker-0.0.3.tar.gz/sqlalchemy-faker-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='sqlalchemy-faker',
+    version='0.0.3',
+    author='GongSakura',
+    author_email='gongskaura@yahoo.com',
+    description='a tool of sqlalchemy for generating fake data',
+    long_description=long_description,
+    url='https://github.com/GongSakura/SQLAlchemy-Faker',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
