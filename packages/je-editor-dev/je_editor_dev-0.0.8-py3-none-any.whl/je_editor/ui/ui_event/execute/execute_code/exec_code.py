875b649808a5720bf9ba6f94f6026376f33db477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:03 +0000

    Add package je-editor-dev/0.0.8/je_editor_dev-0.0.8-py3-none-any.whl

---
+++ packages/je-editor-dev/je_editor_dev-0.0.8-py3-none-any.whl/je_editor/ui/ui_event/execute/execute_code/exec_code.py
@@ -0,0 +1,16 @@
+def execute_code(current_file, save_function, exec_manager):
+    """
+    :param exec_manager:
+    :param current_file will execute file
+    :param save_function if current_file is None or "" save and run
+    """
+    if current_file is not None and current_file != "":
+        exec_manager.exec_code(current_file)
+    else:
+        save_function()
+        if current_file is not None and current_file != "":
+            exec_manager.exec_code(current_file)
+
+
+def stop_program(exec_manager):
+    exec_manager.exit_program()
