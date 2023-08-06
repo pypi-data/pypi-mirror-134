ab2a43517cae78607ce84fdf09ed1152aabed52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:54 +0000

    Add package mautrix-instagram/0.1.2/mautrix_instagram-0.1.2-py3-none-any.whl

---
+++ packages/mautrix-instagram/mautrix_instagram-0.1.2-py3-none-any.whl/mauigpapi/errors/__init__.py
@@ -0,0 +1,22 @@
+from .base import IGError
+from .mqtt import IGMQTTError, IrisSubscribeError, MQTTNotConnected, MQTTNotLoggedIn
+from .response import (
+    IGActionSpamError,
+    IGBad2FACodeError,
+    IGChallengeWrongCodeError,
+    IGCheckpointError,
+    IGInactiveUserError,
+    IGLoginBadPasswordError,
+    IGLoginError,
+    IGLoginInvalidUserError,
+    IGLoginRequiredError,
+    IGLoginTwoFactorRequiredError,
+    IGNotFoundError,
+    IGNotLoggedInError,
+    IGPrivateUserError,
+    IGRateLimitError,
+    IGResponseError,
+    IGSentryBlockError,
+    IGUserHasLoggedOutError,
+)
+from .state import IGCookieNotFoundError, IGNoCheckpointError, IGUserIDNotFoundError
