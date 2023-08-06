15bb8cb37fd6ef9e3b9dfdcd5d52107934035625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:41 +0000

    Add package kimpoet/24863.1.12/kimpoet-24863.1.12.tar.gz

---
+++ packages/kimpoet/kimpoet-24863.1.12.tar.gz/kimpoet-24863.1.12/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 10 00:24:28 2022
+
+@author: 86183
+"""
+
+from setuptools import setup
+setup(
+      name="kimpoet",
+      version="24863.01.12",
+      description="the package is used to define poets",
+      classifiers=[
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10'
+          ],
+      author="pycreaterset",
+      url='https://github.com',
+      include_package_data=False,
+      zip_safe=True,
+      )
