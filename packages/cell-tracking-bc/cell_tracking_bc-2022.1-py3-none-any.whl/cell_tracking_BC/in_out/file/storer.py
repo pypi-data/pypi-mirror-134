826e72cb5e23031594454c19c459d631548549c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:50 +0000

    Add package cell-tracking-bc/2022.1/cell_tracking_bc-2022.1-py3-none-any.whl

---
+++ packages/cell-tracking-bc/cell_tracking_bc-2022.1-py3-none-any.whl/cell_tracking_BC/in_out/file/storer.py
@@ -0,0 +1,69 @@
+# Copyright CNRS/Inria/UCA
+# Contributor(s): Eric Debreuve (since 2021)
+#
+# eric.debreuve@cnrs.fr
+#
+# This software is governed by the CeCILL  license under French law and
+# abiding by the rules of distribution of free software.  You can  use,
+# modify and/ or redistribute the software under the terms of the CeCILL
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# "http://www.cecill.info".
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+
+import pickle as pckl
+from pathlib import Path as path_t
+from typing import Any, Dict, Optional, Sequence, Union
+
+
+def SaveObjects(
+    name: Union[str, Sequence[str]],
+    value: Union[Any, Sequence[Any]],
+    where: Union[str, path_t],
+    /,
+) -> None:
+    """"""
+    if isinstance(name, str):
+        names = (name,)
+        values = (value,)
+    else:
+        names = name
+        values = value
+
+    data = {_nme: _val for _nme, _val in zip(names, values)}
+    with open(where, "wb") as accessor:
+        pckl.dump(data, accessor)
+
+
+def LoadObjects(
+    where: Union[str, path_t], /, *, context: Dict[str, Any] = None
+) -> Optional[Dict[str, Any]]:
+    """
+    context: Use globals() typically
+    """
+    with open(where, "rb") as accessor:
+        data = pckl.load(accessor)
+
+    if context is None:
+        return data
+
+    for name, value in data.items():
+        context[name] = value
