dc55667771f9b7c521ae36f9b73ac3fc750e5979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:58 +0000

    Add package hack-anything/0.1.1/hack-anything-0.1.1.tar.gz

---
+++ packages/hack-anything/hack-anything-0.1.1.tar.gz/hack-anything-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hack_anything']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['tqdm>=4.62.3,<5.0.0']
+
+setup_kwargs = {
+    'name': 'hack-anything',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '\n# hack_anything\n\nAn efficient and robust library to hack anything.\n\n\n## Installation\n\nInstall project with pip\n\n```bash\n  pip install hack_anything  \n```\n    \n## Usage/Examples\n\n```python\nfrom hack_anything import hack_tool\n\nhack_tool.hack("Universal.Co")\n\n```\n\n',
+    'author': 'Praneeth',
+    'author_email': 'spraneeth4@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
