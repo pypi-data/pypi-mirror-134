82776768b353729b3fe4ba45cec4d354ebe2f39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:32 +0000

    Add package cambank/2.3.8/cambank-2.3.8.tar.gz

---
+++ packages/cambank/cambank-2.3.8.tar.gz/cambank-2.3.8/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(name='cambank', author='Camille Huot', author_email='noemail@noemail.com',
+  version = '2.3.8', url = 'https://gitlab.com/cambank',
+  description='CamBank is a personal finance web application',
+  packages = ['cambank'], package_dir= { 'cambank': 'cambank' }
+ )
