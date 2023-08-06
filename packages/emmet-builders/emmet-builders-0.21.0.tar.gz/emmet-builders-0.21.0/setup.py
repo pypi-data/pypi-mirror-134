a13d59e9184ba8644f4442957e9e71ec05366c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:54 +0000

    Add package emmet-builders/0.21.0/emmet-builders-0.21.0.tar.gz

---
+++ packages/emmet-builders/emmet-builders-0.21.0.tar.gz/emmet-builders-0.21.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_namespace_packages, setup
+from setuptools_scm import get_version
+
+version = get_version(root="..", relative_to=__file__, version_scheme="post-release")
+version = version.split(".post")[0]
+
+
+setup(
+    name="emmet-builders",
+    use_scm_version={"root": "..", "relative_to": __file__},
+    setup_requires=["setuptools_scm"],
+    description="Builders for the Emmet Library",
+    author="The Materials Project",
+    author_email="feedback@materialsproject.org",
+    url="https://github.com/materialsproject/emmet",
+    packages=find_namespace_packages(include=["emmet.*"]),
+    install_requires=[f"emmet-core~={version}", "maggma>=0.38.1"],
+    license="modified BSD",
+    zip_safe=False,
+)
