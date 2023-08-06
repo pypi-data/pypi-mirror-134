b1400cffa19fe6b41b1a08c12840d957b26ce7ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:31 +0000

    Add package ShynaTermux/0.7/ShynaTermux-0.7.tar.gz

---
+++ packages/ShynaTermux/ShynaTermux-0.7.tar.gz/ShynaTermux-0.7/ShynaTermux/Shyna_speak_sentence.py
@@ -0,0 +1,21 @@
+from ShynaDatabase import Shdatabase
+from ShynaProcess import Shprocess
+import os
+
+
+class ShynaSpeakOnTermux:
+    """
+    Get sentences from database and speak them as needed
+    """
+    s_data = Shdatabase.ShynaDatabase()
+    s_process = Shprocess.ShynaSpeak()
+
+    def get_sentences(self):
+        try:
+            pass
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    ShynaSpeakOnTermux().get_sentences()
