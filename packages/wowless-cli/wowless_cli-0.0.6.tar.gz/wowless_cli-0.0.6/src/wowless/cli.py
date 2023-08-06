68eb19f6fc5c79528177adfb1e367e3b9d6cff27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:09 +0000

    Add package wowless-cli/0.0.6/wowless_cli-0.0.6.tar.gz

---
+++ packages/wowless-cli/wowless_cli-0.0.6.tar.gz/wowless_cli-0.0.6/src/wowless/cli.py
@@ -0,0 +1,57 @@
+import click
+from progress.spinner import Spinner
+import requests
+import time
+from base64 import urlsafe_b64encode
+import sys
+
+url = "https://wowless.dev/api/v1/run"
+
+
+@click.command()
+@click.option(
+    "--product",
+    "-p",
+    required=True,
+    type=click.Choice(
+        [
+            "wow",
+            "wowt",
+            "wow_classic",
+            "wow_classic_ptr",
+            "wow_classic_era",
+            "wow_classic_era_ptr",
+        ]
+    ),
+)
+@click.argument("filename")
+def run(product, filename):
+    with open(filename, "rb") as f:
+        r = requests.post(
+            url,
+            json={
+                "products": [product],
+                "zip": urlsafe_b64encode(f.read()).decode("ascii"),
+            },
+        )
+    r.raise_for_status()
+    runid = r.json()[product]
+    wait = 1.0
+    for _ in Spinner(f"waiting for runid {runid} to complete... ").iter(
+        range(400)
+    ):
+        wait = wait * 1.1
+        time.sleep(wait)
+        r = requests.get(url, params={"runid": runid})
+        r.raise_for_status()
+        j = r.json()
+        if "status" in j and j["status"] == "done":
+            for k, v in j["rawlogs"].items():
+                print(k)
+                print(v, end="")
+            return
+    print("task never finished", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    run()
