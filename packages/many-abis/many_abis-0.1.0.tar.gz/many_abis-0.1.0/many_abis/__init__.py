a1011a918d205a7304ab65c23efe79e12ea03972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:16 +0000

    Add package many-abis/0.1.0/many_abis-0.1.0.tar.gz

---
+++ packages/many-abis/many_abis-0.1.0.tar.gz/many_abis-0.1.0/many_abis/__init__.py
@@ -0,0 +1,5 @@
+from .chains import *
+from .utils import *
+from .utils.enum.chain_rpc_enum import CHAIN_RPC_API
+from .utils.enum.chain_scan_enum import CHAIN_SCAN_API
+from .utils.enum.contract_api_enum import CHAIN_CONTRACT_API
