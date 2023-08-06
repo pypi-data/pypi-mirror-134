e538e7e95b663603f5d8abac3af4ce3b07ed681d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:55 +0000

    Add package emmental/0.1.1/emmental-0.1.1-py3-none-any.whl

---
+++ packages/emmental/emmental-0.1.1-py3-none-any.whl/emmental/contrib/slicing/modules/utils.py
@@ -0,0 +1,42 @@
+"""Slicing modules."""
+from typing import Any, Dict, Optional
+
+from torch import Tensor
+from torch.nn import functional as F
+
+
+def ce_loss(
+    module_name: str,
+    intermediate_output_dict: Dict[str, Any],
+    Y: Tensor,
+    weight: Optional[Tensor] = None,
+) -> Tensor:
+    """Cross entropy loss.
+
+    Args:
+      module_name: Module name.
+      intermediate_output_dict: output dict.
+      Y: Gold lables.
+      weight: Class weights.
+
+    Returns:
+      Loss.
+    """
+    return F.cross_entropy(
+        intermediate_output_dict[module_name][0],
+        Y.view(-1) - 1,
+        weight,
+    )
+
+
+def output(module_name: str, intermediate_output_dict: Dict[str, Any]) -> Tensor:
+    """Output function.
+
+    Args:
+      module_name: Module name.
+      intermediate_output_dict: output dict.
+
+    Returns:
+      Output.
+    """
+    return F.softmax(intermediate_output_dict[module_name][0], dim=1)
---
+++ packages/emmental/emmental-0.1.1-py3-none-any.whl/emmental/logging/wandb_writer.py
@@ -0,0 +1,40 @@
+"""Emmental tensor board writer."""
+import copy
+from typing import Dict, Union
+
+import wandb
+
+from emmental.logging.log_writer import LogWriter
+from emmental.meta import Meta
+from emmental.utils.utils import convert_to_serializable_json
+
+
+class WandbWriter(LogWriter):
+    """A class for logging to wandb during training process."""
+
+    def __init__(self) -> None:
+        """Initialize TensorBoardWriter."""
+        super().__init__()
+
+        # Set up wandb summary writer and save config
+        wandb.init(
+            project=Meta.config["logging_config"]["writer_config"][
+                "wandb_project_name"
+            ],
+            group=Meta.config["logging_config"]["writer_config"]["wandb_group_name"],
+            name=Meta.config["logging_config"]["writer_config"]["wandb_run_name"],
+            config=convert_to_serializable_json(copy.deepcopy(Meta.config)),
+        )
+
+        self.write_config()
+
+    def add_scalar_dict(
+        self, metric_dict: Dict[str, Union[float, int]], step: Union[float, int]
+    ) -> None:
+        """Log a scalar variable.
+
+        Args:
+          metric_dict: The metric dict.
+          step: The current step.
+        """
+        wandb.log(metric_dict, step=step)
