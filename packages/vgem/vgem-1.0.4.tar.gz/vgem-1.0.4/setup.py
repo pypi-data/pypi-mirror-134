bc3d19c77b6facf0aa63112a9f7c0da1a113b8d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:41 +0000

    Add package vgem/1.0.4/vgem-1.0.4.tar.gz

---
+++ packages/vgem/vgem-1.0.4.tar.gz/vgem-1.0.4/setup.py
@@ -0,0 +1,39 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+VERSION = "1.0.4"
+DESCRIPTION="VerseGroups encryption manager class (RSA and AES sessions through RSA) for secure transmission of data"
+KEYWORDS=['']
+
+setup(
+    name="vgem",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/VerseGroup/vgem-python",
+    author="VERSEGROUPLLC",
+    author_email="officialversegroupllc@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    include_package_data=True,
+    python_requires='>=3.6',    
+    install_requires=["cryptography", "pycparser", "cffi"],
+    packages=['vgem'],
+    keywords=KEYWORDS
+)
