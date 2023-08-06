b9739f617bf77840f6b2b7803bbfab2352dba5f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:20 +0000

    Add package Image-Composer/0.0.19/Image_Composer-0.0.19-py3-none-any.whl

---
+++ packages/Image-Composer/Image_Composer-0.0.19-py3-none-any.whl/Image_Composer/__init__.py
@@ -0,0 +1,12 @@
+from .image_compose import napari_experimental_provide_dock_widget
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+
+def get_module_version():
+    return __version__
+
+
