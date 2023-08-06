2eb73341f1affb251799cd874a8695a0fef6bc81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:59 +0000

    Add package dreye-ext/0.0.1/dreye_ext-0.0.1-py3-none-any.whl

---
+++ packages/dreye-ext/dreye_ext-0.0.1-py3-none-any.whl/dreye/__init__.py
@@ -0,0 +1,16 @@
+"""
+*dreye* (drosophila eye) is a *Python* photoreceptor and (color) vision
+package implementing various photoreceptor and (color) vision models and
+algorithms, mainly for drosophila.
+Part of the code base is inspired by the Colour GitHub project
+(https://github.com/colour-science/colour; BSD-3-Clause license),
+which is a package implementing color vision models and algorithms for human
+observers.
+"""
+
+__author__ = """gucky92"""
+__email__ = 'gucky@gucky.eu'
+__version__ = '0.0.1'
+
+import os
+DREYE_DIR = os.path.dirname(__file__)
