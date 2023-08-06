60e8e2ae15f14ce5379a4d5e847bf661f6f97810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:46 +0000

    Add package prelims/0.0.4/prelims-0.0.4.tar.gz

---
+++ packages/prelims/prelims-0.0.4.tar.gz/prelims-0.0.4/prelims/processor/__init__.py
@@ -0,0 +1,8 @@
+from .base import BaseFrontMatterProcessor
+from .og_file_path_extractor import OpenGraphFilePathExtractor
+from .recommender import Recommender
+
+
+__all__ = ['BaseFrontMatterProcessor',
+           'OpenGraphFilePathExtractor',
+           'Recommender']
