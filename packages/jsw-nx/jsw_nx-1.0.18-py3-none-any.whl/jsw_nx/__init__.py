d6b5cae7efc46d11aac8b96d806822c93ac52572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:55 +0000

    Add package jsw-nx/1.0.18/jsw_nx-1.0.18-py3-none-any.whl

---
+++ packages/jsw-nx/jsw_nx-1.0.18-py3-none-any.whl/jsw_nx/__init__.py
@@ -0,0 +1,31 @@
+__version__ = '1.0.6'
+
+# next base
+from jsw_nx.base.filter import filter
+from jsw_nx.base.find import find
+from jsw_nx.base.find_index import find_index
+from jsw_nx.base.foreach import foreach
+from jsw_nx.base.forin import forin
+from jsw_nx.base.get import get
+from jsw_nx.base.includes import includes
+from jsw_nx.base.index import index
+from jsw_nx.base.map import map
+from jsw_nx.base.mix import mix
+from jsw_nx.base.reduce import reduce
+from jsw_nx.base.set import set
+
+
+# ruby style
+from jsw_nx.rubify.times import times
+from jsw_nx.rubify.to_a import to_a
+from jsw_nx.rubify.to_b import to_b
+from jsw_nx.rubify.to_f import to_f
+from jsw_nx.rubify.to_i import to_i
+from jsw_nx.rubify.to_n import to_n
+from jsw_nx.rubify.to_s import to_s
+
+# next packages
+from jsw_nx.packages.days import days
+
+# next classes
+from jsw_nx.classes.date import Date
