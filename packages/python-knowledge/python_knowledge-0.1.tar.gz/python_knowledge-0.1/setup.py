15d8eaf29a2fa8348113701c23b49477a3afae19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:08 +0000

    Add package python-knowledge/0.1/python_knowledge-0.1.tar.gz

---
+++ packages/python-knowledge/python_knowledge-0.1.tar.gz/python_knowledge-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='python_knowledge',
+      version='0.1',
+      description='knowledge about python for students',
+      packages=['python_knowledge'],
+      author_email='bloodruslan@gmail.com',
+      zip_safe=False)
