857f06906363e849f716f772e9e064a835fa38d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:45 +0000

    Add package myPythonLibrary/2022.1.13/myPythonLibrary-2022.1.13.tar.gz

---
+++ packages/myPythonLibrary/myPythonLibrary-2022.1.13.tar.gz/myPythonLibrary-2022.1.13/setup.py
@@ -0,0 +1,40 @@
+import datetime
+import os
+import setuptools
+
+# version = os.environ['CI_COMMIT_TAG']
+version = datetime.date.today().strftime("%Y.%m.%d")
+
+setuptools.setup(
+    name="myPythonLibrary",
+    version=version,
+    author="Martin Genet",
+    author_email="martin.genet@polytechnique.edu",
+    description=open("README.md", "r").readlines()[1][:-1],
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://gitlab.inria.fr/mgenet/myPythonLibrary",
+    packages=["myPythonLibrary"],
+    license="GPLv3",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            '__init__.py.py = __init__.py.py:main',
+        ],
+    },
+    install_requires=['numpy'],
+    # python_requires='>=3.6',
+)
+
+# keyring set https://upload.pypi.org/legacy martin.genet
+# keyring set https://upload.pypi.org martin.genet
+
+# python setup.py sdist bdist_wheel
+
+# twine upload --skip-existing dist/*
+
+# twine upload --skip-existing --repository-url https://gitlab.inria.fr/api/v4/projects/myPythonLibrary/packages/pypi dist/*
