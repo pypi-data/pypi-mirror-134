3cbd1037bc9b8373754c6ed8aea0180d875c3e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:25 +0000

    Add package jupyterlab-infinstor/2.0.11/jupyterlab_infinstor-2.0.11-py3-none-any.whl

---
+++ packages/jupyterlab-infinstor/jupyterlab_infinstor-2.0.11-py3-none-any.whl/jupyterlab_infinstor/servicedefs.py
@@ -0,0 +1,14 @@
+import builtins
+
+builtins.service = None
+builtins.clientid = None
+builtins.prodcode = ''
+builtins.accesstoken = None
+builtins.refreshtoken = None
+builtins.infinStorAccessKeyId = None
+builtins.infinStorSecretAccessKey = None
+builtins.cognito_username = None
+builtins.cognito_password = None
+builtins.s3_resource = None
+builtins.log = None
+builtins.infinstor_time_spec = None
