5ef26d59fbd8fd8a6ebd9c9b21654b1fa222c45f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:17 +0000

    Add package tbcp-devops/1.5.1/tbcp_devops-1.5.1.tar.gz

---
+++ packages/tbcp-devops/tbcp_devops-1.5.1.tar.gz/tbcp_devops-1.5.1/tbcp_devops/pkgmgmt/_defaults.py
@@ -0,0 +1,6 @@
+"""Provides various constants"""
+
+DEF_COMMIT_TYPES_ADDED = ["feat"]
+DEF_COMMIT_TYPES_FIXED = ["fix"]
+DEF_COMMIT_TYPES_CHANGED = ["refactor", "perf", "revert"]
+DEF_COMMIT_TYPES_MAINTAINED = ["build", "chore", "ci", "docs", "style", "test"]
