bddb4b50a4b6fa3d3776853e71a3d36cd4f7ff0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:28 +0000

    Add package dwj-tools/0.0.6/dwj_tools-0.0.6.tar.gz

---
+++ packages/dwj-tools/dwj_tools-0.0.6.tar.gz/dwj_tools-0.0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dwj_tools', # 项目的名称,pip3 install get-time
+    version='0.0.6', # 项目版本 
+    author='丁文杰', # 项目作者 
+    author_email='359582058@qq.com', # 作者email 
+    url='https://github.com/buwu-DWJ/strategy', # 项目代码仓库
+    description='私人', # 项目描述 
+    packages=find_packages(), # 包名 
+    install_requires=[],
+    include_package_data=True,
+    entry_points={} # 重点
+)
+
+# 1.先更新版本号
+# 2.生成新上传代码
+# python setup.py sdist build
+# 3.推送
+# twine upload dist/*
