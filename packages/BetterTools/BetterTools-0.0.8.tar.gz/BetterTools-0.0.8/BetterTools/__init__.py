de27580f8e0e638330725ae119d90c8a6be8207b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:36 +0000

    Add package BetterTools/0.0.8/BetterTools-0.0.8.tar.gz

---
+++ packages/BetterTools/BetterTools-0.0.8.tar.gz/BetterTools-0.0.8/BetterTools/__init__.py
@@ -0,0 +1,73 @@
+import os
+import time
+"""
+    TODO:
+        ->  Bprint() with speed option
+        ->  function to setup arguments that will always be the same for a function
+        ->  Update docs
+"""
+
+
+def cls():
+    """
+    Function to clear console, work on Windows, MacOS and some other
+    """
+    if os.name in ('nt', 'dos'):
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+def Binput(message="", Input_type=str, error_message="", clear=False, delay=0,
+           func=False, **kwargs):
+    """
+    I recommend to write all arguments if you want to callback a func,
+    for the function arguments you have to assign var to values, like :
+    Binput(message=..., ..., delay=..., func=function to callback, func_args="hahahahahaha", other_func_args="test")
+    For the Input_type and func argument, you have to write them without ""
+    """
+    try:
+        return Input_type(input(message))
+    except:
+        if clear:
+            cls()
+            
+        if error_message:
+            print(error_message)
+            if delay > 0:
+                time.sleep(delay)
+        else:
+            if delay > 0:
+                time.sleep(delay)
+            print(f"You didn't write an {type(Input_type).__name__} !")
+            
+        if func:
+            func(**kwargs)
+            
+def Btype(var, print=False):
+    """
+    Like type()function but return the name only name of a var like: int, not <class 'int'>
+    But can print automatically, or return
+    """
+    if print:
+        print(type(var).__name__)
+    else:
+        return type(var).__name__
+
+# some function to make coding faster, but not more easier to understand
+def i(text):    
+    return input(text)
+
+def p(*objects, sep=' ', end='\n', flush=False):
+    print(*objects, sep=sep, end=end, flush=flush)
+
+def loading(percentage: int):
+    if percentage > 100 or percentage < 0:
+        print(f"Percentage need to be between 0 and 100 !")
+    else:
+        load = ""
+        for _ in range(int(percentage/10)):
+            load += "██"
+        for _ in range(20-len(load)):
+            load += " "
+        print(f"| {load} | {percentage}%", end="\r")
