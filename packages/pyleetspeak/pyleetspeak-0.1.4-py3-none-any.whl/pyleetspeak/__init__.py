ae5eee1b2387d5517765afef9f69a1e0b8c85c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:51 +0000

    Add package pyleetspeak/0.1.4/pyleetspeak-0.1.4-py3-none-any.whl

---
+++ packages/pyleetspeak/pyleetspeak-0.1.4-py3-none-any.whl/pyleetspeak/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.1.4"
+__organization__ = "AIDA (http://aida.etsisi.upm.es/)"
+from .LeetSpeaker import LeetSpeaker, PuntctuationCamouflage, InversionCamouflage
+from .modes import *
