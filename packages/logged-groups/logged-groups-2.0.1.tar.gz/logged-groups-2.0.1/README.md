d9c49659f0d9e5a3846bc113dc181aa3f3548e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:51 +0000

    Add package logged-groups/2.0.1/logged-groups-2.0.1.tar.gz

---
+++ packages/logged-groups/logged-groups-2.0.1.tar.gz/logged-groups-2.0.1/README.md
@@ -0,0 +1,124 @@
+# Python logged groups
+
+This package designed to facilitate logging inside classes and functions with `@logged_group` decorator and also makes it possible
+to manage logging level and any different things for any logging domain mentioned in log_cfg.json
+
+This package maintained on [python-logged-groups github repsitory](https://github.com/rozhkovdmitrii/python-logged-groups) by rozhkovdmitrii<rozhkovdmitrii@yandex.ru> 
+
+## Build package
+
+To build package `dist/logged-based-class-1.0.tar.gz`, for example, use following command:
+
+```
+python setup.py sdist
+```
+
+## Installation 
+
+Then you can install package with the following:
+
+```
+pip install logged-based-class-1.0.tar.gz
+```
+
+## Config file
+
+There should be `log_cfg.json` in where you use `logged_based_class`. It can look like:
+
+```json
+{
+  "format": null,
+  "colored": true,
+  "logged_groups": {
+    "log_group": "DEBUG",
+    "other_log_group": "DEBUG"
+  }
+}
+```
+
+##### format
+
+You can replace format string with your own. There also support extra logging attributes in addition to [standard ones](https://docs.python.org/3/library/logging.html#logrecord-attributes)
+
+  * class - decorated with `logged_group` class name
+  * class_id - class id set by `class_id` argument passed into constructor
+
+```
+{
+  "format": "%(asctime)23s %(levelname)8s %(process)6d:%(threadName)-10s %(class)15s:%(class_id)-8s %(message)s",
+  "colored": true,
+  ...
+}
+```
+
+##### colored
+
+Optional. Default - `false`. If set output well be colored
+
+##### propagation
+
+Optional. Default - `false`. If set log messages will be propagated into parent logger if that are exists
+
+##### logged_groups
+
+Each pair in this dict is definition of logging group and level related to it.
+So it gets possible to reduce logging level for classes related to this group and decorated with `@logged_group` in appropriate way in sources
+
+
+## Using example
+
+```python
+import threading
+
+from logged_groups import logged_group
+
+
+@logged_group("log_group")
+class A:
+
+    def __init__(self, **kws):
+        self.info("Some information, keep focused on it")
+
+    def do_stuff(self, value: int):
+        try:
+            self._do_stuff_impl(value)
+        except BaseException as e:
+            self.error(f"Failed to do stuff because of error: {e}")
+
+    def _do_stuff_impl(self, value):
+        self.debug(f"Doing stuff with value: {value}")
+        if value > 1000:
+            raise Exception(f"Value critically high: {value}")
+
+        if value > 100:
+            self.warning(f"Value higher than 100, it can be wrong: {value}")
+
+
+@logged_group("other_log_group")
+class B:
+
+    def __init__(self):
+        self.debug("Spam spam spam spam spam ")
+        self.debug("Spam spam spam spam spam ")
+        self.debug("Spam spam spam spam spam ")
+        self.error("Spam spam spam spam spam ")
+        self.debug("Spam spam spam spam spam ")
+        self.debug("Spam spam spam spam spam ")
+        self.debug("Spam spam spam spam spam ")
+        self.debug("Spam spam spam spam spam ")
+
+
+if __name__ == "__main__":
+    threading.current_thread().name = "main"
+    a1 = A(class_id="1")
+    a1.do_stuff(100)
+    a1.do_stuff(101)
+    a1.do_stuff(1001)
+
+    b = B()
+
+    a2 = A(class_id="2")
+    a2.do_stuff(100)
+    a2.do_stuff(101)
+    a2.do_stuff(1001)
+```
---
+++ packages/logged-groups/logged-groups-2.0.1.tar.gz/logged-groups-2.0.1/logged_groups/logging.py
@@ -0,0 +1,97 @@
+import json
+import logging
+import logging.config
+from types import FunctionType
+
+import singleton_decorator
+
+
+class ColoredFormatter(logging.Formatter):
+    """Logging Formatter to add colors and count warning / errors"""
+
+    def __init__(self, fmt_str: str, *args, **kwargs):
+        super().__init__()
+        green = "\x1b[32m"
+        grey = "\x1b[38m"
+        yellow = "\x1b[33m"
+        red = "\x1b[31m"
+        bold_red = "\x1b[31;1m"
+        reset = "\x1b[0m"
+        logged_fmt = fmt_str
+        self.FORMATS = {
+            logging.DEBUG: grey + logged_fmt + reset,
+            logging.INFO: green + logged_fmt + reset,
+            logging.WARNING: yellow + logged_fmt + reset,
+            logging.ERROR: red + logged_fmt + reset,
+            logging.CRITICAL: bold_red + logged_fmt + reset
+        }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+@singleton_decorator.singleton
+class LogMng:
+
+    def __init__(self):
+        self.is_init: bool = False
+
+    def init_from_file(self, log_cfg: str = "log_cfg.json"):
+        try:
+            self._init_from_file_impl(log_cfg)
+        except FileNotFoundError:
+            logging.getLogger().error(f"Failed to configure logged_groups package, file not found: {log_cfg}")
+        except json.JSONDecodeError as e:
+            logging.getLogger().error(f"Failed decode logged_groups package from {log_cfg}, error: {e}")
+
+    def _init_from_file_impl(self, log_cfg: str):
+        with open(log_cfg) as log_cfg_file:
+            log_cfg_data = json.load(log_cfg_file)
+            logging.config.dictConfig(log_cfg_data)
+        self.is_init = True
+
+
+def logged_group(logged_group: str):
+    """Designed to provide methods: debug, info, warning, error and critical inside decorated class in logger_group"""
+    log_mng = LogMng()
+    if not log_mng.is_init:
+        log_mng.init_from_file()
+
+    logger = logging.getLogger(logged_group)
+    if len(logger.handlers) == 0:
+        logger.setLevel(logging.CRITICAL)
+
+    def class_wrapper(original_class):
+        orig_init = original_class.__init__
+
+        def __init__(self, *args, **kws):
+            self._class_id = kws.get("class_id", "")
+            self._logger = logging.LoggerAdapter(logger,
+                                                 {"class": original_class.__name__, "class_id": self._class_id})
+            self.debug = self._logger.debug
+            self.info = self._logger.info
+            self.error = self._logger.error
+            self.critical = self._logger.critical
+            self.warning = self._logger.warning
+
+            orig_init(self, *args, **kws)
+
+        original_class.__init__ = __init__
+        return original_class
+
+    def function_wrapper(original_function):
+        _logger = logging.LoggerAdapter(logging.getLogger(logged_group),
+                                        {"class": original_function.__name__, "class_id": ""})
+
+        def inner_wrapper(*args, **kwargs):
+            kwargs.update({"logger": _logger})
+            return original_function(*args, **kwargs)
+
+        return inner_wrapper
+
+    def wrapper(entity):
+        return function_wrapper(entity) if isinstance(entity, FunctionType) else class_wrapper(entity)
+
+    return wrapper
