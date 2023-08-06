a7201a227c4958323ffa20ab882e5d4987127b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:21 +0000

    Add package pymobiledevice3/1.16.4/pymobiledevice3-1.16.4-py3-none-any.whl

---
+++ packages/pymobiledevice3/pymobiledevice3-1.16.4-py3-none-any.whl/pymobiledevice3/cli/notification.py
@@ -0,0 +1,54 @@
+import logging
+
+import click
+
+from pymobiledevice3.cli.cli_common import Command
+from pymobiledevice3.lockdown import LockdownClient
+from pymobiledevice3.resources.firmware_notifications import get_notifications
+from pymobiledevice3.services.notification_proxy import NotificationProxyService
+
+logger = logging.getLogger(__name__)
+
+
+@click.group()
+def cli():
+    """ notification options """
+    pass
+
+
+@cli.group()
+def notification():
+    """ notification options """
+    pass
+
+
+@notification.command(cls=Command)
+@click.argument('names', nargs=-1)
+def post(lockdown: LockdownClient, names):
+    """ API for notify_post(). """
+    service = NotificationProxyService(lockdown=lockdown)
+    for name in names:
+        service.notify_post(name)
+
+
+@notification.command(cls=Command)
+@click.argument('names', nargs=-1)
+def observe(lockdown: LockdownClient, names):
+    """ API for notify_register_dispatch(). """
+    service = NotificationProxyService(lockdown=lockdown)
+    for name in names:
+        service.notify_register_dispatch(name)
+
+    for event in service.receive_notification():
+        logger.info(event)
+
+
+@notification.command('observe-all', cls=Command)
+def observe_all(lockdown: LockdownClient):
+    """ attempt to observe all builtin firmware notifications. """
+    service = NotificationProxyService(lockdown=lockdown)
+    for notification in get_notifications():
+        service.notify_register_dispatch(notification)
+
+    for event in service.receive_notification():
+        logger.info(event)
