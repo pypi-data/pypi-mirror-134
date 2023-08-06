b6ba49e3eabfca4bf8c52d134f1dce976ff161db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:45 +0000

    Add package lordmatrixoid-module/1.0/lordmatrixoid_module-1.0.tar.gz

---
+++ packages/lordmatrixoid-module/lordmatrixoid_module-1.0.tar.gz/lordmatrixoid_module-1.0/lordmatrixoid_module/__init__.py
@@ -0,0 +1,7 @@
+"""
+lordmatrixoid_module
+An example python library.
+"""
+
+__version__ = "1.0"
+__author__ = 'Lord Matrixoid'
---
+++ packages/lordmatrixoid-module/lordmatrixoid_module-1.0.tar.gz/lordmatrixoid_module-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='lordmatrixoid_module',
+    version='1.0',
+    description='An example Python package',
+    url='https://github.com/',
+    author='Lord Matrixoid',
+    author_email='mr.art.pro@mail.ru',
+    license='BSD 2-clause',
+    packages=['lordmatrixoid_module'],
+    install_requires=[
+                      'numpy',
+                      'matplotlib',
+                      ],
+)
