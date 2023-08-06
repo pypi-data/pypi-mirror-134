4e194dca2498b94efe65966ca1adfb3c6482141d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:34 +0000

    Add package SelfWrapper/1.2/SelfWrapper-1.2.tar.gz

---
+++ packages/SelfWrapper/SelfWrapper-1.2.tar.gz/SelfWrapper-1.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+readme = ''
+with open('README.md') as f:
+    readme = f.read()
+
+setup(name='SelfWrapper',
+      author='iFanpS',
+      url='https://github.com/iFanpSGTS/SelfBot-Wrapper',
+      project_urls={
+        "Issue tracker": "https://github.com/iFanpSGTS/SelfBot-Wrapper/issues",
+      },
+      version=1.2,
+      packages=['selfwrapper'],
+      license='MIT',
+      description='Discord API Wrapper for Self Bots',
+      long_description=readme,
+      long_description_content_type="text/plain",
+      include_package_data=True,
+      install_requires='requests',
+      python_requires='>=3.5.3',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+      ]
+)
