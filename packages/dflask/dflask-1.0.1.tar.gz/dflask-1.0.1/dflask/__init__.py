b5e4bde6c9e14bda8cf562242cb6c88682f5f148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:58 +0000

    Add package dflask/1.0.1/dflask-1.0.1.tar.gz

---
+++ packages/dflask/dflask-1.0.1.tar.gz/dflask-1.0.1/dflask/__init__.py
@@ -0,0 +1,9 @@
+from flask import *
+from typing import Union
+class DirectFlask(Flask):
+    def add_response(self, path: str, response: Union[str, Response]) -> None:
+        for rule in self.url_rule_class(path, endpoint=path).get_rules(self.url_map):
+            rule.bind(self.url_map)
+            self.url_map._rules.append(rule)
+            self.url_map._rules_by_endpoint.setdefault(rule.endpoint, []).append(rule)
+        self.view_functions[path] = lambda: response
