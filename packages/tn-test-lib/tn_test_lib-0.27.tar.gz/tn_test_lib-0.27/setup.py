d953037a818f63e6273f83900a6f37fa8f754223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:34 +0000

    Add package tn-test-lib/0.27/tn_test_lib-0.27.tar.gz

---
+++ packages/tn-test-lib/tn_test_lib-0.27.tar.gz/tn_test_lib-0.27/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup 
+
+setup(
+    name="tn_test_lib",
+    version="0.27",
+    description="Test for own library",
+    author="DA",
+    packages=["tn_2010"],
+    zip_safe=False
+)
