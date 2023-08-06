58d28299f7770cb8c8d8dd83849ce1fb95e909a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:03 +0000

    Add package delicatessen/0.1b1/delicatessen-0.1b1-py2.py3-none-any.whl

---
+++ packages/delicatessen/delicatessen-0.1b1-py2.py3-none-any.whl/delicatessen/utilities.py
@@ -0,0 +1,62 @@
+import numpy as np
+from scipy.misc import derivative
+
+
+def partial_derivative(func, var, point, output, dx):
+    """Calculate the partial derivatives for a given function
+
+    Parameters
+    ----------
+    func : function
+        Function with multiple variables to calculate the partial derivatives for
+    var : int
+        Index of variable to take the derivative of.
+    point : ndarray
+        Values to evaluate the partial derivative at
+    output : int
+        Index of the variable to extract the partial derivative of.
+    dx : float
+        Spacing to use to numerically approximate the partial derivatives of the bread matrix.
+
+    Returns
+    -------
+    float
+        Partial derivative at the variable at a particular points
+    """
+    args = point[:]
+
+    def wraps(x):
+        args[var] = x
+        return func(args)[output]
+
+    return derivative(wraps, point[var], dx=dx)
+
+
+def logit(prob):
+    """Logit transformation of probabilities. Input can be a single probability of array of probabilities
+
+    Parameters
+    ----------
+    prob : float, array
+        A single probability or an array of probabilities
+
+    Returns
+    -------
+    logit-transformed probabilities
+    """
+    return np.log(prob / (1 - prob))
+
+
+def inverse_logit(logodds):
+    """Inverse logit transformation. Returns probabilities
+
+    Parameters
+    ----------
+    logodds : float, array
+        A single log-odd or an array of log-odds
+
+    Returns
+    -------
+    inverse-logit transformed results (i.e. probabilities for log-odds)
+    """
+    return 1 / (1 + np.exp(-logodds))
