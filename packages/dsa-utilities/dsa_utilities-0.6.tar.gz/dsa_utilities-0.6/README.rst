a7a06d894c97df7b4dd262b351045ac8a79519a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:17 +0000

    Add package dsa-utilities/0.6/dsa_utilities-0.6.tar.gz

---
+++ packages/dsa-utilities/dsa_utilities-0.6.tar.gz/dsa_utilities-0.6/README.rst
@@ -0,0 +1,6 @@
+Python DSA Utilities
+===========================
+https://pypi.org/project/dsa-utilities/
+
+.. code-block:: python
+  pip install dsa-utilities
---
+++ packages/dsa-utilities/dsa_utilities-0.6.tar.gz/dsa_utilities-0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dsa_utilities",
+    version="0.6",
+    description="Utilities functions for DSA",
+    author="Nischal Lal Shrestha",
+    author_email="aakrist666@gmail.com",
+    url="https://github.com/theonlynischal/dsa_utils_package",
+    packages=find_packages(
+        exclude=("tests*",)
+    ),
+)
