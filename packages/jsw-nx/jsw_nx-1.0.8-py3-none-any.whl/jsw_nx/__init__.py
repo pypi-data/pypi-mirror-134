fa61e6fa50dd467cface3e87ada8275f729678fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:21 +0000

    Add package jsw-nx/1.0.8/jsw_nx-1.0.8-py3-none-any.whl

---
+++ packages/jsw-nx/jsw_nx-1.0.8-py3-none-any.whl/jsw_nx/__init__.py
@@ -0,0 +1,22 @@
+__version__ = '1.0.6'
+
+# next base
+from jsw_nx.base.set import set
+from jsw_nx.base.forin import forin
+from jsw_nx.base.get import get
+from jsw_nx.base.indexof import indexof
+from jsw_nx.base.includes import includes
+from jsw_nx.base.find import find
+from jsw_nx.base.find_index import find_index
+from jsw_nx.base.map import map
+from jsw_nx.base.reduce import reduce
+from jsw_nx.base.filter import filter
+from jsw_nx.base.mix import mix
+
+# ruby style
+from jsw_nx.rubify.to_i import to_i
+from jsw_nx.rubify.to_f import to_f
+from jsw_nx.rubify.to_s import to_s
+
+# next packages
+from jsw_nx.packages.days import days
---
+++ packages/jsw-nx/jsw_nx-1.0.8-py3-none-any.whl/jsw_nx/rubify/to_f.py
@@ -0,0 +1,2 @@
+def to_f(str):
+    return float(str)
