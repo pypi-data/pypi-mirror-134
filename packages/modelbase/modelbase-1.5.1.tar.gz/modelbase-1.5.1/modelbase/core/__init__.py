8a464d7c9dc0e8b2f9097eee63fae8bb5a444dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:01 +0000

    Add package modelbase/1.5.1/modelbase-1.5.1.tar.gz

---
+++ packages/modelbase/modelbase-1.5.1.tar.gz/modelbase-1.5.1/modelbase/core/__init__.py
@@ -0,0 +1,6 @@
+from .algebraicmixin import AlgebraicMixin
+from .basemodel import BaseModel
+from .compoundmixin import CompoundMixin
+from .parametermixin import ParameterMixin
+from .ratemixin import RateMixin
+from .stoichiometricmixin import StoichiometricMixin
---
+++ packages/modelbase/modelbase-1.5.1.tar.gz/modelbase-1.5.1/modelbase/ode/integrators/__init__.py
@@ -0,0 +1,10 @@
+"""Write me."""
+from __future__ import annotations
+
+from .abstract_integrator import AbstractIntegrator
+
+try:
+    from .int_assimulo import _IntegratorAssimulo as Assimulo
+except ImportError:
+    pass
+from .int_scipy import _IntegratorScipy as Scipy
