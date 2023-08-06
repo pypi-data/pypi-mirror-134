6ad898b4cf37a778aa75e03f45be6dd5e1add449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:24 +0000

    Add package rltrade-pypbo/0.0.3/rltrade_pypbo-0.0.3.tar.gz

---
+++ packages/rltrade-pypbo/rltrade_pypbo-0.0.3.tar.gz/rltrade_pypbo-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt",'r+') as f:
+    lines = f.readlines()
+
+requirements = [str(x).strip() for x in lines]
+
+setup(
+    version='0.0.3',
+    name='rltrade_pypbo',
+    python_requires=">=3.7",
+    packages=find_packages(),
+    install_requires=requirements,
+    keywords="Reinforcement Learning",
+    url="https://github.com/esvhd/pypbo",
+    description="probability for overfitting",
+    long_description="probability for overfitting",
+)
