0b7bf413fe1d060906e2e4762652fdef4fc03090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:29 +0000

    Add package offtheface/0.1.2/offtheface-0.1.2-py3-none-any.whl

---
+++ packages/offtheface/offtheface-0.1.2-py3-none-any.whl/offtheface/offtheface.py
@@ -0,0 +1,41 @@
+"""offtheface 모듈.
+
+Please put in a description of the module.
+
+Example:
+    ``offtheface`` 사용법은 아래와 같습니다.
+
+        $ pip install ./
+        $ offtheface-ping
+
+추가적인 설명은 여기에!
+
+Attributes:
+    nnn (int): ``사용되지 않는`` 시범용 변수
+
+Todo:
+    * 무한한 모듈의 발전 ``꿈``꾸며!
+    * ``Dreaming`` of infinite module development!
+
+"""
+import sys
+
+nnn = 1
+
+
+def ping():
+    """Example function with PEP 484 type annotations.
+
+    Returns:
+        리턴 없이 화면 print
+
+    """
+    if len(sys.argv) > 1:
+        n = int(sys.argv[1])
+        print(f"{'o' * n}fftheface")
+    else:
+        print('offtheface')
+
+
+if __name__ == "__main__":
+    ping()
