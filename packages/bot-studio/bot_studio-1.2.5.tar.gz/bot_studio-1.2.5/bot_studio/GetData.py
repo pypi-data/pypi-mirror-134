e452f6695b9b95d8dd9dbea4b3618f55627ec7a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:54 +0000

    Add package bot-studio/1.2.5/bot_studio-1.2.5.tar.gz

---
+++ packages/bot-studio/bot_studio-1.2.5.tar.gz/bot_studio-1.2.5/bot_studio/GetData.py
@@ -0,0 +1,34 @@
+import socket
+import string
+import json
+import random
+from getmac import get_mac_address as gma
+def getipaddress():
+    hostname = socket.gethostname()
+    ipaddress = socket.gethostbyname(hostname)
+    ipaddress=ipaddress.replace('.','-')
+    return ipaddress
+def create_random_id():
+    randomkey = ''.join(random.choices(string.ascii_uppercase +string.digits, k = 10)) 
+    return randomkey
+def get_user_id():
+    with open("C:/DataKundStudio/pip_user_id.json") as d:
+        dta=json.load(d)
+    return dta["user"]["uid"],dta
+def save_user_id(userid,dta):
+    if(dta=={}):
+        dta={"user":{"uid":userid,"email":userid}}
+    dta["user"]["uid"]=userid
+    dta["user"]["email"]=userid
+    with open("C:/DataKundStudio/logininfo.json","w") as d:
+        d.write(json.dumps(dta))
+    with open("C:/DataKundStudio/pip_user_id.json","w") as d:
+        d.write(json.dumps(dta))
+def gettheuserid():
+    dta={}
+    try:
+        userid,dta=get_user_id()
+    except:
+        userid=create_random_id()
+    save_user_id(userid,dta)
+    return userid
