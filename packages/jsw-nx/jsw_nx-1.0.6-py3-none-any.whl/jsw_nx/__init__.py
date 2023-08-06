fb9454dd6b1eab4fcebdc4f6149dcdbcde1b496e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:01 +0000

    Add package jsw-nx/1.0.6/jsw_nx-1.0.6-py3-none-any.whl

---
+++ packages/jsw-nx/jsw_nx-1.0.6-py3-none-any.whl/jsw_nx/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '1.0.6'
+
+from jsw_nx.base.set import set
+from jsw_nx.base.forin import forin
+from jsw_nx.base.get import get
+from jsw_nx.base.indexof import indexof
+from jsw_nx.base.includes import includes
+from jsw_nx.base.find import find
+from jsw_nx.base.find_index import find_index
+from jsw_nx.base.map import map
+from jsw_nx.base.reduce import reduce
+from jsw_nx.packages.days import days
