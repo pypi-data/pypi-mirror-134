e9f8cc2342b366d6b88938f73728b9a8581a2164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:38 +0000

    Add package autom8it/0.0.1/autom8it-0.0.1.tar.gz

---
+++ packages/autom8it/autom8it-0.0.1.tar.gz/autom8it-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="autom8it",
+    version="0.0.1",
+    author="Eldad Bishari",
+    author_email="eldad@1221tlv.org",
+    description="Automate IT operations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eldad1221/autom8it",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'PyYAML',
+        'cerberus',
+        'quickbe',
+        'boto3',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
