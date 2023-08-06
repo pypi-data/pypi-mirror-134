80be11b04182937f6f1a0cba6c0a46d91376233d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:57 +0000

    Add package ufy-ivystar/0.1.0/ufy-ivystar-0.1.0.tar.gz

---
+++ packages/ufy-ivystar/ufy-ivystar-0.1.0.tar.gz/ufy-ivystar-0.1.0/setup.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   setup.py
+@Time    :   2022/01/17 15:31:51
+@Author  :   ufy
+@Contact :   antarm@outlook.com
+@Version :   v1.0
+@Desc    :   None
+'''
+
+# here put the import lib
+from setuptools import setup,find_packages
+
+setup(
+    name='ufy-ivystar',
+    version='0.1.0',
+    description='ivystar packages',
+    author='ufy',
+    author_email='antarm@outlook.com',
+    url='',
+    packages=find_packages('.'),
+    requires=['ufy','numpy','matplotlib']
+)
