cc741f76def8a7358723af1e849fa22c7e7c8689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:06 +0000

    Add package swh.web/0.0.352/swh.web-0.0.352-py3-none-any.whl

---
+++ packages/swh.web/swh.web-0.0.352-py3-none-any.whl/swh.web-0.0.352.data/data/share/swh/web/static/jssources/assets/src/utils/highlightjs.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C) 2018-2021  The Software Heritage developers
+ * See the AUTHORS file at the top-level directory of this distribution
+ * License: GNU Affero General Public License version 3, or any later version
+ * See top-level LICENSE file for more information
+ */
+
+// highlightjs chunk that will be lazily loaded
+
+import 'highlight.js';
+import 'highlightjs-line-numbers.js';
+import './hljs-swh-theme.css';
+import './highlightjs.css';
+
+// add alias to match hljs 10.7 new naming
+hljs.lineNumbersElement = hljs.lineNumbersBlock;
+
+// define a synchronous version of hljs.lineNumbersElement
+hljs.lineNumbersElementSync = function(elt) {
+  elt.innerHTML = hljs.lineNumbersValue(elt.innerHTML);
+};
