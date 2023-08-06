4b9118f03c521654543b47a06c5b1bb8a7bc06a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:22 +0000

    Add package feeluown/3.8/feeluown-3.8.tar.gz

---
+++ packages/feeluown/feeluown-3.8.tar.gz/feeluown-3.8/feeluown/player/__init__.py
@@ -0,0 +1,23 @@
+from .metadata import MetadataFields, Metadata
+from .playlist import PlaybackMode
+from .base_player import State
+from .mpvplayer import MpvPlayer as Player
+from .playlist import PlaylistMode, Playlist
+from .fm import FM
+from .radio import SongRadio
+
+
+__all__ = (
+    'PlaybackMode',
+    'State',
+
+    'FM',
+    'PlaylistMode',
+    'SongRadio',
+
+    'Player',
+    'Playlist',
+
+    'Metadata',
+    'MetadataFields',
+)
