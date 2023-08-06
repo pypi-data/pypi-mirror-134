635b4dc07b8f7beaa8b9dae19559d9c1931ff638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:57 +0000

    Add package hillfit/0.0.1/Hillfit-0.0.1.tar.gz

---
+++ packages/hillfit/Hillfit-0.0.1.tar.gz/Hillfit-0.0.1/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('README.rst', encoding='utf-8') as file:
+    readme = file.read()
+    
+setup(
+  name = 'Hillfit',      
+  package_dir = {'fitting':'hillfit'},
+  version = '0.0.1',
+  license = 'GNU',
+  description = "Model for fitting data with the Hill equation, and exporting the contents", 
+  long_description = readme,
+  author = 'Andrew Freiburger, and Hiroaki Imoto',               
+  author_email = 'andrewfreiburger@gmail.com',
+  url = 'https://github.com/freiburgermsu/hillfit',   
+  keywords = ['biochemistry', 'systems biology', 'computational biology', 'data science'],
+  install_requires = ['matplotlib', 'numpy', 'sklearn', 'scipy', 'pandas', 'sigfig']
+)
