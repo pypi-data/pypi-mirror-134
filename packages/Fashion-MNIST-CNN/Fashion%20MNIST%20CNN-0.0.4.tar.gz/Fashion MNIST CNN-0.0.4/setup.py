04ff15937ff4ca2021ed75cf0fb25ff951cc1fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:31 +0000

    Add package Fashion-MNIST-CNN/0.0.4/Fashion%20MNIST%20CNN-0.0.4.tar.gz

---
+++ packages/Fashion-MNIST-CNN/Fashion%20MNIST%20CNN-0.0.4.tar.gz/Fashion MNIST CNN-0.0.4/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+# package_data = {
+#     "main_common": ["model/saved_model/*.h5"],
+# }
+
+# setuptools.setup(
+#     package_data=package_data
+# )
+
+setuptools.setup()
---
+++ packages/Fashion-MNIST-CNN/Fashion%20MNIST%20CNN-0.0.4.tar.gz/Fashion MNIST CNN-0.0.4/src/main_common/model/__init__.py
@@ -0,0 +1,3 @@
+__all__ = [
+    'load_local_model', 'ssd_model'
+]
