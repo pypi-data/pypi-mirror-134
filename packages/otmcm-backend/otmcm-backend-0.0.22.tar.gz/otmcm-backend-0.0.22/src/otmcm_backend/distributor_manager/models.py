32141c5c4f7eed412d2dfbcbb4ff271064b37190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:56 +0000

    Add package otmcm-backend/0.0.22/otmcm-backend-0.0.22.tar.gz

---
+++ packages/otmcm-backend/otmcm-backend-0.0.22.tar.gz/otmcm-backend-0.0.22/src/otmcm_backend/distributor_manager/models.py
@@ -0,0 +1,44 @@
+from threading import Thread
+from typing import Any
+import zmq
+import logging
+
+from otmcm_backend.common.interfaces.zmq_channel import (
+    ZMQReqRepServer
+)
+
+from otmcm_backend.common.models.channel_models import (
+    DistributorMangerDistributorRequestChannelConfig,
+    DistributorMangerUserScriptRequestChannelConfig
+)
+from otmcm_backend.common.models.mixsin_models import (
+    MixsinLogger
+)
+
+
+class ServerDistributorManagerDistributorRequestChannel(DistributorMangerDistributorRequestChannelConfig, ZMQReqRepServer, MixsinLogger):
+
+    CHANNEL_SOCKET_PATTERN = zmq.REP
+
+    def run_worker(self):
+        print("here 1")
+        self.logger.info(f"DistributorManager's channel {self.__class__.__name__} is up and running at {self.channel_address}")
+        while True:
+            raw_msg = self.socket.recv_json()
+            self.logger.debug(raw_msg)
+
+
+class ServerDistributorMangerUserScriptRequestChannel(DistributorMangerUserScriptRequestChannelConfig, ZMQReqRepServer, MixsinLogger):
+
+    CHANNEL_SOCKET_PATTERN = zmq.REP
+
+    def run_worker(self):
+        print("here 2")
+        self.logger.info(f"DistributorManager's channel {self.__class__.__name__} is up and running at {self.channel_address}")
+        while True:
+            raw_msg = self.socket.recv_json()
+            self.logger.debug(raw_msg)
+
+
+class DistributorRequestChannel(ZMQReqRepServer):
+    pass
