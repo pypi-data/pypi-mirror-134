8f85986d82cd2f8262c6fb79f540f645657bf505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:25 +0000

    Add package scipp/0.11.1/scipp-0.11.1-cp38-cp38-win_amd64.whl

---
+++ packages/scipp/scipp-0.11.1-cp38-cp38-win_amd64.whl/scipp/include/scipp/core/sizes.h
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+/// @file
+/// @author Simon Heybrock
+#pragma once
+
+#include <array>
+
+#include "scipp-core_export.h"
+#include "scipp/common/index.h"
+#include "scipp/common/span.h"
+#include "scipp/core/slice.h"
+#include "scipp/units/dim.h"
+
+namespace scipp::core {
+
+constexpr int32_t NDIM_MAX = 6;
+
+class Dimensions;
+
+/// Small (fixed maximum size) and stable (preserving key order) map
+template <class Key, class Value, int16_t Capacity>
+class SCIPP_CORE_EXPORT small_stable_map {
+public:
+  static constexpr auto capacity = Capacity;
+
+  small_stable_map() = default;
+
+  bool operator==(const small_stable_map &other) const noexcept;
+  bool operator!=(const small_stable_map &other) const noexcept;
+
+  [[nodiscard]] auto begin() const noexcept { return m_keys.begin(); }
+  [[nodiscard]] auto end() const noexcept { return m_keys.begin() + size(); }
+  [[nodiscard]] auto rbegin() const noexcept {
+    return std::reverse_iterator(end());
+  }
+  [[nodiscard]] auto rend() const noexcept {
+    return std::reverse_iterator(begin());
+  }
+  [[nodiscard]] typename std::array<Key, Capacity>::const_iterator
+  find(const Key &key) const;
+  [[nodiscard]] constexpr bool empty() const noexcept { return size() == 0; }
+  [[nodiscard]] constexpr scipp::index size() const noexcept { return m_size; }
+  [[nodiscard]] bool contains(const Key &key) const;
+  [[nodiscard]] scipp::index index(const Key &key) const;
+  [[nodiscard]] const Value &operator[](const Key &key) const;
+  [[nodiscard]] const Value &at(const Key &key) const;
+  void assign(const Key &key, const Value &value);
+  void insert_left(const Key &key, const Value &value);
+  void insert_right(const Key &key, const Value &value);
+  void erase(const Key &key);
+  void clear() noexcept;
+  void replace_key(const Key &from, const Key &to);
+  [[nodiscard]] constexpr scipp::span<const Key> keys() const &noexcept {
+    return {m_keys.data(), static_cast<size_t>(size())};
+  }
+  [[nodiscard]] constexpr scipp::span<const Value> values() const &noexcept {
+    return {m_values.data(), static_cast<size_t>(size())};
+  }
+
+private:
+  int16_t m_size{0};
+  std::array<Key, Capacity> m_keys{};
+  std::array<Value, Capacity> m_values{};
+};
+
+/// Similar to class Dimensions but without implied ordering
+class SCIPP_CORE_EXPORT Sizes
+    : public small_stable_map<Dim, scipp::index, NDIM_MAX> {
+private:
+  using base = small_stable_map<Dim, scipp::index, NDIM_MAX>;
+
+protected:
+  using base::assign;
+  using base::insert_left;
+  using base::insert_right;
+
+public:
+  constexpr Sizes() noexcept = default;
+
+  void set(const Dim dim, const scipp::index size);
+  void resize(const Dim dim, const scipp::index size);
+  [[nodiscard]] bool includes(const Sizes &sizes) const;
+  [[nodiscard]] Sizes slice(const Slice &params) const;
+
+  /// Return the labels of the space defined by *this.
+  [[nodiscard]] constexpr auto labels() const &noexcept { return keys(); }
+  /// Return the shape of the space defined by *this.
+  [[nodiscard]] constexpr auto sizes() const &noexcept { return values(); }
+};
+
+[[nodiscard]] SCIPP_CORE_EXPORT Sizes
+concat(const scipp::span<const Sizes> sizes, const Dim dim);
+
+[[nodiscard]] SCIPP_CORE_EXPORT Sizes merge(const Sizes &a, const Sizes &b);
+
+SCIPP_CORE_EXPORT bool is_edges(const Sizes &sizes, const Sizes &dataSizes,
+                                const Dim dim);
+
+} // namespace scipp::core
+
+namespace scipp {
+using core::Sizes;
+}
---
+++ packages/scipp/scipp-0.11.1-cp38-cp38-win_amd64.whl/scipp/include/scipp/dataset/except.h
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+/// @file
+/// @author Simon Heybrock
+#pragma once
+
+#include <algorithm>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+
+#include "scipp-dataset_export.h"
+#include "scipp/core/except.h"
+#include "scipp/dataset/string.h"
+#include "scipp/variable/except.h"
+
+namespace scipp::dataset {
+
+class Dataset;
+class DataArray;
+
+} // namespace scipp::dataset
+
+namespace scipp::except {
+
+struct SCIPP_DATASET_EXPORT DataArrayError : public Error<dataset::DataArray> {
+  explicit DataArrayError(const std::string &msg);
+};
+
+template <>
+[[noreturn]] SCIPP_DATASET_EXPORT void
+throw_mismatch_error(const dataset::DataArray &expected,
+                     const dataset::DataArray &actual,
+                     const std::string &optional_message);
+
+struct SCIPP_DATASET_EXPORT DatasetError : public Error<dataset::Dataset> {
+  explicit DatasetError(const std::string &msg);
+};
+
+template <>
+[[noreturn]] SCIPP_DATASET_EXPORT void
+throw_mismatch_error(const dataset::Dataset &expected,
+                     const dataset::Dataset &actual,
+                     const std::string &optional_message);
+
+struct SCIPP_DATASET_EXPORT CoordMismatchError : public DatasetError {
+  CoordMismatchError(const Dim dim, const Variable &a, const Variable &b,
+                     std::string_view opname = "");
+  using DatasetError::DatasetError;
+};
+
+} // namespace scipp::except
+
+namespace scipp::dataset::expect {
+
+SCIPP_DATASET_EXPORT void coords_are_superset(const DataArray &a,
+                                              const DataArray &b,
+                                              std::string_view opname);
+SCIPP_DATASET_EXPORT void coords_are_superset(const Coords &a, const Coords &b,
+                                              std::string_view opname);
+SCIPP_DATASET_EXPORT void matching_coord(const Dim dim, const Variable &a,
+                                         const Variable &b,
+                                         std::string_view opname);
+
+SCIPP_DATASET_EXPORT void is_key(const Variable &key);
+
+} // namespace scipp::dataset::expect
