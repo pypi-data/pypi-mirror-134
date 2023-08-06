f314cc5ee3e26ea5793a0c7b54317a0e93883831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:04 +0000

    Add package danbot-status/0.1.1/danbot_status-0.1.1-py3-none-any.whl

---
+++ packages/danbot-status/danbot_status-0.1.1-py3-none-any.whl/danbotstatus/__init__.py
@@ -0,0 +1,8 @@
+from .AsynchronousStatus import *
+from .SynchronousStatus import *
+from .errors import *
+
+__name__ = "danbot-status"
+__version__ = "0.1.1"
+__author__ = "VineyS"
+__license__ = "MIT"
