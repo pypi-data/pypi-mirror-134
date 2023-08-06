725707e680f4ea5dca32e8abb6d2010444e52bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:59 +0000

    Add package magic-class/0.5.17/magic-class-0.5.17.tar.gz

---
+++ packages/magic-class/magic-class-0.5.17.tar.gz/magic-class-0.5.17/magicclass/widgets/__init__.py
@@ -0,0 +1,53 @@
+"""
+Advanced widgets for magic class GUI.
+These widgets are all compatible with the ``append`` method of Container widgets.
+"""
+
+from magicgui.widgets import * # to avoid importing both magicgui.widgets and magicclass.widgets
+
+from .containers import (
+    ButtonContainer,
+    GroupBoxContainer,
+    ListContainer,
+    SubWindowsContainer,
+    ScrollableContainer,
+    DraggableContainer,
+    CollapsibleContainer,
+    HCollapsibleContainer,
+    SplitterContainer,
+    StackedContainer,
+    TabbedContainer,
+    ToolBoxContainer
+    )
+from .pywidgets import ListWidget, DictWidget
+from .misc import Figure, ConsoleTextEdit, CheckButton
+from .separator import Separator
+from .sequence import ListEdit, TupleEdit
+from .threading import ProgressWidget, progress
+from .utils import FreeWidget
+
+
+__all__ = ["ListWidget", 
+           "Figure",
+           "ConsoleTextEdit",
+           "CheckButton",
+           "Separator",
+           "ListEdit",
+           "DictWidget",
+           "TupleEdit",
+           "ProgressWidget",
+           "progress",
+           "FreeWidget",
+           "ButtonContainer",
+           "GroupBoxContainer",
+           "ListContainer",
+           "SubWindowsContainer",
+           "ScrollableContainer",
+           "DraggableContainer",
+           "CollapsibleContainer",
+           "HCollapsibleContainer",
+           "SplitterContainer",
+           "StackedContainer",
+           "TabbedContainer",
+           "ToolBoxContainer",
+           ]
