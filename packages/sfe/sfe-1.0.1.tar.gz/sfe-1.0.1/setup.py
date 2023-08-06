617bb4e6da203ee00fb336153649f6a77c44345d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:14 +0000

    Add package sfe/1.0.1/sfe-1.0.1.tar.gz

---
+++ packages/sfe/sfe-1.0.1.tar.gz/sfe-1.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="UTF-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sfe",
+    version="1.0.1",
+    author="haoy_zhang",
+    author_email="haoy.zhang@foxmail.com",
+    description="Python and finite Elements",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/yonvone/sfe.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    extras_require={"sfe": ["python"]},
+)
