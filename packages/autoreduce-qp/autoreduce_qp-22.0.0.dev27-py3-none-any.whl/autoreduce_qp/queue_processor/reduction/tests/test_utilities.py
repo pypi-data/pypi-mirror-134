64a57c1113f24bd05a14b6acc10e1fce7a93ef62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:41 +0000

    Add package autoreduce-qp/22.0.0.dev27/autoreduce_qp-22.0.0.dev27-py3-none-any.whl

---
+++ packages/autoreduce-qp/autoreduce_qp-22.0.0.dev27-py3-none-any.whl/autoreduce_qp/queue_processor/reduction/tests/test_utilities.py
@@ -0,0 +1,52 @@
+# ############################################################################ #
+# Autoreduction Repository :
+# https://github.com/autoreduction/autoreduce
+#
+# Copyright &copy; 2021 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+# ############################################################################ #
+
+import contextlib
+import io
+import unittest
+from pathlib import Path
+
+from unittest.mock import patch
+from parameterized import parameterized
+
+from autoreduce_qp.queue_processor.reduction.utilities import windows_to_linux_path, channels_redirected
+
+
+class TestReductionRunnerHelpers(unittest.TestCase):
+
+    @parameterized.expand([
+        ["\\\\isis\\inst$\\some\\more\\path.nxs", '/isis/some/more/path.nxs', ''],
+        ["\\\\autoreduce\\data\\some\\more\\path.nxs", '/autoreduce/data/some/more/path.nxs', '/autoreduce'],
+        [["\\\\autoreduce\\data\\path1.nxs", "\\\\autoreduce\\data\\path2.nxs"],
+         ['/autoreduce/data/path1.nxs', '/autoreduce/data/path2.nxs'], '/autoreduce'],
+    ])
+    def test_windows_to_linux_data_path(self, input_path: str, expected_path: str, path_prefix: str):
+        """
+        Test: Windows to linux path is correctly modified to linux format
+        When: Called with a windows formatted path.
+        """
+
+        actual = windows_to_linux_path(input_path, path_prefix)
+        self.assertEqual(actual, expected_path)
+
+    @patch('os.path.isfile')
+    def test_channels_redirect(self, mock_is_file):
+        """
+        Test: A context manager is returned
+        When: Called
+        """
+        mock_is_file.return_value = True
+        log_directory = "/reduction_log/"
+        log_and_error_name = "RB_1234_Run_4321_"
+
+        script_out = Path(f"{log_directory}{log_and_error_name}{'Script.out'}")
+        mantid_out = Path(f"{log_directory}{log_and_error_name}{'Mantid.log'}")
+        out_stream = io.StringIO()
+
+        actual = channels_redirected(out_file=script_out, error_file=mantid_out, out_stream=out_stream)
+        self.assertIsInstance(actual, contextlib._GeneratorContextManager)  # pylint:disable=protected-access
---
+++ packages/autoreduce-qp/autoreduce_qp-22.0.0.dev27-py3-none-any.whl/autoreduce_qp/queue_processor/reduction/utilities.py
@@ -0,0 +1,81 @@
+# ############################################################################### #
+# Autoreduction Repository : https://github.com/autoreduction/autoreduce
+#
+# Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+# ############################################################################### #
+"""
+Post-Process Admin Utilities
+"""
+
+import sys
+from contextlib import contextmanager
+from typing import List, Union
+
+
+@contextmanager
+def channels_redirected(out_file, error_file, out_stream):
+    """
+    This context manager copies the file descriptor(fd) of stdout and stderr to the files given in
+    out_file and err_file respectively. The fd is at the C level and so picks up data sent via
+    Mantid. Both output streams are additionally also sent to out_stream.
+    :param out_file: (str) output path
+    :param error_file: (str) error file path
+    :param out_stream: (class object) io.StringIO
+    """
+
+    old_stdout, old_stderr = sys.stdout, sys.stderr
+
+    class MultipleChannels:
+        # pylint: disable=expression-not-assigned
+        """ Behaves like a stream object, but outputs to multiple streams."""
+
+        def __init__(self, *streams):
+            """
+            :param streams: (tuple) (N-path, io.StringIO)
+            """
+            self.streams = streams
+
+        def write(self, stream_message):
+            """ Write to streams.
+            :param stream_message: (data) stream message
+            """
+            [stream.write(stream_message) for stream in self.streams]
+
+        def flush(self):
+            """ Flush streams. """
+            [stream.flush() for stream in self.streams]
+
+    def _redirect_channels(output_file, err_file):
+        """ Redirect channels
+        :param output_file: stdout stream
+        :param err_file: stderr stream
+        """
+        sys.stdout.flush(), sys.stderr.flush()  # pylint: disable=expression-not-assigned
+        sys.stdout, sys.stderr = output_file, err_file
+
+    with open(out_file, encoding="utf-8", mode='w') as out, open(error_file, encoding="utf-8", mode='w') as err:
+        _redirect_channels(MultipleChannels(out, out_stream), MultipleChannels(err, out_stream))
+        try:
+            # allow code to be run with the redirected channels
+            yield
+        finally:
+            _redirect_channels(old_stdout, old_stderr)  # restore stderr.
+
+
+def windows_to_linux_path(paths: Union[str, List[str]], temp_root_directory) -> Union[str, List[str]]:
+    """ Convert windows path to linux path -> replace \\ with /
+    :param path:
+    :param temp_root_directory:
+    :return: (str) linux formatted file path
+    """
+
+    if isinstance(paths, str):
+        return paths.replace('\\\\isis\\inst$\\', '/isis/').replace('\\\\autoreduce\\data\\',
+                                                                    temp_root_directory + '/data/').replace('\\', '/')
+    else:
+        for i, path in enumerate(paths):
+            paths[i] = path.replace('\\\\isis\\inst$\\',
+                                    '/isis/').replace('\\\\autoreduce\\data\\',
+                                                      temp_root_directory + '/data/').replace('\\', '/')
+        return paths
