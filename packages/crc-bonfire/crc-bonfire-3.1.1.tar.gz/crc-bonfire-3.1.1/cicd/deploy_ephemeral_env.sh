445f445f3c2c2e52a09ab1dd09b06e2c0884bbf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:23 +0000

    Add package crc-bonfire/3.1.1/crc-bonfire-3.1.1.tar.gz

---
+++ packages/crc-bonfire/crc-bonfire-3.1.1.tar.gz/crc-bonfire-3.1.1/cicd/deploy_ephemeral_env.sh
@@ -0,0 +1,22 @@
+source ${CICD_ROOT}/_common_deploy_logic.sh
+
+# Caller can define any extra deploy arguments to be passed to bonfire
+: ${EXTRA_DEPLOY_ARGS:=""}
+
+# Deploy k8s resources for app and its dependencies (use insights-stage instead of insights-production for now)
+# -> use this PR as the template ref when downloading configurations for this component
+# -> use this PR's newly built image in the deployed configurations
+set -x
+export NAMESPACE=$(bonfire namespace reserve)
+bonfire deploy \
+    ${APP_NAME} \
+    --source=appsre \
+    --ref-env insights-stage \
+    --set-template-ref ${COMPONENT_NAME}=${GIT_COMMIT} \
+    --set-image-tag ${IMAGE}=${IMAGE_TAG} \
+    --namespace ${NAMESPACE} \
+    --timeout ${DEPLOY_TIMEOUT} \
+    ${COMPONENTS_ARG} \
+    ${COMPONENTS_RESOURCES_ARG} \
+    ${EXTRA_DEPLOY_ARGS}
+set +x
