d54ceddc14c4df210b8cbd0f119a794013519974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:04 +0000

    Add package marckraw-hello-world/1.0.7/marckraw-hello-world-1.0.7.tar.gz

---
+++ packages/marckraw-hello-world/marckraw-hello-world-1.0.7.tar.gz/marckraw-hello-world-1.0.7/setup.py
@@ -0,0 +1,18 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="marckraw-hello-world",
+    version="1.0.7",
+    packages=["marckraw_hello_world"], # find_package() - search for packages that are available
+    description="Sample PYPI App",
+    long_description=README,
+    long_description_content_type="text/markdown",
+)
