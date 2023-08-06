95f986abdfb9a0b8efe536966290899bcb214b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:59 +0000

    Add package TarikDrevonUtils/1.0.11/TarikDrevonUtils-1.0.11.tar.gz

---
+++ packages/TarikDrevonUtils/TarikDrevonUtils-1.0.11.tar.gz/TarikDrevonUtils-1.0.11/utils/__init__.py
@@ -0,0 +1,26 @@
+#print('importing...')
+
+# py standard library
+#from math import*
+import importlib as imp
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.special as spe
+import pandas as pd
+
+
+# specific libraries
+from . import displayStandards as dsp
+from . import regionManager as regMg
+from . import materialManager as matMg
+from . import newtonRaphson as nwt
+from . import bissection as bis
+from . import commonInputOutput as iostd
+from . import physicsConstants as cst
+from . import glob_colors  as colors
+from .glob_colors import *
+
+def funcs(obj):
+    import inspect
+    functions = '\n'.join([f[0] for f in inspect.getmembers(obj,predicate=inspect.isfunction)])
+    print(functions)
