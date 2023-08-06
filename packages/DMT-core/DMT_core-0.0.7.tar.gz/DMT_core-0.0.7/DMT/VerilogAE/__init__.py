99afca2e231d57783644b7ff7c0ff4fc1bbc1355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:20 +0000

    Add package DMT-core/0.0.7/DMT_core-0.0.7.tar.gz

---
+++ packages/DMT-core/DMT_core-0.0.7.tar.gz/DMT_core-0.0.7/DMT/VerilogAE/__init__.py
@@ -0,0 +1,25 @@
+# DMT_core
+# Copyright (C) 2019  Markus Müller and Mario Krattenmacher and the DMT contributors <https://gitlab.hrz.tu-chemnitz.de/CEDIC_Bipolar/DMT/>
+#
+# This file is part of DMT_core.
+#
+# DMT_core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DMT_core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+name = "VerilogAE"
+
+from .helpers import get_param_list
+from .helpers import get_modelcard
+from .helpers import get_dmt_model
+from .helpers import HICUM_L0
+from .helpers import HICUM_L2
+from .helpers import SGP
