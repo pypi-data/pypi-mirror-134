1e7f2342d38950a3ca40eeb0a81853c1dddbad23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:42 +0000

    Add package PDFpy/0.0.11/PDFpy-0.0.11.tar.gz

---
+++ packages/PDFpy/PDFpy-0.0.11.tar.gz/PDFpy-0.0.11/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.11'
+DESCRIPTION = 'Simple PDF editing made fast'
+LONG_DESCRIPTION = 'A package that allows to manipulate PDF pages as you would do with lists based on PyPDF2.'
+
+# Setting up
+setup(
+    name="PDFpy",
+    version=VERSION,
+    author="Jonas Ha",
+    author_email="<jonasharriehausen@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['PyPDF2'],
+    keywords=['python', 'PDF', 'PDFpy'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
