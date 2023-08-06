b18cfae8db323918b9842b2225f7375b89317a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:27 +0000

    Add package blrec/1.2.4/blrec-1.2.4.tar.gz

---
+++ packages/blrec/blrec-1.2.4.tar.gz/blrec-1.2.4/src/blrec/path/__init__.py
@@ -0,0 +1,18 @@
+from .helpers import (
+    file_exists,
+    create_file,
+    danmaku_path,
+    raw_danmaku_path,
+    extra_metadata_path,
+    escape_path,
+)
+
+
+__all__ = (
+    'file_exists',
+    'create_file',
+    'danmaku_path',
+    'raw_danmaku_path',
+    'extra_metadata_path',
+    'escape_path',
+)
