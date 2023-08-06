b54e6a9eccb9ca57f6509dadb1acabb829c6fd14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:28 +0000

    Add package quick-arguments/0.0.1/quick-arguments-0.0.1.tar.gz

---
+++ packages/quick-arguments/quick-arguments-0.0.1.tar.gz/quick-arguments-0.0.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='quick-arguments',
+    version='0.0.1',
+    description='A simple argument parser',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Thomas Ellison',
+    author_email='thomasjlsn@gmail.com',
+    url='https://gitlab.com/thomasjlsn/qargs',
+    packages=['qargs'],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+    ]
+)
