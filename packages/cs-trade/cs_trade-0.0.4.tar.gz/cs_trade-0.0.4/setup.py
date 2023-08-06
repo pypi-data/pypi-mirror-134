276a123df6013730e7ef647617faba780e55d316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:43 +0000

    Add package cs-trade/0.0.4/cs_trade-0.0.4.tar.gz

---
+++ packages/cs-trade/cs_trade-0.0.4.tar.gz/cs_trade-0.0.4/setup.py
@@ -0,0 +1,10 @@
+from struct import pack
+from setuptools import setup, find_packages
+
+setup(
+    name="cs_trade",
+    version="0.0.4",
+    packages=find_packages(),
+    setup_requires=['wheel'],
+    install_requires=["pandas>=1.3.4", "plotly>=5.5.0", "numpy>=1.19.2"],
+)
