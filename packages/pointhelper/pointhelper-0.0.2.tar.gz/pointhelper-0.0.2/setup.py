552fe92c41c4d2a5a55d8717581a11dcb509cff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:31 +0000

    Add package pointhelper/0.0.2/pointhelper-0.0.2.tar.gz

---
+++ packages/pointhelper/pointhelper-0.0.2.tar.gz/pointhelper-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pointhelper',
+    version='0.0.2',
+    packages=['pointhelper'],
+    author='jim',
+    author_email='847734876@qq.com',
+    license='MIT',
+    url='https://github.com/JimXu1989/pointhelper'
+)
