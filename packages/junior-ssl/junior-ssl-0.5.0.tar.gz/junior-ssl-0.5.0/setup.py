ef8862c984cf2324ec605e77e126fde86a5e821d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:22 +0000

    Add package junior-ssl/0.5.0/junior-ssl-0.5.0.tar.gz

---
+++ packages/junior-ssl/junior-ssl-0.5.0.tar.gz/junior-ssl-0.5.0/setup.py
@@ -0,0 +1,49 @@
+import setuptools
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            if '__pycache__' not in path:
+                paths.append(os.path.join('..', path, filename))
+    return paths
+
+setuptools.setup(
+    name="junior-ssl",
+    version="0.5.0",
+    author="Rhoban team",
+    author_email="team@rhoban.com",
+    description="Junior SSL - An omniwheel soccer setup",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rhoban/junior-ssl/",
+    packages=setuptools.find_packages(),
+    scripts=['jssl-gc'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Free for non-commercial use",
+        "Operating System :: OS Independent",
+    ],
+    keywords="robot holonomic omniwheel ssl robocup junior soccer standard localized tracking",
+    install_requires=[
+        "numpy",
+        "zmq",
+    ],
+    extras_require={
+        'gc': [ # Game controller extra requirements
+            "pyserial",
+            "flask",
+            "flask-cors",
+            "waitress",
+            "opencv-python-headless",
+            "opencv-contrib-python-headless"
+        ]
+    },
+    include_package_data=True,
+    package_data={"": package_files("jssl")},
+    python_requires='>=3.6',
+)
