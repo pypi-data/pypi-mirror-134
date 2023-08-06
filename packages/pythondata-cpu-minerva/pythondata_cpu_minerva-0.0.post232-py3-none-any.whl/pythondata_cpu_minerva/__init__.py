f1ca1253dddd2a94f210ed9699886f874cda8b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:11 +0000

    Add package pythondata-cpu-minerva/0.0.post232/pythondata_cpu_minerva-0.0.post232-py3-none-any.whl

---
+++ packages/pythondata-cpu-minerva/pythondata_cpu_minerva-0.0.post232-py3-none-any.whl/pythondata_cpu_minerva/__init__.py
@@ -0,0 +1,50 @@
+import os.path
+__dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
+data_location = os.path.join(__dir__, "sources")
+src = "https://github.com/lambdaconcept/minerva"
+
+# Module version
+version_str = "0.0.post232"
+version_tuple = (0, 0, 232)
+try:
+    from packaging.version import Version as V
+    pversion = V("0.0.post232")
+except ImportError:
+    pass
+
+# Data version info
+data_version_str = "0.0.post112"
+data_version_tuple = (0, 0, 112)
+try:
+    from packaging.version import Version as V
+    pdata_version = V("0.0.post112")
+except ImportError:
+    pass
+data_git_hash = "3cb469f8fe451838ee2105842d8d4695a64d234c"
+data_git_describe = "v0.0-112-g3cb469f"
+data_git_msg = """\
+commit 3cb469f8fe451838ee2105842d8d4695a64d234c
+Author: Catherine <whitequark@whitequark.org>
+Date:   Sun Dec 12 15:39:03 2021 +0000
+
+    nMigen has been renamed to Amaranth HDL.
+
+"""
+
+# Tool version info
+tool_version_str = "0.0.post120"
+tool_version_tuple = (0, 0, 120)
+try:
+    from packaging.version import Version as V
+    ptool_version = V("0.0.post120")
+except ImportError:
+    pass
+
+
+def data_file(f):
+    """Get absolute path for file inside pythondata_cpu_minerva."""
+    fn = os.path.join(data_location, f)
+    fn = os.path.abspath(fn)
+    if not os.path.exists(fn):
+        raise IOError("File {f} doesn't exist in pythondata_cpu_minerva".format(f))
+    return fn
