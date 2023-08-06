8a728feb2a8fa720c753772f54dc387238a04d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:01 +0000

    Add package moon-coverage/0.10.0/moon-coverage-0.10.0.tar.gz

---
+++ packages/moon-coverage/moon-coverage-0.10.0.tar.gz/moon-coverage-0.10.0/moon_coverage/esa/__init__.py
@@ -0,0 +1,27 @@
+"""ESA specific module."""
+
+from .api import debug_esa_api, get_mk, get_tag
+from .crema import ESA_CREMAS, JUICE_CREMAS, EsaCremasCollection
+from .event_file import EsaMissionEvents, EsaMissionPhases, EsaMissionTimeline
+from ..misc import DeprecationHelper
+from ..spice import MetaKernel
+
+
+__all__ = [
+    'ESA_CREMAS',
+    'JUICE_CREMAS',
+    'EsaCremasCollection',
+    'EsaMissionEvents',
+    'EsaMissionPhases',
+    'EsaMissionTimeline',
+    'get_mk',
+    'get_tag',
+    'debug_esa_api',
+]
+
+
+# Depreciations
+CReMAs = DeprecationHelper('CReMAs', 'ESA_CREMAS', ESA_CREMAS)
+JUICE_CReMA = DeprecationHelper('JUICE_CReMA', 'JUICE_CREMAS', JUICE_CREMAS)
+CReMAMetaKernel = DeprecationHelper('CReMAMetaKernel', 'MetaKernel', MetaKernel)
+debug_esa_crema = DeprecationHelper('debug_esa_crema', 'debug_esa_api', debug_esa_api)
