5d904c7127e923519e06dd654ccf712987fe007f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:29 +0000

    Add package flask-FormRequest/0.0.14/flask-FormRequest-0.0.14.tar.gz

---
+++ packages/flask-FormRequest/flask-FormRequest-0.0.14.tar.gz/flask-FormRequest-0.0.14/src/flaskFormRequest/validators/__init__.py
@@ -0,0 +1,11 @@
+from .validator import Validator, ValidationError, StopValidation
+from .accepted import Accepted
+from .alpha import Alpha
+from .alphaDash import AlphaDash
+from .alphaNum import AlphaNum
+from .between import Between
+from .boolean import Boolean
+from .confirmed import Confirmed
+from .currentPassword import CurrentPassword
+from .required import Required
+from .size import Size
