cef89fb16a4e1a81e5c156fd7e91aa5d0831801d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:56:12 +0000

    Add package decaylanguage/0.14.0/DecayLanguage-0.14.0.tar.gz

---
+++ packages/decaylanguage/DecayLanguage-0.14.0.tar.gz/DecayLanguage-0.14.0/src/decaylanguage/utils/__init__.py
@@ -0,0 +1,15 @@
+from .errors import LineFailure
+from .particleutils import charge_conjugate_name
+from .utilities import filter_lines, iter_flatten, split
+
+__all__ = (
+    "LineFailure",
+    "iter_flatten",
+    "split",
+    "filter_lines",
+    "charge_conjugate_name",
+)
+
+
+def __dir__():
+    return __all__
---
+++ packages/decaylanguage/DecayLanguage-0.14.0.tar.gz/DecayLanguage-0.14.0/src/decaylanguage/utils/utilities.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2018-2022, Eduardo Rodrigues and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/decaylanguage for details.
+
+
+def iter_flatten(iterable):
+    """
+    Flatten nested tuples and lists
+    """
+    for e in iterable:
+        if isinstance(e, (list, tuple)):
+            yield from iter_flatten(e)
+        else:
+            yield e
+
+
+def split(x):
+    """
+    Break up a comma separated list, but respect curly brackets.
+
+    For example:
+    this, that { that { this, that } }
+    would only break on the first comma, since the second is in a {} list
+    """
+
+    c = 0
+    i = 0
+    out = []
+    while len(x) > 0:
+        if i + 1 == len(x):
+            out.append(x[: i + 1])
+            return out
+        elif x[i] == "," and c == 0:
+            out.append(x[:i])
+            x = x[i + 1 :]
+            i = -1
+        elif x[i] == "{":
+            c += 1
+        elif x[i] == "}":
+            c -= 1
+        i += 1
+
+
+def filter_lines(matcher, inp):
+    """
+    Filter out lines into new variable if they match a regular expression
+    """
+    output = [
+        matcher.match(ln).groupdict() for ln in inp if matcher.match(ln) is not None
+    ]
+    new_inp = [ln for ln in inp if matcher.match(ln) is None]
+    return output, new_inp
