3c63a59002d6df5716984a24e7ed9bd1f9a09f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:56:12 +0000

    Add package iconfont-export/1.1/iconfont_export-1.1.tar.gz

---
+++ packages/iconfont-export/iconfont_export-1.1.tar.gz/iconfont_export-1.1/iconfont_export.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+
+def test():
+    print("123")
+
+if __name__ == '__main__':
+    test()
---
+++ packages/iconfont-export/iconfont_export-1.1.tar.gz/iconfont_export-1.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="iconfont_export",
+    version="1.1",
+    author="lifei",
+    author_email="alphapkbeta@163.com",
+    py_modules=['iconfont_export'],
+)
