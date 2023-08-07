69ed046c03b856249d13d58b980c765c5e97594d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:56:12 +0000

    Add package django-whistle/2.4.2/django-whistle-2.4.2.tar.gz

---
+++ packages/django-whistle/django-whistle-2.4.2.tar.gz/django-whistle-2.4.2/whistle/settings.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+
+EVENTS = getattr(settings, 'WHISTLE_NOTIFICATION_EVENTS', [])
+URL_HANDLER = getattr(settings, 'WHISTLE_URL_HANDLER', None)
+URL_PARAM = getattr(settings, 'WHISTLE_URL_PARAM', 'read-notification')
+TIMEOUT = getattr(settings, 'WHISTLE_CACHE_TIMEOUT', DEFAULT_TIMEOUT)
+USE_RQ = getattr(settings, 'WHISTLE_USE_RQ', True)
+REDIS_QUEUE = getattr(settings, 'WHISTLE_REDIS_QUEUE', 'default')
+SIGNING_KEY = getattr(settings, 'WHISTLE_SIGNING_KEY', settings.SECRET_KEY)
+SIGNING_SALT = getattr(settings, 'WHISTLE_SIGNING_SALT', 'whistle')
+AUTH_USER_MODEL = getattr(settings, 'WHISTLE_AUTH_USER_MODEL', settings.AUTH_USER_MODEL)
+PUSH_NOTIFICATIONS_ENABLED = 'fcm_django' in settings.INSTALLED_APPS
